42d0da3af0da6c6fb1dff8cfec2d95cdacd2885c
Defnull <define.null@gmail.com>
2022-07-13 15:31:20 +0200

    Add 1.13, drop 1.11 as it does not compile on it

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Electric.MixProject do
     [
       app: :electric,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
