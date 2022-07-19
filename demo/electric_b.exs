988cb2f24696273136d432f8538519c940adcd5f
Ilia Borovitinov <ilia@vaxine.io>
2022-07-19 12:42:02 +0300

    Added a second electric instance and a bidirectional test

---
+++ demo/electric_b.exs
@@ -0,0 +1,39 @@
+import Config
+
+config :electric, Electric.ReplicationServer.VaxineLogConsumer,
+  producer: Electric.ReplicationServer.VaxineLogProducer,
+  hostname: "vaxine",
+  port: 8088
+
+config :electric, Electric.VaxRepo,
+  hostname: "vaxine",
+  port: 8087
+
+config :electric, Electric.Replication.Connectors,
+  postgres_3: [
+    producer: Electric.Replication.Producer,
+    connection: [
+      host: 'db_c',
+      port: 5432,
+      database: 'electric',
+      username: 'electric',
+      password: 'password',
+      replication: 'database',
+      ssl: false
+    ],
+    replication: [
+      publication: "all_tables",
+      slot: "all_changes",
+      electric_connection: [
+        host: "electric_b",
+        port: 5433,
+        dbname: "test"
+      ]
+    ]
+  ]
+
+config :logger, backends: [:console], level: :debug
+
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:client, :connection, :slot, :origin]
