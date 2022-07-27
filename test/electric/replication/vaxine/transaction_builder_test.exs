9232e20f9323274f6ee3884adbdd9b7fd8956cfa
v0idpwn <v0idpwn@gmail.com>
2022-07-27 11:51:05 +0200

    fix tests

--- test/electric/replication/vaxine/transaction_builder_test.exs
+++ test/electric/replication/vaxine/transaction_builder_test.exs
@@ -9,49 +9,51 @@ defmodule Electric.Replication.Vaxine.TransactionBuilderTest do
   test "correctly extracts metadata" do
     metadata = TransactionBuilder.extract_metadata(@message)
 
-    assert %Electric.Replication.Metadata{
-             commit_timestamp: %DateTime{},
-             id: "0",
-             publication: "all_tables"
-           } = metadata
+    assert {:ok,
+            %Electric.Replication.Metadata{
+              commit_timestamp: %DateTime{},
+              id: "0",
+              publication: "all_tables"
+            }} = metadata
   end
 
   test "extracts origin transaction" do
-    metadata = TransactionBuilder.extract_metadata(@message)
+    {:ok, metadata} = TransactionBuilder.extract_metadata(@message)
     origin_transaction = TransactionBuilder.build_transaction_for_origin(@message, metadata)
 
-    assert %Electric.Replication.Changes.Transaction{
-             changes: [
-               %Electric.Replication.Changes.UpdatedRecord{
-                 record: %{
-                   "content" => "iliketrains1",
-                   "content_b" => nil,
-                   "id" => "cb2fb0f0-dd12-4e87-a3cd-4eaff087995c"
-                 },
-                 relation: {"public", "entries"}
-               }
-             ],
-             commit_timestamp: %DateTime{}
-           } = origin_transaction
+    assert {:ok,
+            %Electric.Replication.Changes.Transaction{
+              changes: [
+                %Electric.Replication.Changes.UpdatedRecord{
+                  record: %{
+                    "content" => "iliketrains1",
+                    "content_b" => nil,
+                    "id" => "cb2fb0f0-dd12-4e87-a3cd-4eaff087995c"
+                  },
+                  relation: {"public", "entries"}
+                }
+              ],
+              commit_timestamp: %DateTime{}
+            }} = origin_transaction
   end
 
   test "extracts peer transaction" do
-    metadata = TransactionBuilder.extract_metadata(@message)
-    origin_transaction = TransactionBuilder.build_transaction_for_origin(@message, metadata)
+    {:ok, metadata} = TransactionBuilder.extract_metadata(@message)
     peers_transaction = TransactionBuilder.build_transaction_for_peers(@message, metadata)
 
-    assert %Electric.Replication.Changes.Transaction{
-             changes: [
-               %Electric.Replication.Changes.NewRecord{
-                 record: %{
-                   "content" => "iliketrains1",
-                   "content_b" => nil,
-                   "id" => "cb2fb0f0-dd12-4e87-a3cd-4eaff087995c"
-                 },
-                 relation: {"public", "entries"}
-               }
-             ],
-             commit_timestamp: %DateTime{}
-           } = peers_transaction
+    assert {:ok,
+            %Electric.Replication.Changes.Transaction{
+              changes: [
+                %Electric.Replication.Changes.NewRecord{
+                  record: %{
+                    "content" => "iliketrains1",
+                    "content_b" => nil,
+                    "id" => "cb2fb0f0-dd12-4e87-a3cd-4eaff087995c"
+                  },
+                  relation: {"public", "entries"}
+                }
+              ],
+              commit_timestamp: %DateTime{}
+            }} = peers_transaction
   end
 end
