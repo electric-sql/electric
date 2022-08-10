cfe87d6599312258467e30afd44cfc4d9a4287d6
Ilia Borovitinov <ilia@vaxine.io>
2022-08-10 16:05:14 +0300

    Established a logger format for prod

--- config/prod.exs
+++ config/prod.exs
@@ -2,3 +2,7 @@ import Config
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:client, :connection, :slot, :origin]
