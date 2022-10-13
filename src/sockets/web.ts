ef80f0687ae0dda0352e9d164b27b60c71a2d130
Valter Balegas <balegas@live.com>
2022-10-13 23:26:42 +0100

    Add websocket for web

---
+++ src/sockets/web.ts
@@ -0,0 +1,57 @@
+import { ConnectionOptions, Data, Socket } from '.';
+
+export class WebSocketWeb implements Socket {
+    private socket?: WebSocket;
+
+    private connectCallbacks: (() => void)[];
+    private errorCallbacks: ((error: Error) => void)[];
+
+    constructor() {
+        this.connectCallbacks = [];
+        this.errorCallbacks = [];
+    }
+
+    open(opts: ConnectionOptions): Socket {
+        this.socket = new WebSocket(opts.url);
+        this.socket.binaryType = 'arraybuffer';
+
+        this.socket.addEventListener('open', () => {
+            while (this.connectCallbacks.length > 0) {
+                this.connectCallbacks.pop()!();
+            }
+        });
+
+        this.socket.addEventListener('error', (event: any) => {
+            while (this.errorCallbacks.length > 0) {
+                this.errorCallbacks.pop()!(new Error(event.message));
+            }
+        });
+
+        return this;
+    }
+
+    write(data: Data): Socket {
+        this.socket?.send(data);
+        return this;
+    }
+
+    closeAndRemoveListeners(): Socket {
+        this.socket?.close();
+        return this;
+    }
+
+    onMessage(cb: (data: Data) => void): void {
+        this.socket?.addEventListener('message', (event) => {
+            const buffer = new Uint8Array(event.data)
+            cb(buffer)
+        })
+    }
+
+    onceConnect(cb: () => void): void {
+        this.connectCallbacks.push(cb);
+    }
+
+    onceError(cb: (error: Error) => void): void {
+        this.errorCallbacks.push(cb);
+    }
+}
