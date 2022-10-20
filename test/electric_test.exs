4ab0a793e8262e1a4620aa8efaba2e5d6206914d
Garry Hill <garry@magnetised.net>
2022-10-20 15:18:43 +0100

    fix electric test for database_id

--- test/electric_test.exs
+++ test/electric_test.exs
@@ -1,7 +1,7 @@
 defmodule ElectricTest do
   use ExUnit.Case, async: true
 
-  test "cluster_name/0" do
-    assert Electric.cluster_name() == "electric-development-cluster-0000"
+  test "database_id/0" do
+    assert Electric.database_id() == "electric-development-cluster-0000"
   end
 end
