c9c74a3254862df90afd61e616086e28f2754aa2
define-null <vasilii@vaxine.io>
2022-10-24 20:09:12 +0200

    Follow example structure with version directories (#57)

--- lib/electric/migration_utils.ex
+++ lib/electric/migration_utils.ex
@@ -1,16 +1,6 @@
 defmodule Electric.Migration.Utils do
   @type vsn() :: String.t()
 
-  @spec available_migrations() :: [vsn()]
-  def available_migrations() do
-    migration_path = get_migration_dir()
-
-    Enum.map(
-      Path.wildcard(Path.join(migration_path, "*.sql")),
-      &Path.basename(&1, ".sql")
-    )
-  end
-
   @spec read_migration_file(vsn()) :: {:ok, binary} | {:error, term}
   def read_migration_file(vsn) do
     file = get_migration_path(vsn)
@@ -27,7 +17,7 @@ defmodule Electric.Migration.Utils do
   @spec get_migration_path(vsn()) :: binary
   def get_migration_path(vsn) do
     migration_path = get_migration_dir()
-    Path.join(migration_path, vsn <> ".sql")
+    Path.join(migration_path, vsn <> "/postgres.sql")
   end
 
   defp get_migration_dir() do
