f55cdc0099eaa1628ce8e1d7bd8f784f4a3db0b0
Garry Hill <garry@electric-sql.com>
2022-11-17 11:34:48 +0000

    fix tests

--- test/electric_test.exs
+++ test/electric_test.exs
@@ -2,10 +2,10 @@ defmodule ElectricTest do
   use ExUnit.Case, async: true
 
   test "regional_id/0" do
-    assert Electric.regional_id() == "local.dev.electric-db"
+    assert Electric.regional_id() == "region-1.test.electric-db"
   end
 
   test "instance_id/0" do
-    assert Electric.instance_id() == "dev.electric-db"
+    assert Electric.instance_id() == "instance-1.region-1.test.electric-db"
   end
 end
