bc85e8f519ad7f92a3623aa0eb27e52fd369e8ec
Garry Hill <garry@electric-sql.com>
2022-12-06 10:52:42 +0000

    remove out-of-date comment

--- lib/electric/replication/changes/ownership.ex
+++ lib/electric/replication/changes/ownership.ex
@@ -13,7 +13,6 @@ defmodule Electric.Replication.Changes.Ownership do
     Enum.all?(transaction.changes, &change_belongs_to_user?(&1, user_id, @user_id_column))
   end
 
-  # FIXME: this requires that all tables in the schema have a `electric_user_id` column.
   @spec change_belongs_to_user?(Changes.change(), Auth.user_id(), Changes.db_identifier()) ::
           boolean()
   defp change_belongs_to_user?(%Changes.DeletedRecord{old_record: record}, user_id, owner_column) do
