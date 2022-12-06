8b4e1fad9c892ded584a68c5e56861b0421aa5a4
Garry Hill <garry@electric-sql.com>
2022-12-06 10:33:10 +0000

    add doc for belongs_to_user?/2 function

--- lib/electric/replication/changes/ownership.ex
+++ lib/electric/replication/changes/ownership.ex
@@ -7,6 +7,21 @@ defmodule Electric.Replication.Changes.Ownership do
   @spec id_column_name() :: Changes.db_identifier()
   def id_column_name, do: @user_id_column
 
+  @doc """
+  Given a `%Changes.Transaction{}` checks each update within it to verify that they all belong to
+  the given user_id.
+
+  If any of the changes fail this test then the transaction is deemed not to belong to the user.
+
+  For a change to belong to a user with id `user_id` every changed row has to satisfy one of the
+  following conditions:
+
+  1. The table being modified has an `electric_user_id` and the value of this column is either
+     `user_id`, `""` or `NULL`.
+
+  2. The table being modified does not have an `electric_user_id` column. This would be classified
+     as a "global" table where all entries are visible by everyone.
+  """
   @spec belongs_to_user?(Changes.Transaction.t(), Auth.user_id()) :: boolean()
   def belongs_to_user?(transaction, user_id) do
     # FIXME: for now we hard-code the user id column for a table to be `electric_user_id`
