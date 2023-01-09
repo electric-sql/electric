261c81daf1ed99dcb89f2c89996c57d3172c40ca
Ilia Borovitinov <ilia@electric-sql.com>
2023-01-09 21:29:05 +0300

    Made sure cleanup works on success

--- integration_tests/common.mk
+++ integration_tests/common.mk
@@ -62,7 +62,9 @@ start_electric_%:
 	docker-compose -f ${DOCKER_COMPOSE_FILE} up --no-color --no-log-prefix electric_$*
 
 stop_dev_env:
-	docker ps --filter name=elixir_client --format '{{.Names}}' | xargs docker kill
+	if [ -n "`docker ps --filter name=elixir_client --format '{{.Names}}'`" ]; then \
+		docker ps --filter name=elixir_client --format '{{.Names}}' | xargs docker kill; \
+	fi
 	docker-compose -f ${DOCKER_COMPOSE_FILE} down
 	docker-compose -f ${DOCKER_COMPOSE_FILE} stop
 
