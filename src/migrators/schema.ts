d3f4919f66dcea6f5984051f4b8857106b37aaa7
Valter Balegas <balegas@live.com>
2023-01-11 22:57:33 +0000

    VAX-482: Keys in `electric_meta` tables have to be unique
    Made table column 'key' primary key.
    Added test.
    Satellite schema uses default table names instead of hard-coded.

--- src/migrators/schema.ts
+++ src/migrators/schema.ts
@@ -1,12 +1,17 @@
+import { satelliteDefaults } from '../satellite/config'
+
+const { metaTable, migrationsTable, oplogTable, triggersTable } =
+  satelliteDefaults
+
 export const data = {
   migrations: [
     {
       satellite_body: [
-        '-- The ops log table\nCREATE TABLE IF NOT EXISTS _electric_oplog (\n  rowid INTEGER PRIMARY KEY AUTOINCREMENT,\n  namespace String NOT NULL,\n  tablename String NOT NULL,\n  optype String NOT NULL,\n  primaryKey String NOT NULL,\n  newRow String,\n  oldRow String,\n  timestamp TEXT\n);',
-        '-- Somewhere to keep our metadata\nCREATE TABLE IF NOT EXISTS _electric_meta (\n  key TEXT,\n  value BLOB\n);',
-        '-- Somewhere to track migrations\nCREATE TABLE IF NOT EXISTS _electric_migrations (\n  id INTEGER PRIMARY KEY AUTOINCREMENT,\n  name TEXT NOT NULL UNIQUE,\n  sha256 TEXT NOT NULL,\n  applied_at TEXT NOT NULL\n);',
-        "-- Initialisation of the metadata table\nINSERT INTO _electric_meta (key, value) VALUES ('compensations', 0), ('lastAckdRowId','0'), ('lastSentRowId', '0'), ('lsn', ''), ('clientId', ''), ('token', 'INITIAL_INVALID_TOKEN'), ('refreshToken', '');",
-        '-- These are toggles for turning the triggers on and off\nDROP TABLE IF EXISTS _electric_trigger_settings;',
+        `-- The ops log table\nCREATE TABLE IF NOT EXISTS ${oplogTable} (\n  rowid INTEGER PRIMARY KEY AUTOINCREMENT,\n  namespace String NOT NULL,\n  tablename String NOT NULL,\n  optype String NOT NULL,\n  primaryKey String NOT NULL,\n  newRow String,\n  oldRow String,\n  timestamp TEXT\n);`,
+        `-- Somewhere to keep our metadata\nCREATE TABLE IF NOT EXISTS ${metaTable} (\n  key TEXT PRIMARY KEY,\n  value BLOB\n);`,
+        `-- Somewhere to track migrations\nCREATE TABLE IF NOT EXISTS ${migrationsTable} (\n  id INTEGER PRIMARY KEY AUTOINCREMENT,\n  name TEXT NOT NULL UNIQUE,\n  sha256 TEXT NOT NULL,\n  applied_at TEXT NOT NULL\n);`,
+        `-- Initialisation of the metadata table\nINSERT INTO ${metaTable} (key, value) VALUES ('compensations', 0), ('lastAckdRowId','0'), ('lastSentRowId', '0'), ('lsn', ''), ('clientId', ''), ('token', 'INITIAL_INVALID_TOKEN'), ('refreshToken', '');`,
+        `-- These are toggles for turning the triggers on and off\nDROP TABLE IF EXISTS ${triggersTable};`,
         'CREATE TABLE _electric_trigger_settings(tablename STRING PRIMARY KEY, flag INTEGER);',
       ],
       encoding: 'escaped',
