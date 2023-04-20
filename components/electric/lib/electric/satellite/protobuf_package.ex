51aaaef87630da1ad1cdd3b3316f13b1931c91d3
Garry Hill <garry@electric-sql.com>
2023-04-20 12:34:59 +0100

    Add migration flag to operation transactions

--- components/electric/lib/electric/satellite/protobuf_package.ex
+++ components/electric/lib/electric/satellite/protobuf_package.ex
@@ -1,8 +1,8 @@
 # File generated from protobuf file: ../../protocol/satellite.proto
-defmodule Electric.Satellite.V11 do
+defmodule Electric.Satellite.V12 do
   @moduledoc false
   @spec package() :: String.t()
   def package() do
-    "Electric.Satellite.v1_1"
+    "Electric.Satellite.v1_2"
   end
 end
