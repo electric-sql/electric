73c170c2c372e00cf30687d55539e36e89a67563
Kevin De Porre <kevin@electric-sql.com>
2023-04-24 15:24:56 +0200

    Remove unused imports in test.

--- clients/typescript/test/satellite/process.timing.test.ts
+++ clients/typescript/test/satellite/process.timing.test.ts
@@ -1,33 +1,8 @@
-import { mkdir, rm as removeFile } from 'node:fs/promises'
-
 import test from 'ava'
-
-import Database from 'better-sqlite3'
-import { DatabaseAdapter } from '../../src/drivers/better-sqlite3/adapter'
-
-import { MockSatelliteClient } from '../../src/satellite/mock'
-import { BundleMigrator } from '../../src/migrators/bundle'
-import { MockNotifier } from '../../src/notifiers/mock'
-import { MockConsoleClient } from '../../src/auth/mock'
-import { randomValue } from '../../src/util/random'
 import { sleepAsync } from '../../src/util/timer'
 
-import { SatelliteConfig, satelliteDefaults } from '../../src/satellite/config'
-import { SatelliteProcess } from '../../src/satellite/process'
-
-import { initTableInfo } from '../support/satellite-helpers'
-import { Satellite } from '../../src/satellite'
-
-import config from '../support/.electric/@config/index'
+import { satelliteDefaults } from '../../src/satellite/config'
 import {makeContext, stopSatellite} from "./common";
-const { migrations } = config
-
-type ContextType = {
-  dbName: string
-  adapter: DatabaseAdapter
-  satellite: Satellite
-  client: MockSatelliteClient
-}
 
 // Speed up the intervals for testing.
 const opts = Object.assign({}, satelliteDefaults, {
