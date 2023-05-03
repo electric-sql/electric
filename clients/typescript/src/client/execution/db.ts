2a70d4e50750c7f0e851922d41634291d8ef5e99
Kevin De Porre <kevin@electric-sql.com>
2023-05-03 14:16:37 +0200

    Comment explaining callback-based style for DB interface.

--- clients/typescript/src/client/execution/db.ts
+++ clients/typescript/src/client/execution/db.ts
@@ -2,6 +2,14 @@ import { RunResult } from '../../electric/adapter'
 import { QueryBuilder } from 'squel'
 import * as z from 'zod'
 
+/**
+ * Interface that must be implemented by DB implementations.
+ * The `run` and `query` methods are callback-based
+ * because the `transactionalDB` is implemented atop
+ * Electric's DatabaseAdapter which supports transactions
+ * but requires a callback-based style because
+ * some underlying drivers do not support promises.
+ */
 export interface DB {
   run(
     statement: string,
