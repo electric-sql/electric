15c95191539075db6babdeb8069a2863a44bad15
Oleksii Sholik <oleksii@sholik.dev>
2023-05-11 11:03:23 +0300

    Verify that insecure auth works with signed JWTs

--- components/electric/test/electric/satellite/auth/insecure_test.exs
+++ components/electric/test/electric/satellite/auth/insecure_test.exs
@@ -5,6 +5,7 @@ defmodule Electric.Satellite.Auth.InsecureTest do
   alias Electric.Satellite.Auth
 
   @namespace "https://electric-sql.com/jwt/claims"
+  @signing_key 'abcdefghijklmnopqrstuvwxyz012345' |> Enum.shuffle() |> List.to_string()
 
   describe "unsigned validate_token()" do
     test "successfully validates a token that has no signature" do
@@ -68,6 +69,54 @@ defmodule Electric.Satellite.Auth.InsecureTest do
       map
       |> Jason.encode!()
       |> Base.url_encode64(padding: false)
+    end
+  end
+
+  describe "signed validate_token()" do
+    test "successfully validates a signed token with no signing key configured" do
+      claims = %{
+        "iat" => DateTime.to_unix(~U[2023-05-01 00:00:00Z]),
+        "nbf" => DateTime.to_unix(~U[2023-05-01 00:00:00Z]),
+        "exp" => DateTime.to_unix(~U[2123-05-01 00:00:00Z]),
+        @namespace => %{"user_id" => "12345"}
+      }
+
+      token = signed_token(claims)
+
+      assert {:ok, %Auth{user_id: "12345"}} ==
+               validate_token(token, config(namespace: @namespace))
+    end
+
+    test "successfully extracts the namespaced user_id claim" do
+      claims = %{"custom_namespace" => %{"user_id" => "000"}}
+      token = signed_token(claims)
+
+      assert {:ok, %Auth{user_id: "000"}} ==
+               validate_token(token, config(namespace: "custom_namespace"))
+
+      claims = %{"user_id" => "111"}
+      token = signed_token(claims)
+      assert {:ok, %Auth{user_id: "111"}} == validate_token(token, config(namespace: ""))
+    end
+
+    test "verifies that user_id is present and is not empty" do
+      for claims <- [
+            %{@namespace => %{}},
+            %{@namespace => %{"user_id" => ""}},
+            %{@namespace => %{"user_id" => 555}},
+            %{"custom_namespace" => %{"user_id" => "123"}}
+          ] do
+        token = signed_token(claims)
+
+        assert {:error, %Auth.TokenError{message: "Missing or invalid 'user_id'"}} ==
+                 validate_token(token, config(namespace: @namespace))
+      end
+    end
+
+    defp signed_token(claims) do
+      signer = Joken.Signer.create("HS256", @signing_key)
+      {:ok, token, _claims} = Joken.encode_and_sign(claims, signer)
+      token
     end
   end
 
