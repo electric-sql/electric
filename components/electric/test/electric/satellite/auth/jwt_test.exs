6dc22df42d33366fed9e514fa04005068b86f946
Oleksii Sholik <oleksii@sholik.dev>
2023-05-11 11:30:02 +0300

    Verify that jwt auth correctly extracts user_id from signed JWTs

--- components/electric/test/electric/satellite/auth/jwt_test.exs
+++ components/electric/test/electric/satellite/auth/jwt_test.exs
@@ -6,6 +6,7 @@ defmodule Electric.Satellite.Auth.JWTTest do
   alias Electric.Satellite.Auth.ConfigError
 
   @namespace "https://electric-sql.com/jwt/claims"
+  @signing_key 'abcdefghijklmnopqrstuvwxyz012345' |> Enum.shuffle() |> List.to_string()
 
   describe "build_config!()" do
     test "returns a clean map when all checks pass" do
@@ -109,6 +110,55 @@ defmodule Electric.Satellite.Auth.JWTTest do
 
       assert {:error, %Auth.TokenError{message: "Signing algorithm mismatch"}} ==
                validate_token(token, config([]))
+    end
+  end
+
+  describe "validate_token(<signed token>)" do
+    test "successfully extracts the namespaced user_id claim" do
+      token = signed_token(claims(%{"custom_namespace" => %{"user_id" => "000"}}))
+
+      assert {:ok, %Auth{user_id: "000"}} ==
+               validate_token(token, config(namespace: "custom_namespace"))
+
+      ###
+
+      token = signed_token(claims(%{"user_id" => "111"}))
+      assert {:ok, %Auth{user_id: "111"}} == validate_token(token, config(namespace: ""))
+    end
+
+    test "verifies that user_id is present and is not empty" do
+      for claims <- [
+            %{@namespace => %{}},
+            %{@namespace => %{"user_id" => ""}},
+            %{@namespace => %{"user_id" => 555}},
+            %{"custom_namespace" => %{"user_id" => "123"}}
+          ] do
+        token = signed_token(claims(claims))
+
+        assert {:error, %Auth.TokenError{message: "Missing or invalid 'user_id'"}} ==
+                 validate_token(token, config(namespace: @namespace))
+      end
+    end
+
+    defp signed_token(claims) do
+      signer = Joken.Signer.create("HS256", @signing_key)
+      {:ok, token, _} = Joken.encode_and_sign(claims, signer)
+      token
+    end
+
+    defp config(opts) do
+      [alg: "HS256", key: @signing_key, namespace: @namespace]
+      |> Keyword.merge(opts)
+      |> build_config!()
+    end
+
+    defp claims(claims) do
+      valid_required_claims = %{
+        "iat" => DateTime.to_unix(~U[2023-05-01 00:00:00Z]),
+        "exp" => DateTime.to_unix(~U[2123-05-01 00:00:00Z])
+      }
+
+      Map.merge(valid_required_claims, claims)
     end
   end
 end
