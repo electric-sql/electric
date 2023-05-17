da4c4d431cd5c430c9dc613daa9821530fead395
Garry Hill <garry@electric-sql.com>
2023-05-17 16:44:02 +0100

    Fix unit tests

--- components/electric/test/electric/postgres/logical_replication/messages_test.exs
+++ components/electric/test/electric/postgres/logical_replication/messages_test.exs
@@ -1,37 +1,37 @@
 defmodule Electric.Postgres.LogicalReplication.MessagesTest do
   use ExUnit.Case, async: true
 
-  alias Electric.Postgres.LogicalReplication.Messages
+  alias Electric.Replication.Changes
 
-  describe "Relation" do
+  describe "Changes.Relation" do
     test "to_schema_table/1" do
-      msg = %Messages.Relation{
+      msg = %Changes.Relation{
         columns: [
-          %Messages.Relation.Column{
+          %Changes.Relation.Column{
             flags: [:key],
             name: "id",
             type: :int8,
             type_modifier: -1
           },
-          %Messages.Relation.Column{
+          %Changes.Relation.Column{
             flags: [],
             name: "txid",
             type: :xid8,
             type_modifier: -1
           },
-          %Messages.Relation.Column{
+          %Changes.Relation.Column{
             flags: [],
             name: "txts",
             type: :timestamptz,
             type_modifier: -1
           },
-          %Messages.Relation.Column{
+          %Changes.Relation.Column{
             flags: [],
             name: "version",
             type: :varchar,
             type_modifier: 259
           },
-          %Messages.Relation.Column{
+          %Changes.Relation.Column{
             flags: [],
             name: "query",
             type: :text,
@@ -72,7 +72,7 @@ defmodule Electric.Postgres.LogicalReplication.MessagesTest do
         %{name: "query", part_of_identity?: false, type: :text, type_modifier: -1}
       ]
 
-      assert Messages.Relation.to_schema_table(msg) == {table, columns}
+      assert Changes.Relation.to_schema_table(msg) == {table, columns}
     end
   end
 end
