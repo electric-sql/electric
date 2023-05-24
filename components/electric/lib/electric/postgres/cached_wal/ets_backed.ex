45dfa721964baa0949021474851f76030a26195b
Ilia Borovitinov <ilia@electric-sql.com>
2023-05-24 12:46:53 +0300

    Added notification logic to ets backend

--- components/electric/lib/electric/postgres/cached_wal/ets_backed.ex
+++ components/electric/lib/electric/postgres/cached_wal/ets_backed.ex
@@ -24,6 +24,7 @@ defmodule Electric.Postgres.CachedWal.EtsBacked do
   @typep state :: %{
            notification_requests: %{optional(reference()) => {Api.wal_pos(), pid()}},
            table: ETS.Set.t(),
+           last_seen_wal_pos: Api.wal_pos(),
            current_cache_count: non_neg_integer(),
            max_cache_count: non_neg_integer()
          }
@@ -31,9 +32,11 @@ defmodule Electric.Postgres.CachedWal.EtsBacked do
   # Public API
 
   @doc """
-  Get a `:gproc` via-tuple based on the origin that fills this cache
+  Get a `:gproc` via-tuple based on the origin that fills this cache.
+
+  Origin is provided only as a legacy option, as in the future only one PG is ever going to be present
   """
-  def get_name(origin) do
+  def get_name(origin \\ :default) do
     {:via, :gproc, {:n, :l, {__MODULE__, origin}}}
   end
 
@@ -66,19 +69,19 @@ defmodule Electric.Postgres.CachedWal.EtsBacked do
          {:ok, next_key} <- ETS.Set.next(table, wal_pos) do
       {:ok, ETS.Set.get_element!(table, next_key, 2), next_key}
     else
-     {:error, :end_of_table} -> :latest
-     error -> error
+      {:error, :end_of_table} -> :latest
+      error -> error
     end
   end
 
   @impl Api
-  def request_notification(_wal_pos) do
-    raise "Not implemented"
+  def request_notification(wal_pos, origin \\ :default) do
+    GenStage.call(get_name(origin), {:request_notification, wal_pos})
   end
 
   @impl Api
-  def cancel_notification_request(_ref) do
-    raise "Not implemented"
+  def cancel_notification_request(ref, origin \\ :default) do
+    GenStage.call(get_name(origin), {:cancel_notification, ref})
   end
 
   # Internal API
@@ -90,6 +93,7 @@ defmodule Electric.Postgres.CachedWal.EtsBacked do
     state = %{
       notification_requests: %{},
       table: set,
+      last_seen_wal_pos: 0,
       current_cache_count: 0,
       max_cache_count: Keyword.get(opts, :max_cache_count, 10000)
     }
@@ -101,17 +105,36 @@ defmodule Electric.Postgres.CachedWal.EtsBacked do
   end
 
   @impl GenStage
+  def handle_call({:request_notification, wal_pos}, from, state) do
+    ref = make_ref()
+    state = Map.update!(state, :notification_requests, &Map.put(&1, ref, {wal_pos, from}))
+
+    if wal_pos < state.max_seen_wal_pos do
+      send(self(), :fulfill_notifications)
+    end
+
+    {:reply, {:ok, ref}, [], state}
+  end
+
+  @impl GenStage
+  def handle_call({:cancel_notification, ref}, _, state) do
+    state = Map.update!(state, :notification_requests, &Map.delete(&1, ref))
+
+    {:reply, :ok, [], state}
+  end
+
+  @impl GenStage
   def handle_cast(:clear, state) do
     ETS.Set.delete_all!(state.table)
 
     # This doesn't do anything with notification requests, but this function is not meant to be used in production
-    {:noreply, [], %{state | current_cache_count: 0}}
+    {:noreply, [], %{state | current_cache_count: 0, last_seen_wal_pos: 0}}
   end
 
   @impl GenStage
   @spec handle_events([Transaction.t()], term(), state()) :: {:noreply, [], any}
   def handle_events(events, _, state) do
-    events = Enum.filter(events, & &1.changes != [])
+    events = Enum.reject(events, &Enum.empty?(&1.changes))
 
     events
     |> Enum.map(fn %Transaction{lsn: lsn} = tx -> {lsn_to_position(lsn), %{tx | ack_fn: nil}} end)
@@ -125,15 +148,21 @@ defmodule Electric.Postgres.CachedWal.EtsBacked do
 
     state =
       state
-      |> fulfill_notification_requests(lsn_to_position(max_lsn))
+      |> Map.put(:last_seen_wal_pos, lsn_to_position(max_lsn))
+      |> fulfill_notification_requests()
       |> Map.update!(:current_cache_count, &(&1 + total))
       |> trim_cache()
 
     {:noreply, [], state}
   end
 
-  @spec fulfill_notification_requests(state(), Api.wal_pos()) :: state()
-  defp fulfill_notification_requests(state, new_max_lsn) do
+  @impl GenStage
+  def handle_info(:fulfill_notifications, state) do
+    {:noreply, [], fulfill_notification_requests(state)}
+  end
+
+  @spec fulfill_notification_requests(state()) :: state()
+  defp fulfill_notification_requests(%{last_seen_wal_pos: new_max_lsn} = state) do
     fulfilled_refs =
       state.notification_requests
       |> Stream.filter(fn {_, {target, _}} -> target <= new_max_lsn end)
