adfc797164a61890409114a916c9a022e8e7ba24
TachyonicBytes <support@tachyonicbytes.com>
2023-06-21 01:15:52 +0300

    Ran the prettifier

--- clients/typescript/src/util/random.ts
+++ clients/typescript/src/util/random.ts
@@ -3,29 +3,35 @@ export const randomValue = (): string => {
 }
 
 export const genUUID = (): string => {
-  const bytes = new Uint8Array(16);
+  const bytes = new Uint8Array(16)
 
   if (!!crypto && !!crypto.getRandomValues) {
     // use the crypto api
-    crypto.getRandomValues(bytes);
+    crypto.getRandomValues(bytes)
   } else {
     // fallback to Math.random
     for (let i = 0; i < bytes.length; i++) {
-      bytes[i] = Math.floor(Math.random() * 256);
+      bytes[i] = Math.floor(Math.random() * 256)
     }
   }
 
-  bytes[6] = (bytes[6] & 0x0f) | 0x40; // Set the 4 most significant bits to 0100
-  bytes[8] = (bytes[8] & 0x3f) | 0x80; // Set the 2 most significant bits to 10
+  bytes[6] = (bytes[6] & 0x0f) | 0x40 // Set the 4 most significant bits to 0100
+  bytes[8] = (bytes[8] & 0x3f) | 0x80 // Set the 2 most significant bits to 10
 
-  const hexValues: string[] = [];
-  bytes.forEach(byte => {
-    hexValues.push(byte.toString(16).padStart(2, '0'));
-  });
+  const hexValues: string[] = []
+  bytes.forEach((byte) => {
+    hexValues.push(byte.toString(16).padStart(2, '0'))
+  })
 
-  return hexValues.slice(0, 4).join('') + '-' +
-         hexValues.slice(4, 6).join('') + '-' +
-         hexValues.slice(6, 8).join('') + '-' +
-         hexValues.slice(8, 10).join('') + '-' +
-         hexValues.slice(10).join('');
+  return (
+    hexValues.slice(0, 4).join('') +
+    '-' +
+    hexValues.slice(4, 6).join('') +
+    '-' +
+    hexValues.slice(6, 8).join('') +
+    '-' +
+    hexValues.slice(8, 10).join('') +
+    '-' +
+    hexValues.slice(10).join('')
+  )
 }
