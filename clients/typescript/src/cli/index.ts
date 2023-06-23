f132d5d23e6c92ee9b70ba1d15266ba9b72555f6
Kevin De Porre <kevin@electric-sql.com>
2023-06-23 08:30:10 +0200

    Replaced migration shell script by NodeJS script.

--- clients/typescript/src/cli/index.ts
+++ clients/typescript/src/cli/index.ts
@@ -1,1 +1,57 @@
-export { buildMigrations, loadMigrations } from './migrator'
+#!/usr/bin/env node
+
+//import { spawn, StdioOptions } from 'child_process'
+//import path from 'path'
+import { migrate } from './migrations'
+
+const args = process.argv
+
+if (args.length < 3) {
+  throw new Error(
+    'Too few arguments passed to CLI bin. Expected at least a command but got none.'
+  )
+}
+
+// When this file is called as follows: `node bin.js migrate`
+// the arguments will be the path to node, the path to bin.js, "migrate",
+// followed by the rest of the arguments
+const [_node, _file, command, ...commandArgs] = process.argv
+
+const commandHandlers = {
+  migrate: handleMigrate,
+}
+
+if (!commandHandlers.hasOwnProperty(command)) {
+  throw new Error('Unknown command: ' + command)
+}
+
+const handler = commandHandlers[command as keyof typeof commandHandlers]
+handler(...commandArgs)
+
+function handleMigrate(...args: string[]) {
+  if (args.length > 1) {
+    throw new Error(
+      'migrate command accepts 1 optional argument (the path to the Prisma schema) but got: ' +
+        args.length
+    )
+  }
+
+  const pathToPrismaSchema = args[0] ?? 'prisma/schema.prisma'
+  migrate(pathToPrismaSchema)
+
+  /*
+  const appRoot = path.resolve() // path where the user ran `npx electric migrate`
+
+  const opts = {
+    cwd: appRoot,
+    stdio: 'inherit' as StdioOptions,
+  }
+
+  // Execute the migration.sh script
+  spawn(
+    './node_modules/electric-sql/dist/migrate.sh',
+    ['-p', pathToPrismaSchema],
+    opts
+  )
+  */
+}
