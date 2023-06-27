cebdad2c02fe36ed78820bc73b7d0c6ba8227694
Ilia Borovitinov <ilia@electric-sql.com>
2023-06-27 19:19:46 +0300

    feedback: formatting

--- components/electric/lib/electric/postgres/extension/migrations/20230512000000_conflict_resolution_triggers.ex
+++ components/electric/lib/electric/postgres/extension/migrations/20230512000000_conflict_resolution_triggers.ex
@@ -23,7 +23,7 @@ defmodule Electric.Postgres.Extension.Migrations.Migration_20230512000000_confli
       @contents["electric_tag_type_and_operators"],
       @contents["utility_functions"],
       @contents["trigger_function_installers"],
-      @contents["shadow_table_creation_and_update"],
+      @contents["shadow_table_creation_and_update"]
       # We need to actually run shadow table creation/updates, but that's handled in the next migration.
     ]
     |> Enum.map(&String.replace(&1, "electric", schema))
