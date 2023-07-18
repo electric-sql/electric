818c75e7ceb98db9c59498a2e469e5319898026e
Ilia Borovitinov <ilia@electric-sql.com>
2023-07-18 16:41:28 +0300

    Fixed tests

--- clients/typescript/src/cli/migrations/handler.ts
+++ clients/typescript/src/cli/migrations/handler.ts
@@ -78,11 +78,11 @@ export function parseGenerateArgs(args: string[]): GeneratorArgs {
       process.exit(9)
     }
   }
-  genArgs.service = genArgs.service?.trim()
-
+  const service = genArgs.service?.trim()
+  
   // prepend protocol if not provided in service url
-  if (genArgs.service && !/^https?:\/\//.test(genArgs.service)) {
-    genArgs.service = 'http://' + genArgs.service
+  if (service && !/^https?:\/\//.test(service)) {
+    genArgs.service = 'http://' + service
   }
 
   return genArgs
