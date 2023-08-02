a18899fc0ed3471558acfa2bac84716ed9d9c883
Oleksii Sholik <oleksii@sholik.dev>
2023-08-02 10:53:13 +0300

    Fix 'mix release' by inlining prod config in runtime.exs

--- components/electric/config/runtime.exs
+++ components/electric/config/runtime.exs
@@ -75,4 +75,41 @@ config :electric, Electric.Satellite.WsServer,
 config :electric, Electric.PostgresServer,
   port: System.get_env("LOGICAL_PUBLISHER_PORT", "5433") |> String.to_integer()
 
-Code.require_file("runtime.#{config_env()}.exs", __DIR__)
+
+# The :prod environment is inlined here because by default Mix won't copy any config/runtime.*.exs files when assembling
+# a release, and we want a single configuration file in our release.
+if config_env() == :prod do
+  postgresql_connection =
+    System.fetch_env!("DATABASE_URL")
+    |> PostgresqlUri.parse()
+    |> then(&Keyword.put(&1, :host, &1[:hostname]))
+    |> Keyword.delete(:hostname)
+    |> Keyword.put_new(:ssl, false)
+    |> Keyword.update(:timeout, 5_000, &String.to_integer/1)
+    |> Keyword.put(:replication, "database")
+
+  pg_server_host =
+    System.get_env("LOGICAL_PUBLISHER_HOST") ||
+      raise("Env variable LOGICAL_PUBLISHER_HOST is not set")
+
+  connectors = [
+    {"postgres_1",
+     producer: Electric.Replication.Postgres.LogicalReplicationProducer,
+     connection: postgresql_connection,
+     replication: [
+       electric_connection: [
+         host: pg_server_host,
+         port: pg_server_port,
+         dbname: "electric",
+         connect_timeout: postgresql_connection[:timeout]
+       ]
+     ]}
+  ]
+
+  config :electric, Electric.Replication.Connectors, connectors
+
+  config :electric, Electric.Replication.OffsetStorage,
+    file: System.get_env("OFFSET_STORAGE_FILE", default_offset_storage_path)
+else
+  Code.require_file("runtime.#{config_env()}.exs", __DIR__)
+end
