60d646d29ed4885b87b19bcc03b47c964b2ba2e3
James Arthur <thruflo@gmail.com>
2023-08-02 12:58:41 +0200

    client: remove legacy absurd-sql references. (#306)
    Noticed some stray absurd-sql / worker stuff still in the code.

--- clients/typescript/test/support/mock-worker-with-bridge.js
+++
@@ -1,6 +0,0 @@
-import { MockElectricWorker } from '../../src/drivers/absurd-sql/mock'
-import { WorkerBridgeNotifier } from '../../src/notifiers/bridge'
-
-const workerServer = new MockElectricWorker(self, {})
-const bridgeNotifier = new WorkerBridgeNotifier('test.db', workerServer)
-workerServer.opts = { notifier: bridgeNotifier }
