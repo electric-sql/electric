bc09f6db976fd81f7268971a70634adf94dfada9
James Arthur <thruflo@gmail.com>
2023-08-24 13:54:09 +0200

    util: address tabId review comments.

--- clients/typescript/src/util/tab.ts
+++ clients/typescript/src/util/tab.ts
@@ -28,45 +28,53 @@ const defaultKey = 'electric-sql.utils.tab:id'
 // Used for in-memory storage when `sessionStorage` is not supported.
 let tabId: string | null = null
 
-// Returns a unique ID for each browser tab. This can be used to construct a
-// tab scoped DB name. This allows data to sync between tabs by using one
-// named SQLite database file per tab. This is sub-optimal compared with
-// sharing a database but avoids concurrent database access.
-//
-// Handles duplicate tabs with some potential for false positives.
-// False positives result in a new tabID which means a new database name
-// and thus additional data transfer and storage.
-//
-// Uses window.sessionStorage. Some browsers disable access to sessionStorage
-// even when saying its available (e.g.: as a result of disabling third party
-// cookies o_O). So we handle this by falling back to an in-memory tabId
-// singleton -- which means each page load syncs data into a new DB.
-//
-// Other platforms can pass in a storage implementation or default to the
-// in-memory tabId singleton.
+/**
+ * Returns a unique ID for each browser tab. This can be used to construct a
+ * tab scoped DB name. This allows data to sync between tabs by using one
+ * named SQLite database file per tab. This is sub-optimal compared with
+ * sharing a database but avoids concurrent database access.
+ *
+ * Handles duplicate tabs with some potential for false positives.
+ * False positives result in a new tabID which means a new database name
+ * and thus additional data transfer and storage.
+ *
+ * Uses window.sessionStorage. Some browsers disable access to sessionStorage
+ * even when saying its available (e.g.: as a result of disabling third party
+ * cookies o_O). So we handle this by falling back to an in-memory tabId
+ * singleton -- which means each page load syncs data into a new DB.
+ *
+ * Other platforms can pass in a storage implementation or default to the
+ * in-memory tabId singleton.
+ */
 export const uniqueTabId = (opts: Options = {}): ReturnValue => {
   const key = opts.key ?? defaultKey
 
   // Use sessionStorage as the default storage if available.
-  let defaultStorage: Storage
-  try {
-    defaultStorage = window.sessionStorage
-  } catch {
-    // We catch errors in the Storage use (we have to to handle
-    // browser security where they pretend Storage is available
-    // but then throw errors when you use it) so this cast isn't
-    // as bad as it looks.
-    defaultStorage = {} as Storage
+  let storage: Storage
+  if (opts.storage !== undefined) {
+    storage = opts.storage
+  } else {
+    try {
+      storage = window.sessionStorage
+    } catch {
+      // We catch errors in the Storage use (we have to to handle
+      // browser security where they pretend Storage is available
+      // but then throw errors when you use it) so this cast isn't
+      // as bad as it looks.
+      storage = {} as Storage
+    }
   }
-  const storage: Storage = opts.storage ?? defaultStorage
 
-  let defaultNavEntries: NavEntry[]
-  try {
-    defaultNavEntries = window.performance.getEntriesByType('navigation')
-  } catch {
-    defaultNavEntries = []
+  let navEntries: NavEntry[]
+  if (opts.navEntries !== undefined) {
+    navEntries = opts.navEntries
+  } else {
+    try {
+      navEntries = window.performance.getEntriesByType('navigation')
+    } catch {
+      navEntries = []
+    }
   }
-  const navEntries = opts.navEntries ?? defaultNavEntries
 
   // Lookup the tabId
   let existingTabId: string | null
@@ -90,6 +98,11 @@ export const uniqueTabId = (opts: Options = {}): ReturnValue => {
   }
 
   // If it's not a duplicate tab then use it.
+  //
+  // Note that only the current document is included in the [performance timeline][1],
+  // so there is only one PerformanceNavigationTiming object in the navEntries array.
+  //
+  // [1]: https://developer.mozilla.org/en-US/docs/Web/API/PerformanceNavigationTiming
   const navEntry =
     navEntries && navEntries.length ? navEntries[0] : { type: null }
   const mayBeDuplicate = navEntry.type === 'back_forward'
