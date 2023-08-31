b0fcbff548cc50a0b8e8f47c02efed2a465416d4
Garry Hill <garry@electric-sql.com>
2023-08-31 16:27:48 +0100

    shortcut tables in the electric schema

--- components/electric/lib/electric/postgres/extension/schema_cache.ex
+++ components/electric/lib/electric/postgres/extension/schema_cache.ex
@@ -134,11 +134,19 @@ defnitmodule Electric.Postgres.Extension.SchemaCache do
   end
 
   @impl SchemaLoader
+  def table_electrified?(_origin, {"electric", _name}) do
+    {:ok, false}
+  end
+
   def table_electrified?(origin, {schema, name}) do
     call(origin, {:table_electrified?, schema, name})
   end
 
   @impl SchemaLoader
+  def index_electrified?(_origin, {"electric", _name}) do
+    {:ok, false}
+  end
+
   def index_electrified?(origin, {schema, name}) do
     call(origin, {:index_electrified?, schema, name})
   end
