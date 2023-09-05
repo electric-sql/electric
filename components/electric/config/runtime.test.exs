ef5e2ac6ebd5a35d0cda7a257462fb279777e2c4
Garry Hill <garry@electric-sql.com>
2023-09-05 13:48:44 +0100

    add commented config to enable sasl reports

--- components/electric/config/runtime.test.exs
+++ components/electric/config/runtime.test.exs
@@ -9,6 +9,12 @@ auth_config =
     iss: "electric-sql-test-issuer"
   )
 
+# it can be useful to turn on sasl_reports which ensure that stacktraces
+# from crashed processes are outputted before the vm shuts down
+# config :logger,
+#   handle_otp_reports: true,
+#   handle_sasl_reports: true
+
 config :electric, Electric.Satellite.Auth, provider: {Auth.Secure, auth_config}
 
 config :electric, disable_listeners: true
