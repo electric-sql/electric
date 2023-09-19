0c2713b8ecdec2716d0e27e22450a3ccaf42b483
Gregorio Zanon <gfzanondv@gmail.com>
2023-09-19 19:08:17 +0200

    Expose only relevant functions of the Capacitor SQLite plugin in database.ts

--- clients/typescript/src/drivers/capacitor-sqlite/database.ts
+++ clients/typescript/src/drivers/capacitor-sqlite/database.ts
@@ -1,16 +1,19 @@
+import { SQLiteDBConnection } from '@capacitor-community/sqlite'
 import { DbName } from '../../util/types'
 
 // A bit of a hack, but that lets us reference the actual types of the library
-type OriginalDatabase = SQLitePlugin.Database
+type OriginalDatabase = SQLiteDBConnection;
 
 // The relevant subset of the SQLitePlugin database client API
 // that we need to ensure the client we're electrifying provides.
+// TODO: verify which functions we actually need.
 export interface Database
   extends Pick<
     OriginalDatabase,
-    'executeSql' | 'transaction' | 'readTransaction' | 'sqlBatch'
+    'execute' | 'executeTransaction' | 'query' | 'run'
   > {
   // Cordova calls the database name `.dbname` using camel case.
   // this is different to React Native which uses `.dbname`.
+  // TODO: understand this???
   dbname?: DbName
 }
