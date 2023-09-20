44108b2dcbe407dcde199d98432529d5e2ec3167
Garry Hill <garry@electric-sql.com>
2023-09-20 13:56:26 +0100

    include proxy config in dev

--- components/electric/config/runtime.dev.exs
+++ components/electric/config/runtime.dev.exs
@@ -5,6 +5,11 @@ config :logger, level: :debug
 auth_provider = System.get_env("AUTH_MODE", "secure") |> Electric.Satellite.Auth.build_provider!()
 config :electric, Electric.Satellite.Auth, provider: auth_provider
 
+proxy_port = System.get_env("PG_PROXY_PORT", "65432") |> String.to_integer()
+
+proxy_password =
+  System.get_env("PG_PROXY_PASSWORD", "password")
+
 config :electric, Electric.Replication.Connectors,
   postgres_1: [
     producer: Electric.Replication.Postgres.LogicalReplicationProducer,
@@ -23,5 +28,14 @@ config :electric, Electric.Replication.Connectors,
         port: 5433,
         dbname: "test"
       ]
+    ],
+    proxy: [
+      # listen opts are ThousandIsland.options()
+      # https://hexdocs.pm/thousand_island/ThousandIsland.html#t:options/0
+      listen: [
+        port: proxy_port
+      ],
+      password: proxy_password,
+      log_level: :info
     ]
   ]
