6c6616a89bb5f64535da2073c0f40405782d876d
Sam Willis <sam.willis@gmail.com>
2023-09-20 09:21:46 +0100

    Upgrade to electric v0.6.2, use Vite env vars to electric url and debug mode

--- examples/linearlite/src/electric.tsx
+++ examples/linearlite/src/electric.tsx
@@ -1,6 +1,7 @@
-import { version } from 'electric-sql/package.json'
+import { LIB_VERSION } from 'electric-sql/version'
 import { makeElectricContext } from 'electric-sql/react'
-import { uniqueTabId } from 'electric-sql/util'
+import { uniqueTabId, genUUID } from 'electric-sql/util'
+import { insecureAuthToken } from 'electric-sql/auth'
 import { electrify, ElectricDatabase } from 'electric-sql/wa-sqlite'
 import { Electric, schema } from './generated/client'
 export type { Issue } from './generated/client'
@@ -10,20 +11,34 @@ export const { ElectricProvider, useElectric } = makeElectricContext<Electric>()
 const discriminator = 'linearlite'
 const distPath = '/'
 
-const searchParams = new URLSearchParams(window.location.search)
-export const DEBUG = searchParams.get('debug') === 'true'
+// import.meta.env is a special object that Vite provides for accessing
+// environment variables at build time and runtime.
+// They are replaced at build time with the actual values.
+// https://vitejs.dev/guide/env-and-mode.html
+const DEV_MODE = import.meta.env.DEV
+const ELECTRIC_URL = import.meta.env.ELECTRIC_URL
+const DEBUG_ENV = import.meta.env.DEBUG
 
+// We can override the debug mode with a query param: ?debug=true or ?debug=false
+const searchParams = new URLSearchParams(window.location.search)
+const debugParam = searchParams.get('debug')
+
+// DEBUG defaults to true in dev mode, false in prod mode
+export const DEBUG = debugParam ? debugParam === 'true' : DEV_MODE || DEBUG_ENV
+
+// We export dbName so that we can delete the database if the schema changes
 export let dbName: string
 
 export const initElectric = async () => {
   const { tabId } = uniqueTabId()
-  dbName = `${discriminator}-${version}-${tabId}.db`
+  const electricUrl = ELECTRIC_URL ?? 'ws://localhost:5133'
+  dbName = `${discriminator}-${LIB_VERSION}-${tabId}.db`
 
   const config = {
     auth: {
-      token:
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsb2NhbC1kZXZlbG9wbWVudCIsInR5cGUiOiJhY2Nlc3MiLCJ1c2VyX2lkIjoidGVzdC11c2VyIiwiaWF0IjoxNjg3ODc3OTQ1LCJleHAiOjE2OTc4ODE1NDV9.L5Ui2sA9o5MeYDuy67u9lBV-2FzpOWL9dKcitRvgorg',
+      token: insecureAuthToken({ user_id: genUUID() }),
     },
+    url: electricUrl,
     debug: DEBUG,
   }
 
