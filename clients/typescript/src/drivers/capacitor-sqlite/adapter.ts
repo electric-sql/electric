53923048ca992f325c1c5bfc6c16e9de750a90fb
Gregorio Zanon <gfzanondv@gmail.com>
2023-09-22 09:50:58 +0200

    MockDatabase updated to mock errors. Tests added, modelled after wa-sqlite tests.

--- clients/typescript/src/drivers/capacitor-sqlite/adapter.ts
+++ clients/typescript/src/drivers/capacitor-sqlite/adapter.ts
@@ -82,7 +82,7 @@ export class DatabaseAdapter
             reject(err);
           });
         }
-      } );
+      });
     });
   }
 }
--- clients/typescript/src/drivers/capacitor-sqlite/mock.ts
+++ clients/typescript/src/drivers/capacitor-sqlite/mock.ts
@@ -1,40 +1,58 @@
 import { capSQLiteSet, capSQLiteChanges, DBSQLiteValues } from '@capacitor-community/sqlite'
-import { DbName, Row } from '../../util/types'
+import { DbName } from '../../util/types'
 import { Database } from './database'
 
 export class MockDatabase implements Database {
-  dbname: DbName
+  dbname: DbName;
+  fail: Error | undefined;
 
-  constructor(dbName: DbName) {
-    this.dbname = dbName
+  constructor(dbName: DbName, fail?: Error) {
+    this.dbname = dbName;
+    this.fail = fail;
   }
 
   executeSet(set: capSQLiteSet[], transaction?: boolean | undefined, returnMode?: string | undefined, isSQL92?: boolean | undefined): Promise<capSQLiteChanges> {
+    if (typeof this.fail !== 'undefined') throw this.fail;
+
     return new Promise<capSQLiteChanges>( (resolve, reject) => {
-      resolve({ changes: {changes: set.length}});
+      resolve({ changes: {changes: 0}});
     }); 
   }
   query(statement: string, values?: any[] | undefined, isSQL92?: boolean | undefined): Promise<DBSQLiteValues> {
+    if (typeof this.fail !== 'undefined') throw this.fail;
+
     return new Promise<DBSQLiteValues>( (resolve, reject) => {
-      resolve({values: [{i: 0}]});
+      resolve({values: 
+        [
+          {textColumn: 'text1', numberColumn: 1},
+          {textColumn: 'text2', numberColumn: 2}
+        ]});
     }); 
   }
   run(statement: string, values?: any[] | undefined, transaction?: boolean | undefined, returnMode?: string | undefined, isSQL92?: boolean | undefined): Promise<capSQLiteChanges> {
+    if (typeof this.fail !== 'undefined') throw this.fail;
+
     return new Promise<capSQLiteChanges>( (resolve, reject) => {
-      resolve({ changes: {changes: 1}});
+      resolve({ changes: {changes: 0}});
     }); 
   }
   beginTransaction(): Promise<capSQLiteChanges> {
+    if (typeof this.fail !== 'undefined') throw this.fail;
+
     return new Promise<capSQLiteChanges>( (resolve, reject) => {
       resolve({ changes: {changes: 0}});
     }); 
   }
   commitTransaction(): Promise<capSQLiteChanges> {
+    if (typeof this.fail !== 'undefined') throw this.fail;
+
     return new Promise<capSQLiteChanges>( (resolve, reject) => {
       resolve({ changes: {changes: 0}});
     }); 
   }
   rollbackTransaction(): Promise<capSQLiteChanges> {
+    if (typeof this.fail !== 'undefined') throw this.fail;
+    
     return new Promise<capSQLiteChanges>( (resolve, reject) => {
       resolve({ changes: {changes: 0}});
     }); 
