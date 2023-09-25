4ffd5fc4e4177575f6b58c5d52e6a18d17d072c9
Oleksii Sholik <oleksii@sholik.dev>
2023-09-25 19:05:03 +0300

    Define electric.install_function__perform_reordered_op() at Electric startup

--- components/electric/lib/electric/postgres/extension/migrations/20230512000000_conflict_resolution_triggers.ex
+++ components/electric/lib/electric/postgres/extension/migrations/20230512000000_conflict_resolution_triggers.ex
@@ -20,6 +20,9 @@ defmodule Electric.Postgres.Extension.Migrations.Migration_20230512000000_confli
     [
       @contents["electric_tag_type_and_operators"],
       @contents["utility_functions"],
+      # This function definition is included here because it is referenced in the definition of
+      # "trigger_function_installers" below it.
+      Extension.Functions.by_name(:perform_reordered_op_installer_function),
       @contents["trigger_function_installers"],
       @contents["shadow_table_creation_and_update"]
       # We need to actually run shadow table creation/updates, but that's handled in the next migration.
