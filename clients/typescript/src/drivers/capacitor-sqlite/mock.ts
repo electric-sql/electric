3d98c1f6c6225039372bd3361c4254347b4e4c09
Gregorio Zanon <2883729+gregzo@users.noreply.github.com>
2023-10-05 12:31:05 +0200

    feat(client): capacitor-sqlite driver (#484)
    Compatibility with [capacitor-community/sqlite](https://github.com/capacitor-community/sqlite) via a new adapter and accompanying plumbing and tests.

---
+++ clients/typescript/src/drivers/capacitor-sqlite/mock.ts
@@ -0,0 +1,38 @@
+import { capSQLiteChanges, DBSQLiteValues } from '@capacitor-community/sqlite'
+import { DbName } from '../../util/types'
+import { Database } from './database'
+
+export class MockDatabase implements Database {
+  constructor(public dbname: DbName, public fail?: Error) {}
+
+  executeSet(): Promise<capSQLiteChanges> {
+    return this.resolveIfNotFail({ changes: { changes: 0 } })
+  }
+
+  query(): Promise<DBSQLiteValues> {
+    return this.resolveIfNotFail({
+      values: [
+        { textColumn: 'text1', numberColumn: 1 },
+        { textColumn: 'text2', numberColumn: 2 },
+      ],
+    })
+  }
+
+  run(): Promise<capSQLiteChanges> {
+    return this.resolveIfNotFail({ changes: { changes: 0 } })
+  }
+  beginTransaction(): Promise<capSQLiteChanges> {
+    return this.resolveIfNotFail({ changes: { changes: 0 } })
+  }
+  commitTransaction(): Promise<capSQLiteChanges> {
+    return this.resolveIfNotFail({ changes: { changes: 0 } })
+  }
+  rollbackTransaction(): Promise<capSQLiteChanges> {
+    return this.resolveIfNotFail({ changes: { changes: 0 } })
+  }
+
+  private resolveIfNotFail<T>(value: T): Promise<T> {
+    if (typeof this.fail !== 'undefined') return Promise.reject(this.fail)
+    else return Promise.resolve(value)
+  }
+}
