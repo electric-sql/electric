34ecf6820f03661aaf63e9be1f373c7badd264aa
Daven <dubbinpaul@gmail.com>
2023-10-07 23:25:29 -0600

    Use docker psql

--- examples/expo/db/connect.js
+++ examples/expo/db/connect.js
@@ -1,3 +1,3 @@
 const { DATABASE_URL } = require('./util.js')
 const { spawn } = require('child_process')
-spawn(`psql ${DATABASE_URL}`, [], { cwd: __dirname, stdio: 'inherit', shell: true })
+spawn(`docker compose --file ../backend/compose/docker-compose.yaml exec  -it postgres psql -h localhost -U postgres`, [], { cwd: __dirname, stdio: 'inherit', shell: true })
