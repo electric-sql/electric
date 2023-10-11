cd8c95df56951405defa5ed8b3cabbc5d97f4605
Garry Hill <garry@electric-sql.com>
2023-10-11 14:32:14 +0100

    normalise migration name

--- components/electric/lib/electric/postgres/extension.ex
+++ components/electric/lib/electric/postgres/extension.ex
@@ -312,7 +312,7 @@ defmodule Electric.Postgres.Extension do
       Migrations.Migration_20230921161045_DropEventTriggers,
       Migrations.Migration_20230921161418_ProxyCompatibility,
       Migrations.Migration_20231009121515_AllowLargeMigrations,
-      Migrations.Migration20231010123118_AddPriorityToVersion
+      Migrations.Migration_20231010123118_AddPriorityToVersion
     ]
   end
 
