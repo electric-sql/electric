77586cd4c0dd30c0345696cd11c68cbcdc437172
Oleksii Sholik <oleksii@sholik.dev>
2023-10-17 13:28:47 +0300

    Fix db:psql to connect to the proxy

--- examples/starter/template/db/util.js
+++ examples/starter/template/db/util.js
@@ -9,7 +9,7 @@ shell.config.silent = true // don't log output of child processes
 // that intend to use the DDLX syntax extension of SQL.
 const appName = fetchAppName() ?? 'electric'
 const proxyPort = fetchHostProxyPortElectric() ?? 65432
-const DEFAULT_URL = `postgresql://postgres:password@${appName}.localhost:${proxyPort}/electric`
+const DEFAULT_URL = `postgresql://electric:password@localhost:${proxyPort}/${appName}`
 const DATABASE_URL = process.env.DATABASE_URL || DEFAULT_URL
 const PUBLIC_DATABASE_URL = DATABASE_URL.split('@')[1]
 
