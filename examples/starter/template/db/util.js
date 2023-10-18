eea24dff9a5e77c7eca4fe0adc1bfb151f851831
Oleksii Sholik <oleksii@sholik.dev>
2023-10-18 16:52:04 +0300

    Remove the ability to override DATABASE_URL in scripts

--- examples/starter/template/db/util.js
+++ examples/starter/template/db/util.js
@@ -9,8 +9,7 @@ shell.config.silent = true // don't log output of child processes
 // that intend to use the DDLX syntax extension of SQL.
 const appName = fetchAppName() ?? 'electric'
 const proxyPort = fetchHostProxyPortElectric() ?? 65432
-const DEFAULT_URL = `postgresql://electric:proxy_password@localhost:${proxyPort}/${appName}`
-const DATABASE_URL = process.env.DATABASE_URL || DEFAULT_URL
+const DATABASE_URL = `postgresql://electric:proxy_password@localhost:${proxyPort}/${appName}`
 const PUBLIC_DATABASE_URL = DATABASE_URL.split('@')[1]
 
 function error(err) {
