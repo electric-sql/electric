2b64324ab7925503c16b3c8a9066a9a3c2d56b03
Oleksii Sholik <oleksii@sholik.dev>
2023-10-24 14:38:17 +0300

    Fix tests (keep connecting as superuser for now)

--- components/electric/dev/init.sql
+++ components/electric/dev/init.sql
@@ -1,1 +1,7 @@
-CREATE ROLE min_privilege REPLICATION LOGIN PASSWORD 'password';
+-- This file is also evaluated when creating new databases within the same Postgres dev cluster in unit tests.
+-- Hence the need to guard against the role already existing.
+DO $$ BEGIN
+  IF NOT EXISTS(SELECT 1 FROM pg_user WHERE usename = 'min_privilege') THEN
+    CREATE ROLE min_privilege REPLICATION LOGIN PASSWORD 'password';
+  END IF;
+END $$;
