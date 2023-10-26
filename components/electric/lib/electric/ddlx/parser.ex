986558d49e2ea9d9de8c3ccaf8ca375af7870335
Garry Hill <garry@electric-sql.com>
2023-10-26 15:23:54 +0100

    simplify parser chain

--- components/electric/lib/electric/ddlx/parser.ex
+++ components/electric/lib/electric/ddlx/parser.ex
@@ -1,43 +1,33 @@
 defmodule Electric.DDLX.Parser do
-  alias Electric.DDLX.Parser.{Statement, Tokenizer}
+  alias Electric.DDLX.Parser.Tokenizer
   alias Electric.DDLX.Command
 
   @type opt() :: {:default_schema, String.t()}
   @type opts() :: [opt()]
 
-  def statement(stmt) do
-    tokens = tokens(stmt)
-
-    %Statement{stmt: stmt, tokens: tokens, cmd: nil}
-  end
-
+  @spec parse(String.t(), Keyword.t()) :: {:ok, Command.t()} | {:error, Command.Error.t()}
   def parse(ddlx, opts \\ []) do
     ddlx
-    |> statement()
+    |> Tokenizer.tokens()
     |> do_parse()
-    |> build_cmd(opts)
+    |> build_cmd(ddlx, opts)
   end
 
-  defp do_parse(stmt) do
-    {:electric_ddlx_parser.parse(stmt.tokens), stmt}
+  defp do_parse(tokens) do
+    :electric_ddlx_parser.parse(tokens)
   end
 
-  defp build_cmd({{:ok, {module, attrs}}, _stmt}, opts) do
+  defp build_cmd({:ok, {module, attrs}}, _ddlx, opts) do
     module.build(attrs, opts)
   end
 
-  defp build_cmd({{:error, {{line, position, _}, :electric_ddlx_parser, messages}}, stmt}, _opts) do
+  defp build_cmd({:error, {{line, position, _}, :electric_ddlx_parser, messages}}, ddlx, _opts) do
     {:error,
      %Command.Error{
        line: line,
        position: position,
        message: IO.iodata_to_binary(:electric_ddlx_parser.format_error(messages)),
-       sql: stmt.stmt
+       sql: ddlx
      }}
-  end
-
-  @spec tokens(String.t()) :: [Tokenizer.t()]
-  def tokens(str) do
-    Tokenizer.tokens(str)
   end
 end
--- components/electric/lib/electric/ddlx/parser/statement.ex
+++
@@ -1,7 +0,0 @@
-defmodule Electric.DDLX.Parser.Statement do
-  defstruct [:stmt, :tokens, :cmd]
-
-  def command(%__MODULE__{} = stmt) do
-    stmt.cmd
-  end
-end
