347bf84cffe6a743bc06aa5fd44a688639973bf8
Oleksii Sholik <oleksii@sholik.dev>
2023-11-01 17:19:29 +0200

    chore(examples): Update examples/beer-stars to work with Electric Proxy (#612)

--- examples/beer-stars/server/lib/beer_stars/release.ex
+++ examples/beer-stars/server/lib/beer_stars/release.ex
@@ -7,6 +7,7 @@ defmodule BeerStars.Release do
   alias Ecto.Migrator
 
   alias BeerStars.Model
+  alias BeerStars.Repo
   alias BeerStars.ProxyRepo
 
   @app :beer_stars
@@ -21,7 +22,7 @@ defmodule BeerStars.Release do
     load_app()
 
     {:ok, _, _} =
-      Ecto.Migrator.with_repo(ProxyRepo, fn _repo ->
+      Ecto.Migrator.with_repo(Repo, fn _repo ->
         Model.allocate_beers()
       end)
   end
@@ -30,7 +31,7 @@ defmodule BeerStars.Release do
     load_app()
 
     seeds_file = Path.join(["#{:code.priv_dir(@app)}", "repo", "seeds.exs"])
-    {:ok, _, _} = Migrator.with_repo(ProxyRepo, fn _ -> Code.eval_file(seeds_file) end)
+    {:ok, _, _} = Migrator.with_repo(Repo, fn _ -> Code.eval_file(seeds_file) end)
   end
 
   def rollback(version) do
