2195bf45121e4127e6e3324dfa029f301fdbb886
TachyonicBytes <support@tachyonicbytes.com>
2023-11-02 00:29:14 +0200

    Added all config files

---
+++ examples/tauri-postgres/vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    envPrefix: 'ELECTRIC_',
+    plugins: [
+        react(),
+        svgr({
+            svgrOptions: {
+                svgo: true,
+                plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+                svgoConfig: {
+                    plugins: [
+                        'preset-default',
+                        'removeTitle',
+                        'removeDesc',
+                        'removeDoctype',
+                        'cleanupIds',
+                    ],
+                },
+            },
+        }),
+    ],
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'public/index.html'),
+            nested: resolve(__dirname, 'public/index.html'),
+          },
+        },
+    },
+});
---
+++ examples/tauri-postgres/vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  envPrefix: 'ELECTRIC_',
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        svgo: true,
+        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+        svgoConfig: {
+          plugins: [
+            'preset-default',
+            'removeTitle',
+            'removeDesc',
+            'removeDoctype',
+            'cleanupIds',
+          ],
+        },
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
+})
