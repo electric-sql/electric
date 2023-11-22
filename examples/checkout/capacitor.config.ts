9e2278797e3b96ea418a9646c69c9eb3005860de
Sam Willis <sam.willis@gmail.com>
2023-11-22 11:01:26 +0000

    Intial ionic setup

---
+++ examples/checkout/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'checkout',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
