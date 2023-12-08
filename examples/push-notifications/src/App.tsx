33503a12f0e0bcedefeb1ca272c3b00f7678e8eb
Stefanos Mousafeiris <msfstef@gmail.com>
2023-12-08 11:55:53 +0200

    Adds custom module recognizing SVG

--- examples/push-notifications/src/App.tsx
+++ examples/push-notifications/src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo.toString()} className="App-logo" alt="logo" />
         <Example />
       </header>
     </div>
