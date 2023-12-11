b52ef6a8cb8fa59f38d785ca231739344dcbee1a
Stefanos Mousafeiris <msfstef@gmail.com>
2023-12-11 11:41:18 +0200

    Slightly refactor Toast component for clarity

--- examples/push-notifications/src/toast/Toast.tsx
+++ examples/push-notifications/src/toast/Toast.tsx
@@ -4,11 +4,11 @@ import { ToastData } from "./types.toast";
 import './Toast.css';
 
 interface ToastComponentProps extends ToastData {
-  onDismiss: () => void
+  onDismiss: () => void,
+  animationDurationMs: number,
 }
 
-const TOAST_HIDE_ANIMATION_DURATION_MS = 150;
-
+const TOAST_ANIMATION_DURATION_MS = 150;
 
 export const Toast = ({
   title,
@@ -17,6 +17,7 @@ export const Toast = ({
   dismissable,
   durationInMs,
   onDismiss,
+  animationDurationMs = TOAST_ANIMATION_DURATION_MS,
 }: ToastComponentProps) => {
   const dismissFn = useRef(onDismiss);
   const [show, setShow] = useState(false);
@@ -28,32 +29,36 @@ export const Toast = ({
     onAnimatedDismiss();
   }, [action?.actionFn, onAnimatedDismiss]);
 
-  // keep reference to most upd to date callback
-  useEffect(() => {
-    dismissFn.current = onDismiss;
-  }, [onDismiss]);
+  // keep reference to most up to date callback to avoid stale
+  // callback calls
+  useEffect(() => (dismissFn.current = onDismiss), [onDismiss]);
 
   useEffect(() => {
+    // show in the next animation frame to allow for an entry animation
     if (!hide && !show) {
       const frame = requestAnimationFrame(() => setShow(true));
       return () => cancelAnimationFrame(frame);
     }
 
+    // keep the timer running for the toast to disappear after set duration
     if (!hide) {
       const maxDuration = Math.max(
-        durationInMs - TOAST_HIDE_ANIMATION_DURATION_MS,
-        TOAST_HIDE_ANIMATION_DURATION_MS
+        durationInMs - animationDurationMs,
+        animationDurationMs
       )
       const timer = setTimeout(onAnimatedDismiss, maxDuration);
       return () => clearTimeout(timer);
     }
     
-    const timer = setTimeout(() => dismissFn.current(), TOAST_HIDE_ANIMATION_DURATION_MS);
+    // once toast is set to be hidden, allow for the animation to run
+    const timer = setTimeout(() => dismissFn.current(), animationDurationMs);
     return () => clearTimeout(timer)
   }, [hide, show])
 
   return (
-    <div className={"toastContainer" + (show ? " show" : "") + (hide ? " hide" : "")}>
+    <div
+      className={"toastContainer" + (show ? " show" : "") + (hide ? " hide" : "")}
+      style={{ transitionDuration: `${animationDurationMs}ms` }}>
       <div className="contentContainer">
           {title !== undefined ? <h2>{title}</h2> : null }
           <p>{message}</p>
