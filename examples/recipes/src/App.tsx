37b13a647c733589256e7d191ff67aecb248cbd7
Stefanos Mousafeiris <msfstef@gmail.com>
2023-12-12 18:35:09 +0200

    Create simple recipe directory at root

--- examples/recipes/src/App.tsx
+++ examples/recipes/src/App.tsx
@@ -1,14 +1,19 @@
+import { Box, Link } from '@mui/material'
 import logo from './assets/electric_logo.svg'
+import { Link as RouterLink } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://electric-sql.com" target="_blank" rel="noreferrer">
-          <img src={logo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <Link href="https://electric-sql.com" target="_blank" rel="noreferrer">
+          <img src={logo} />
+        </Link>
+        <h1>Electric SQL Recipes</h1>
+        <Link component={RouterLink} to="/activity-events" underline="hover">
+          Actvity Events
+        </Link>
+      </Box>
     </>
   )
 }
