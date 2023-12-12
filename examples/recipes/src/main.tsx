2a6d93ccbe6ef5bcaea52d4f0ccb2dbd7826ded9
Stefanos Mousafeiris <msfstef@gmail.com>
2023-12-12 12:01:26 +0200

    Add boilerplate react-router logic

--- examples/recipes/src/main.tsx
+++ examples/recipes/src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
