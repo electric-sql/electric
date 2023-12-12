f9b62d3ceeb55b33934e52d4274d0d6da611d2e3
Stefanos Mousafeiris <msfstef@gmail.com>
2023-12-12 15:29:56 +0200

    Create activity event schema

---
+++ examples/recipes/db/migrations/02-activity_events_table.sql
@@ -0,0 +1,25 @@
+/* This is an example of an SQL DDL migration. It creates an `items` table and
+ * then calls an `electric.electrify` procedure to expose the table to the
+ * ElectricSQL replication machinery.
+ *
+ * Note that these statements are applied directly to the *Postgres* database.
+ * Electric then handles keeping the local SQLite database schema in sync with
+ * the electrified subset of your Postgres database schema.
+ *
+ * See https://electric-sql.com/docs/usage/data-modelling for more information.
+ */
+
+-- Create a simple activity events table.
+CREATE TABLE IF NOT EXISTS activity_events (
+  id UUID PRIMARY KEY NOT NULL,
+  source TEXT NOT NULL,
+  activity_type TEXT NOT NULL,
+  timestamp TIMESTAMPTZ NOT NULL,
+  message TEXT NOT NULL,
+  action TEXT,
+  read_at TIMESTAMPTZ
+);
+
+-- ⚡
+-- Electrify the items table
+ALTER TABLE activity_events ENABLE ELECTRIC;
