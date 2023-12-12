0ea03b6aee84c9e92f93e7ffed74a3da591d5ad9
Stefanos Mousafeiris <msfstef@gmail.com>
2023-12-12 12:16:54 +0200

    Move ElectricProvider to root level

--- examples/recipes/src/App.tsx
+++ examples/recipes/src/App.tsx
@@ -1,16 +1,15 @@
 import logo from './assets/electric_logo.svg'
-import { ElectricWrapper } from './electric/ElectricWrapper'
 
 function App() {
   return (
-    <ElectricWrapper>
+    <>
       <div>
         <a href="https://electric-sql.com" target="_blank" rel="noreferrer">
           <img src={logo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-    </ElectricWrapper>
+    </>
   )
 }
 
--- examples/recipes/src/main.tsx
+++ examples/recipes/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ElectricWrapper } from './electric/ElectricWrapper.tsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css'
 
@@ -13,6 +14,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ElectricWrapper>
+      <RouterProvider router={router} />
+    </ElectricWrapper>
   </React.StrictMode>,
 )
