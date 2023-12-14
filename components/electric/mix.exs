4222f45c8cbfbd6b536c2b7e21eb940010fcd6f9
Oleksii Sholik <oleksii@sholik.dev>
2023-12-14 16:03:28 +0200

    Upgrade Bandit to a stable version

--- components/electric/mix.exs
+++ components/electric/mix.exs
@@ -61,7 +61,7 @@ defmodule Electric.MixProject do
       {:tzdata, "~> 1.1", only: [:dev, :test]},
       {:pg_query_ex, github: "electric-sql/pg_query_ex"},
       {:nimble_pool, "~> 1.0"},
-      {:bandit, "~> 1.0-pre"},
+      {:bandit, "~> 1.0.0"},
       {:thousand_island, "~> 1.2"},
       {:mint_web_socket, "~> 1.0"},
       {:mint, "~> 1.5"},
