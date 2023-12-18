a418dcd8b8c18018c79999c499e4525b8fb85d7d
Stefanos Mousafeiris <msfstef@gmail.com>
2023-12-18 18:59:08 +0200

    Add trigger to requests and responses to handle

--- examples/recipes/db/migrations/03-request_response_tables.sql
+++ examples/recipes/db/migrations/03-request_response_tables.sql
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS requests (
 
 );
 
+-- Create a responses table referencing requests.
 CREATE TABLE IF NOT EXISTS responses (
   id UUID PRIMARY KEY NOT NULL,
   request_id UUID NOT NULL REFERENCES requests(id) ON DELETE CASCADE,
@@ -31,3 +32,38 @@ CREATE TABLE IF NOT EXISTS responses (
 -- Electrify the requests and responses table
 ALTER TABLE requests ENABLE ELECTRIC;
 ALTER TABLE responses ENABLE ELECTRIC;
+
+-- When request is received, set it as processing notify API to process it
+CREATE OR REPLACE FUNCTION process_request()
+RETURNS TRIGGER AS $$
+BEGIN
+  UPDATE "public"."requests" SET "processing" = 'true' WHERE "id" = NEW.id;
+  PERFORM pg_notify('api_trigger', row_to_json(NEW)::TEXT);
+  RETURN NULL;
+END
+$$ LANGUAGE plpgsql;
+
+-- When response is received, mark relevant request as processed
+CREATE OR REPLACE FUNCTION finish_processing_request()
+RETURNS TRIGGER AS $$
+BEGIN
+  UPDATE "public"."requests" SET "processing" = 'false' WHERE "id" = NEW.request_id;
+  RETURN NULL;
+END
+$$ LANGUAGE plpgsql;
+
+-- Create a trigger to execute the function on INSERT into "requests" table
+CREATE TRIGGER "process_request_trigger"
+AFTER INSERT ON "public"."requests"
+FOR EACH ROW
+EXECUTE FUNCTION process_request();
+
+-- Create a trigger to execute the function on INSERT into "responses" table
+CREATE TRIGGER "finish_processing_request_trigger"
+AFTER INSERT ON "public"."responses"
+FOR EACH ROW
+EXECUTE FUNCTION finish_processing_request();
+
+-- Enable the triggers on the tables
+ALTER TABLE "public"."requests" ENABLE ALWAYS TRIGGER process_request_trigger;
+ALTER TABLE "public"."responses" ENABLE ALWAYS TRIGGER finish_processing_request_trigger;
