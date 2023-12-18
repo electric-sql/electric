511dc1f9217607d8dd2c1a397725c2fb8560cc9c
Stefanos Mousafeiris <msfstef@gmail.com>
2023-12-18 18:58:46 +0200

    Add express app to handle requests

--- examples/recipes/backend/demo-server/app.js
+++ examples/recipes/backend/demo-server/app.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
+const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
 const cron = require('cron');
 const {
   runOnInterval,
   generateWebServerLog
 } = require('./utilities');
-
 
 const pool = new Pool({
   user: process.env.PG_USER,
@@ -16,24 +18,33 @@ const pool = new Pool({
 });
 
 
+
 function startGeneratingWebServerLogs() {
+  let numLogsInserted = 0;
   const insertWebServerLog= async () => {
     try {
       const query = 'INSERT INTO logs(id, timestamp, content) VALUES($1, $2, $3) RETURNING *';
       const values = [uuidv4(), new Date().toISOString(), generateWebServerLog()];
-      const result = await pool.query(query, values);
-      console.log('Inserted log:', Object.values(result.rows[0]).join(' - '));
+      await pool.query(query, values);
+      numLogsInserted++;
+      if (numLogsInserted % 100 == 0) {
+        console.log(`Inserted ${numLogsInserted} logs.`);
+        numLogsInserted = 0;
+      }
+      
     } catch (err) {
-      console.error('Error executing log insertion query:', err);
+      if (!err.message.includes('"logs" does not exist')) {
+        console.error('Error executing log insertion query:', err.message);
+      }
     }
   }
 
   const cleanUpOldWebServerLogs= async () => {
     try {
-      const oneDayAgo = new Date();
-      oneDayAgo.setDate(oneDayAgo.getDate() - 1);
+      const oneHourAgo = new Date();
+      oneHourAgo.setHours(oneHourAgo.getHours() - 1);
       const query = 'DELETE FROM logs WHERE timestamp < $1';
-      const values = [oneDayAgo.toISOString()];
+      const values = [oneHourAgo.toISOString()];
       const result = await pool.query(query, values);
       console.log(`Cleaned up ${result.rowCount} old log entries.`);
     } catch (error) {
@@ -49,8 +60,71 @@ function startGeneratingWebServerLogs() {
 }
 
 
+const app = express();
+const PORT = process.env.DEMO_APP_PORT || 3123;
+
+// Middleware to parse JSON data in the request body
+app.use(bodyParser.json());
+
+// Endpoint to handle requests
+app.post('/sum', async (req, res) => {
+  try {
+    const sum = req.body.summands.reduce((acc, value) => acc + value, 0);
+    await new Promise((res) => setTimeout(res, 3000));
+    res.status(200).json({ sum });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+async function startListeningToPgRequests() {
+  const pgClient = await pool.connect()
+  // Listen for PostgreSQL notifications
+  await pgClient.query('LISTEN api_trigger');
+
+  // Handle notifications
+  pgClient.on('notification', async (notification) => {
+    const payload = JSON.parse(notification.payload);
+    console.log(`Received request: ${payload.path} - ${payload.method} - ${payload.data}`);
+
+    let response = { status: 500, data: { message: 'Failed to process' }}
+    try {
+      // Make an API request using the information from the notification payload
+      response = await axios({
+        method: payload.method,
+        url: `http://localhost:${PORT}${payload.path}`,
+        data: JSON.parse(payload.data),
+      });
+    } catch (err) {
+      response = {
+        status: err?.response?.status ?? 500,
+        data: { message: err.message }
+      }
+    }
+
+    console.log(`Received response: ${response.status} - ${JSON.stringify(response.data)}`)
+
+    // Insert the API response into the 'responses' table
+    await pool.query({
+      text: 'INSERT INTO responses (id, request_id, status_code, data) VALUES ($1, $2, $3, $4)',
+      values: [uuidv4(), payload.id, response.status, JSON.stringify(response.data)],
+    });
+  });
+}
+
+
+
+
+
+
 function main() {
+  // Start the Express server
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
   startGeneratingWebServerLogs();
+  startListeningToPgRequests()
 }
 
 main();
