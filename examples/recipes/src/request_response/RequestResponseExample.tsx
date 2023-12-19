87a3d08f2399ed8bb38fa19a8694c0c4547547d8
Stefanos Mousafeiris <msfstef@gmail.com>
2023-12-19 15:57:34 +0200

    Add connectivity toggle for simulating offline

--- examples/recipes/src/request_response/RequestResponseExample.tsx
+++ examples/recipes/src/request_response/RequestResponseExample.tsx
@@ -1,11 +1,16 @@
-import { Box, Container } from "@mui/material"
+import { Box, Container, Switch } from "@mui/material"
 import { NavigationBar } from "../components/NavigationBar"
 import { useEffect } from "react"
 import { useElectric } from "../electric/ElectricWrapper"
 import { Calculator } from "./Calculator"
+import { useConnectivityState } from "electric-sql/react"
+import { Wifi, WifiOff } from "@mui/icons-material"
 
 export const RequestResponseExample = () => {
   const { db } = useElectric()!
+  const { connectivityState, toggleConnectivityState } = useConnectivityState();
+  const connected = connectivityState == 'connected';
+
   useEffect(() => {
     const syncItems = async () => {
       // Resolves when the shape subscription has been established.
@@ -22,10 +27,15 @@ export const RequestResponseExample = () => {
     syncItems()
   }, [db])
 
-
   return (
     <Box>
-      <NavigationBar title="Request/Response Pattern" />
+      <NavigationBar title="Request/Response Pattern" items={[
+        <Box key="connectivity" sx={{ display: 'flex', alignItems: 'center' }}>
+          <Switch color="secondary" size="small" sx={{ mr: 1 }}
+            checked={connected} onChange={toggleConnectivityState} />
+          { connected ? <Wifi />  : <WifiOff /> }
+        </Box>
+      ]}/>
       <Container maxWidth="md" sx={{ py: 4 }}>
         <Calculator />
       </Container>
