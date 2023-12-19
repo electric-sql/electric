b35d284373a3c4474c16eaf1c8b36fcc7d322190
Stefanos Mousafeiris <msfstef@gmail.com>
2023-12-19 11:23:59 +0200

    Simplify request response schema

--- examples/recipes/db/migrations/03-request_response_tables.sql
+++ examples/recipes/db/migrations/03-request_response_tables.sql
@@ -33,13 +33,20 @@ CREATE TABLE IF NOT EXISTS responses (
 ALTER TABLE requests ENABLE ELECTRIC;
 ALTER TABLE responses ENABLE ELECTRIC;
 
+
+/* Set up the triggers that will notify the appropriate APIs and perform
+ * the work required to process incoming requests on the requests table
+ * and generate responses to be added to the responses table, which will
+ * in turn sync with the client and simulate an request/response pattern
+ */
+
 -- When request is received, set it as processing notify API to process it
 CREATE OR REPLACE FUNCTION process_request()
 RETURNS TRIGGER AS $$
 BEGIN
-  UPDATE "public"."requests" SET "processing" = 'true' WHERE "id" = NEW.id;
+  NEW.processing := true;
   PERFORM pg_notify('api_trigger', row_to_json(NEW)::TEXT);
-  RETURN NULL;
+  RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
 
@@ -47,23 +54,23 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION finish_processing_request()
 RETURNS TRIGGER AS $$
 BEGIN
-  UPDATE "public"."requests" SET "processing" = 'false' WHERE "id" = NEW.request_id;
-  RETURN NULL;
+  UPDATE requests SET "processing" = 'false' WHERE "id" = NEW.request_id;
+  RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
 
 -- Create a trigger to execute the function on INSERT into "requests" table
 CREATE TRIGGER "process_request_trigger"
-AFTER INSERT ON "public"."requests"
+BEFORE INSERT ON requests
 FOR EACH ROW
 EXECUTE FUNCTION process_request();
 
 -- Create a trigger to execute the function on INSERT into "responses" table
 CREATE TRIGGER "finish_processing_request_trigger"
-AFTER INSERT ON "public"."responses"
+AFTER INSERT ON responses
 FOR EACH ROW
 EXECUTE FUNCTION finish_processing_request();
 
 -- Enable the triggers on the tables
-ALTER TABLE "public"."requests" ENABLE ALWAYS TRIGGER process_request_trigger;
-ALTER TABLE "public"."responses" ENABLE ALWAYS TRIGGER finish_processing_request_trigger;
+ALTER TABLE requests ENABLE ALWAYS TRIGGER process_request_trigger;
+ALTER TABLE responses ENABLE ALWAYS TRIGGER finish_processing_request_trigger;
