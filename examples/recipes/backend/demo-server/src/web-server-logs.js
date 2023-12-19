6eb9f113b9aef8c80203cb330bfacaa3d56766e3
Stefanos Mousafeiris <msfstef@gmail.com>
2023-12-19 12:08:34 +0200

    Extract timing parameters for web server log generation

--- examples/recipes/backend/demo-server/src/web-server-logs.js
+++ examples/recipes/backend/demo-server/src/web-server-logs.js
@@ -13,41 +13,70 @@ const {
  * regularly cleans up old logs
  * @param {Pool} pgPool 
  */
-function startGeneratingWebServerLogs(pgPool) {
+async function startGeneratingWebServerLogs(pgPool) {
+  const WAIT_INTERVAL_FOR_TABLE_MS = 3000;
+  const LOG_GENERATION_FREQUENCY_MS = 250;
+  const LOG_GENERATION_FREQUENCY_VARIATION_MS = 200;
+  const LOG_GENERATION_LOGGING_INTERVAL_MS = 60000;
+
   let numLogsInserted = 0;
-  const insertWebServerLog= async () => {
+  let lastLoggedTime = Date.now();
+
+  const checkLogsTableExists = async () => {
+    try {
+      const result = await pgPool.query(
+        `SELECT EXISTS (
+          SELECT 1 FROM information_schema.tables
+          WHERE table_name = 'logs'
+        );`
+      );
+      return result?.rows[0]?.exists ?? false;
+    } catch (err) {
+      return false;
+    }
+  };
+
+  const insertWebServerLog = async () => {
     try {
       const query = 'INSERT INTO logs(id, timestamp, content) VALUES($1, $2, $3) RETURNING *';
       const values = [uuidv4(), new Date().toISOString(), generateWebServerLog()];
       await pgPool.query(query, values);
       numLogsInserted++;
-      if (numLogsInserted % 100 == 0) {
-        console.log(`Inserted ${numLogsInserted} logs.`);
+      if (Date.now() - lastLoggedTime > LOG_GENERATION_LOGGING_INTERVAL_MS) {
+        console.log(`${(new Date()).toISOString()} - Inserted ${numLogsInserted} new logs`);
+        lastLoggedTime = Date.now();
         numLogsInserted = 0;
       }
-      
     } catch (err) {
-      if (!err.message.includes('"logs" does not exist')) {
-        console.error('Error executing log insertion query:', err.message);
-      }
+      console.error('Error executing log insertion query:', err.message);
     }
   }
 
-  const cleanUpOldWebServerLogs= async () => {
+  const cleanUpOldWebServerLogs = async () => {
     try {
       const oneHourAgo = new Date();
       oneHourAgo.setHours(oneHourAgo.getHours() - 1);
       const query = 'DELETE FROM logs WHERE timestamp < $1';
       const values = [oneHourAgo.toISOString()];
       const result = await pgPool.query(query, values);
-      console.log(`Cleaned up ${result.rowCount} old log entries.`);
+      console.log(`${(new Date()).toISOString()} - Cleaned up ${result.rowCount} old log entries`);
     } catch (error) {
       console.error('Error executing cleanup query', error);
     }
   }
 
-  // generate logs every 250 +- 200 ms
-  runOnInterval(() => insertWebServerLog(), 250, 200);
+  // wait for table to be created before attempting to generate logs
+  while (!(await checkLogsTableExists())) {
+    console.warn('Waiting for "logs" table to be created...')
+    await new Promise(res => setTimeout(res, WAIT_INTERVAL_FOR_TABLE_MS));
+  }
+
+  // generate logs with a given frequency +- a variation
+  runOnInterval(
+    () => insertWebServerLog(),
+    LOG_GENERATION_FREQUENCY_MS,
+    LOG_GENERATION_FREQUENCY_VARIATION_MS
+  );
 
   // clean up day-old logs every hour
   (new cron.CronJob('0 * * * *', cleanUpOldWebServerLogs)).start();
