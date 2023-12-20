0d78f60ad31d1dab8a27bbeebbf3789820c5f259
Oleksii Sholik <oleksii@sholik.dev>
2023-12-20 13:49:07 +0200

    Use Dotenvy for runtime configuration

--- components/electric/config/runtime.exs
+++ components/electric/config/runtime.exs
@@ -1,11 +1,5 @@
-# This file is executed after the code compilation on all environments
-# (dev, test, and prod) - for both Mix and releases.
-#
-# We use it for runtime configuration of releases in production --
-# because that allows us to read environment variables at runtime
-# rather than compile time.
-
 import Config
+import Dotenvy
 
 ### Static configuration
 
@@ -17,48 +11,20 @@ config :electric, Electric.Postgres.CachedWal.Api, adapter: Electric.Postgres.Ca
 
 default_log_level = "info"
 default_auth_mode = "secure"
-default_http_server_port = "5133"
-default_pg_server_port = "5433"
+default_http_server_port = 5133
+default_pg_server_port = 5433
 default_pg_proxy_port = "65432"
-default_listen_on_ipv6 = "true"
-default_database_require_ssl = "true"
-default_database_use_ipv6 = "true"
+default_listen_on_ipv6 = true
+default_database_require_ssl = true
+default_database_use_ipv6 = true
 default_write_to_pg_mode = "logical_replication"
-
-# These defaults are only set for the dev environment, so in prod errors will still get raises if any of the required
-# configuration options aren't set by the user.
-default_database_url =
-  if config_env() == :dev, do: "postgresql://postgres:password@localhost:54321/electric"
-
-default_logical_publisher_host = if config_env() == :dev, do: "host.docker.internal"
-default_pg_proxy_password = if config_env() == :dev, do: "password"
+default_proxy_tracing_enable = false
 
 ###
-
-# We only want to raise in prod because this config is also loaded in the test environment where we do not define
-# default values for all required options.
-raise_in_prod =
-  if config_env() == :prod do
-    fn msg -> raise msg end
-  else
-    fn _ -> nil end
-  end
-
-get_env_bool = fn name, default ->
-  case String.downcase(System.get_env(name, default)) do
-    truthy when truthy in ~w[yes true] -> true
-    falsy when falsy in ~w[no false] -> false
-    other -> raise "Invalid value for `#{name}`: #{other}"
-  end
-end
-
-get_env_int = fn name, default ->
-  System.get_env(name, default) |> String.to_integer()
-end
-
+# Logger
 ###
 
-log_level = System.get_env("LOG_LEVEL", default_log_level) |> String.to_existing_atom()
+log_level = env!("LOG_LEVEL", :string, default_log_level) |> String.to_existing_atom()
 
 config :logger,
   handle_otp_reports: true,
@@ -105,19 +71,19 @@ config :logger, :console,
     :proxy_session_id
   ]
 
-pg_server_port = get_env_int.("LOGICAL_PUBLISHER_PORT", default_pg_server_port)
-listen_on_ipv6? = get_env_bool.("ELECTRIC_USE_IPV6", default_listen_on_ipv6)
+pg_server_port = env!("LOGICAL_PUBLISHER_PORT", :integer, default_pg_server_port)
+listen_on_ipv6? = env!("ELECTRIC_USE_IPV6", :boolean, default_listen_on_ipv6)
 
 write_to_pg_mode =
-  case System.get_env("ELECTRIC_WRITE_TO_PG_MODE", default_write_to_pg_mode) do
+  case env!("ELECTRIC_WRITE_TO_PG_MODE", :string, default_write_to_pg_mode) do
     "logical_replication" -> :logical_replication
     "direct_writes" -> :direct_writes
   end
 
 config :electric,
   # Used in telemetry, and to identify the server to the client
-  instance_id: System.get_env("ELECTRIC_INSTANCE_ID", Electric.Utils.uuid4()),
-  http_port: get_env_int.("HTTP_PORT", default_http_server_port),
+  instance_id: env!("ELECTRIC_INSTANCE_ID", :string, Electric.Utils.uuid4()),
+  http_port: env!("HTTP_PORT", :integer, default_http_server_port),
   pg_server_port: pg_server_port,
   listen_on_ipv6?: listen_on_ipv6?,
   write_to_pg_mode: write_to_pg_mode
@@ -144,7 +110,7 @@ config :electric, Electric.Features,
   proxy_ddlx_assign: false,
   proxy_ddlx_unassign: false
 
-require_ssl? = get_env_bool.("DATABASE_REQUIRE_SSL", default_database_require_ssl)
+require_ssl? = env!("DATABASE_REQUIRE_SSL", :boolean, default_database_require_ssl)
 
 # Always try connecting with SSL first.
 #
@@ -160,31 +126,24 @@ use_ssl? =
     true
   end
 
-use_ipv6? = get_env_bool.("DATABASE_USE_IPV6", default_database_use_ipv6)
+use_ipv6? = env!("DATABASE_USE_IPV6", :boolean, default_database_use_ipv6)
+
+database_url = env!("DATABASE_URL", :string, nil)
 
 postgresql_connection =
-  case System.get_env("DATABASE_URL", default_database_url) do
-    nil ->
-      raise_in_prod.("Required environment variable DATABASE_URL is not set")
-      nil
-
-    database_url ->
-      database_url
-      |> Electric.Utils.parse_postgresql_uri()
-      |> Keyword.put(:ssl, use_ssl?)
-      |> Keyword.put(:ipv6, use_ipv6?)
-      |> Keyword.put(:replication, "database")
-      |> Keyword.update(:timeout, 5_000, &String.to_integer/1)
+  if database_url do
+    database_url
+    |> Electric.Utils.parse_postgresql_uri()
+    |> Keyword.put(:ssl, use_ssl?)
+    |> Keyword.put(:ipv6, use_ipv6?)
+    |> Keyword.put(:replication, "database")
+    |> Keyword.update(:timeout, 5_000, &String.to_integer/1)
   end
 
-pg_server_host =
-  if write_to_pg_mode == :logical_replication do
-    System.get_env("LOGICAL_PUBLISHER_HOST", default_logical_publisher_host) ||
-      raise_in_prod.("Required environment variable LOGICAL_PUBLISHER_HOST is not set")
-  end
+pg_server_host = env!("LOGICAL_PUBLISHER_HOST", :string, nil)
 
 {use_http_tunnel?, proxy_port_str} =
-  case String.downcase(System.get_env("PG_PROXY_PORT", default_pg_proxy_port)) do
+  case String.downcase(env!("PG_PROXY_PORT", :string, default_pg_proxy_port)) do
     "http" -> {true, default_pg_proxy_port}
     "http:" <> port_str -> {true, port_str}
     port_str -> {false, port_str}
@@ -192,9 +151,7 @@ pg_server_host =
 
 proxy_port = String.to_integer(proxy_port_str)
 
-proxy_password =
-  System.get_env("PG_PROXY_PASSWORD", default_pg_proxy_password) ||
-    raise_in_prod.("Required environment variable PG_PROXY_PASSWORD is not set")
+proxy_password = env!("PG_PROXY_PASSWORD", :string, nil)
 
 proxy_listener_opts =
   if listen_on_ipv6? do
@@ -203,53 +160,52 @@ proxy_listener_opts =
     []
   end
 
-config :electric, Electric.Replication.Connectors,
-  postgres_1: [
-    producer: Electric.Replication.Postgres.LogicalReplicationProducer,
-    connection: postgresql_connection,
-    replication: [
-      electric_connection: [
-        host: pg_server_host,
-        port: pg_server_port,
-        dbname: "electric",
-        connect_timeout: postgresql_connection[:timeout]
+if postgresql_connection do
+  config :electric, Electric.Replication.Connectors,
+    postgres_1: [
+      producer: Electric.Replication.Postgres.LogicalReplicationProducer,
+      connection: postgresql_connection,
+      replication: [
+        electric_connection: [
+          host: pg_server_host,
+          port: pg_server_port,
+          dbname: "electric",
+          connect_timeout: postgresql_connection[:timeout]
+        ]
+      ],
+      proxy: [
+        # listen opts are ThousandIsland.options()
+        # https://hexdocs.pm/thousand_island/ThousandIsland.html#t:options/0
+        listen: [port: proxy_port] ++ proxy_listener_opts,
+        use_http_tunnel?: use_http_tunnel?,
+        password: proxy_password,
+        log_level: log_level
       ]
-    ],
-    proxy: [
-      # listen opts are ThousandIsland.options()
-      # https://hexdocs.pm/thousand_island/ThousandIsland.html#t:options/0
-      listen: [port: proxy_port] ++ proxy_listener_opts,
-      use_http_tunnel?: use_http_tunnel?,
-      password: proxy_password,
-      log_level: log_level
     ]
-  ]
+end
 
-enable_proxy_tracing? = System.get_env("PROXY_TRACING_ENABLE", "false") in ["yes", "true"]
+enable_proxy_tracing? = env!("PROXY_TRACING_ENABLE", :boolean, default_proxy_tracing_enable)
 
 config :electric, Electric.Postgres.Proxy.Handler.Tracing,
   enable: enable_proxy_tracing?,
   colour: false
 
-# The :prod environment is inlined here because by default Mix won't copy any config/runtime.*.exs files when assembling
-# a release, and we want a single configuration file in our release.
+# This is intentionally an atom and not a boolean - we expect to add `:extended` state
+telemetry =
+  case env!("ELECTRIC_TELEMETRY", :string, nil) do
+    nil -> :enabled
+    x when x in ~w[0 f false disable disabled n no off] -> :disabled
+    x when x in ~w[1 t true enable enabled y yes on] -> :enabled
+    x -> raise "Invalid value for `ELECTRIC_TELEMETRY`: #{x}"
+  end
+
+config :electric, :telemetry, telemetry
+
 if config_env() == :prod do
   auth_provider =
     System.get_env("AUTH_MODE", default_auth_mode) |> Electric.Satellite.Auth.build_provider!()
 
   config :electric, Electric.Satellite.Auth, provider: auth_provider
-
-  # This is intentionally an atom and not a boolean - we expect to add `:extended` state
-  telemetry =
-    case System.get_env("ELECTRIC_TELEMETRY") do
-      nil -> :enabled
-      x when x in ~w|0 f false disable disabled n no off| -> :disabled
-      x when x in ~w|1 t true enable enabled y yes on| -> :enabled
-      x -> raise "Invalid value for `ELECTRIC_TELEMETRY`: #{x}"
-    end
-
-  config :electric, :telemetry, telemetry
 else
-  config :electric, :telemetry, :disabled
   Code.require_file("runtime.#{config_env()}.exs", __DIR__)
 end
