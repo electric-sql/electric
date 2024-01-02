2f3e15011c867f0809b1062f1b64efbbe15063f0
Stefanos Mousafeiris <msfstef@gmail.com>
2024-01-02 20:20:34 +0200

    Basic view for background job generation and progress

--- examples/recipes/src/background_jobs/BackgroundJobs.tsx
+++ examples/recipes/src/background_jobs/BackgroundJobs.tsx
@@ -1,7 +1,11 @@
 import {
   Button,
+  CircularProgress,
   List, ListItem,
   Paper,
+  Typography,
+  Collapse,
+  Box,
 } from "@mui/material"
 import { useElectric } from "../electric/ElectricWrapper"
 import { useLiveQuery } from "electric-sql/react"
@@ -15,7 +19,7 @@ export const BackgroundJobs = () => {
     orderBy: {
       timestamp: 'desc',
     },
-    take: 5,
+    take: 10,
   }))
 
   const onSubmitJob = useCallback(() => {
@@ -32,15 +36,31 @@ export const BackgroundJobs = () => {
   
 
   return (
-    <Paper>
+    <Paper sx={{ p: 2 }}>
       <Button onClick={onSubmitJob}>
         Submit new job
       </Button>
       <List>
         {
           jobs.map((job) => (
-            <ListItem key={job.id}>
-              {[job.id, job.progress, job.completed, job.result].join(' - ')}
+            <ListItem key={job.id} sx={{ display: 'flex', justifyContent: 'space-between'}}>
+              <Typography>
+                {job.id.slice(0,6)}
+              </Typography>
+              <Box>
+                <Collapse in={!job.completed}>
+                  {(job.progress * 100).toLocaleString(undefined, { maximumSignificantDigits: 2 }) + '%'}
+                  <CircularProgress
+                    variant="determinate"
+                    size="1rem"
+                    sx={{ ml: 1 }}
+                    value={job.progress * 100} />
+                  
+                </Collapse>
+                <Collapse in={job.completed}>
+                  {JSON.stringify(job.result)}
+                </Collapse>
+              </Box>
             </ListItem>
           ))
         }
