e45a511e75f01bdb75dbd81e1ed9ec180875318a
Oleksii Sholik <oleksii@sholik.dev>
2024-01-05 14:24:45 +0200

    Bring back a log entry for CREATE TABLE for consistency
    This reverts commit db9c2e2989ee6c76aa1bb1643e882b958b35b96c.

--- components/electric/lib/electric/postgres/schema/update.ex
+++ components/electric/lib/electric/postgres/schema/update.ex
@@ -86,6 +86,9 @@ defmodule Electric.Postgres.Schema.Update do
   end
 
   defp do_update(%Pg.CreateStmt{} = action, schema, opts) do
+    %{relation: name} = action
+    Logger.info("CREATE TABLE #{name.relname} (...)")
+
     table = AST.create(action, opts)
 
     schema = %{schema | tables: schema.tables ++ [table]}
