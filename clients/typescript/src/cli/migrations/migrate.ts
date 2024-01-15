9e3b9711ef6694bb6e9cffd67caaadc434f95fc3
Sam Willis <sam.willis@gmail.com>
2024-01-15 21:52:43 +0000

    Fix linting error

--- clients/typescript/src/cli/migrations/migrate.ts
+++ clients/typescript/src/cli/migrations/migrate.ts
@@ -45,7 +45,7 @@ export interface GeneratorOptions {
 }
 
 export async function generate(options: GeneratorOptions) {
-  let opts = {
+  const opts = {
     exitOnError: true,
     ...options,
   }
