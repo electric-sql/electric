54ae90a53ea2647a9b5674c2e4bcf9d432055cf9
msfstef <stefanos@electric-sql.com>
2024-01-16 15:39:46 +0200

    Fix formatting issue

--- components/electric/test/electric/postgres/extension/triggers_test.exs
+++ components/electric/test/electric/postgres/extension/triggers_test.exs
@@ -13,50 +13,49 @@ defmodule Electric.Postgres.Extension.TriggersTest do
 
   test "shadow table update does not fail when a column changes", cxt do
     assert [
-      {:ok, [], []},
-      {:ok, [], []},
-      {:ok, 1},
-      {:ok, 1},
-      # assert _modified_columns_bit_mask as having changed the flag
-      {:ok, _, [{"{t,f,f}"}]}
-    ] =
-      :epgsql.squery(cxt.conn, """
-        CREATE TABLE public.trigger_test (
-          val TEXT PRIMARY KEY,
-          flag BOOLEAN,
-          content TEXT,
-          content_json JSONB
-        );
-        CALL electric.electrify('public.trigger_test');
-        INSERT INTO public.trigger_test (val, flag, content_json)
-              VALUES ('1', 'false', '{"a": 3}');
-        UPDATE public.trigger_test SET flag = 'true' WHERE val = '1';
-        SELECT _modified_columns_bit_mask FROM electric.shadow__public__trigger_test;
-      """)
+             {:ok, [], []},
+             {:ok, [], []},
+             {:ok, 1},
+             {:ok, 1},
+             # assert _modified_columns_bit_mask as having changed the flag
+             {:ok, _, [{"{t,f,f}"}]}
+           ] =
+             :epgsql.squery(cxt.conn, """
+               CREATE TABLE public.trigger_test (
+                 val TEXT PRIMARY KEY,
+                 flag BOOLEAN,
+                 content TEXT,
+                 content_json JSONB
+               );
+               CALL electric.electrify('public.trigger_test');
+               INSERT INTO public.trigger_test (val, flag, content_json)
+                     VALUES ('1', 'false', '{"a": 3}');
+               UPDATE public.trigger_test SET flag = 'true' WHERE val = '1';
+               SELECT _modified_columns_bit_mask FROM electric.shadow__public__trigger_test;
+             """)
   end
-
 
   test "shadow table update does not fail even if no columns change", cxt do
     assert [
-      {:ok, [], []},
-      {:ok, [], []},
-      {:ok, 1},
-      {:ok, 1},
-      # assert _modified_columns_bit_mask as having changed nothing
-      {:ok, _, [{"{f,f,f}"}]}
-    ] =
-      :epgsql.squery(cxt.conn, """
-        CREATE TABLE public.trigger_test (
-          val TEXT PRIMARY KEY,
-          flag BOOLEAN,
-          content TEXT,
-          content_json JSONB
-        );
-        CALL electric.electrify('public.trigger_test');
-        INSERT INTO public.trigger_test (val, flag, content_json)
-              VALUES ('1', 'false', '{"a": 3}');
-        UPDATE public.trigger_test SET flag = 'false' WHERE val = '1';
-        SELECT _modified_columns_bit_mask FROM electric.shadow__public__trigger_test;
-      """)
+             {:ok, [], []},
+             {:ok, [], []},
+             {:ok, 1},
+             {:ok, 1},
+             # assert _modified_columns_bit_mask as having changed nothing
+             {:ok, _, [{"{f,f,f}"}]}
+           ] =
+             :epgsql.squery(cxt.conn, """
+               CREATE TABLE public.trigger_test (
+                 val TEXT PRIMARY KEY,
+                 flag BOOLEAN,
+                 content TEXT,
+                 content_json JSONB
+               );
+               CALL electric.electrify('public.trigger_test');
+               INSERT INTO public.trigger_test (val, flag, content_json)
+                     VALUES ('1', 'false', '{"a": 3}');
+               UPDATE public.trigger_test SET flag = 'false' WHERE val = '1';
+               SELECT _modified_columns_bit_mask FROM electric.shadow__public__trigger_test;
+             """)
   end
 end
