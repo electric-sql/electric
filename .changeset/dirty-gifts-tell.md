af32de9a0a8f3bc660631b716452f10c5c9de7f8
Sam Willis <sam.willis@gmail.com>
2024-01-18 18:31:33 +0000

    fix(example/web-wa-sqlite) Fix VAX-1549 - Expose ELECTRIC_ prefixed env vars to Vite build (#852)

---
+++ .changeset/dirty-gifts-tell.md
@@ -0,0 +1,5 @@
+---
+"create-electric-app": patch
+---
+
+Expose ELECTRIC\_ prefixed env vars to Vite build - Fix VAX-1549
--- examples/web-wa-sqlite/vite.config.ts
+++ examples/web-wa-sqlite/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envPrefix: 'ELECTRIC_',
   optimizeDeps: {
     exclude: ['wa-sqlite'],
   },
