69cbd00063273407aff840b88b548a4425613fd6
msfstef <stefanos@electric-sql.com>
2024-01-29 11:40:57 +0200

    Fix up family shopping schema migration

--- examples/expo-family-shopping-list/db/migrations/01-create_family_shopping_list_tables.sql
+++ examples/expo-family-shopping-list/db/migrations/01-create_family_shopping_list_tables.sql
@@ -1,45 +1,47 @@
+
+CREATE TABLE IF NOT EXISTS image (
+    image_id UUID PRIMARY KEY,
+    image_base_64 TEXT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS family (
     family_id UUID PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    image_id UUID REFERECES image(image_id)
+    name VARCHAR NOT NULL,
+    image_id UUID REFERENCES image(image_id)
 );
 
 CREATE TABLE IF NOT EXISTS member (
     member_id UUID PRIMARY KEY,
     family_id UUID REFERENCES family(family_id),
-    name VARCHAR(100) NOT NULL,
-    image_id UUID REFERECES image(image_id)
+    name TEXT NOT NULL,
+    image_id UUID REFERENCES image(image_id)
 );
 
 CREATE TABLE IF NOT EXISTS shopping_list (
     list_id UUID PRIMARY KEY,
-    family_id UUID REFERENCES family(family_id)
-      NOT NULL
-      ON DELETE CASCADE,
+    family_id UUID NOT NULL
+        REFERENCES family(family_id)
+        ON DELETE CASCADE,
     created_by UUID REFERENCES member(member_id),
-    title VARCHAR(255) NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    title VARCHAR NOT NULL,
+    created_at TIMESTAMPTZ
 );
 
 CREATE TABLE IF NOT EXISTS shopping_list_item (
     item_id UUID PRIMARY KEY,
-    list_id INTEGER REFERENCES shopping_list(list_id)
-      NOT NULL
-      ON DELETE CASCADE,
+    list_id UUID NOT NULL
+        REFERENCES shopping_list(list_id)
+        ON DELETE CASCADE,
     added_by UUID REFERENCES member(member_id),
-    name VARCHAR(255) NOT NULL,
-    quantity INTEGER NOT NULL DEFAULT 1,
+    name VARCHAR NOT NULL,
+    quantity INTEGER NOT NULL,
     comment TEXT,
-    image_id UUID REFERECES image(image_id),
-    completed BOOLEAN DEFAULT FALSE
+    image_id UUID REFERENCES image(image_id),
+    completed BOOLEAN
 );
 
-CREATE TABLE IF NOT EXISTS item (
-    item_id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS image (
-    image_id UUID PRIMARY KEY,
-    url VARCHAR(255) NOT NULL
-);
+ALTER TABLE image ENABLE ELECTRIC;
+ALTER TABLE family ENABLE ELECTRIC;
+ALTER TABLE member ENABLE ELECTRIC;
+ALTER TABLE shopping_list ENABLE ELECTRIC;
+ALTER TABLE shopping_list_item ENABLE ELECTRIC;
