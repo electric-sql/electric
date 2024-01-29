dbd75243fc51dcf028bbc8ee39ee1c770e42bec7
Ilia Borovitinov <ilia@electric-sql.com>
2024-01-29 09:15:27 +0300

    Small refactoring

--- clients/typescript/src/satellite/client.ts
+++ clients/typescript/src/satellite/client.ts
@@ -865,6 +865,17 @@ export class SatelliteClient implements Client {
     }
   }
 
+  private getRelation({ relationId }: {relationId: number}): Relation {
+    const rel = this.inbound.relations.get(relationId)
+    if (!rel) {
+      throw new SatelliteError(
+        SatelliteErrorCode.PROTOCOL_VIOLATION,
+        `missing relation ${relationId} for incoming operation`
+      )
+    }
+    return rel
+  }
+
   private processOpLogMessage(opLogMessage: SatOpLog): void {
     const replication = this.inbound
     opLogMessage.ops.map((op) => {
@@ -898,14 +909,7 @@ export class SatelliteClient implements Client {
       }
 
       if (op.insert) {
-        const rid = op.insert.relationId
-        const rel = replication.relations.get(rid)
-        if (!rel) {
-          throw new SatelliteError(
-            SatelliteErrorCode.PROTOCOL_VIOLATION,
-            `missing relation ${op.insert.relationId} for incoming operation`
-          )
-        }
+        const rel = this.getRelation(op.insert)
 
         const change = {
           relation: rel,
@@ -918,14 +922,7 @@ export class SatelliteClient implements Client {
       }
 
       if (op.update) {
-        const rid = op.update.relationId
-        const rel = replication.relations.get(rid)
-        if (!rel) {
-          throw new SatelliteError(
-            SatelliteErrorCode.PROTOCOL_VIOLATION,
-            'missing relation for incoming operation'
-          )
-        }
+        const rel = this.getRelation(op.update)
 
         const change = {
           relation: rel,
@@ -943,14 +940,7 @@ export class SatelliteClient implements Client {
       }
 
       if (op.delete) {
-        const rid = op.delete.relationId
-        const rel = replication.relations.get(rid)
-        if (!rel) {
-          throw new SatelliteError(
-            SatelliteErrorCode.PROTOCOL_VIOLATION,
-            'missing relation for incoming operation'
-          )
-        }
+        const rel = this.getRelation(op.delete)
 
         const change = {
           relation: rel,
