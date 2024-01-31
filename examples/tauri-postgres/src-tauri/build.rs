2388cbb7b39b18fcd5d962c23e5f42b5a667e8b4
tachyonicbytes <support@tachyonicbytes.com>
2024-01-31 23:34:59 +0200

    Added liblzma.5.dylib to the build

--- examples/tauri-postgres/src-tauri/build.rs
+++ examples/tauri-postgres/src-tauri/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     // if cfg!(target_os = "macos") {
     //     println!("cargo:rustc-link-lib=onnxruntime");
-    //     println!("cargo:rustc-link-lib=dylib=libonnxruntime.dylib");
     // }
+    println!("cargo:rustc-link-arg=-Lliblzma.5.dylib");
     tauri_build::build()
 }
