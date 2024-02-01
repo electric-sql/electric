b49f92a60870f66b8898dfaa12d78feb12118a52
msfstef <stefanos@electric-sql.com>
2024-02-01 19:24:07 +0200

    Change shopping list query to go from user ID until permissions are sorted

--- examples/expo-family-shopping-list/src/app/(app)/(home)/shopping_lists.tsx
+++ examples/expo-family-shopping-list/src/app/(app)/(home)/shopping_lists.tsx
@@ -1,32 +1,53 @@
 import { useLiveQuery } from 'electric-sql/react'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { FlatList, SafeAreaView, View } from 'react-native'
 import { List, FAB, Text } from 'react-native-paper'
 import { useElectric } from '../../../components/ElectricProvider'
 import ShoppingListCard from '../../../components/ShoppingListCard'
 import { Link } from 'expo-router'
 import FlatListSeparator from '../../../components/FlatListSeparator'
+import { useAuthenticatedUser } from '../../../components/AuthProvider'
 
 export default function ShoppingLists () {
+  const userId = useAuthenticatedUser()!
   const { db } = useElectric()!
-  const { results: shopping_lists = [] } = useLiveQuery(db.shopping_list.liveMany({
-    select: {
-      list_id: true
+  const { results: memberships = [] } = useLiveQuery(db.member.liveMany({
+    include: {
+      family: {
+        include: {
+          shopping_list: {
+            select: {
+              list_id: true,
+              updated_at: true
+            }
+          }
+        }
+      }
     },
-    orderBy: {
-      updated_at: 'desc',
+    where: {
+      user_id: userId
     }
   }))
+
+  const shoppingLists = useMemo(() => memberships.reduce(
+      (allLists, membership) => [
+        ...allLists,
+        ...(membership.family?.shopping_list ?? [])],
+      []
+    ).sort((a: any, b: any) => b.updated_at.getTime() - a.updated_at.getTime()),
+    [memberships]
+  )
+
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1, paddingHorizontal: 16 }}>
         <List.Section style={{ flex: 1 }}>
           <List.Subheader>Your Shopping Lists</List.Subheader>
-          { shopping_lists.length > 0 ?
+          { shoppingLists.length > 0 ?
             <FlatList
             style={{ padding: 6 }}
-            data={shopping_lists}
+            data={shoppingLists}
             renderItem={(item) => (
               <Link href={`/shopping_list/${item.item.list_id}`} asChild>
                 <ShoppingListCard shoppingListId={item.item.list_id} />
