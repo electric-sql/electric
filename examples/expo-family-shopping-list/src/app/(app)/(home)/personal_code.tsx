a9d047733c82c1392b34c6412525827c2c5f4e7e
msfstef <stefanos@electric-sql.com>
2024-02-01 20:40:32 +0200

    Add name as query parameter for link creation

--- examples/expo-family-shopping-list/src/app/(app)/(home)/personal_code.tsx
+++ examples/expo-family-shopping-list/src/app/(app)/(home)/personal_code.tsx
@@ -4,15 +4,29 @@ import QRCode from 'react-native-qrcode-svg';
 import * as Linking from 'expo-linking';
 import { useAuthenticatedUser } from '../../../components/AuthProvider'
 import { Button, Text } from 'react-native-paper';
+import { useElectric } from '../../../components/ElectricProvider';
+import { useLiveQuery } from 'electric-sql/react';
 
 const windowDimensions = Dimensions.get('window');
 
 export default function PersonalCode () {
   const userId = useAuthenticatedUser()!
+  const { db } = useElectric()!
+
+  // select a name to initialize the membership in the new family
+  const { results: { name } = {} } = useLiveQuery<{ name: string }>(
+    db.member.liveFirst({
+      select: { name: true },
+      where: { user_id: userId }
+    })
+  )
 
   const joinFamilyLink = useMemo(() => Linking.createURL('/invite', {
-    queryParams: { user_id: userId },
-  }), [ userId ]);
+    queryParams: {
+      user_id: userId,
+      name: name
+    },
+  }), [ userId, name ]);
 
   const shareLink = () => Share.share({
     title: 'I\'d like to join your family on Electric Shopping List!',
