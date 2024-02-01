718cf891b2c38c9312ce775af3cc9fe6e5147b1a
Garry Hill <garry@electric-sql.com>
2024-02-01 11:00:47 +0000

    use absolute paths in installation

--- examples/tauri-postgres/utils-linux.sh
+++ examples/tauri-postgres/utils-linux.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -ex
+
+__dir__="$(cd "$(dirname "$0")"; pwd)"
+__electric__="$(cd "${__dir__}/../.."; pwd)"
+__src__="${__dir__}/src-tauri"
+
+mkdir -p "$__src__"
+
 # This script runs the demo using this tauri example on Linux
 # This script is necessary because there are many external tools that this demo depends on
 
@@ -11,24 +19,40 @@ get_electric() {
 
 # We need to have electric set up for local development, because we made a lot of changes to the sources
 build_electric() {
-    mkdir -p examples/tauri-postgres/db/data/
+    mkdir -p "${__dir__}/db/data/"
     #TODO: We need to get the issues.json with embeddings here.
     # ln -s $(PWD)/db/data/issues.json $(PWD)/electric/examples/tauri-postgres/db/data/issues.json
-    cd clients/typescript && pnpm install && pnpm build
-    cd ../../generator/ && pnpm install && pnpm build
-    cd ../examples/tauri-postgres/
+    (
+        cd "${__electric__}/clients/typescript"
+        pnpm install
+        pnpm build
+    )
+    (
+        cd "${__electric__}/generator"
+        pnpm install
+        pnpm build
+    )
 }
 
 # Clone the sources
 git_clone_third_parties() {
     # We are in root
-    mkdir -p src-tauri/crates/ && cd src-tauri/crates/
-    git clone https://github.com/pepperoni21/ollama-rs # Solves a bug where in Cargo.toml `git` would still not work as expected
-    cd ollama-rs && git checkout f610472689ec113689ab06fb58304ec723c93111 && cd ..
-    git clone https://github.com/faokunega/pg-embed # We need to modify this in order to have a different location for postgres
-    cd pg-embed
-    git apply ../../../pg-embed.patch
-    cd ../../..
+    mkdir -p "${__src__}/crates"
+    cd "${__src__}/crates"
+
+    if [[ ! -d ${__src__}/crates/ollama-rs ]]; then
+        git clone https://github.com/pepperoni21/ollama-rs "${__src__}/crates/ollama-rs" # Solves a bug where in Cargo.toml `git` would still not work as expected
+    fi
+
+    cd "${__src__}/crates/ollama-rs"
+    git checkout f610472689ec113689ab06fb58304ec723c93111
+
+    # remove any existing pg-embed and start from scratch, otherwise the patch below won't apply
+    rm -rf "${__src__}/crates/pg-embed"
+    git clone https://github.com/faokunega/pg-embed "${__src__}/crates/pg-embed"
+    # We need to modify this in order to have a different location for postgres
+    cd "${__src__}/crates/pg-embed"
+    git apply "${__dir__}/pg-embed.patch"
 }
 
 
@@ -37,11 +61,11 @@ git_clone_third_parties() {
 install_ollama() {
     echo "Installing ollama"
     # wget https://github.com/jmorganca/ollama/releases/download/v0.1.20/ollama-darwin
-    wget https://github.com/ollama/ollama/releases/download/v0.1.22/ollama-linux-amd64
-    chmod +x ollama-linux-amd64
+    curl -L -o ${__src__}/ollama-linux-amd64 https://github.com/ollama/ollama/releases/download/v0.1.22/ollama-linux-amd64
+    chmod +x ${__src__}/ollama-linux-amd64
 
     # Tauri needs this specific name
-    mv ollama-linux-amd64 src-tauri/ollama-x86_64-unknown-linux-gnu
+    mv ${__src__}/ollama-linux-amd64 ${__src__}/ollama-x86_64-unknown-linux-gnu
 }
 
 # Install postgres
@@ -50,8 +74,8 @@ install_postgres() {
     # We are in root
     echo "Installing postgres"
     # Create the directory where postgres will live as an external resource
-    mkdir -p src-tauri/pgdir/
-    cd src-tauri/pgdir/
+    mkdir -p "${__src__}/pgdir"
+    cd "${__src__}/pgdir"
 
     # wget https://repo1.maven.org/maven2/io/zonky/test/postgres/embedded-postgres-binaries-darwin-arm64v8/15.5.1/embedded-postgres-binaries-darwin-arm64v8-15.5.1.jar
     wget https://repo1.maven.org/maven2/io/zonky/test/postgres/embedded-postgres-binaries-linux-amd64/15.5.1/embedded-postgres-binaries-linux-amd64-15.5.1.jar
@@ -66,7 +90,6 @@ install_postgres() {
     # rm embedded-postgres-binaries-darwin-arm64v8-15.5.1.jar
     rm embedded-postgres-binaries-linux-amd64-15.5.1.jar
     rm -rf META-INF
-    cd ../../ # root
 
     # TODO: get postgres15 on linux some way, so you can compile pgvector
     # # NOTE: The below steps are used just to build the three files the `pgvector` extension use.
@@ -77,7 +100,7 @@ install_postgres() {
     # unzip postgresql-15.5-1-osx-binaries.zip # The directory is called `pgsql`
     # rm postgresql-15.5-1-osx-binaries.zip
 
-    # git clone --branch v0.5.1 https://github.com/pgvector/pgvector.git
+    git clone --branch v0.5.1 https://github.com/pgvector/pgvector.git "${__src__}/pgvector"
     # cd pgvector
     # # Build pgvector with the downloaded postgres
     # PG_CONFIG=../pgsql/bin/pg_config make
@@ -85,27 +108,29 @@ install_postgres() {
     # rm -rf ../pgsql/
 
     # We need these files: TODO:
-    mv sql/vector--0.5.1.sql ../src-tauri/pgdir/share/postgresql/extension/vector--0.5.1.sql
-    mv vector.control ../src-tauri/pgdir/share/postgresql/extension/vector.control
-    mv vector.so ../src-tauri/pgdir/lib/postgresql/vector.so
-    cd ..
+    mv "${__src__}/pgvector/sql/vector.sql" "${__src__}/pgdir/share/postgresql/extension/vector--0.5.1.sql"
+    mv "${__src__}/pgvector/vector.control" "${__src__}/pgdir/share/postgresql/extension/vector.control"
+    # mv pgvector/vector.so ../src-tauri/pgdir/lib/postgresql/vector.so
     # We don't need pgvector sources from this point
-    rm -rf pgvector
+    rm -rf "${__src__}/pgvector"
 }
 
 # We also need to download the dynamic libraries for onnx
 install_onnxruntime() {
     # We are in root
-    wget https://github.com/microsoft/onnxruntime/releases/download/v1.16.3/onnxruntime-linux-x64-1.16.3.tgz
+    cd "${__src__}"
+    curl -L -O https://github.com/microsoft/onnxruntime/releases/download/v1.16.3/onnxruntime-linux-x64-1.16.3.tgz
     tar -xzvf onnxruntime-linux-x64-1.16.3.tgz
     rm onnxruntime-linux-x64-1.16.3.tgz
-    cp onnxruntime-linux-x64-1.16.3/lib/libonnxruntime.so.1.16.3 src-tauri/libonnxruntime.so
+    cp onnxruntime-linux-x64-1.16.3/lib/libonnxruntime.so.1.16.3 "${__src__}/libonnxruntime.so"
     rm -rf onnxruntime-linux-x64-1.16.3
 }
 
 # Build the Tauri app
 build_the_app() {
     # We are in root
-    pnpm install && pnpm run tauri:package
+    cd "${__electric__}"
+    pnpm install
+    pnpm run tauri:package
     pnpm tauri build # This also installs the app
 }
