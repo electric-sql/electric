2e591e5a7153d82bf3b210d5c3ca1eb02321d042
tachyonicbytes <support@tachyonicbytes.com>
2024-02-01 02:05:25 +0200

    Commented unused sql

--- clients/typescript/src/satellite/process.ts
+++ clients/typescript/src/satellite/process.ts
@@ -1393,13 +1393,13 @@ export class SatelliteProcess implements Satellite {
       const tableName = table.name
 
       // TODO: Check for the primary key some other way, but this should work for the demo.
-      const sql = `
-      SELECT c.column_name as name, c.data_type as type
-      FROM information_schema.table_constraints tc
-      JOIN information_schema.constraint_column_usage AS ccu USING (constraint_schema, constraint_name)
-      JOIN information_schema.columns AS c ON c.table_schema = tc.constraint_schema
-        AND tc.table_name = c.table_name AND ccu.column_name = c.column_name
-      WHERE constraint_type = 'PRIMARY KEY' and tc.table_name = $1;` // gets only the primary keys
+      // const sql = `
+      // SELECT c.column_name as name, c.data_type as type
+      // FROM information_schema.table_constraints tc
+      // JOIN information_schema.constraint_column_usage AS ccu USING (constraint_schema, constraint_name)
+      // JOIN information_schema.columns AS c ON c.table_schema = tc.constraint_schema
+      //   AND tc.table_name = c.table_name AND ccu.column_name = c.column_name
+      // WHERE constraint_type = 'PRIMARY KEY' and tc.table_name = $1;` // gets only the primary keys
 
       const sql2 = `SELECT c.column_name as name, c.data_type as type
       FROM information_schema.table_constraints tc
