74812ab97e2107b67137e3690672f0f88402ebab
Ilia Borovitinov <ilia@electric-sql.com>
2024-02-02 22:36:27 +0300

    spec fix

--- components/electric/lib/electric/postgres/extension/schema_loader/version.ex
+++ components/electric/lib/electric/postgres/extension/schema_loader/version.ex
@@ -19,7 +19,7 @@ defmodule Electric.Postgres.Extension.SchemaLoader.Version do
           primary_keys: %{relation() => [String.t()]}
         }
 
-  @spec new(version(), Schema.t()) :: t()
+  @spec new(nil | version(), Schema.t()) :: t()
   def new(version, %Schema.Proto.Schema{} = schema) do
     %__MODULE__{version: version, schema: schema}
     |> Map.update!(:tables, &cache_tables_by_name(&1, schema))
