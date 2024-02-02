16e78675ee064990615811950b5bc96b54d664a3
msfstef <stefanos@electric-sql.com>
2024-02-02 11:27:38 +0200

    Add ELECTRIC_WRITE_TO_PG_MODE env var to CLI

--- clients/typescript/src/cli/config-options.ts
+++ clients/typescript/src/cli/config-options.ts
@@ -175,6 +175,23 @@ export const configOptions : Record<string, AnyConfigOption> = {
     `,
     groups: ['electric'],
   },
+  ELECTRIC_WRITE_TO_PG_MODE: {
+    defaultVal: 'logical_replication',
+    valueType: String,
+    valueTypeName: 'logical_replication | direct_writes',
+    doc: dedent`
+      In logical_replication mode, Electric provides a logical replication publisher
+      service over TCP that speaks the Logical Streaming Replication Protocol.
+      Postgres connects to Electric and establishes a subscription to this.
+      Writes are then streamed in and applied using logical replication.
+      
+      In direct_writes mode, Electric writes data to Postgres using a standard
+      interactive client connection. This avoids the need for Postgres to be
+      able to connect to Electric and reduces the permissions required for the
+      database user that Electric connects to Postgres as.
+    `,
+    groups: ['electric'],
+  },
   LOGICAL_PUBLISHER_HOST: {
     valueType: String,
     valueTypeName: 'url',
