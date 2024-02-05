3a78a767703f45458aac17904188fd25f781d904
Stefanos Mousafeiris <msfstef@gmail.com>
2024-02-05 16:47:12 +0200

    fix(electric): Do not include extra comma if list is empty (#831)
    Solves https://github.com/electric-sql/electric/issues/801.
    
    Update statement was including an extra comma even if `columns_to_write`
    is empty - that being said I'm unsure why columns to write would be
    empty in this particular case, but trying to figure out the internals
    one bite at a time :)
    
    I've reproduced it locally ad-hoc and verified it fixes it, and I'd love
    to add a test for this but I couldn't figure out if/where we had any
    others - @icehaunter let me know if I've missed them, and if not if you
    have suggestions as to where to add them?
    
    ---------
    
    Co-authored-by: Oleksii Sholik <oleksii@sholik.dev>

---
+++ components/electric/test/electric/postgres/extension/triggers_test.exs
@@ -0,0 +1,61 @@
+# NOTE(msfstef): tests should ideally also look at shadow table values
+defmodule Electric.Postgres.Extension.TriggersTest do
+  use ExUnit.Case, async: false
+  import Electric.Postgres.TestConnection
+
+  setup do
+    context = create_test_db()
+
+    assert {:ok, _versions} = Electric.Postgres.Extension.migrate(context.conn)
+
+    {:ok, Map.merge(context, %{repo: Electric.Postgres.Proxy.TestRepo})}
+  end
+
+  test "shadow table update does not fail when a column changes", cxt do
+    assert [
+             {:ok, [], []},
+             {:ok, [], []},
+             {:ok, 1},
+             {:ok, 1},
+             # assert _modified_columns_bit_mask as having changed the flag
+             {:ok, _, [{"{t,f,f}"}]}
+           ] =
+             :epgsql.squery(cxt.conn, """
+               CREATE TABLE public.trigger_test (
+                 val TEXT PRIMARY KEY,
+                 flag BOOLEAN,
+                 content TEXT,
+                 content_json JSONB
+               );
+               CALL electric.electrify('public.trigger_test');
+               INSERT INTO public.trigger_test (val, flag, content_json)
+                     VALUES ('1', 'false', '{"a": 3}');
+               UPDATE public.trigger_test SET flag = 'true' WHERE val = '1';
+               SELECT _modified_columns_bit_mask FROM electric.shadow__public__trigger_test;
+             """)
+  end
+
+  test "shadow table update does not fail even if no columns change", cxt do
+    assert [
+             {:ok, [], []},
+             {:ok, [], []},
+             {:ok, 1},
+             {:ok, 1},
+             # assert _modified_columns_bit_mask as having changed nothing
+             {:ok, _, [{"{f,f,f}"}]}
+           ] =
+             :epgsql.squery(cxt.conn, """
+               CREATE TABLE public.trigger_test (
+                 val TEXT PRIMARY KEY,
+                 flag BOOLEAN,
+                 content TEXT,
+                 content_json JSONB
+               );
+               CALL electric.electrify('public.trigger_test');
+               INSERT INTO public.trigger_test (val, flag, content_json)
+                     VALUES ('1', 'false', '{"a": 3}');
+               UPDATE public.trigger_test SET flag = 'false' WHERE val = '1';
+               SELECT _modified_columns_bit_mask FROM electric.shadow__public__trigger_test;
+             """)
+  end
+end
