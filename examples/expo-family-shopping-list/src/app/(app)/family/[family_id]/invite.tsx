b723b6f62693608e61142899d31cbedc8cbb8888
msfstef <stefanos@electric-sql.com>
2024-02-05 16:44:43 +0200

    Add permission request logic for QR code scanner

--- examples/expo-family-shopping-list/src/app/(app)/family/[family_id]/invite.tsx
+++ examples/expo-family-shopping-list/src/app/(app)/family/[family_id]/invite.tsx
@@ -1,14 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { CameraView, useCameraPermissions, BarcodeScanningResult } from 'expo-camera/next'
 
 import { View } from 'react-native'
-import { Text } from 'react-native-paper'
+import { Button, Text } from 'react-native-paper'
 import * as Linking from 'expo-linking'
 import { router, useLocalSearchParams } from 'expo-router'
 
 export default function FamilyInvite () {
   const { family_id } = useLocalSearchParams<{ family_id: string }>()
+  const [ permission, requestPermission ] = useCameraPermissions();
+  const { granted = false, canAskAgain = false } = permission ?? {}
 
+  const onPermissionRequest = () => {
+    if (canAskAgain) return requestPermission()
+    else Linking.openSettings()
+  }
 
   const onBarcodeScanned = (result: BarcodeScanningResult) => {
     try {
@@ -30,6 +36,11 @@ export default function FamilyInvite () {
       console.warn(`Ignoring parsed QR code: ${result.data}`)
     }
   }
+
+  // request permissions immediately if possible
+  useEffect(() => {
+    if (!granted && canAskAgain) requestPermission()
+  }, [granted, canAskAgain])
   
   return (
     <View style={{ flex: 1, alignItems: 'center', gap: 16}}>
@@ -38,15 +49,21 @@ export default function FamilyInvite () {
         which they can find in their side menu under
         <Text style={{ fontWeight: "bold" }}> Join a Family.</Text>
       </Text>
-      <View style={{ flexDirection: 'row' }}>
-        <CameraView
-          style={{ flex: 1, aspectRatio: 1 }}
-          barcodeScannerSettings={{
-            barCodeTypes: ["qr"],
-          }}
-          onBarcodeScanned={onBarcodeScanned}
-          />
-      </View>
+      { !granted ?
+        <View style={{ flexDirection: 'row' }}>
+          <CameraView
+            style={{ flex: 1, aspectRatio: 1 }}
+            barcodeScannerSettings={{
+              barCodeTypes: ["qr"],
+            }}
+            onBarcodeScanned={onBarcodeScanned}
+            />
+        </View>
+        :
+        <Button mode="contained-tonal" onPress={onPermissionRequest}>
+          Grant access to camera
+        </Button>
+      }
     </View>
   )
 }
