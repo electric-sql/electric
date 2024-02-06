1d947de2b150f820d2eadf1c204d5fc33dfe37c7
Oleksii Sholik <oleksii@sholik.dev>
2024-02-06 15:47:23 +0200

    Remove redundant "describe" block from migration consumer tests

--- components/electric/test/electric/replication/postgres/migration_consumer_test.exs
+++ components/electric/test/electric/replication/postgres/migration_consumer_test.exs
@@ -55,201 +55,199 @@ defmodule Electric.Replication.Postgres.MigrationConsumerTest do
     end
   end
 
-  describe "migrations" do
-    setup do
-      origin = "logical_replication_producer_test_1"
-      producer_name = Electric.name(FakeProducer, origin)
+  setup do
+    origin = "logical_replication_producer_test_1"
+    producer_name = Electric.name(FakeProducer, origin)
 
-      {:ok, producer} = start_supervised({FakeProducer, producer_name})
+    {:ok, producer} = start_supervised({FakeProducer, producer_name})
 
-      version = "20220421"
+    version = "20220421"
 
-      # provide fake oids for the new tables
-      oids = %{
-        table: %{
-          {"public", "something_else"} => 1111,
-          {"public", "other_thing"} => 2222,
-          {"public", "yet_another_thing"} => 3333,
-          {"electric", "shadow__public__something_else"} => 201_111,
-          {"electric", "shadow__public__other_thing"} => 202_222,
-          {"electric", "shadow__public__yet_another_thing"} => 203_333
-        }
+    # provide fake oids for the new tables
+    oids = %{
+      table: %{
+        {"public", "something_else"} => 1111,
+        {"public", "other_thing"} => 2222,
+        {"public", "yet_another_thing"} => 3333,
+        {"electric", "shadow__public__something_else"} => 201_111,
+        {"electric", "shadow__public__other_thing"} => 202_222,
+        {"electric", "shadow__public__yet_another_thing"} => 203_333
       }
+    }
 
-      pks = %{
-        {"public", "mistakes"} => ["id"]
+    pks = %{
+      {"public", "mistakes"} => ["id"]
+    }
+
+    backend =
+      MockSchemaLoader.start_link([oids: oids, pks: pks], name: __MODULE__.Loader)
+
+    {:ok, pid} =
+      start_supervised(
+        {MigrationConsumer,
+         {[origin: origin, connection: [], replication: []],
+          [
+            producer: producer_name,
+            backend: backend
+          ]}}
+      )
+
+    {:ok, _consumer} = start_supervised({FakeConsumer, {pid, self()}})
+
+    {:ok, origin: origin, producer: producer, version: version, loader: backend}
+  end
+
+  test "refreshes subscription after receiving a migration", cxt do
+    %{producer: producer, origin: origin, version: version} = cxt
+    assert_receive {MockSchemaLoader, {:connect, _}}
+
+    events = [
+      %Transaction{
+        changes: [
+          %NewRecord{
+            relation: {"electric", "ddl_commands"},
+            record: %{
+              "id" => "6",
+              "query" => "create table something_else (id uuid primary key);",
+              "txid" => "101",
+              "txts" => "201"
+            },
+            tags: []
+          }
+        ]
       }
+    ]
 
-      backend =
-        MockSchemaLoader.start_link([oids: oids, pks: pks], name: __MODULE__.Loader)
+    GenStage.call(producer, {:emit, cxt.loader, events, version})
 
-      {:ok, pid} =
-        start_supervised(
-          {MigrationConsumer,
-           {[origin: origin, connection: [], replication: []],
-            [
-              producer: producer_name,
-              backend: backend
-            ]}}
-        )
+    assert_receive {MockSchemaLoader, {:refresh_subscription, ^origin}}, 1500
+  end
 
-      {:ok, _consumer} = start_supervised({FakeConsumer, {pid, self()}})
+  test "captures migration records", cxt do
+    %{origin: origin, producer: producer, version: version} = cxt
+    assert_receive {MockSchemaLoader, {:connect, _}}
 
-      {:ok, origin: origin, producer: producer, version: version, loader: backend}
-    end
-
-    test "migration consumer refreshes subscription after receiving a migration", cxt do
-      %{producer: producer, origin: origin, version: version} = cxt
-      assert_receive {MockSchemaLoader, {:connect, _}}
-
-      events = [
-        %Transaction{
-          changes: [
-            %NewRecord{
-              relation: {"electric", "ddl_commands"},
-              record: %{
-                "id" => "6",
-                "query" => "create table something_else (id uuid primary key);",
-                "txid" => "101",
-                "txts" => "201"
-              },
-              tags: []
-            }
-          ]
-        }
-      ]
-
-      GenStage.call(producer, {:emit, cxt.loader, events, version})
-
-      assert_receive {MockSchemaLoader, {:refresh_subscription, ^origin}}, 1500
-    end
-
-    test "migration consumer stage captures migration records", cxt do
-      %{origin: origin, producer: producer, version: version} = cxt
-      assert_receive {MockSchemaLoader, {:connect, _}}
-
-      events = [
-        %Transaction{
-          changes: [
-            %NewRecord{
-              relation: {"electric", "ddl_commands"},
-              record: %{
-                "id" => "6",
-                "query" => "create table something_else (id uuid primary key);",
-                "txid" => "100",
-                "txts" => "200"
-              },
-              tags: []
+    events = [
+      %Transaction{
+        changes: [
+          %NewRecord{
+            relation: {"electric", "ddl_commands"},
+            record: %{
+              "id" => "6",
+              "query" => "create table something_else (id uuid primary key);",
+              "txid" => "100",
+              "txts" => "200"
             },
-            %NewRecord{
-              relation: {"electric", "ddl_commands"},
-              record: %{
-                "id" => "7",
-                "query" => "create table other_thing (id uuid primary key);",
-                "txid" => "100",
-                "txts" => "200"
-              },
-              tags: []
+            tags: []
+          },
+          %NewRecord{
+            relation: {"electric", "ddl_commands"},
+            record: %{
+              "id" => "7",
+              "query" => "create table other_thing (id uuid primary key);",
+              "txid" => "100",
+              "txts" => "200"
             },
-            %NewRecord{
-              relation: {"electric", "ddl_commands"},
-              record: %{
-                "id" => "8",
-                "query" => "create table yet_another_thing (id uuid primary key);",
-                "txid" => "100",
-                "txts" => "200"
-              },
-              tags: []
-            }
-          ],
-          commit_timestamp: ~U[2023-05-02 10:08:00.948788Z],
-          origin: origin,
-          publication: "mock_pub",
-          origin_type: :postgresql
-        }
-      ]
-
-      GenStage.call(producer, {:emit, cxt.loader, events, version})
-
-      assert_receive {FakeConsumer, :events, ^events}, 500
-      assert_receive {MockSchemaLoader, :load}, 500
-      assert_receive {MockSchemaLoader, {:save, ^version, schema, [_, _, _]}}
-      # only receive 1 save instruction
-      refute_receive {MockSchemaLoader, {:save, _, _schema}}
-
-      assert Enum.map(schema.tables, & &1.name.name) == [
-               "something_else",
-               "other_thing",
-               "yet_another_thing",
-               "shadow__public__something_else",
-               "shadow__public__other_thing",
-               "shadow__public__yet_another_thing"
-             ]
-    end
-
-    test "migration consumer filters non-migration records", cxt do
-      %{origin: origin, producer: producer, version: version} = cxt
-      assert_receive {MockSchemaLoader, {:connect, _}}
-
-      raw_events = [
-        %Transaction{
-          changes: [
-            %NewRecord{
-              relation: {"electric", "ddl_commands"},
-              record: %{
-                "id" => "6",
-                "query" => "create table something_else (id uuid primary key);",
-                "txid" => "101",
-                "txts" => "201"
-              },
-              tags: []
+            tags: []
+          },
+          %NewRecord{
+            relation: {"electric", "ddl_commands"},
+            record: %{
+              "id" => "8",
+              "query" => "create table yet_another_thing (id uuid primary key);",
+              "txid" => "100",
+              "txts" => "200"
             },
-            %NewRecord{
-              relation: {"electric", "schema"},
-              record: %{
-                "id" => "7",
-                "version" => version,
-                "schema" => "{}"
-              },
-              tags: []
-            }
-          ],
-          commit_timestamp: ~U[2023-05-02 10:08:00.948788Z],
-          origin: origin,
-          publication: "mock_pub",
-          origin_type: :postgresql
-        }
-      ]
+            tags: []
+          }
+        ],
+        commit_timestamp: ~U[2023-05-02 10:08:00.948788Z],
+        origin: origin,
+        publication: "mock_pub",
+        origin_type: :postgresql
+      }
+    ]
 
-      filtered_events = [
-        %Transaction{
-          changes: [
-            %NewRecord{
-              relation: {"electric", "ddl_commands"},
-              record: %{
-                "id" => "6",
-                "query" => "create table something_else (id uuid primary key);",
-                "txid" => "101",
-                "txts" => "201"
-              },
-              tags: []
-            }
-          ],
-          commit_timestamp: ~U[2023-05-02 10:08:00.948788Z],
-          origin: origin,
-          publication: "mock_pub",
-          origin_type: :postgresql
-        }
-      ]
+    GenStage.call(producer, {:emit, cxt.loader, events, version})
 
-      GenStage.call(producer, {:emit, cxt.loader, raw_events, version})
+    assert_receive {FakeConsumer, :events, ^events}, 500
+    assert_receive {MockSchemaLoader, :load}, 500
+    assert_receive {MockSchemaLoader, {:save, ^version, schema, [_, _, _]}}
+    # only receive 1 save instruction
+    refute_receive {MockSchemaLoader, {:save, _, _schema}}
 
-      assert_receive {FakeConsumer, :events, ^filtered_events}, 1000
-      assert_receive {MockSchemaLoader, :load}, 500
+    assert Enum.map(schema.tables, & &1.name.name) == [
+             "something_else",
+             "other_thing",
+             "yet_another_thing",
+             "shadow__public__something_else",
+             "shadow__public__other_thing",
+             "shadow__public__yet_another_thing"
+           ]
+  end
 
-      assert_receive {MockSchemaLoader,
-                      {:save, ^version, _schema,
-                       ["create table something_else (id uuid primary key);"]}}
-    end
+  test "filters non-migration records", cxt do
+    %{origin: origin, producer: producer, version: version} = cxt
+    assert_receive {MockSchemaLoader, {:connect, _}}
+
+    raw_events = [
+      %Transaction{
+        changes: [
+          %NewRecord{
+            relation: {"electric", "ddl_commands"},
+            record: %{
+              "id" => "6",
+              "query" => "create table something_else (id uuid primary key);",
+              "txid" => "101",
+              "txts" => "201"
+            },
+            tags: []
+          },
+          %NewRecord{
+            relation: {"electric", "schema"},
+            record: %{
+              "id" => "7",
+              "version" => version,
+              "schema" => "{}"
+            },
+            tags: []
+          }
+        ],
+        commit_timestamp: ~U[2023-05-02 10:08:00.948788Z],
+        origin: origin,
+        publication: "mock_pub",
+        origin_type: :postgresql
+      }
+    ]
+
+    filtered_events = [
+      %Transaction{
+        changes: [
+          %NewRecord{
+            relation: {"electric", "ddl_commands"},
+            record: %{
+              "id" => "6",
+              "query" => "create table something_else (id uuid primary key);",
+              "txid" => "101",
+              "txts" => "201"
+            },
+            tags: []
+          }
+        ],
+        commit_timestamp: ~U[2023-05-02 10:08:00.948788Z],
+        origin: origin,
+        publication: "mock_pub",
+        origin_type: :postgresql
+      }
+    ]
+
+    GenStage.call(producer, {:emit, cxt.loader, raw_events, version})
+
+    assert_receive {FakeConsumer, :events, ^filtered_events}, 1000
+    assert_receive {MockSchemaLoader, :load}, 500
+
+    assert_receive {MockSchemaLoader,
+                    {:save, ^version, _schema,
+                     ["create table something_else (id uuid primary key);"]}}
   end
 end
