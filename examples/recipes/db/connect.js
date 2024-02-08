5b88bc1cd22dbdc98ba72abc1676aef0cfe68e4d
msfstef <stefanos@electric-sql.com>
2024-02-08 12:44:23 +0200

    Update recipes to more recent electric version

--- examples/recipes/db/connect.js
+++
@@ -1,6 +0,0 @@
-import { dockerCompose } from '../scripts/util.js'
-import { CONTAINER_DATABASE_URL, PUBLIC_DATABASE_URL } from './util.js'
-
-console.info(`Connecting to proxy at ${PUBLIC_DATABASE_URL}`)
-
-dockerCompose('exec', ['-it', 'postgres', 'psql', CONTAINER_DATABASE_URL])
