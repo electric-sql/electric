81cc34b8df301dd7e1fea832c8755f82714c091e
msfstef <stefanos@electric-sql.com>
2024-02-12 15:54:04 +0200

    Remove redundant migration schema comment

--- examples/recipes/db/migrations/06-chat_room_table.sql
+++ examples/recipes/db/migrations/06-chat_room_table.sql
@@ -1,14 +1,3 @@
-/* This is an example of an SQL DDL migration. It creates tables and
- * then calls an `electric.electrify` procedure to expose the tables to the
- * ElectricSQL replication machinery.
- *
- * Note that these statements are applied directly to the *Postgres* database.
- * Electric then handles keeping the local SQLite database schema in sync with
- * the electrified subset of your Postgres database schema.
- *
- * See https://electric-sql.com/docs/usage/data-modelling for more information.
- */
-
 -- Create a simple chat room table.
 CREATE TABLE IF NOT EXISTS chat_room (
   id UUID PRIMARY KEY NOT NULL,
