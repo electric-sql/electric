5888dd28c61dc0d5978cdd7d0ea1ec57794a09ef
msfstef <stefanos@electric-sql.com>
2024-02-15 20:30:16 +0200

    Change useLiveQuery implementation to emphasize refs

--- clients/typescript/src/frameworks/vuejs/reactive/useLiveQuery.ts
+++ clients/typescript/src/frameworks/vuejs/reactive/useLiveQuery.ts
@@ -1,7 +1,6 @@
 import {
   watch,
   WatchSource,
-  ref,
   Ref,
   computed,
   readonly,
@@ -10,6 +9,9 @@ import {
   onUnmounted,
   toRefs,
   ToRefs,
+  isRef,
+  shallowRef,
+  unref,
 } from 'vue'
 import {
   LiveResultContext,
@@ -24,10 +26,13 @@ import { UnsubscribeFunction } from '../../../notifiers'
  * which injects an {@link ElectricClient} into the component
  * tree in order to be able to form live queries.
  *
- * Live query provided can be dynamic, but it will be a hash of
- * the provided query will be computed on every render.
+ * The provided query will not update reactively, so if you want
+ * the query itself to respond to changes make sure to use the
+ * form that takes a Ref or live query getter as an argument
  *
- * @param runQuery - a live query.
+ * @param runQuery - a static live query.
+ * @param runQueryDependencies - Optional list of dependencies that
+ *    will cause the query to rerun
  *
  * @example Using a simple live query. The table will depend on your application
  * ```ts
@@ -35,7 +40,8 @@ import { UnsubscribeFunction } from '../../../notifiers'
  * ```
  */
 function useLiveQuery<Res>(
-  runQuery: LiveResultContext<Res>
+  runQuery: LiveResultContext<Res>,
+  runQueryDependencies?: WatchSource[]
 ): ToRefs<DeepReadonly<LiveResultUpdate<Res>>>
 
 /**
@@ -44,11 +50,17 @@ function useLiveQuery<Res>(
  * which injects an {@link ElectricClient} into the component
  * tree in order to be able to form live queries.
  *
- * You can provide a reference to a live query, and the reactivity
- * provided by the reference to the query will be carried forward
- * to the results, ensuring that changes to the query are reflected
+ * You can provide a reference to or getter for a live query, and
+ * the query results will update with the query reactively.
  *
- * @param runQueryRef - a reference to a live query
+ * Providing a list of dependencies will cause the query to rerun
+ * reactively only based on those dependencies, and not on the
+ * query itself.
+ *
+ * @param runQueryRef - a reference to or getter for a live query
+ * @param runQueryDependencies - Optional list of dependencies that
+ *    will cause the query to rerun - if provided, the the reactivity
+ *    of the query reference itself will not cause it to rerun
  *
  * @example Using a simple live query with a dependency. The table will depend on your application
  * ```ts
@@ -57,28 +69,48 @@ function useLiveQuery<Res>(
  * ```
  */
 function useLiveQuery<Res>(
-  runQueryRef: Ref<LiveResultContext<Res>>
+  runQueryRef: Ref<LiveResultContext<Res>> | (() => LiveResultContext<Res>),
+  runQueryDependencies?: WatchSource[]
 ): ToRefs<DeepReadonly<LiveResultUpdate<Res>>>
 
 function useLiveQuery<Res>(
-  runQueryOrRef: LiveResultContext<Res> | Ref<LiveResultContext<Res>>
-) {
-  if (typeof runQueryOrRef === 'function') {
-    return useLiveQueryWithQueryHash(runQueryOrRef as LiveResultContext<Res>)
+  runQuery:
+    | LiveResultContext<Res>
+    | Ref<LiveResultContext<Res>>
+    | (() => LiveResultContext<Res>),
+  runQueryDependencies?: WatchSource[]
+): ToRefs<DeepReadonly<LiveResultUpdate<Res>>> {
+  if ('sourceQuery' in runQuery) {
+    return useLiveQueryWithRef(shallowRef(runQuery), runQueryDependencies)
   }
 
-  const runQueryRef = runQueryOrRef as Ref<LiveResultContext<Res>>
-  return useLiveQueryWithQueryUpdates(runQueryRef.value, [runQueryRef])
+  if (isRef(runQuery)) {
+    return useLiveQueryWithRef(runQuery, runQueryDependencies)
+  }
+
+  return useLiveQueryWithRef(
+    computed(runQuery as () => LiveResultContext<Res>),
+    runQueryDependencies
+  )
 }
 
-function useLiveQueryWithQueryHash<Res>(runQuery: LiveResultContext<Res>) {
-  const queryHash = computed(() => hash(runQuery.sourceQuery))
+function useLiveQueryWithRef<Res>(
+  runQueryRef: Ref<LiveResultContext<Res>>,
+  runQueryDependencies?: WatchSource[]
+): ToRefs<DeepReadonly<LiveResultUpdate<Res>>> {
+  // if dependencies are specified, only rerun using those
+  if (runQueryDependencies) {
+    return useLiveQueryWithQueryUpdates(runQueryRef, runQueryDependencies)
+  }
 
-  return useLiveQueryWithQueryUpdates(runQuery, [queryHash])
+  // ensure that we re-subscribe to the query if the hash changes, regardless
+  // of how the reference is structured
+  const queryHash = computed(() => hash(unref(runQueryRef).sourceQuery))
+  return useLiveQueryWithQueryUpdates(runQueryRef, [runQueryRef, queryHash])
 }
 
 function useLiveQueryWithQueryUpdates<Res>(
-  runQuery: LiveResultContext<Res>,
+  runQuery: Ref<LiveResultContext<Res>>,
   runQueryDependencies: WatchSource[]
 ): ToRefs<DeepReadonly<LiveResultUpdate<Res>>> {
   const liveUpdate = shallowReactive<LiveResultUpdate<Res>>({
@@ -87,13 +119,14 @@ function useLiveQueryWithQueryUpdates<Res>(
     updatedAt: undefined,
   })
 
-  const unsubscribeRef = ref<UnsubscribeFunction>()
+  // keep track of subscriptions and unsubscribe from unused ones
+  const unsubscribeRef = shallowRef<UnsubscribeFunction>()
 
   watch(
     runQueryDependencies,
     () => {
       unsubscribeRef.value?.()
-      unsubscribeRef.value = runQuery.subscribe((newResults) => {
+      unsubscribeRef.value = unref(runQuery).subscribe((newResults) => {
         liveUpdate.results = newResults.results
         liveUpdate.error = newResults.error
         liveUpdate.updatedAt = newResults.updatedAt
