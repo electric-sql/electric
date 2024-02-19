51e1a5f1bab2ce36d8d1c3df7b4d651b0692b394
Oleksii Sholik <oleksii@sholik.dev>
2024-02-19 16:26:46 +0200

    Deprecate PgColumnType.sqlite_type
    sqlite_type is not needed since the client can always use
    pg_type which is the source of truth for the actual column type

--- clients/typescript/src/satellite/process.ts
+++ clients/typescript/src/satellite/process.ts
@@ -1650,13 +1650,7 @@ export function generateTriggersForTable(tbl: MigrationTable): Statement[] {
       }
     }),
     columnTypes: Object.fromEntries(
-      tbl.columns.map((col) => [
-        col.name,
-        {
-          sqliteType: col.sqliteType.toUpperCase(),
-          pgType: col.pgType!.name.toUpperCase(),
-        },
-      ])
+      tbl.columns.map((col) => [col.name, col.pgType!.name.toUpperCase()])
     ),
   }
   const fullTableName = table.namespace + '.' + table.tableName
