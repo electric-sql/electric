14f60bd7aa7759b1a4e5c46383ad54405c557285
msfstef <stefanos@electric-sql.com>
2024-02-19 17:14:52 +0200

    Extract chart data logic

---
+++ examples/recipes/src/data_viewer/use_chart_data.ts
@@ -0,0 +1,85 @@
+import { useLiveQuery } from 'electric-sql/react'
+import { useElectric } from '../electric/ElectricWrapper'
+import { useMemo } from 'react'
+
+type Chartable = string | number | boolean
+interface PropertyValue<T extends Chartable> {
+  property: T
+  value: number
+}
+
+interface MonthlyPropertyValue<T extends Chartable> extends PropertyValue<T> {
+  month: string
+}
+
+export const useChartData = <T extends Chartable>({
+  propertyToChart,
+  whereClause = '1=1',
+  maxDistinctPropertyValues = 10,
+}: {
+  propertyToChart: T
+  whereClause?: string
+  maxDistinctPropertyValues?: number
+}) => {
+  const { db } = useElectric()!
+
+  // Find the top values for the given query and select a few to display
+  const { results: topValues = [] } = useLiveQuery<PropertyValue<T>[]>(
+    db.liveRawQuery({
+      sql: `
+      SELECT ${propertyToChart} as property, COUNT(${propertyToChart}) as value
+      FROM commerce_orders
+      WHERE ${whereClause}
+      GROUP BY property
+      ORDER BY value DESC
+      LIMIT ${maxDistinctPropertyValues}
+    `,
+    }),
+  )
+  const propertyLabels = useMemo(
+    () => topValues.map((r) => r.property.toString()).filter((p) => p !== null),
+    [topValues],
+  )
+
+  // Get the aggregated number of orders, grouped by the given property, for the top keys
+  const { results: aggregatedValues = [] } = useLiveQuery<MonthlyPropertyValue<T>[]>(
+    db.liveRawQuery({
+      sql: `
+      SELECT
+        strftime('%Y-%m', timestamp) AS month,
+        ${propertyToChart} as property,
+        COUNT(${propertyToChart}) as value
+      FROM commerce_orders
+      WHERE ${whereClause}
+      GROUP BY month, property
+      ORDER BY month ASC, value DESC
+    `,
+    }),
+  )
+
+  // Convert to appropriate format to show on the chart
+  const dataset = useMemo(
+    () =>
+      Object.values(
+        aggregatedValues.reduce(
+          (aggregated, row) => ({
+            ...aggregated,
+            [row.month]: {
+              ...(aggregated[row.month] ?? {
+                month: new Date(row.month),
+                ...propertyLabels.reduce((agg, key) => ({ ...agg, [key]: 0 }), {}),
+              }),
+              [row.property.toString()]: row.value,
+            },
+          }),
+          {} as Record<string, Record<string, number>>,
+        ),
+      ),
+    [aggregatedValues, propertyLabels],
+  )
+
+  return {
+    dataset,
+    propertyLabels,
+  }
+}
