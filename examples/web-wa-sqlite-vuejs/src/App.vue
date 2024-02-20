2f207ac15dfad2db6cdf5c114ff19117435d4397
msfstef <stefanos@electric-sql.com>
2024-02-20 16:26:28 +0200

    Convert to more idiomatic SFC Vue components

--- examples/web-wa-sqlite-vuejs/src/App.vue
+++ examples/web-wa-sqlite-vuejs/src/App.vue
@@ -1,33 +1,13 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
-import logo from './assets/logo.svg';
-import './App.css';
-import './style.css';
-import ItemsExample from './ItemsExample.vue';
+<script lang="ts" setup>
+import logo from './assets/logo.svg'
+import ItemsExample from './ItemsExample.vue'
 import ElectricProvider from './ElectricProvider.vue'
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    ElectricProvider,
-    ItemsExample,
-  },
-  data() {
-    return {
-      logo: logo,
-    };
-  },
-});
 </script>
 
 <template>
   <div class="App">
     <header class="App-header">
-      <img
-        :src="logo"
-        class="App-logo"
-        alt="logo"
-      >
+      <img :src="logo" class="App-logo" alt="logo" />
       <ElectricProvider>
         <ItemsExample />
       </ElectricProvider>
@@ -35,4 +15,30 @@ export default defineComponent({
   </div>
 </template>
 
-<style src="./App.css" />
+<style>
+.App {
+  text-align: center;
+}
+
+.App-logo {
+  height: min(160px, 30vmin);
+  pointer-events: none;
+  margin-top: min(30px, 5vmin);
+  margin-bottom: min(30px, 5vmin);
+}
+
+.App-header {
+  background-color: #1c1e20;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: top;
+  justify-content: top;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+.App-link {
+  color: #61dafb;
+}
+</style>
--- examples/web-wa-sqlite-vuejs/src/ElectricProvider.vue
+++ examples/web-wa-sqlite-vuejs/src/ElectricProvider.vue
@@ -1,52 +1,42 @@
-<script lang="ts">
-import { computed, defineComponent, onMounted, shallowRef } from 'vue';
-import { LIB_VERSION } from 'electric-sql/version';
-import { uniqueTabId } from 'electric-sql/util';
-import { ElectricDatabase, electrify } from 'electric-sql/wa-sqlite';
-import { authToken } from './auth';
+<script lang="ts" setup>
+import { onMounted, shallowRef } from 'vue'
+import { LIB_VERSION } from 'electric-sql/version'
+import { uniqueTabId } from 'electric-sql/util'
+import { ElectricDatabase, electrify } from 'electric-sql/wa-sqlite'
+import { authToken } from './auth'
 import { provideElectric } from './electric'
-import { Electric, schema } from './generated/client';
+import { Electric, schema } from './generated/client'
 
+const electric = shallowRef<Electric>()
 
-export default defineComponent({
-  setup() {
-    
-    const electricRef = shallowRef<Electric>()
-    const showChild = computed(() => electricRef.value !== undefined)
+onMounted(async () => {
+  const config = {
+    auth: {
+      token: authToken(),
+    },
+    debug: import.meta.env.DEV,
+    url: import.meta.env.ELECTRIC_SERVICE,
+  }
 
-    onMounted(async () => {
-      const config = {
-        auth: {
-          token: authToken(),
-        },
-        debug: import.meta.env.DEV,
-        url: import.meta.env.ELECTRIC_SERVICE,
-      };
+  const { tabId } = uniqueTabId()
+  const scopedDbName = `basic-${LIB_VERSION}-${tabId}.db`
 
-      const { tabId } = uniqueTabId();
-      const scopedDbName = `basic-${LIB_VERSION}-${tabId}.db`;
+  const conn = await ElectricDatabase.init(scopedDbName)
+  const client = await electrify(conn, schema, config)
 
-      const conn = await ElectricDatabase.init(scopedDbName);
-      const electric = await electrify(conn, schema, config);
+  // Resolves when the shape subscription has been established.
+  const shape = await client.db.items.sync()
 
-      // Resolves when the shape subscription has been established.
-      const shape = await electric.db.items.sync();
+  // Resolves when the data has been synced into the local database.
+  await shape.synced
+  electric.value = client
+})
 
-      // Resolves when the data has been synced into the local database.
-      await shape.synced;
-      electricRef.value = electric
-      
-    });
-
-    provideElectric(electricRef)
-    return { showChild };
-  },
-});
+provideElectric(electric)
 </script>
 
 <template>
-  <div v-if="showChild">
+  <div v-if="electric">
     <slot />
   </div>
 </template>
-
