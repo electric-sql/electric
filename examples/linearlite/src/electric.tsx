e0982c1c2506aef598b13a5fe4ab4cd2ba82f9db
Sam Willis <sam.willis@gmail.com>
2024-02-20 15:22:37 +0000

    chore(examples): Upgrade Linearlite to electric v0.9.1 (#902)

--- examples/linearlite/src/electric.tsx
+++ examples/linearlite/src/electric.tsx
@@ -9,14 +9,14 @@ export type { Issue } from './generated/client'
 export const { ElectricProvider, useElectric } = makeElectricContext<Electric>()
 
 const discriminator = 'linearlite'
-const distPath = '/'
 
 // import.meta.env is a special object that Vite provides for accessing
 // environment variables at build time and runtime.
 // They are replaced at build time with the actual values.
 // https://vitejs.dev/guide/env-and-mode.html
 const DEV_MODE = import.meta.env.DEV
-const ELECTRIC_URL = import.meta.env.ELECTRIC_URL
+const ELECTRIC_SERVICE =
+  import.meta.env.ELECTRIC_SERVICE || import.meta.env.ELECTRIC_URL
 const DEBUG_ENV = import.meta.env.DEBUG
 
 // We can override the debug mode with a query param: ?debug=true or ?debug=false
@@ -31,7 +31,7 @@ export let dbName: string
 
 export const initElectric = async () => {
   const { tabId } = uniqueTabId()
-  const electricUrl = ELECTRIC_URL ?? 'ws://localhost:5133'
+  const electricUrl = ELECTRIC_SERVICE ?? 'ws://localhost:5133'
   dbName = `${discriminator}-${LIB_VERSION}-${tabId}.db`
 
   const config = {
@@ -42,7 +42,7 @@ export const initElectric = async () => {
     debug: DEBUG,
   }
 
-  const conn = await ElectricDatabase.init(dbName, distPath)
+  const conn = await ElectricDatabase.init(dbName)
   if (DEBUG) {
     console.log('initElectric')
     console.log('dbName', dbName)
