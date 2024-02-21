3c6c9b8812570de69803a374480f4572adef43b9
Sam Willis <sam.willis@gmail.com>
2024-02-21 16:17:41 +0000

    Fix tauri-sqlite

--- examples/linearlite/src-tauri/src/main.rs
+++ examples/linearlite/src-tauri/src/main.rs
@@ -10,6 +10,7 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_sql::Builder::default().build())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
