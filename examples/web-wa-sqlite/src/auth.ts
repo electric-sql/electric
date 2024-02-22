a20026d476ae07cb9fa384a57ee0412a1eb5e874
msfstef <stefanos@electric-sql.com>
2024-02-22 10:24:53 +0200

    Store user id in session storage to stop HMR breaking

--- examples/web-wa-sqlite/src/auth.ts
+++ examples/web-wa-sqlite/src/auth.ts
@@ -1,14 +1,17 @@
 import { insecureAuthToken } from 'electric-sql/auth'
 import { genUUID } from 'electric-sql/util'
 
-// This is just a demo. In a real app, the user ID would
-// usually come from somewhere else :)
-const dummyUserId = genUUID()
-
 // Generate an insecure authentication JWT.
 // See https://electric-sql.com/docs/usage/auth for more details.
 export const authToken = () => {
-  const claims = {'sub': dummyUserId}
-
+  const subKey = '__electric_sub'
+  let sub = window.sessionStorage.getItem(subKey)
+  if (!sub) {
+    // This is just a demo. In a real app, the user ID would
+    // usually come from somewhere else :)
+    sub = genUUID()
+    window.sessionStorage.setItem(subKey, sub)
+  }
+  const claims = { sub }
   return insecureAuthToken(claims)
 }
