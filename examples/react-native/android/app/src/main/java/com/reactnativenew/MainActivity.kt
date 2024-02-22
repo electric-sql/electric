8942a27fbe23f19e83207f16b72dabb9f5f3ad0f
msfstef <stefanos@electric-sql.com>
2024-02-22 16:39:27 +0200

    Rename the app

--- examples/react-native/android/app/src/main/java/com/reactnativenew/MainActivity.kt
+++ examples/react-native/android/app/src/main/java/com/reactnativenew/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "reactNativeNew"
+  override fun getMainComponentName(): String = "react-native"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
