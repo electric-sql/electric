d25ce6597d003adcd33746340c369cc33d9e00b5
msfstef <stefanos@electric-sql.com>
2024-02-22 17:16:29 +0200

    Sort out remaining details for react native example

--- examples/react-native/index.js
+++ examples/react-native/index.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-import {AppRegistry} from 'ElectricSQLExample';
+import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
