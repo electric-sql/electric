5c3d698b3ec12485bb84509cd58fd7a34d5aa64c
msfstef <stefanos@electric-sql.com>
2024-02-26 18:17:22 +0200

    Remove date assertions on secure JWT token test

--- clients/typescript/test/auth/secure.test.ts
+++ clients/typescript/test/auth/secure.test.ts
@@ -1,35 +1,19 @@
 import test from 'ava'
 
 import { secureAuthToken } from '../../src/auth/secure'
-import { jwtDecode } from '../../src/auth/decode'
-
-const originalDateNow = Date.now
-
-test.afterEach(() => {
-  Date.now = originalDateNow
-})
+import { JwtPayload, jwtDecode } from '../../src/auth/decode'
 
 test('secureAuthToken generates expected JWT token', async (t) => {
-  const mockDate = new Date()
-  Date.now = () => mockDate.getTime()
-
   const token = await secureAuthToken({
     claims: { sub: 'test-user' },
     iss: 'test-issuer',
     key: 'test-key',
-    exp: '2h',
   })
-
-  const expectedIat = Math.floor(mockDate.getTime() / 1000)
-  const expectedExp = expectedIat + 2 * 60 * 60
 
   t.is(typeof token, 'string')
   t.regex(token, /^eyJh[\w-.]+$/)
-  t.deepEqual(jwtDecode(token), {
-    iat: expectedIat,
-    exp: expectedExp,
-    sub: 'test-user',
-    type: 'access',
-    iss: 'test-issuer',
-  })
+  const decodedToken = jwtDecode(token) as JwtPayload & { type: string }
+  t.is(decodedToken.sub, 'test-user')
+  t.is(decodedToken.iss, 'test-issuer')
+  t.is(decodedToken.type, 'access')
 })
