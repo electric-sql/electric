0a17a70465d6c0c2d018198aa114314d1ece5d08
msfstef <stefanos@electric-sql.com>
2024-03-04 17:06:10 +0200

    Specify condition naes for resolver

--- examples/expo/metro.config.js
+++ examples/expo/metro.config.js
@@ -5,10 +5,11 @@ const config = getDefaultConfig(__dirname)
 config.resolver = {
   ...config.resolver,
   unstable_enablePackageExports: true,
-  // default conditions for 'import' and 'require' can
+  // default condition names 'import' and 'require' can
   // accidentally require module that assume node
-  // libraries and APIs are present - leave empty
-  unstable_conditionNames: [],
+  // libraries and APIs are present - specify expo/react-native
+  // for info, see: https://metrobundler.dev/docs/package-exports/
+  unstable_conditionNames: ['expo', 'react-native'],
 }
 
 module.exports = config
