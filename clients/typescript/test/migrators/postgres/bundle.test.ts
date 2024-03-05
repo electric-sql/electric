e62a84ba06aea5ed7998b38e8d3c73835464e695
Kevin De Porre <kevin@electric-sql.com>
2024-03-05 11:02:08 +0100

    Refactored migrator bundle tests

--- clients/typescript/test/migrators/postgres/bundle.test.ts
+++ clients/typescript/test/migrators/postgres/bundle.test.ts
@@ -1,13 +1,16 @@
-import test from 'ava'
+import anyTest, { TestFn } from 'ava'
 
 import { DatabaseAdapter } from '../../../src/drivers/node-postgres'
 import { PgBundleMigrator as BundleMigrator } from '../../../src/migrators/bundle'
-import { makeStmtMigration } from '../../../src/migrators'
 
 import { randomValue } from '../../../src/util/random'
 
-import migrations from '../../support/migrations/pg-migrations.js'
 import { makePgDatabase } from '../../support/node-postgres'
+import { ContextType, bundleTests } from '../bundle.test'
+
+import migrations from '../../support/migrations/pg-migrations.js'
+
+const test = anyTest as TestFn<ContextType>
 
 let port = 5532
 test.beforeEach(async (t) => {
@@ -16,46 +19,17 @@ test.beforeEach(async (t) => {
   const adapter = new DatabaseAdapter(db)
 
   t.context = {
-    adapter,
     dbName,
-    stopPG: stop,
+    adapter,
+    migrations,
+    BundleMigrator,
+    stop,
   }
 })
 
 test.afterEach.always(async (t) => {
-  const { stopPG } = t.context as any
-  await stopPG()
+  const { stop } = t.context as ContextType
+  await stop()
 })
 
-test('run the bundle migrator', async (t) => {
-  const { adapter } = t.context as any
-
-  const migrator = new BundleMigrator(adapter, migrations)
-  t.is(await migrator.up(), 3)
-  t.is(await migrator.up(), 0)
-})
-
-test('applyIfNotAlready applies new migrations', async (t) => {
-  const { adapter } = t.context as any
-
-  const allButLastMigrations = migrations.slice(0, -1)
-  const lastMigration = makeStmtMigration(migrations[migrations.length - 1])
-
-  const migrator = new BundleMigrator(adapter, allButLastMigrations)
-  t.is(await migrator.up(), 2)
-
-  const wasApplied = await migrator.applyIfNotAlready(lastMigration)
-  t.assert(wasApplied)
-})
-
-test('applyIfNotAlready ignores already applied migrations', async (t) => {
-  const { adapter } = t.context as any
-
-  const migrator = new BundleMigrator(adapter, migrations)
-  t.is(await migrator.up(), 3)
-
-  const wasApplied = await migrator.applyIfNotAlready(
-    makeStmtMigration(migrations[0])
-  )
-  t.assert(!wasApplied)
-})
+bundleTests(test)
