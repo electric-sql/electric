e6212bc17fc3dfe4f709bc2853ff5f0aecbfa856
Kevin De Porre <kevin@electric-sql.com>
2024-03-07 14:03:42 +0100

    Unit tests for generic serial database adapter

---
+++ clients/typescript/src/drivers/generic/mock.ts
@@ -0,0 +1,33 @@
+import { SerialDatabaseAdapter } from '.'
+import { AnyDatabase } from '..'
+import { RunResult } from '../../electric/adapter'
+import { Row, Statement } from '../../util'
+
+export class MockDatabaseAdapter extends SerialDatabaseAdapter {
+  readonly db: AnyDatabase = undefined as unknown as AnyDatabase // not used
+
+  private expectRun: ((stmt: Statement) => Promise<RunResult>) | undefined
+  private expectQuery: ((stmt: Statement) => Promise<Row[]>) | undefined
+
+  constructor() {
+    super()
+  }
+
+  mockRun(check: (stmt: Statement) => Promise<RunResult>) {
+    this.expectRun = check
+  }
+
+  mockQuery(check: (stmt: Statement) => Promise<Row[]>) {
+    this.expectQuery = check
+  }
+
+  async _run(stmt: Statement): Promise<RunResult> {
+    return (
+      (await this.expectRun?.(stmt)) || Promise.resolve({ rowsAffected: 0 })
+    )
+  }
+
+  async _query(stmt: Statement): Promise<Row[]> {
+    return (await this.expectQuery?.(stmt)) || Promise.resolve([])
+  }
+}
