eebd03cfb89f74a8ca6afbdabf8e4c5af6204263
Kevin <kevin@electric-sql.com>
2024-03-07 15:07:49 +0100

    fix (client): fix generic serial database adapter to always release the lock (#1033)
    This PR fixes a bug with the serial database adapter that caused the
    lock not to be released. This could happen when `COMMIT` failed in the
    `finally` block (e.g. due to a FK constraint violation) which would
    cause an error to be thrown and execution would never reach the call to
    `release()`. We fixed this by moving the `COMMIT` inside the `try` block
    such that any exception thrown by commit is caught. If `COMMIT` fails we
    will `ROLLBACK` in the catch block and we will always release the lock
    in the `finally` block.

--- clients/typescript/src/drivers/generic/adapter.ts
+++ clients/typescript/src/drivers/generic/adapter.ts
@@ -57,7 +57,7 @@ abstract class DatabaseAdapter
       throw e
     }
 
-    return new Promise((resolve, reject) => {
+    return new Promise<T>((resolve, reject) => {
       const releaseAndReject = (err?: any) => {
         // if the tx is rolled back, release the lock and reject the promise
         release()
@@ -75,10 +75,20 @@ abstract class DatabaseAdapter
             resolve(res)
           })
           .catch((err) => {
-            release()
+            // Failed to commit
             reject(err)
           })
       })
+    }).catch((err) => {
+      // something went wrong
+      // let's roll back
+      return this._run({ sql: 'ROLLBACK' })
+        .then(() => {
+          throw err
+        }) // rethrow the error
+        .finally(() => {
+          release()
+        })
     })
   }
 
@@ -96,6 +106,10 @@ abstract class DatabaseAdapter
     return this.txMutex.runExclusive(() => {
       return this._query(stmt)
     })
+  }
+
+  get isLocked(): boolean {
+    return this.txMutex.isLocked()
   }
 }
 
@@ -131,26 +145,25 @@ export abstract class SerialDatabaseAdapter
   async runInTransaction(...statements: Statement[]): Promise<RunResult> {
     // Uses a mutex to ensure that transactions are not interleaved.
     const release = await this.txMutex.acquire()
-    let open = false
+    let transactionBegan = false
     let rowsAffected = 0
     try {
       await this._run({ sql: 'BEGIN' })
-      open = true
+      transactionBegan = true
       for (const stmt of statements) {
         const { rowsAffected: rowsModified } = await this._run(stmt)
         rowsAffected += rowsModified
       }
+      await this._run({ sql: 'COMMIT' })
       return {
         rowsAffected: rowsAffected,
       }
     } catch (error) {
-      await this._run({ sql: 'ROLLBACK' })
-      open = false
+      if (transactionBegan) {
+        await this._run({ sql: 'ROLLBACK' })
+      }
       throw error // rejects the promise with the reason for the rollback
     } finally {
-      if (open) {
-        await this._run({ sql: 'COMMIT' })
-      }
       release()
     }
   }
