018dc50fe5b5ae565db070139fad7cb78c0f2966
msfstef <stefanos@electric-sql.com>
2024-03-12 17:09:54 +0200

    Enable eslint only for source files

--- examples/react-native/.eslintrc.js
+++ examples/react-native/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  ignorePatterns: [
+    'src/generated',
+    'metro.config.js',
+    'jest.config.js',
+    'babel.config.js',
+  ],
 };
