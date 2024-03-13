c9a92445fafcd7d63198505792b6ce59e9d4408c
Oleksii Sholik <oleksii@sholik.dev>
2024-03-13 13:09:08 +0200

    Make SatelliteCollectorProducer origin-scoped

--- components/electric/test/electric/replication/satellite_collector_producer_test.exs
+++ components/electric/test/electric/replication/satellite_collector_producer_test.exs
@@ -5,83 +5,84 @@ defmodule Electric.Replication.SatelliteCollectorProducerTest do
   alias Electric.Replication.SatelliteCollectorProducer, as: Producer
   alias Electric.DummyConsumer
 
-  test "producer immediately fulfills the demand if txs are available" do
-    {:ok, pid} = Producer.start_link([])
-    assert :ok = Producer.store_incoming_transactions(pid, [tx(), tx(), tx()])
-
-    {:ok, _} = DummyConsumer.start_link(notify: self(), subscribe_to: [{pid, []}])
-
-    assert_receive {:dummy_consumer, ^pid, [_, _, _]}
+  setup do
+    {:ok, pid} = Producer.start_link(origin: "satellite-collector-producer-test")
+    %{producer: pid}
   end
 
-  test "producer allows specifying a starting point on reconnect" do
-    {:ok, pid} = Producer.start_link([])
-    assert :ok = Producer.store_incoming_transactions(pid, [tx(), tx(), tx()])
+  test "producer immediately fulfills the demand if txs are available", %{producer: producer} do
+    assert :ok = Producer.store_incoming_transactions(producer, [tx(), tx(), tx()])
+
+    {:ok, _} = DummyConsumer.start_link(notify: self(), subscribe_to: [{producer, []}])
+
+    assert_receive {:dummy_consumer, ^producer, [_, _, _]}
+  end
+
+  test "producer allows specifying a starting point on reconnect", %{producer: producer} do
+    assert :ok = Producer.store_incoming_transactions(producer, [tx(), tx(), tx()])
 
     {:ok, consumer} =
-      DummyConsumer.start_link(notify: self(), subscribe_to: [{pid, cancel: :temporary}])
+      DummyConsumer.start_link(notify: self(), subscribe_to: [{producer, cancel: :temporary}])
 
     # Get all the transactions
-    assert_receive {:dummy_consumer, ^pid, [_, {_, second}, {_, third}]}
+    assert_receive {:dummy_consumer, ^producer, [_, {_, second}, {_, third}]}
     GenServer.stop(consumer)
-    assert :ok = Producer.store_incoming_transactions(pid, [tx()])
+    assert :ok = Producer.store_incoming_transactions(producer, [tx()])
 
     # Tell the server that we want everything since second tx
     {:ok, _} =
-      DummyConsumer.start_link(notify: self(), subscribe_to: [{pid, starting_from: second}])
+      DummyConsumer.start_link(notify: self(), subscribe_to: [{producer, starting_from: second}])
 
     # Get the third and the fourth items
-    assert_receive {:dummy_consumer, ^pid, [{_, ^third}, _]}
+    assert_receive {:dummy_consumer, ^producer, [{_, ^third}, _]}
   end
 
-  test "producer correctly forwards new events" do
-    {:ok, pid} = Producer.start_link([])
-    assert :ok = Producer.store_incoming_transactions(pid, [tx(), tx(), tx()])
+  test "producer correctly forwards new events", %{producer: producer} do
+    assert :ok = Producer.store_incoming_transactions(producer, [tx(), tx(), tx()])
 
-    {:ok, _} = DummyConsumer.start_link(notify: self(), subscribe_to: [{pid, cancel: :temporary}])
+    {:ok, _} =
+      DummyConsumer.start_link(notify: self(), subscribe_to: [{producer, cancel: :temporary}])
 
     # Get all the transactions
-    assert_receive {:dummy_consumer, ^pid, [_, _, {_, third}]}
+    assert_receive {:dummy_consumer, ^producer, [_, _, {_, third}]}
 
-    assert :ok = Producer.store_incoming_transactions(pid, [tx(), tx()])
-    assert_receive {:dummy_consumer, ^pid, [{_, fourth}, _]}
+    assert :ok = Producer.store_incoming_transactions(producer, [tx(), tx()])
+    assert_receive {:dummy_consumer, ^producer, [{_, fourth}, _]}
 
     assert third < fourth
   end
 
-  test "producer allows notifying about persisted items to garbage-collect" do
-    {:ok, pid} = Producer.start_link([])
-    assert :ok = Producer.store_incoming_transactions(pid, [tx(), tx(), tx()])
+  test "producer allows notifying about persisted items to garbage-collect", %{producer: producer} do
+    assert :ok = Producer.store_incoming_transactions(producer, [tx(), tx(), tx()])
 
     {:ok, consumer} =
-      DummyConsumer.start_link(notify: self(), subscribe_to: [{pid, cancel: :temporary}])
+      DummyConsumer.start_link(notify: self(), subscribe_to: [{producer, cancel: :temporary}])
 
     # Get all the transactions
-    assert_receive {:dummy_consumer, ^pid, [_, {_, second}, {_, third}]}
+    assert_receive {:dummy_consumer, ^producer, [_, {_, second}, {_, third}]}
     GenServer.stop(consumer)
-    assert :ok = Producer.store_incoming_transactions(pid, [tx()])
+    assert :ok = Producer.store_incoming_transactions(producer, [tx()])
 
     # Notify for GC
-    send(pid, {:sent_all_up_to, third})
+    send(producer, {:sent_all_up_to, third})
 
     # Connection from second TX will not yield third because it has been garbage collected
     {:ok, _} =
-      DummyConsumer.start_link(notify: self(), subscribe_to: [{pid, starting_from: second}])
+      DummyConsumer.start_link(notify: self(), subscribe_to: [{producer, starting_from: second}])
 
-    assert_receive {:dummy_consumer, ^pid, [{_, fourth}]}
+    assert_receive {:dummy_consumer, ^producer, [{_, fourth}]}
     refute third == fourth
   end
 
-  test ~s'produce connection "in the future" starts from scratch' do
-    {:ok, pid} = Producer.start_link([])
-    assert :ok = Producer.store_incoming_transactions(pid, [tx(), tx(), tx()])
+  test ~s'produce connection "in the future" starts from scratch', %{producer: producer} do
+    assert :ok = Producer.store_incoming_transactions(producer, [tx(), tx(), tx()])
 
     # Arbitrary point in the future
     {:ok, _} =
-      DummyConsumer.start_link(notify: self(), subscribe_to: [{pid, starting_from: 10000}])
+      DummyConsumer.start_link(notify: self(), subscribe_to: [{producer, starting_from: 10000}])
 
     # Get all the transactions
-    assert_receive {:dummy_consumer, ^pid, [_, _, _]}
+    assert_receive {:dummy_consumer, ^producer, [_, _, _]}
   end
 
   defp tx() do
