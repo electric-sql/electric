4dee99c01e54d368922d984a98728c55facf3b58
Stefanos Mousafeiris <msfstef@gmail.com>
2024-03-13 15:07:10 +0200

    chore(examples): Update Ionic demo (#1045)
    Updates the ionic demo to use the latest version of Electric.
    
    The capacitor driver has a few issues, the iOS ones have been fixed
    which is why I'm opening this PR but Android still isn't working and I
    believe is because `RETURNING` statements are not working properly but I
    need to dig deeper.

--- examples/ionic-demo/src/App.tsx
+++ examples/ionic-demo/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Redirect, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react'
 import { IonReactRouter } from '@ionic/react-router'
 
