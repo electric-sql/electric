6cbc22eadf5e5ab6ce1e57a8bcc3aed95430c3f6
msfstef <stefanos@electric-sql.com>
2024-03-20 12:59:37 +0200

    Remove vite optimization rules for demo mode

--- examples/recipes/vite.config.ts
+++ examples/recipes/vite.config.ts
@@ -3,18 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
   envPrefix: 'ELECTRIC_',
   optimizeDeps: {
-    // disable optimization for demo mode to resolve issues
-    // when running in Web Containers
-    ...(mode === 'demo'
-      ? {
-          noDiscovery: true,
-          include: [],
-        }
-      : {}),
     exclude: ['wa-sqlite'],
   },
-}))
+})
