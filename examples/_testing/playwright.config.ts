22f92cb256f541a4dd50e6b453211912b803fa44
msfstef <msfstef@gmail.com>
2024-04-17 16:50:50 +0300

    Add documentation for the e2e tests

--- examples/_testing/playwright.config.ts
+++ examples/_testing/playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
+  /* Opt out of parallel tests to avoid crossing wires between tests */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'dot' : 'line',
