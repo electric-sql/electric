4cc61a60733c1affb903a50a982c7ea60fde1da6
msfstef <msfstef@gmail.com>
2024-04-17 16:33:59 +0300

    Add separate testing dir for example e2es

---
+++ examples/_testing/e2e/web/basic.spec.ts
@@ -0,0 +1,65 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('basic example', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+  })
+
+  test('has title and buttons', async ({ page }) => {
+    await expect(page).toHaveTitle(/Web Example/)
+    await expect(page.getByRole('button', { name: 'Add' })).toBeVisible()
+    await expect(page.getByRole('button', { name: 'Clear' })).toBeVisible()
+
+    // clear items for good measure
+    await page.getByRole('button', { name: 'Clear' }).click()
+  })
+
+  test('can add items', async ({ page }) => {
+    const addButton = page.getByRole('button', { name: 'Add' })
+    const items = page.getByRole('code')
+
+    // no items initially
+    await expect(await items.count()).toBe(0)
+
+    // possible to add an item
+    await addButton.click()
+    await page.waitForTimeout(300)
+    await expect(await items.count()).toBe(1)
+
+    // item should contain UUID (not be empty)
+    await expect(items.first()).toContainText(
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+    )
+
+    // possible to add more
+    await addButton.click()
+    await page.waitForTimeout(300)
+    await addButton.click()
+    await page.waitForTimeout(300)
+    await expect(await items.count()).toBe(3)
+
+    // added items persist reload
+    await page.reload()
+    await page.waitForTimeout(300)
+    await expect(await items.count()).toBe(3)
+  })
+
+  test('can clear items', async ({ page }) => {
+    const clearButton = page.getByRole('button', { name: 'Clear' })
+    const items = page.getByRole('code')
+
+    // should have some items
+    await page.waitForSelector('code')
+    await expect(await items.count()).toBeGreaterThan(0)
+
+    // should be able to clear them
+    await clearButton.click()
+    await page.waitForTimeout(300)
+    await expect(await items.count()).toBe(0)
+
+    // cleared items persist reload
+    await page.reload()
+    await page.waitForTimeout(300)
+    await expect(await items.count()).toBe(0)
+  })
+})
