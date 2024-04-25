5187e60bd963d3388f232a88560bcc3f54c66355
Kevin De Porre <kevin@electric-sql.com>
2024-04-25 12:37:17 +0200

    Modify GH workflow to disable embedded-postgres tests in CI

--- clients/typescript/ava.config.js
+++ clients/typescript/ava.config.js
@@ -1,5 +1,17 @@
 const [major, minor, _patch] = process.versions.node.split('.').map(Number)
-const testDialect = process.env.DIALECT
+
+// Developers can provide a `TEST_ONLY_DIALECT` value of `postgres`, `pglite`, or `sqlite`
+// to run the unit tests only for that dialect.
+// Developers can also provide a `DISABLE_DIALECT` value of `postgres`, `pglite`, or `sqlite`
+// to disable the unit tests for that dialect but run all others.
+const testOnlyDialect = process.env.TEST_ONLY_DIALECT
+const disableDialect = process.env.DISABLE_DIALECT
+
+if (testOnlyDialect && disableDialect) {
+  throw new Error(
+    'Cannot set both TEST_ONLY_DIALECT and DISABLE_DIALECT environment variables.'
+  )
+}
 
 let loaderArg
 if (
@@ -17,16 +29,28 @@ const ignorePostgres = ['!test/**/postgres/**']
 const ignorePglite = ['!test/**/pglite/**']
 const ignoreSqlite = ['!test/**/sqlite/**']
 
-if (testDialect === 'postgres') {
-  files.push(...ignorePglite, ...ignoreSqlite)
+switch (testOnlyDialect) {
+  case 'postgres':
+    files.push(...ignorePglite, ...ignoreSqlite)
+    break
+  case 'pglite':
+    files.push(...ignorePostgres, ...ignoreSqlite)
+    break
+  case 'sqlite':
+    files.push(...ignorePostgres, ...ignorePglite)
+    break
 }
 
-if (testDialect === 'pglite') {
-  files.push(...ignorePostgres, ...ignoreSqlite)
-}
-
-if (testDialect === 'sqlite') {
-  files.push(...ignorePostgres, ...ignorePglite)
+switch (disableDialect) {
+  case 'postgres':
+    files.push(...ignorePostgres)
+    break
+  case 'pglite':
+    files.push(...ignorePglite)
+    break
+  case 'sqlite':
+    files.push(...ignoreSqlite)
+    break
 }
 
 export default {
