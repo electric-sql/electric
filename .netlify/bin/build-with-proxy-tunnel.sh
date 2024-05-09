8675b61245bbedf469d6e78c395e3c3da0210b34
msfstef <msfstef@gmail.com>
2024-05-09 15:54:25 +0300

    Use `python` rather than `python3`

--- .netlify/bin/build-with-proxy-tunnel.sh
+++ .netlify/bin/build-with-proxy-tunnel.sh
@@ -16,7 +16,7 @@ tunnel_pid=$!
 
 proxy_test="import socket; import sys; sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM); result = sock.connect_ex((\"127.0.0.1\",${tunnel_port})); sock.close(); sys.exit(result)"
 
-while ! python3 -c "$proxy_test"; do
+while ! python -c "$proxy_test"; do
     sleep 0.5
 done
 
