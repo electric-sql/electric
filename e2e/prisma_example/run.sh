6c0df5b67ea54ef9e6115a75dd3e29c08eb7d85b
Garry Hill <garry@electric-sql.com>
2024-05-13 11:50:22 +0000

    more info in script

--- e2e/prisma_example/run.sh
+++ e2e/prisma_example/run.sh
@@ -2,4 +2,11 @@
 
 set -ex
 
-docker compose -f ${DOCKER_COMPOSE_FILE} run "$@"
+pwd
+
+if [ ! -f "${DOCKER_COMPOSE_FILE}" ]; then
+    echo "Compose file ${DOCKER_COMPOSE_FILE} does not exist"
+    exit 1
+fi
+
+docker compose -f "${DOCKER_COMPOSE_FILE}" run "$@"
