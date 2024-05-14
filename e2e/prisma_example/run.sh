a6c2563bafbad5c5295305fdb087705f63325cf5
Garry Hill <garry@electric-sql.com>
2024-05-14 13:50:17 +0100

    chore(e2e): Fix flaky E2E test (#1256)

--- e2e/prisma_example/run.sh
+++ e2e/prisma_example/run.sh
@@ -2,4 +2,9 @@
 
 set -ex
 
-docker compose -f ${DOCKER_COMPOSE_FILE} run $ARGS
+if [ ! -f "${DOCKER_COMPOSE_FILE}" ]; then
+    echo "Compose file ${DOCKER_COMPOSE_FILE} does not exist"
+    exit 1
+fi
+
+docker compose -f "${DOCKER_COMPOSE_FILE}" run --no-TTY "$@"
