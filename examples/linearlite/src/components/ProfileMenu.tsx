84bebd03abd1955445ccc40083b7b1b37aafbb03
msfstef <msfstef@gmail.com>
2024-05-15 10:50:08 +0300

    Fix more headlessui transition elements

--- examples/linearlite/src/components/ProfileMenu.tsx
+++ examples/linearlite/src/components/ProfileMenu.tsx
@@ -68,51 +68,52 @@ export default function ProfileMenu({
         leave="transition easy-in duration-75"
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
-        className={classes}
       >
-        <button
-          className="flex items-center h-8 px-3 hover:bg-gray-100"
-          onClick={newProject}
-        >
-          New Project
-        </button>
-        <button
-          className="flex items-center h-8 px-3 hover:bg-gray-100"
-          onClick={() => {
-            setShowAboutModal?.(true)
-            onDismiss?.()
-          }}
-        >
-          About
-        </button>
-        <a
-          href="https://www.electric-sql.com"
-          className="flex items-center h-8 px-3 hover:bg-gray-100"
-        >
-          Visit ElectricSQL
-        </a>
-        <a
-          href="https://www.electric-sql.com/docs"
-          className="flex items-center h-8 px-3 hover:bg-gray-100"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/electric-sql/electric/tree/main/examples/linearlite"
-          className="flex items-center h-8 px-3 hover:bg-gray-100"
-        >
-          GitHub
-        </a>
-        <div className="border-t flex items-center h-8 px-3">
-          <span className="text-gray-500 me-auto">
-            {connectivityStateDisplay}
-          </span>
-          <Toggle
-            value={connectivityConnected}
-            onChange={toggleConnectivityState}
-            activeClass="bg-green-500 hover:bg-green-700"
-            activeLabelClass="border-green-500"
-          />
+        <div className={classes}>
+          <button
+            className="flex items-center h-8 px-3 hover:bg-gray-100"
+            onClick={newProject}
+          >
+            New Project
+          </button>
+          <button
+            className="flex items-center h-8 px-3 hover:bg-gray-100"
+            onClick={() => {
+              setShowAboutModal?.(true)
+              onDismiss?.()
+            }}
+          >
+            About
+          </button>
+          <a
+            href="https://www.electric-sql.com"
+            className="flex items-center h-8 px-3 hover:bg-gray-100"
+          >
+            Visit ElectricSQL
+          </a>
+          <a
+            href="https://www.electric-sql.com/docs"
+            className="flex items-center h-8 px-3 hover:bg-gray-100"
+          >
+            Documentation
+          </a>
+          <a
+            href="https://github.com/electric-sql/electric/tree/main/examples/linearlite"
+            className="flex items-center h-8 px-3 hover:bg-gray-100"
+          >
+            GitHub
+          </a>
+          <div className="border-t flex items-center h-8 px-3">
+            <span className="text-gray-500 me-auto">
+              {connectivityStateDisplay}
+            </span>
+            <Toggle
+              value={connectivityConnected}
+              onChange={toggleConnectivityState}
+              activeClass="bg-green-500 hover:bg-green-700"
+              activeLabelClass="border-green-500"
+            />
+          </div>
         </div>
       </Transition>
     </div>
