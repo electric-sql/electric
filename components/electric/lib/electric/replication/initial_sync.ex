a431a22dc936b325bbf7fde44af5a704234b067d
Oleksii Sholik <oleksii@sholik.dev>
2024-05-20 15:06:31 +0300

    Cover initial shape subscription with OT spans

--- components/electric/lib/electric/replication/initial_sync.ex
+++ components/electric/lib/electric/replication/initial_sync.ex
@@ -8,6 +8,7 @@ defmodule Electric.Replication.InitialSync do
 
   alias Electric.Utils
   alias Electric.Telemetry.Metrics
+  alias Electric.Telemetry.OpenTelemetry
   alias Electric.Replication.Shapes
   alias Electric.Postgres.{CachedWal, Extension}
   alias Electric.Replication.Changes.{NewRecord, Transaction}
@@ -96,29 +97,36 @@ defmodule Electric.Replication.InitialSync do
                                                              {acc_graph, results, req_ids} ->
         start = System.monotonic_time()
 
-        case Shapes.ShapeRequest.query_initial_data(
-               request,
-               conn,
-               schema_version,
-               origin,
-               context
-             ) do
-          {:ok, data, graph} ->
-            Metrics.span_event(
-              span,
-              :shape_data,
-              %{duration: System.monotonic_time() - start},
-              %{shape_hash: request.hash}
-            )
+        request_attrs = Map.take(request, [:request_id, :source_table, :target_table])
+        span_attrs = Map.merge(request_attrs, %{subscription_id: subscription_id})
 
-            {:cont,
-             {Utils.merge_graph_edges(acc_graph, graph),
-              Map.merge(results, data, fn _, {change, v1}, {_, v2} -> {change, v1 ++ v2} end),
-              [request.id | req_ids]}}
+        OpenTelemetry.with_span("initial_sync.query_subscription_data", span_attrs, fn ->
+          Shapes.ShapeRequest.query_initial_data(request, conn, schema_version, origin, context)
+          |> case do
+            {:ok, data, graph} ->
+              Metrics.span_event(
+                span,
+                :shape_data,
+                %{duration: System.monotonic_time() - start},
+                %{shape_hash: request.hash}
+              )
 
-          {:error, reason} ->
-            {:halt, {:error, reason}}
-        end
+              graph_attrs = graph |> Graph.info() |> Map.new(fn {k, v} -> {"graph.#{k}", v} end)
+
+              OpenTelemetry.add_span_attributes(
+                Map.merge(graph_attrs, %{num_rows: map_size(data)})
+              )
+
+              {:cont,
+               {Utils.merge_graph_edges(acc_graph, graph),
+                Map.merge(results, data, fn _, {change, v1}, {_, v2} -> {change, v1 ++ v2} end),
+                [request.id | req_ids]}}
+
+            {:error, reason} ->
+              OpenTelemetry.record_exception(inspect(reason))
+              {:halt, {:error, reason}}
+          end
+        end)
       end)
       |> case do
         {:error, reason} ->
@@ -189,7 +197,12 @@ defmodule Electric.Replication.InitialSync do
 
           xmin = String.to_integer(xmin_str)
           send(parent, {:data_insertion_point, ref, xmin})
-          fun.(conn, xmin)
+
+          OpenTelemetry.with_span(
+            "initial_sync.readonly_txn_with_checkpoint",
+            [marker: marker],
+            fn -> fun.(conn, xmin) end
+          )
         end
       )
     end)
