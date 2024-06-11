0210b9196923be961fec99672ce48df51768ad3a
msfstef <msfstef@gmail.com>
2024-06-11 13:46:34 +0300

    Unify add and clear items tests

--- examples/_testing/e2e/web/basic.spec.ts
+++ examples/_testing/e2e/web/basic.spec.ts
@@ -6,19 +6,23 @@ const INTERACITON_TIME_MS = 300
 test.describe('basic example', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
+    await page.waitForTimeout(PAGE_LOAD_TIME_MS)
   })
 
   test('has title and buttons', async ({ page }) => {
+    const clearButton = page.getByRole('button', { name: 'Clear' })
+
     await expect(page).toHaveTitle(/Web Example/)
     await expect(page.getByRole('button', { name: 'Add' })).toBeVisible()
-    await expect(page.getByRole('button', { name: 'Clear' })).toBeVisible()
+    await expect(clearButton).toBeVisible()
 
     // clear items for good measure
-    await page.getByRole('button', { name: 'Clear' }).click()
+    await clearButton.click()
   })
 
-  test('can add items', async ({ page }) => {
+  test('can add and clear items', async ({ page }) => {
     const addButton = page.getByRole('button', { name: 'Add' })
+    const clearButton = page.getByRole('button', { name: 'Clear' })
     const items = page.getByRole('code')
 
     // no items initially
@@ -44,17 +48,8 @@ test.describe('basic example', () => {
     // added items persist reload
     await page.reload()
     await page.waitForTimeout(PAGE_LOAD_TIME_MS)
-    await expect(page.getByRole('button', { name: 'Clear' })).toBeVisible()
+    await expect(clearButton).toBeVisible()
     await expect(await items.count()).toBe(3)
-  })
-
-  test('can clear items', async ({ page }) => {
-    const clearButton = page.getByRole('button', { name: 'Clear' })
-    const items = page.getByRole('code')
-
-    // should have some items
-    await page.waitForSelector('code')
-    await expect(await items.count()).toBeGreaterThan(0)
 
     // should be able to clear them
     await clearButton.click()
@@ -63,7 +58,8 @@ test.describe('basic example', () => {
 
     // cleared items persist reload
     await page.reload()
-    await page.waitForTimeout(INTERACITON_TIME_MS)
+    await page.waitForTimeout(PAGE_LOAD_TIME_MS)
+    await expect(clearButton).toBeVisible()
     await expect(await items.count()).toBe(0)
   })
 })
