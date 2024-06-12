81b1d001342ad0fbef344313c1c6c57aa3926eb0
Stefanos Mousafeiris <msfstef@gmail.com>
2024-06-12 14:12:09 +0300

    chore(examples): Update examples to 0.12 (#1322)
    Update most examples to electric 0.12 and bump starter CLI to release
    version with the new templates

---
+++ .changeset/shy-tigers-repair.md
@@ -0,0 +1,5 @@
+---
+"create-electric-app": patch
+---
+
+Update starter templates to use `electric-sql` version `0.12.x`
--- examples/_testing/e2e/web/basic.spec.ts
+++ examples/_testing/e2e/web/basic.spec.ts
@@ -1,21 +1,28 @@
 import { test, expect } from '@playwright/test'
 
+const PAGE_LOAD_TIME_MS = 2000
+const INTERACTION_TIME_MS = 300
+
 test.describe('basic example', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
+    await page.waitForTimeout(PAGE_LOAD_TIME_MS)
   })
 
   test('has title and buttons', async ({ page }) => {
+    const clearButton = page.getByRole('button', { name: 'Clear' })
+
     await expect(page).toHaveTitle(/Web Example/)
     await expect(page.getByRole('button', { name: 'Add' })).toBeVisible()
-    await expect(page.getByRole('button', { name: 'Clear' })).toBeVisible()
+    await expect(clearButton).toBeVisible()
 
     // clear items for good measure
-    await page.getByRole('button', { name: 'Clear' }).click()
+    await clearButton.click()
   })
 
-  test('can add items', async ({ page }) => {
+  test('can add and clear items', async ({ page }) => {
     const addButton = page.getByRole('button', { name: 'Add' })
+    const clearButton = page.getByRole('button', { name: 'Clear' })
     const items = page.getByRole('code')
 
     // no items initially
@@ -23,7 +30,7 @@ test.describe('basic example', () => {
 
     // possible to add an item
     await addButton.click()
-    await page.waitForTimeout(300)
+    await page.waitForTimeout(INTERACTION_TIME_MS)
     await expect(await items.count()).toBe(1)
 
     // item should contain UUID (not be empty)
@@ -33,33 +40,26 @@ test.describe('basic example', () => {
 
     // possible to add more
     await addButton.click()
-    await page.waitForTimeout(300)
+    await page.waitForTimeout(INTERACTION_TIME_MS)
     await addButton.click()
-    await page.waitForTimeout(300)
+    await page.waitForTimeout(INTERACTION_TIME_MS)
     await expect(await items.count()).toBe(3)
 
     // added items persist reload
     await page.reload()
-    await page.waitForTimeout(1000)
+    await page.waitForTimeout(PAGE_LOAD_TIME_MS)
+    await expect(clearButton).toBeVisible()
     await expect(await items.count()).toBe(3)
-  })
-
-  test('can clear items', async ({ page }) => {
-    const clearButton = page.getByRole('button', { name: 'Clear' })
-    const items = page.getByRole('code')
-
-    // should have some items
-    await page.waitForSelector('code')
-    await expect(await items.count()).toBeGreaterThan(0)
 
     // should be able to clear them
     await clearButton.click()
-    await page.waitForTimeout(300)
+    await page.waitForTimeout(INTERACTION_TIME_MS)
     await expect(await items.count()).toBe(0)
 
     // cleared items persist reload
     await page.reload()
-    await page.waitForTimeout(300)
+    await page.waitForTimeout(PAGE_LOAD_TIME_MS)
+    await expect(clearButton).toBeVisible()
     await expect(await items.count()).toBe(0)
   })
 })
