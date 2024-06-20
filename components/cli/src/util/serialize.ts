1775eda591b56f5788dac25085be6f78a13834d8
Kevin De Porre <kevin@electric-sql.com>
2024-06-20 11:04:49 +0200

    Add comment explaining custom serialization function for DB description.

--- components/cli/src/util/serialize.ts
+++ components/cli/src/util/serialize.ts
@@ -1,6 +1,13 @@
 import { dedent } from 'ts-dedent'
 import { DbSchema } from 'electric-sql/client'
 
+/**
+ * Custom serialization function that serializes the DB description
+ * into source code that is meant to be bundled in Electric applications.
+ * The generated string is NOT in JSON format but is actual JS source code
+ * (as it instantiates `Relation` objects)
+ * that is meant to be imported by the Electric application.
+ */
 export function serializeDbDescription(dbDescription: DbSchema) {
   const tables = Object.entries(dbDescription)
     .map(([table, schema]) => {
