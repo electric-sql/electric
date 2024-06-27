0f11eb23a00b8dcc6b78a5f9bc8651bd2ecc44c5
Oleksii Sholik <oleksii@sholik.dev>
2024-06-27 18:10:09 +0300

    fix(electric): Skip loading cacerts (#1407)
    Merging https://github.com/electric-sql/electric/pull/1370 into `main`
    inadvertently broke the fix that had been introduced in
    https://github.com/electric-sql/electric/pull/1396. That latter PR's
    description goes into detail about why we are not ready to validate
    server certificates.

--- components/electric/lib/electric/replication/postgres_manager.ex
+++ components/electric/lib/electric/replication/postgres_manager.ex
@@ -389,22 +389,31 @@ defmodule Electric.Replication.PostgresConnectorMng do
   #
   # As explained in https://github.com/erlang/otp/issues/8604, the function spec of
   # `:public_key.cacerts_load()` is incorrect.
-  @dialyzer {:nowarn_function, load_cacerts: 0}
+  #
+  # ssl_verify_opts also needs to have its warnings ignored due to the hacky nature of below
+  # code.
+  @dialyzer {:nowarn_function, load_cacerts: 0, ssl_verify_opts: 0}
 
-  defp load_cacerts do
-    case :public_key.cacerts_load() do
-      :ok ->
-        cacerts = :public_key.cacerts_get()
-        Logger.info("Successfully loaded #{length(cacerts)} cacerts from the OS")
-        {:ok, cacerts}
+  defp load_cacerts, do: :error
 
-      {:error, reason} ->
-        Logger.warning("Failed to load cacerts from the OS: #{inspect(reason)}")
-        :error
+  # Skip loading cacerts because managed database providers tend to have certificate issues
+  # that we haven't yet decided how to deal with.
+  if false do
+    defp load_cacerts do
+      case :public_key.cacerts_load() do
+        :ok ->
+          cacerts = :public_key.cacerts_get()
+          Logger.info("Successfully loaded #{length(cacerts)} cacerts from the OS")
+          {:ok, cacerts}
 
-      :undefined ->
-        Logger.warning("Failed to load cacerts from the OS.")
-        :error
+        {:error, reason} ->
+          Logger.warning("Failed to load cacerts from the OS: #{inspect(reason)}")
+          :error
+
+        :undefined ->
+          Logger.warning("Failed to load cacerts from the OS.")
+          :error
+      end
     end
   end
 
