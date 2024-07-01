dc1196155b5de516a9801145ae4b6071a0719f98
Kyle Mathews <mathews.kyle@gmail.com>
2024-07-01 11:54:16 -0600

    Fix typescript type

--- types.ts
+++ types.ts
@@ -1,11 +1,18 @@
-type Header = {
-  key: string
-  value: any
+type JsonSerializable =
+  | string
+  | number
+  | boolean
+  | null
+  | JsonSerializable[]
+  | { [key: string]: JsonSerializable }
+
+interface Header {
+  [key: string]: JsonSerializable
 }
 
 // Define the type for a record
 export type Message = {
-  key?: any
+  key?: string
   value?: any
   headers?: Header
   offset?: number
