8b7b1121ce06c01cb4be794767b59928b2bfe240
Ilia Borovitinov <ilia@electric-sql.com>
2024-07-02 08:43:59 +0300

    chore(service): fixed file locations & formatting

--- sync_service/.formatter.exs
+++ sync_service/.formatter.exs
@@ -1,4 +1,5 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  import_deps: [:plug]
 ]
