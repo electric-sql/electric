8a826aa3d41a22f6671831be905ec4e4f2ebd52c
Kyle Mathews <mathews.kyle@gmail.com>
2024-07-09 14:51:53 -0600

    Also filter by status

--- examples/linearlite/src/pages/List/index.tsx
+++ examples/linearlite/src/pages/List/index.tsx
@@ -13,11 +13,13 @@ function List({ showSearch = false }) {
     baseUrl,
   })! as Issue[]
 
-  console.time(`filter issues`)
   const filteredIssues = issues.filter((issue) => {
     const tests = [true]
     if (filterState.priority && filterState.priority.length > 0) {
       tests.push(filterState.priority.includes(issue.priority))
+    }
+    if (filterState.status && filterState.status.length > 0) {
+      tests.push(filterState.status.includes(issue.status))
     }
 
     if (typeof filterState.query !== `undefined`) {
@@ -27,7 +29,6 @@ function List({ showSearch = false }) {
     // Return true only if all tests are true
     return tests.every((test) => test)
   })
-  console.timeEnd(`filter issues`)
 
   return (
     <div className="flex flex-col flex-grow">
