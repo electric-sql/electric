fedf95c102afd8cbe4f67ef6c433e59511a5d48e
Kyle Mathews <mathews.kyle@gmail.com>
2024-07-24 06:18:59 -0600

    fix: make packaging work in Remix, etc. (#169)
    I started working on a Remix/ElectricSQL-next starter today and...
    quickly ground to a halt because it wouldn't import our packages. 4-5
    hours I _think_ I have it working.
    
    Most useful ultimately was this blog post —
    https://blog.isquaredsoftware.com/2023/08/esm-modernization-lessons/ &
    just copying what Redux has done ([tsup
    config](https://github.com/reduxjs/redux/blob/master/tsup.config.ts),
    [package.json](https://github.com/reduxjs/redux/blob/master/package.json)).
    
    Doing these changes locally got everything working w/o a hitch in Remix.

--- packages/typescript-client/tsup.config.ts
+++ packages/typescript-client/tsup.config.ts
@@ -1,18 +1,51 @@
-import { globSync } from 'glob'
+import type { Options } from 'tsup'
 import { defineConfig } from 'tsup'
 
-const entries = globSync('src/**/*.{ts,tsx,js,jsx}', { posix: true })
+export default defineConfig(options => {
+  const commonOptions: Partial<Options> = {
+    entry: {
+      index: 'src/index.ts'
+    },
+    sourcemap: true,
+    ...options
+  }
 
-export default defineConfig({
-  entry: entries,
-  format: ['esm'],
-  bundle: false,
-  splitting: false,
-  sourcemap: true,
-  outExtension({ format }) {
-    return {
-      js: format === 'cjs' ? '.cjs' : '.js',
+  return [
+    // Standard ESM, embedded `process.env.NODE_ENV` checks
+    {
+      ...commonOptions,
+      format: ['esm'],
+      outExtension: () => ({ js: '.mjs' }), // Add dts: '.d.ts' when egoist/tsup#1053 lands
+      dts: true,
+      clean: true
+    },
+    // Support Webpack 4 by pointing `"module"` to a file with a `.js` extension
+    {
+      ...commonOptions,
+      format: ['esm'],
+      target: 'es2017',
+      dts: false,
+      outExtension: () => ({ js: '.js' }),
+      entry: { 'index.legacy-esm': 'src/index.ts' }
+    },
+    // Browser-ready ESM, production + minified
+    {
+      ...commonOptions,
+      entry: {
+        'index.browser': 'src/index.ts'
+      },
+      define: {
+        'process.env.NODE_ENV': JSON.stringify('production')
+      },
+      format: ['esm'],
+      outExtension: () => ({ js: '.mjs' }),
+      minify: true
+    },
+    {
+      ...commonOptions,
+      format: 'cjs',
+      outDir: './dist/cjs/',
+      outExtension: () => ({ js: '.cjs' })
     }
-  },
-  tsconfig: 'tsconfig.build.json',
+  ]
 })
