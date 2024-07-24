ccc2606d55b36657ca143f53f0dda181ade91f8d
Ilia Borovitinov <ilia@electric-sql.com>
2024-07-24 13:21:29 +0300

    fix: cover truncated relation handling for where-claused shapes (#174)

---
+++ packages/sync-service/test/support/stub_inspector.ex
@@ -0,0 +1,22 @@
+defmodule Support.StubInspector do
+  @behaviour Electric.Postgres.Inspector
+
+  def stub_inspector(opts), do: {__MODULE__, opts}
+
+  @impl true
+  def load_column_info(_relation, column_list) when is_list(column_list) do
+    column_list
+    |> Enum.map(fn column ->
+      column
+      |> Map.put_new(:pk_position, nil)
+      |> Map.put_new(:type, "text")
+    end)
+    |> then(&{:ok, &1})
+  end
+
+  def load_column_info(relation, opts) when is_map(opts) and is_map_key(opts, relation) do
+    opts
+    |> Map.fetch!(relation)
+    |> then(&load_column_info(relation, &1))
+  end
+end
