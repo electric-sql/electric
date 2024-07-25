9bf2c1bb0f5fdba7b9c6b303f8eae1655057e11a
Oleksii Sholik <oleksii@sholik.dev>
2024-07-25 11:17:48 +0300

    fix: Replace :unchanged_toast placeholders with actual column values in logical message decoding (#180)
    Closes #171.

--- packages/sync-service/test/electric/postgres/replication_client_test.exs
+++ packages/sync-service/test/electric/postgres/replication_client_test.exs
@@ -1,17 +1,20 @@
 defmodule Electric.Postgres.ReplicationClientTest do
-  alias Electric.Postgres.Lsn
-  alias Electric.Replication.Changes.NewRecord
-  alias Electric.Replication.Changes.Transaction
-  alias Electric.Postgres.ReplicationClient
   use ExUnit.Case, async: true
+
+  alias Electric.Postgres.Lsn
+  alias Electric.Postgres.ReplicationClient
+
+  alias Electric.Replication.Changes.{
+    DeletedRecord,
+    NewRecord,
+    Transaction,
+    UpdatedRecord
+  }
 
   @moduletag :capture_log
   @publication_name "test_electric_publication"
 
-  def create_publication_for_all_tables(conn),
-    do: Postgrex.query!(conn, "CREATE PUBLICATION #{@publication_name} FOR ALL TABLES", [])
-
-  describe "ReplicationClient against real db" do
+  describe "ReplicationClient init" do
     setup {Support.DbSetup, :with_unique_db}
     setup {Support.DbStructureSetup, :with_basic_tables}
 
@@ -19,15 +22,35 @@ defmodule Electric.Postgres.ReplicationClientTest do
       %{
         init_opts: [
           publication_name: @publication_name,
-          transaction_received: {__MODULE__, :test_transaction_received, [self()]},
-          try_creating_publication?: false
+          transaction_received: {__MODULE__, :test_transaction_received, [self()]}
         ]
       }
     end
 
+    test "creates an empty publication on startup if requested", %{
+      db_config: config,
+      init_opts: init_opts,
+      db_conn: conn
+    } do
+      init_opts = Keyword.put(init_opts, :try_creating_publication?, true)
+      assert {:ok, _} = ReplicationClient.start_link(config ++ [init_opts: init_opts])
+
+      assert %{rows: [[@publication_name]]} =
+               Postgrex.query!(conn, "SELECT pubname FROM pg_publication", [])
+
+      assert %{rows: []} = Postgrex.query!(conn, "SELECT pubname FROM pg_publication_tables", [])
+    end
+  end
+
+  describe "ReplicationClient against real db" do
+    setup [
+      {Support.DbSetup, :with_unique_db},
+      {Support.DbStructureSetup, :with_basic_tables},
+      :setup_publication_and_init_opts
+    ]
+
     test "calls a provided function when receiving it from the PG",
          %{db_config: config, init_opts: init_opts, db_conn: conn} do
-      create_publication_for_all_tables(conn)
       assert {:ok, _pid} = ReplicationClient.start_link(config ++ [init_opts: init_opts])
 
       {:ok, _} =
@@ -42,8 +65,6 @@ defmodule Electric.Postgres.ReplicationClientTest do
 
     test "logs a message when connected & replication has started",
          %{db_config: config, init_opts: init_opts, db_conn: conn} do
-      create_publication_for_all_tables(conn)
-
       log =
         ExUnit.CaptureLog.capture_log(fn ->
           assert {:ok, _pid} = ReplicationClient.start_link(config ++ [init_opts: init_opts])
@@ -61,29 +82,100 @@ defmodule Electric.Postgres.ReplicationClientTest do
       log =~ "Started replication from postgres"
     end
 
-    test "creates an empty publication on startup if requested", %{
-      db_config: config,
-      init_opts: init_opts,
-      db_conn: conn
-    } do
-      init_opts = Keyword.put(init_opts, :try_creating_publication?, true)
-      assert {:ok, _} = ReplicationClient.start_link(config ++ [init_opts: init_opts])
-
-      assert %{rows: [[@publication_name]]} =
-               Postgrex.query!(conn, "SELECT pubname FROM pg_publication", [])
-
-      assert %{rows: []} = Postgrex.query!(conn, "SELECT pubname FROM pg_publication_tables", [])
-    end
-
     test "doesn't fail to start when publicaiton already exists", %{
       db_config: config,
-      init_opts: init_opts,
-      db_conn: conn
+      init_opts: init_opts
     } do
       init_opts = Keyword.put(init_opts, :try_creating_publication?, true)
-      create_publication_for_all_tables(conn)
-
       assert {:ok, _} = ReplicationClient.start_link(config ++ [init_opts: init_opts])
+    end
+  end
+
+  describe "ReplicationClient against real db (toast)" do
+    setup [
+      {Support.DbSetup, :with_unique_db},
+      {Support.DbStructureSetup, :with_basic_tables},
+      :setup_publication_and_init_opts
+    ]
+
+    setup %{db_config: config, init_opts: init_opts, db_conn: conn} do
+      Postgrex.query!(
+        conn,
+        "CREATE TABLE items2 (id UUID PRIMARY KEY, val1 TEXT, val2 TEXT, num INTEGER)",
+        []
+      )
+
+      Postgrex.query!(conn, "ALTER TABLE items2 REPLICA IDENTITY FULL", [])
+
+      assert {:ok, _pid} = ReplicationClient.start_link(config ++ [init_opts: init_opts])
+
+      :ok
+    end
+
+    test "detoasts column values in deletes", %{db_conn: conn} do
+      id = Ecto.UUID.generate()
+      {:ok, bin_uuid} = Ecto.UUID.dump(id)
+      long_string_1 = gen_random_string(2500)
+      long_string_2 = gen_random_string(3000)
+
+      Postgrex.query!(conn, "INSERT INTO items2 (id, val1, val2) VALUES ($1, $2, $3)", [
+        bin_uuid,
+        long_string_1,
+        long_string_2
+      ])
+
+      assert_receive {:from_replication, %Transaction{changes: [change]}}
+
+      assert %NewRecord{
+               record: %{"id" => ^id, "val1" => ^long_string_1, "val2" => ^long_string_2},
+               relation: {"public", "items2"}
+             } = change
+
+      Postgrex.query!(conn, "DELETE FROM items2 WHERE id = $1", [bin_uuid])
+
+      assert_receive {:from_replication, %Transaction{changes: [change]}}
+
+      assert %DeletedRecord{
+               old_record: %{"id" => ^id, "val1" => ^long_string_1, "val2" => ^long_string_2},
+               relation: {"public", "items2"}
+             } = change
+    end
+
+    test "detoasts column values in updates", %{db_conn: conn} do
+      id = Ecto.UUID.generate()
+      {:ok, bin_uuid} = Ecto.UUID.dump(id)
+      long_string_1 = gen_random_string(2500)
+      long_string_2 = gen_random_string(3000)
+
+      Postgrex.query!(conn, "INSERT INTO items2 (id, val1, val2) VALUES ($1, $2, $3)", [
+        bin_uuid,
+        long_string_1,
+        long_string_2
+      ])
+
+      assert_receive {:from_replication, %Transaction{changes: [change]}}
+
+      assert %NewRecord{
+               record: %{"id" => ^id, "val1" => ^long_string_1, "val2" => ^long_string_2},
+               relation: {"public", "items2"}
+             } = change
+
+      Postgrex.query!(conn, "UPDATE items2 SET num = 11 WHERE id = $1", [bin_uuid])
+
+      assert_receive {:from_replication, %Transaction{changes: [change]}}
+
+      assert %UpdatedRecord{
+               record: %{
+                 "id" => ^id,
+                 "val1" => ^long_string_1,
+                 "val2" => ^long_string_2,
+                 "num" => "11"
+               },
+               changed_columns: changed_columns,
+               relation: {"public", "items2"}
+             } = change
+
+      assert MapSet.new(["num"]) == changed_columns
     end
   end
 
@@ -96,6 +188,27 @@ defmodule Electric.Postgres.ReplicationClientTest do
     assert Lsn.from_integer(wal) == Lsn.from_string("0/11")
   end
 
+  defp setup_publication_and_init_opts(%{db_conn: conn}) do
+    create_publication_for_all_tables(conn)
+
+    %{
+      init_opts: [
+        publication_name: @publication_name,
+        transaction_received: {__MODULE__, :test_transaction_received, [self()]},
+        try_creating_publication?: false
+      ]
+    }
+  end
+
   def test_transaction_received(transaction, test_pid),
     do: send(test_pid, {:from_replication, transaction})
+
+  defp create_publication_for_all_tables(conn),
+    do: Postgrex.query!(conn, "CREATE PUBLICATION #{@publication_name} FOR ALL TABLES", [])
+
+  defp gen_random_string(length) do
+    Stream.repeatedly(fn -> :rand.uniform(125 - 32) + 32 end)
+    |> Enum.take(length)
+    |> List.to_string()
+  end
 end
