4b6c4a6a460141587adac1a6806f1c0fb38cb8f9
Rob A'Court <robacourt@users.noreply.github.com>
2024-07-30 16:56:54 +0100

    fix: Fix 500 for two concurrent requests of the same shape that is not in cache (#214)
    As found by @kevin-dp here:
    https://github.com/electric-sql/electric-next/actions/runs/10096466484/job/27919065981#step:13:17

--- packages/sync-service/lib/electric/postgres/inspector/ets_inspector.ex
+++ packages/sync-service/lib/electric/postgres/inspector/ets_inspector.ex
@@ -16,12 +16,10 @@ defmodule Electric.Postgres.Inspector.EtsInspector do
       )
 
   @impl Electric.Postgres.Inspector
-  def load_column_info({namespace, tbl}, opts) do
-    ets_table = Access.get(opts, :pg_info_table, @default_pg_info_table)
-
-    case :ets.lookup_element(ets_table, {{namespace, tbl}, :columns}, 2, :not_found) do
+  def load_column_info({_namespace, _table_name} = table, opts) do
+    case column_info_from_ets(table, opts) do
       :not_found ->
-        case GenServer.call(opts[:server], {:load_column_info, {namespace, tbl}}) do
+        case GenServer.call(opts[:server], {:load_column_info, table}) do
           {:error, err, stacktrace} -> reraise err, stacktrace
           result -> result
         end
@@ -46,23 +44,30 @@ defmodule Electric.Postgres.Inspector.EtsInspector do
   end
 
   @impl GenServer
-  def handle_call({:load_column_info, {namespace, tbl}}, _from, state) do
-    case :ets.lookup(state.pg_info_table, {{namespace, tbl}, :columns}) do
-      [found] ->
-        {:reply, {:ok, found}, state}
-
-      [] ->
-        case DirectInspector.load_column_info({namespace, tbl}, state.pg_pool) do
+  def handle_call({:load_column_info, table}, _from, state) do
+    case column_info_from_ets(table, state) do
+      :not_found ->
+        case DirectInspector.load_column_info(table, state.pg_pool) do
           :table_not_found ->
             {:reply, :table_not_found, state}
 
           {:ok, info} ->
             # store
-            :ets.insert(state.pg_info_table, {{{namespace, tbl}, :columns}, info})
+            :ets.insert(state.pg_info_table, {{table, :columns}, info})
             {:reply, {:ok, info}, state}
         end
+
+      found ->
+        {:reply, {:ok, found}, state}
     end
   rescue
     e -> {:reply, {:error, e, __STACKTRACE__}, state}
+  end
+
+  @column_info_position 2
+  defp column_info_from_ets(table, opts_or_state) do
+    ets_table = Access.get(opts_or_state, :pg_info_table, @default_pg_info_table)
+
+    :ets.lookup_element(ets_table, {table, :columns}, @column_info_position, :not_found)
   end
 end
