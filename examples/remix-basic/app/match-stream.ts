ae2bb385b223abb8fefd59f91877dd4dbe7b909b
Kyle Mathews <mathews.kyle@gmail.com>
2024-08-05 07:00:17 -0600

    Make timeout 10 seconds

--- examples/remix-basic/app/match-stream.ts
+++ examples/remix-basic/app/match-stream.ts
@@ -1,42 +1,42 @@
-import { ShapeStream, ChangeMessage } from "@electric-sql/next"
+import { ShapeStream, ChangeMessage } from "@electric-sql/next";
 
 export async function matchStream({
   stream,
   operations,
   matchFn,
-  timeout = 5000,
+  timeout = 10000,
 }: {
-  stream: ShapeStream
-  operations: Array<`insert` | `update` | `delete`>
+  stream: ShapeStream;
+  operations: Array<`insert` | `update` | `delete`>;
   matchFn: ({
     operationType,
     message,
   }: {
-    operationType: string
-    message: ChangeMessage<any>
-  }) => boolean
-  timeout?: number
+    operationType: string;
+    message: ChangeMessage<any>;
+  }) => boolean;
+  timeout?: number;
 }): Promise<ChangeMessage<any>> {
   return new Promise((resolve, reject) => {
     const unsubscribe = stream.subscribe((messages) => {
       messages.forEach((message) => {
         if (`key` in message && operations.includes(message.headers.action)) {
           if (matchFn({ operationType: message.headers.action, message })) {
-            finish(message)
+            finish(message);
           }
         }
-      })
-    })
+      });
+    });
 
     const timeoutId = setTimeout(() => {
-      console.error(`matchStream timed out after ${timeout}ms`)
-      reject(`matchStream timed out after ${timeout}ms`)
-    }, timeout)
+      console.error(`matchStream timed out after ${timeout}ms`);
+      reject(`matchStream timed out after ${timeout}ms`);
+    }, timeout);
 
     function finish(message: ChangeMessage<any>) {
-      clearTimeout(timeoutId)
-      unsubscribe()
-      return resolve(message)
+      clearTimeout(timeoutId);
+      unsubscribe();
+      return resolve(message);
     }
-  })
+  });
 }
