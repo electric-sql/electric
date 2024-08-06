b9d42cdff68ef627bd6e7465ff45acb820e71569
Kyle Mathews <mathews.kyle@gmail.com>
2024-08-06 07:12:35 -0600

    feat: add remix basic example (#246)
    I followed the "Optimistic add to list" example here:
    https://github.com/remix-run/remix/discussions/7698
    
    The meat of the code is in
    [`app/routes/_index.tsx`](https://github.com/electric-sql/electric-next/pull/246/files#diff-a293c58f3ef9dd65aea8e9b911e909f873b9b5a3a7bfa2f09ff79ffa2d30d36d)
    
    Demo — where the browser on the left is throttled to slow 3g and the
    right browser is unthrottled.
    
    
    https://github.com/user-attachments/assets/3d3008b0-fff4-4e26-9b41-9e4e8a90c009

--- examples/linearlite/vite.config.ts
+++ examples/linearlite/vite.config.ts
@@ -4,7 +4,6 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envPrefix: `ELECTRIC_`,
   plugins: [
     react(),
     svgr({
---
+++ examples/remix-basic/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite"
+import { vitePlugin as remix } from "@remix-run/dev"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    remix({
+      // ssr: false,
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+  ],
+})
