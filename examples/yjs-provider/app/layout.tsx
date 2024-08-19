6f9b19f5d6184875892e511f8bcac42b2dca3b2e
Valter Balegas <balegas@live.com>
2024-08-19 23:39:35 +0100

    Removed all Electric swag

--- examples/yjs-provider/app/layout.tsx
+++ examples/yjs-provider/app/layout.tsx
@@ -1,6 +1,3 @@
-import "./style.css"
-import "./App.css"
-
 export const metadata = {
   title: `Next.js Forms Example`,
   description: `Example application with forms and Postgres.`,
@@ -13,14 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <div className="App">
-          <header className="App-header">
-            <img src="/logo.svg" className="App-logo" alt="logo" />
-            {children}
-          </header>
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
