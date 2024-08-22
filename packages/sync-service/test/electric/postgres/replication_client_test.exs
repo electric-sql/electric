bba27eb85b5fb68fd9587210c3a2a47a09684d04
Oleksii Sholik <oleksii@sholik.dev>
2024-08-23 00:38:09 +0300

    Increase receive timeout to reduce flake

--- packages/sync-service/test/electric/postgres/replication_client_test.exs
+++ packages/sync-service/test/electric/postgres/replication_client_test.exs
@@ -154,7 +154,7 @@ defmodule Electric.Postgres.ReplicationClientTest do
       num_txn = 2
       num_ops = 8
       max_sleep = 20
-      receive_timeout = (num_txn + num_ops) * max_sleep
+      receive_timeout = (num_txn + num_ops) * max_sleep * 2
 
       # Insert `num_txn` transactions, each in a separate process. Every transaction has
       # `num_ops` INSERTs with a random delay between each operation.
