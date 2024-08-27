aa42d57a712914e52a06d6ff2592dfcab5ed5907
msfstef <msfstef@gmail.com>
2024-08-27 16:53:27 +0300

    Add offline persistence

--- examples/tanstack-example/src/App.tsx
+++ examples/tanstack-example/src/App.tsx
@@ -1,23 +1,41 @@
+import { ShapesProvider } from "@electric-sql/react"
+import { QueryClient } from "@tanstack/react-query"
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client"
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister"
+
+import { Example } from "./Example"
 import logo from "./assets/logo.svg"
 import "./App.css"
 import "./style.css"
 
-import { Example } from "./Example"
-import { ShapesProvider } from "@electric-sql/react"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+})
 
-const queryClient = new QueryClient()
+// Using offline persistence, see:
+// https://tanstack.com/query/latest/docs/framework/react/guides/mutations#persisting-offline-mutations
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+})
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <QueryClientProvider client={queryClient}>
+
+        <PersistQueryClientProvider
+          client={queryClient}
+          persistOptions={{ persister }}
+        >
           <ShapesProvider>
             <Example />
           </ShapesProvider>
-        </QueryClientProvider>
+        </PersistQueryClientProvider>
       </header>
     </div>
   )
