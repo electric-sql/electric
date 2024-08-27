2676eb8b583f8797986e519142dfe4dbaac1fb92
Stefanos Mousafeiris <msfstef@gmail.com>
2024-08-27 18:16:48 +0300

    feat: Tanstack Query example (#1568)
    Addresses https://github.com/electric-sql/electric/issues/1478
    
    Using [TanStack Query](https://tanstack.com/query/latest) in an example.
    
    Following a similar pattern to the rest of the examples, I used
    `useShape` to get the actual `items` list, and only used
    [`useMutation`](https://tanstack.com/query/latest/docs/framework/react/guides/mutations)
    along with `useMutationState` for optimistic updates from the TanStack
    query module.
    
    I'd love to use `useOptimistic` as it was done in the Next.js example
    but it's in React's Canary release still, so I reused the pattern from
    the Remix example to integrate optimistic updates.
    
    I also had to spin up a simple node http server to proxy the database
    requests - I could also proxy the shape requests from there as well like
    it was done in Next.js but not sure if it's worth it and if it just
    complicates the example more.
    
    Not sure if a separate example is needed to address
    https://github.com/electric-sql/electric/issues/1479, since it will be
    mostly the exact same but with some offline persistence for mutations?
    Should I just integrate that as well?

---
+++ examples/tanstack-example/src/Example.tsx
@@ -0,0 +1,127 @@
+import { getShapeStream, useShape } from "@electric-sql/react"
+import {
+  useMutation,
+  useMutationState,
+  useQueryClient,
+} from "@tanstack/react-query"
+import { matchStream } from "./match-stream"
+import { v4 as uuidv4 } from "uuid"
+import "./Example.css"
+
+type Item = { id: string }
+
+const baseUrl = import.meta.env.ELECTRIC_URL ?? `http://localhost:3000`
+const baseApiUrl = `http://localhost:3001`
+
+const itemShape = () => ({
+  url: new URL(`/v1/shape/items`, baseUrl).href,
+})
+
+async function createItem(newId: string) {
+  const itemsStream = getShapeStream(itemShape())
+
+  // Match the insert
+  const findUpdatePromise = matchStream<{ id: string }>({
+    stream: itemsStream,
+    operations: [`insert`],
+    matchFn: ({ message }) => message.value.id === newId,
+  })
+
+  // Insert item
+  const fetchPromise = fetch(`${baseApiUrl}/items`, {
+    method: `POST`,
+    body: JSON.stringify({ id: newId }),
+  })
+
+  return await Promise.all([findUpdatePromise, fetchPromise])
+}
+
+async function clearItems(numItems: number) {
+  const itemsStream = getShapeStream(itemShape())
+
+  // Match the delete
+  const findUpdatePromise =
+    numItems > 0
+      ? matchStream({
+          stream: itemsStream,
+          operations: [`delete`],
+          // First delete will match
+          matchFn: () => true,
+        })
+      : Promise.resolve()
+
+  // Delete all items
+  const fetchPromise = fetch(`${baseApiUrl}/items`, { method: `DELETE` })
+
+  return await Promise.all([findUpdatePromise, fetchPromise])
+}
+
+export const Example = () => {
+  const queryClient = useQueryClient()
+
+  const { data: items } = useShape(itemShape()) as unknown as { data: Item[] }
+  const submissions: Item[] = useMutationState({
+    filters: { status: `pending` },
+    select: (mutation) => mutation.state.context as Item | undefined,
+  }).filter((item) => item !== undefined)
+
+  const { mutateAsync: addItemMut } = useMutation({
+    scope: { id: `items` },
+    mutationKey: [`add-item`],
+    mutationFn: (newId: string) => createItem(newId),
+    onMutate: (id) => {
+      const optimisticItem: Item = { id }
+      return optimisticItem
+    },
+  })
+
+  const { mutateAsync: clearItemsMut, isPending: isClearing } = useMutation({
+    scope: { id: `items` },
+    mutationKey: [`clear-items`],
+    mutationFn: (numItems: number) => clearItems(numItems),
+    onMutate: () => {
+      const addMutations = queryClient
+        .getMutationCache()
+        .findAll({ mutationKey: [`add-item`] })!
+      addMutations?.forEach((mut) => queryClient.getMutationCache().remove(mut))
+    },
+  })
+
+  // Merge data from shape & optimistic data from fetchers. This removes
+  // possible duplicates as there's a potential race condition where
+  // useShape updates from the stream slightly before the action has finished.
+  const itemsMap = new Map<string, Item>()
+  if (!isClearing) {
+    items.concat(submissions).forEach((item) => {
+      itemsMap.set(item.id, { ...itemsMap.get(item.id), ...item })
+    })
+  } else {
+    submissions.forEach((item) => itemsMap.set(item.id, item))
+  }
+
+  return (
+    <div>
+      <div>
+        <button
+          type="submit"
+          className="button"
+          onClick={() => addItemMut(uuidv4())}
+        >
+          Add
+        </button>
+        <button
+          type="submit"
+          className="button"
+          onClick={() => clearItemsMut(items.length)}
+        >
+          Clear
+        </button>
+      </div>
+      {[...itemsMap.values()].map((item: Item, index: number) => (
+        <p key={index} className="item">
+          <code>{item.id}</code>
+        </p>
+      ))}
+    </div>
+  )
+}
---
+++ examples/tanstack-example/src/server/app.js
@@ -0,0 +1,71 @@
+import http from "http"
+import pg from "pg"
+
+const db = new pg.Pool({
+  host: `localhost`,
+  port: 54321,
+  password: `password`,
+  user: `postgres`,
+  database: `electric`,
+})
+
+// Async function to handle reading the body of the request
+const getRequestBody = async (req) => {
+  return new Promise((resolve, reject) => {
+    let body = ``
+    req.on(`data`, (chunk) => (body += chunk.toString()))
+    req.on(`end`, () => resolve(body))
+    req.on(`error`, reject)
+  })
+}
+
+const JSON_HEADERS = {
+  "Content-Type": `application/json`,
+}
+const CORS_HEADERS = {
+  "Access-Control-Allow-Origin": `*`,
+  "Access-Control-Allow-Methods": `GET, POST, DELETE, OPTIONS`,
+  "Access-Control-Allow-Headers": `Content-Type`,
+}
+
+const server = http.createServer(async (req, res) => {
+  console.log(req.method, req.url)
+  try {
+    // Handle CORS preflight requests
+    if (req.method === `OPTIONS`) {
+      res.writeHead(204, CORS_HEADERS)
+      res.writeHead
+      res.end()
+      return
+    }
+
+    // Handle adding an item
+    if (req.method === `POST` && req.url === `/items`) {
+      const body = await getRequestBody(req)
+      const { id: newId } = JSON.parse(body)
+      await db.query(`INSERT INTO items (id) VALUES ($1);`, [newId])
+      res.writeHead(200, { ...JSON_HEADERS, ...CORS_HEADERS })
+      res.end(JSON.stringify({ message: `Item added with id ${newId}` }))
+      return
+    }
+
+    // Handle deleting all items
+    if (req.method === `DELETE` && req.url === `/items`) {
+      await db.query(`DELETE FROM items;`)
+      res.writeHead(200, { ...JSON_HEADERS, ...CORS_HEADERS })
+      res.end(JSON.stringify({ message: `All items deleted` }))
+      return
+    }
+
+    res.writeHead(404, { ...JSON_HEADERS, ...CORS_HEADERS })
+    res.end(JSON.stringify({ error: `Not Found` }))
+  } catch (error) {
+    res.writeHead(500, { ...JSON_HEADERS, ...CORS_HEADERS })
+    res.end(JSON.stringify({ error: `Something went wrong` }))
+  }
+})
+
+const PORT = process.env.PORT || 3001
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
