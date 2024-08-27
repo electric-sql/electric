a050ab070f7ee40045352877654d263c4d2967c1
msfstef <msfstef@gmail.com>
2024-08-27 17:55:02 +0300

    Set GC time to infinity

--- examples/tanstack-example/src/App.tsx
+++ examples/tanstack-example/src/App.tsx
@@ -11,7 +11,7 @@ import "./style.css"
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      gcTime: 1000 * 60 * 60 * 24, // 24 hours
+      gcTime: Infinity,
     },
   },
 })
