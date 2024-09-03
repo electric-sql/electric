fc82314fcca62bae5113383cd096113e6fb68ae9
Stefanos Mousafeiris <msfstef@gmail.com>
2024-09-03 21:01:37 +0300

    Fix: `useShape` not correctly updating data when `selector` prop changes (#1615)
    Addresses https://github.com/electric-sql/electric/issues/1446
    
    There is some weirdness with the `useSyncExternalStoreWithSelector` hook
    when passed a ref w.r.t. to selector changing - need to look into this
    deeper, I suspect memoization is using referential equality checks which
    is messing things up.

--- packages/react-hooks/vitest.config.ts
+++ packages/react-hooks/vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globalSetup: `test/support/global-setup.ts`,
+    setupFiles: [`test/support/react-setup.ts`],
+    environment: 'jsdom',
     typecheck: { enabled: true },
   },
 })
