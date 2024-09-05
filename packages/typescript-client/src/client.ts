682a4eb9af53f26d0109c4222d7e9f932f5ebb3c
msfstef <msfstef@gmail.com>
2024-09-05 13:42:13 +0300

    Use hash for private fields in `ShapeStream`

--- packages/typescript-client/src/client.ts
+++ packages/typescript-client/src/client.ts
@@ -97,34 +97,33 @@ export interface ShapeStreamInterface<T extends Row = Row> {
 export class ShapeStream<T extends Row = Row>
   implements ShapeStreamInterface<T>
 {
-  private options: ShapeStreamOptions
+  readonly #options: ShapeStreamOptions
 
-  private fetchClient: typeof fetch
-  private schema?: Schema
+  readonly #fetchClient: typeof fetch
+  readonly #messageParser: MessageParser<T>
 
-  private subscribers = new Map<
+  readonly #subscribers = new Map<
     number,
     [MessageProcessor<Message<T>>, ((error: Error) => void) | undefined]
   >()
-  private upToDateSubscribers = new Map<
+  readonly #upToDateSubscribers = new Map<
     number,
     [() => void, (error: FetchError | Error) => void]
   >()
 
-  private lastOffset: Offset
-  private messageParser: MessageParser<T>
-  public isUpToDate: boolean = false
-
-  shapeId?: string
+  #lastOffset: Offset
+  #isUpToDate: boolean = false
+  #shapeId?: string
+  #schema?: Schema
 
   constructor(options: ShapeStreamOptions) {
-    this.validateOptions(options)
-    this.options = { subscribe: true, ...options }
-    this.lastOffset = this.options.offset ?? `-1`
-    this.shapeId = this.options.shapeId
-    this.messageParser = new MessageParser<T>(options.parser)
+    validateOptions(options)
+    this.#options = { subscribe: true, ...options }
+    this.#lastOffset = this.#options.offset ?? `-1`
+    this.#shapeId = this.#options.shapeId
+    this.#messageParser = new MessageParser<T>(options.parser)
 
-    this.fetchClient = createFetchWithBackoff(
+    this.#fetchClient = createFetchWithBackoff(
       options.fetchClient ??
         ((...args: Parameters<typeof fetch>) => fetch(...args)),
       options.backoffOptions
@@ -133,28 +132,36 @@ export class ShapeStream<T extends Row = Row>
     this.start()
   }
 
+  get shapeId() {
+    return this.#shapeId
+  }
+
+  get isUpToDate() {
+    return this.#isUpToDate
+  }
+
   async start() {
-    this.isUpToDate = false
+    this.#isUpToDate = false
 
-    const { url, where, signal } = this.options
+    const { url, where, signal } = this.#options
 
-    while ((!signal?.aborted && !this.isUpToDate) || this.options.subscribe) {
+    while ((!signal?.aborted && !this.#isUpToDate) || this.#options.subscribe) {
       const fetchUrl = new URL(url)
       if (where) fetchUrl.searchParams.set(`where`, where)
-      fetchUrl.searchParams.set(`offset`, this.lastOffset)
+      fetchUrl.searchParams.set(`offset`, this.#lastOffset)
 
-      if (this.isUpToDate) {
+      if (this.#isUpToDate) {
         fetchUrl.searchParams.set(`live`, `true`)
       }
 
-      if (this.shapeId) {
+      if (this.#shapeId) {
         // This should probably be a header for better cache breaking?
-        fetchUrl.searchParams.set(`shape_id`, this.shapeId!)
+        fetchUrl.searchParams.set(`shape_id`, this.#shapeId!)
       }
 
       let response!: Response
       try {
-        response = await this.fetchClient(fetchUrl.toString(), { signal })
+        response = await this.#fetchClient(fetchUrl.toString(), { signal })
       } catch (e) {
         if (e instanceof FetchBackoffAborted) break // interrupted
         if (!(e instanceof FetchError)) throw e // should never happen
@@ -162,13 +169,13 @@ export class ShapeStream<T extends Row = Row>
           // Upon receiving a 409, we should start from scratch
           // with the newly provided shape ID
           const newShapeId = e.headers[`x-electric-shape-id`]
-          this.reset(newShapeId)
-          this.publish(e.json as Message<T>[])
+          this.#reset(newShapeId)
+          this.#publish(e.json as Message<T>[])
           continue
         } else if (e.status >= 400 && e.status < 500) {
           // Notify subscribers
-          this.sendErrorToUpToDateSubscribers(e)
-          this.sendErrorToSubscribers(e)
+          this.#sendErrorToUpToDateSubscribers(e)
+          this.#sendErrorToSubscribers(e)
 
           // 400 errors are not actionable without additional user input, so we're throwing them.
           throw e
@@ -178,23 +185,23 @@ export class ShapeStream<T extends Row = Row>
       const { headers, status } = response
       const shapeId = headers.get(`X-Electric-Shape-Id`)
       if (shapeId) {
-        this.shapeId = shapeId
+        this.#shapeId = shapeId
       }
 
       const lastOffset = headers.get(`X-Electric-Chunk-Last-Offset`)
       if (lastOffset) {
-        this.lastOffset = lastOffset as Offset
+        this.#lastOffset = lastOffset as Offset
       }
 
       const getSchema = (): Schema => {
         const schemaHeader = headers.get(`X-Electric-Schema`)
         return schemaHeader ? JSON.parse(schemaHeader) : {}
       }
-      this.schema = this.schema ?? getSchema()
+      this.#schema = this.#schema ?? getSchema()
 
       const messages = status === 204 ? `[]` : await response.text()
 
-      const batch = this.messageParser.parse(messages, this.schema)
+      const batch = this.#messageParser.parse(messages, this.#schema)
 
       // Update isUpToDate
       if (batch.length > 0) {
@@ -202,13 +209,13 @@ export class ShapeStream<T extends Row = Row>
         if (
           isControlMessage(lastMessage) &&
           lastMessage.headers.control === `up-to-date` &&
-          !this.isUpToDate
+          !this.#isUpToDate
         ) {
-          this.isUpToDate = true
-          this.notifyUpToDateSubscribers()
+          this.#isUpToDate = true
+          this.#notifyUpToDateSubscribers()
         }
 
-        this.publish(batch)
+        this.#publish(batch)
       }
     }
   }
@@ -220,27 +227,15 @@ export class ShapeStream<T extends Row = Row>
     const subscriptionId = Math.random()
     const subscriber = new MessageProcessor(callback)
 
-    this.subscribers.set(subscriptionId, [subscriber, onError])
+    this.#subscribers.set(subscriptionId, [subscriber, onError])
 
     return () => {
-      this.subscribers.delete(subscriptionId)
+      this.#subscribers.delete(subscriptionId)
     }
   }
 
   unsubscribeAll(): void {
-    this.subscribers.clear()
-  }
-
-  private publish(messages: Message<T>[]) {
-    this.subscribers.forEach(([subscriber, _]) => {
-      subscriber.process(messages)
-    })
-  }
-
-  private sendErrorToSubscribers(error: Error) {
-    this.subscribers.forEach(([_, errorFn]) => {
-      errorFn?.(error)
-    })
+    this.#subscribers.clear()
   }
 
   subscribeOnceToUpToDate(
@@ -249,26 +244,38 @@ export class ShapeStream<T extends Row = Row>
   ) {
     const subscriptionId = Math.random()
 
-    this.upToDateSubscribers.set(subscriptionId, [callback, error])
+    this.#upToDateSubscribers.set(subscriptionId, [callback, error])
 
     return () => {
-      this.upToDateSubscribers.delete(subscriptionId)
+      this.#upToDateSubscribers.delete(subscriptionId)
     }
   }
 
   unsubscribeAllUpToDateSubscribers(): void {
-    this.upToDateSubscribers.clear()
+    this.#upToDateSubscribers.clear()
   }
 
-  private notifyUpToDateSubscribers() {
-    this.upToDateSubscribers.forEach(([callback]) => {
+  #publish(messages: Message<T>[]) {
+    this.#subscribers.forEach(([subscriber, _]) => {
+      subscriber.process(messages)
+    })
+  }
+
+  #sendErrorToSubscribers(error: Error) {
+    this.#subscribers.forEach(([_, errorFn]) => {
+      errorFn?.(error)
+    })
+  }
+
+  #notifyUpToDateSubscribers() {
+    this.#upToDateSubscribers.forEach(([callback]) => {
       callback()
     })
   }
 
-  private sendErrorToUpToDateSubscribers(error: FetchError | Error) {
+  #sendErrorToUpToDateSubscribers(error: FetchError | Error) {
     // eslint-disable-next-line
-    this.upToDateSubscribers.forEach(([_, errorCallback]) =>
+    this.#upToDateSubscribers.forEach(([_, errorCallback]) =>
       errorCallback(error)
     )
   }
@@ -277,31 +284,34 @@ export class ShapeStream<T extends Row = Row>
    * Resets the state of the stream, optionally with a provided
    * shape ID
    */
-  private reset(shapeId?: string) {
-    this.lastOffset = `-1`
-    this.shapeId = shapeId
-    this.isUpToDate = false
-    this.schema = undefined
+  #reset(shapeId?: string) {
+    this.#lastOffset = `-1`
+    this.#shapeId = shapeId
+    this.#isUpToDate = false
+    this.#schema = undefined
+  }
+}
+
+function validateOptions(
+  options: Partial<ShapeStreamOptions>
+): options is ShapeStreamOptions {
+  if (!options.url) {
+    throw new Error(`Invalid shape option. It must provide the url`)
+  }
+  if (options.signal && !(options.signal instanceof AbortSignal)) {
+    throw new Error(
+      `Invalid signal option. It must be an instance of AbortSignal.`
+    )
   }
 
-  private validateOptions(options: ShapeStreamOptions): void {
-    if (!options.url) {
-      throw new Error(`Invalid shape option. It must provide the url`)
-    }
-    if (options.signal && !(options.signal instanceof AbortSignal)) {
-      throw new Error(
-        `Invalid signal option. It must be an instance of AbortSignal.`
-      )
-    }
-
-    if (
-      options.offset !== undefined &&
-      options.offset !== `-1` &&
-      !options.shapeId
-    ) {
-      throw new Error(
-        `shapeId is required if this isn't an initial fetch (i.e. offset > -1)`
-      )
-    }
+  if (
+    options.offset !== undefined &&
+    options.offset !== `-1` &&
+    !options.shapeId
+  ) {
+    throw new Error(
+      `shapeId is required if this isn't an initial fetch (i.e. offset > -1)`
+    )
   }
+  return true
 }
