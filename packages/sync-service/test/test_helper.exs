afe3f9de1ff156afd3d8ccab3c6a89d4351ea4d0
Oleksii Sholik <oleksii@sholik.dev>
2024-10-10 13:50:07 +0300

    Get rid of another indentation level in the application callback
    It's simpler to say that we don't start any processes in the test
    environment that are part of the root supervision tree in the other
    environments. Only start what's needed in tests, including the process
    registry which is started in test_helper.exs to make it available to all
    test modules.

--- packages/sync-service/test/test_helper.exs
+++ packages/sync-service/test/test_helper.exs
@@ -1,1 +1,7 @@
+# The process registry is implicitly used by processes in the dev, prod and test environments alike.
+#
+# Explicitly start the process registry here since the OTP application does not start a
+# supervision tree in the test environment.
+Registry.start_link(name: Electric.Application.process_registry(), keys: :unique)
+
 ExUnit.start(assert_receive_timeout: 400)
