1760952b177bdd6e0e6f829c3b0f81e46ba7991f
Oleksii Sholik <oleksii@sholik.dev>
2024-10-10 13:07:19 +0300

    Make the application configuration step explicit

---
+++ packages/sync-service/lib/electric/application/configuration.ex
@@ -0,0 +1,25 @@
+defmodule Electric.Application.Configuration do
+  defstruct ~w[
+    electric_instance_id
+    storage
+    persistent_kv
+    connection_opts
+    replication_opts
+    pool_opts
+    inspector
+    child_specs
+  ]a
+
+  @type t :: %__MODULE__{}
+
+  @persistent_key __MODULE__
+
+  @spec save(t) :: t
+  def save(config) do
+    :ok = :persistent_term.put(@persistent_key, config)
+    config
+  end
+
+  @spec get :: t
+  def get, do: :persistent_term.get(@persistent_key)
+end
