2c8ae3630aa812e19d10f0b498f661a3b16b0553
msfstef <msfstef@gmail.com>
2024-10-10 14:55:57 +0300

    Add column filtering capabilities to client

--- packages/typescript-client/src/constants.ts
+++ packages/typescript-client/src/constants.ts
@@ -7,4 +7,5 @@ export const SHAPE_SCHEMA_HEADER = `electric-schema`
 export const SHAPE_ID_QUERY_PARAM = `shape_id`
 export const OFFSET_QUERY_PARAM = `offset`
 export const WHERE_QUERY_PARAM = `where`
+export const COLUMNS_QUERY_PARAM = `columns`
 export const LIVE_QUERY_PARAM = `live`
