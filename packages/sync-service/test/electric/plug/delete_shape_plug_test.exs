f0b31962eca9fced66b208ada3ed33a8e818da87
Oleksii Sholik <oleksii@sholik.dev>
2024-10-18 01:20:30 +0300

    (tmp) Load requested or default tenant in DeleteShapePlug

--- packages/sync-service/test/electric/plug/delete_shape_plug_test.exs
+++ packages/sync-service/test/electric/plug/delete_shape_plug_test.exs
@@ -4,6 +4,10 @@ defmodule Electric.Plug.DeleteShapePlugTest do
 
   alias Electric.Plug.DeleteShapePlug
   alias Electric.Shapes.Shape
+  alias Electric.TenantManager
+
+  import Support.ComponentSetup
+  import Support.TestUtils, only: [with_electric_instance_id: 1]
 
   alias Support.Mock
 
@@ -25,6 +29,7 @@ defmodule Electric.Plug.DeleteShapePlugTest do
     }
   }
   @test_shape_id "test-shape-id"
+  @test_pg_id "12345"
 
   def load_column_info({"public", "users"}, _),
     do: {:ok, @test_shape.table_info[{"public", "users"}][:columns]}
@@ -37,26 +42,42 @@ defmodule Electric.Plug.DeleteShapePlugTest do
     :ok
   end
 
-  def conn(method, "?" <> _ = query_string, allow \\ true) do
+  def conn(ctx, method, "?" <> _ = query_string, allow \\ true) do
     # Pass mock dependencies to the plug
-    config = %{
+    tenant = [
+      electric_instance_id: ctx.electric_instance_id,
+      tenant_id: ctx.tenant_id,
+      pg_id: @test_pg_id,
       shape_cache: {Mock.ShapeCache, []},
+      storage: {Mock.Storage, []},
       inspector: {__MODULE__, []},
       registry: @registry,
-      long_poll_timeout: 20_000,
-      max_age: 60,
-      stale_age: 300,
+      long_poll_timeout: Access.get(ctx, :long_poll_timeout, 20_000),
+      max_age: Access.get(ctx, :max_age, 60),
+      stale_age: Access.get(ctx, :stale_age, 300)
+    ]
+
+    # because test mode creates a tenant by default
+    TenantManager.delete_tenant(ctx.tenant_id)
+    :ok = TenantManager.store_tenant(tenant)
+
+    config = [
+      storage: {Mock.Storage, []},
+      tenant_manager: Electric.TenantManager,
       allow_shape_deletion: allow
-    }
+    ]
 
     Plug.Test.conn(method, "/" <> query_string)
     |> assign(:config, config)
   end
 
   describe "DeleteShapePlug" do
-    test "returns 404 if shape deletion is not allowed" do
+    setup [:with_electric_instance_id, :with_tenant_id]
+
+    test "returns 404 if shape deletion is not allowed", ctx do
       conn =
-        conn("DELETE", "?root_table=.invalid_shape", false)
+        ctx
+        |> conn("DELETE", "?root_table=.invalid_shape", false)
         |> DeleteShapePlug.call([])
 
       assert conn.status == 404
@@ -66,9 +87,10 @@ defmodule Electric.Plug.DeleteShapePlugTest do
              }
     end
 
-    test "returns 400 for invalid params" do
+    test "returns 400 for invalid params", ctx do
       conn =
-        conn("DELETE", "?root_table=.invalid_shape")
+        ctx
+        |> conn("DELETE", "?root_table=.invalid_shape")
         |> DeleteShapePlug.call([])
 
       assert conn.status == 400
@@ -80,24 +102,26 @@ defmodule Electric.Plug.DeleteShapePlugTest do
              }
     end
 
-    test "should clean shape based on shape definition" do
+    test "should clean shape based on shape definition", ctx do
       Mock.ShapeCache
       |> expect(:get_or_create_shape_id, fn @test_shape, _opts -> {@test_shape_id, 0} end)
       |> expect(:clean_shape, fn @test_shape_id, _ -> :ok end)
 
       conn =
-        conn(:delete, "?root_table=public.users")
+        ctx
+        |> conn(:delete, "?root_table=public.users")
         |> DeleteShapePlug.call([])
 
       assert conn.status == 202
     end
 
-    test "should clean shape based on shape_id" do
+    test "should clean shape based on shape_id", ctx do
       Mock.ShapeCache
       |> expect(:clean_shape, fn @test_shape_id, _ -> :ok end)
 
       conn =
-        conn(:delete, "?root_table=public.users&shape_id=#{@test_shape_id}")
+        ctx
+        |> conn(:delete, "?root_table=public.users&shape_id=#{@test_shape_id}")
         |> DeleteShapePlug.call([])
 
       assert conn.status == 202
