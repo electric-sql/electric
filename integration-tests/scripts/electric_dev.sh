79a275083995da182ed80bed1be7ed59e0ec0692
Stefanos Mousafeiris <msfstef@gmail.com>
2024-10-29 16:51:42 +0200

    fix: Fix `REPLICATION_STREAM_ID` parsing (#1901)

--- integration-tests/scripts/electric_dev.sh
+++ integration-tests/scripts/electric_dev.sh
@@ -5,4 +5,4 @@ set -e
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 cd "$SCRIPT_DIR"/../../packages/sync-service
-STORAGE_DIR="$SCRIPT_DIR/../_storage" iex -S mix
+STORAGE_DIR="$SCRIPT_DIR/../_storage" REPLICATION_STREAM_ID=integration iex -S mix
