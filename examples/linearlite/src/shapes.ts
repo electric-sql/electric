26f8c3b7881477d8207999961b9a3b75a1b04aa0
Ilia Borovitinov <ilia@electric-sql.com>
2024-10-31 19:16:12 +0300

    Added basic cloud deployment against a Neon DB

--- examples/linearlite/src/shapes.ts
+++ examples/linearlite/src/shapes.ts
@@ -1,5 +1,12 @@
-import { baseUrl } from './electric'
+import { ShapeStreamOptions } from '@electric-sql/next'
+import { baseUrl, databaseId, token } from './electric'
 
-export const issueShape = {
+console.log(import.meta.env)
+
+export const issueShape: ShapeStreamOptions = {
   url: `${baseUrl}/v1/shape/issue`,
+  databaseId,
+  headers: {
+    'Authorization': `Bearer ${token}`
+  }
 }
