32ff8ea33be26f134b766064db402ff758c6ac04
Kyle Mathews <mathews.kyle@gmail.com>
2024-11-01 14:53:09 -0600

    remove console.log

--- examples/linearlite/src/shapes.ts
+++ examples/linearlite/src/shapes.ts
@@ -1,8 +1,6 @@
 import { ShapeStreamOptions } from '@electric-sql/next'
 import { baseUrl, databaseId, token } from './electric'
 
-console.log(import.meta.env, baseUrl, databaseId, token)
-
 export const issueShape: ShapeStreamOptions = {
   url: `${baseUrl}/v1/shape/issue`,
   databaseId,
