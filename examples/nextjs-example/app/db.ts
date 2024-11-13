d7c73562a67ef15291aa231d76039dfe485ae070
Valter Balegas <balegas@live.com>
2024-11-13 17:54:42 +0000

    redeploy

--- examples/nextjs-example/app/db.ts
+++ examples/nextjs-example/app/db.ts
@@ -1,8 +1,6 @@
 import pgPkg from "pg"
 const { Client } = pgPkg
 
-console.log("connecting to DATABASE_URL", process.env.DATABASE_URL)
-
 const db = new Client({
   connectionString: process.env.DATABASE_URL,
 })
