dc9da790a10a3abd84dfaf13c5216fcf10b284c3
Valter Balegas <balegas@live.com>
2024-11-18 11:22:55 +0000

    Ready for Cloud

--- examples/nextjs-ssr-example/app/db.ts
+++ examples/nextjs-ssr-example/app/db.ts
@@ -2,11 +2,7 @@ import pgPkg from "pg"
 const { Client } = pgPkg
 
 const db = new Client({
-  host: `localhost`,
-  port: 54321,
-  password: `password`,
-  user: `postgres`,
-  database: `electric`,
+  connectionString: process.env.DATABASE_URL,
 })
 
 db.connect()
--- examples/nextjs-ssr-example/app/shape-proxy/[...table]/route.ts
+++ examples/nextjs-ssr-example/app/shape-proxy/[...table]/route.ts
@@ -1,8 +1,27 @@
 export async function GET(request: Request) {
   const url = new URL(request.url)
-  const originUrl = new URL(`http://localhost:3000/v1/shape`)
+  const originUrl = new URL(
+    process.env.ELECTRIC_URL
+      ? `${process.env.ELECTRIC_URL}/v1/shape`
+      : `http://localhost:3000/v1/shape`
+  )
+
   url.searchParams.forEach((value, key) => {
     originUrl.searchParams.set(key, value)
+  })
+
+  if (process.env.DATABASE_ID) {
+    originUrl.searchParams.set(`database_id`, process.env.DATABASE_ID)
+  }
+
+  const headers = new Headers()
+  if (process.env.ELECTRIC_TOKEN) {
+    originUrl.searchParams.set(`token`, process.env.ELECTRIC_TOKEN)
+  }
+
+  const newRequest = new Request(originUrl.toString(), {
+    method: `GET`,
+    headers,
   })
 
   // When proxying long-polling requests, content-encoding & content-length are added
@@ -10,7 +29,7 @@ export async function GET(request: Request) {
   // them to avoid content decoding errors in the browser.
   //
   // Similar-ish problem to https://github.com/wintercg/fetch/issues/23
-  let resp = await fetch(originUrl.toString())
+  let resp = await fetch(newRequest)
   if (resp.headers.get(`content-encoding`)) {
     const headers = new Headers(resp.headers)
     headers.delete(`content-encoding`)
