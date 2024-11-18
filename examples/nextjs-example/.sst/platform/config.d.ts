4b36367279f56395f7f526620db17ab52d1230b4
Kyle Mathews <mathews.kyle@gmail.com>
2024-11-18 14:14:38 -0700

    add config.d.ts

--- examples/nextjs-example/.sst/platform/config.d.ts
+++ examples/nextjs-example/.sst/platform/config.d.ts
@@ -2,22 +2,22 @@ import "./src/global.d.ts"
 import "../types.generated"
 import { AppInput, App, Config } from "./src/config"
 import * as _neon from "@sst-provider/neon";
-import * as _cloudflare from "@pulumi/cloudflare";
 import * as _aws from "@pulumi/aws";
+import * as _cloudflare from "@pulumi/cloudflare";
 
 
 declare global {
   // @ts-expect-error
   export import neon = _neon
   // @ts-expect-error
-  export import cloudflare = _cloudflare
-  // @ts-expect-error
   export import aws = _aws
+  // @ts-expect-error
+  export import cloudflare = _cloudflare
   interface Providers {
     providers?: {
       "neon"?:  (_neon.ProviderArgs & { version?: string }) | boolean | string;
-      "cloudflare"?:  (_cloudflare.ProviderArgs & { version?: string }) | boolean | string;
       "aws"?:  (_aws.ProviderArgs & { version?: string }) | boolean | string;
+      "cloudflare"?:  (_cloudflare.ProviderArgs & { version?: string }) | boolean | string;
     }
   }
   export const $config: (
