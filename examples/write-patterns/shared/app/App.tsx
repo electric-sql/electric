eca11830b07d6e59974dc2a0547f8bf2e36f0d50
James Arthur <thruflo@gmail.com>
2024-11-22 13:11:38 +0100

    examples: initial boilerplate for write-path example.

---
+++ examples/write-patterns/shared/app/App.tsx
@@ -0,0 +1,28 @@
+import './style.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <table>
+        <tr>
+          <td>
+            A
+          </td>
+          <td>
+            B
+          </td>
+        </tr>
+        <tr>
+          <td>
+            C
+          </td>
+          <td>
+            D
+          </td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export default App
---
+++ examples/write-patterns/shared/backend/api.js
@@ -0,0 +1,142 @@
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import express from 'express'
+import pg from 'pg'
+
+import { z } from 'zod'
+
+// Connect to Postgres.
+const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://postgres:password@localhost:54321/electric'
+const DATABASE_USE_SSL = process.env.DATABASE_USE_SSL === 'true' || false
+const pool = new pg.Pool({connectionString: DATABASE_URL, ssl: DATABASE_USE_SSL})
+const db = await pool.connect()
+
+// Expose an HTTP server.
+const PORT = parseInt(process.env.PORT || '3001')
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+// Validate user input
+const idSchema = z.string().uuid()
+const createSchema = z.object({
+  id: z.string().uuid(),
+  title: z.string(),
+})
+const updateSchema = z.object({
+  completed: z.boolean().optional(),
+  title: z.string().optional(),
+})
+
+// Define functions to create, update and delete todos
+// using the `db` client.
+
+const createTodo = async (id, title) => {
+  sql = `
+    INSERT INTO todos (id, title, completed, created_at)
+    VALUES ($1, $2, false, $3)
+  `
+
+  now = new Date()
+  params = [id, title, now]
+
+  return await db.query(sql, params)
+}
+
+const updateTodo = async (id, data) => {
+  const [sql, params] = updateQuery('todos', id, data)
+
+  return await db.query(sql, params)
+}
+
+const updateQuery = (table, id, updates) => {
+  const setClauses = []
+  const params = []
+
+  let index = 1
+  for (const [key, value] of Object.entries(updates)) {
+    setClauses.push(`'${key}' = $${index}`)
+    params.push(value)
+    index++
+  }
+
+  const sql = `
+    UPDATE '${table}'
+    SET ${setClauses.join(`, `)}
+    WHERE id == '${id}'
+  `
+
+  return [sql, params]
+}
+
+const deleteTodo = async (id) => {
+  const sql = `DELETE from todos where id = $1`
+  const params = [id]
+
+  return await db.query(sql, params)
+}
+
+// Expose the API.
+
+app.post(`/todos`, async (req, res) => {
+  let data
+  try {
+    data = createSchema.parse(req.body)
+  }
+  catch (err) {
+    return res.status(400).json({ errors: err.errors })
+  }
+
+  try {
+    await createTodo(data.id, data.title)
+  }
+  catch (err) {
+    return res.status(500).json({ errors: err })
+  }
+
+  return res.status(200).json({ status: 'OK' })
+})
+
+app.put(`/todos/:id`, async (req, res) => {
+  let id, data
+  try {
+    id = idSchema.parse(req.params.id)
+    data = updateSchema.parse(req.body)
+  }
+  catch (err) {
+    return res.status(400).json({ errors: err.errors })
+  }
+
+  try {
+    await createTodo(id, data)
+  }
+  catch (err) {
+    return res.status(500).json({ errors: err })
+  }
+
+  return res.status(200).json({ status: 'OK' })
+})
+
+app.delete(`/todos/:id`, async (req, res) => {
+  let id
+  try {
+    id = idSchema.parse(req.params.id)
+  }
+  catch (err) {
+    return res.status(400).json({ errors: err.errors })
+  }
+
+  try {
+    await deleteTodo(id)
+  }
+  catch (err) {
+    return res.status(500).json({ errors: err })
+  }
+
+  return res.status(200).json({ status: 'OK' })
+})
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
