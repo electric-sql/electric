d0b847d3dcecd737ca7c06ab2dca606f7f0b3fcc
James Arthur <thruflo@gmail.com>
2024-11-26 14:52:53 +0100

    examples: simplify the basic-example code.

--- examples/basic-example/src/Example.tsx
+++ examples/basic-example/src/Example.tsx
@@ -1,34 +1,16 @@
-import { useShape } from "@electric-sql/react"
-import "./Example.css"
+import { useShape } from '@electric-sql/react'
+import './Example.css'
 
 type Item = { id: string }
 
-const baseUrl = import.meta.env.ELECTRIC_URL ?? `http://localhost:3000`
+const baseUrl = import.meta.env.ELECTRIC_URL ?? 'http://localhost:3000'
 
 export const Example = () => {
   const { data: items } = useShape<Item>({
     url: `${baseUrl}/v1/shape`,
-    table: `items`,
+    table: 'items',
   })
 
-  /*
-  const addItem = async () => {
-    console.log(`'addItem' is not implemented`)
-  }
-
-  const clearItems = async () => {
-    console.log(`'clearItems' is not implemented`)
-  }
-
-      <div className="controls">
-        <button className="button" onClick={addItem}>
-          Add
-        </button>
-        <button className="button" onClick={clearItems}>
-          Clear
-        </button>
-      </div>
-      */
   return (
     <div>
       {items.map((item: Item, index: number) => (
