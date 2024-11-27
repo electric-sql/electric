e95ee8cfc83bda51516069b798a4246aa410ce63
James Arthur <thruflo@gmail.com>
2024-11-27 19:05:33 +0100

    examples: update write-patterns to use Vite PWA.

--- examples/write-patterns/vite.config.ts
+++ examples/write-patterns/vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: [
+        'shared/app/icons/*'
+      ],
+      manifest: {
+        name: 'Write Patterns Example - ElectricSQL ',
+        short_name: 'Writes',
+        description: 'Four different write-patterns that work with Electric.',
+        theme_color: '#1c1e20',
+        icons: [
+          {
+            src: './shared/app/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './shared/app/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
