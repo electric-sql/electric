2881ad27189ca0dabfae080c770483f7d085f990
rob <rob.acourt@gmail.com>
2024-11-29 10:48:41 +0000

    Remove redundant arg

--- packages/sync-service/test/support/transaction_consumer.ex
+++ packages/sync-service/test/support/transaction_consumer.ex
@@ -35,12 +35,9 @@ defmodule Support.TransactionConsumer do
     {:ok, producer} = Keyword.fetch(opts, :producer)
     {:ok, parent} = Keyword.fetch(opts, :parent)
     {:ok, id} = Keyword.fetch(opts, :id)
-    # TODO: Remove
-    partition = Keyword.get(opts, :partition, :transaction)
     shape = Keyword.fetch!(opts, :shape)
 
-    {:consumer, {id, nil, parent},
-     subscribe_to: [{producer, [partition: partition, shape: shape]}]}
+    {:consumer, {id, nil, parent}, subscribe_to: [{producer, [shape: shape]}]}
   end
 
   def handle_subscribe(:producer, _options, from, {id, _, parent}) do
