b64546d34fd455c8405e217a83cb8c8b3df611b3
James Arthur <thruflo@gmail.com>
2024-12-03 19:43:34 +0100

    example: modernise the build a touch.

--- examples/write-patterns/vite.config.ts
+++ examples/write-patterns/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext'
+  },
   optimizeDeps: {
     exclude: ['@electric-sql/pglite'],
   },
@@ -11,8 +14,11 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 ** 2
+      },
       devOptions: {
-        enabled: true,
+        enabled: false,
       },
       includeAssets: ['shared/app/icons/*'],
       manifest: {
