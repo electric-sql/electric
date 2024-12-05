ffc44f344a00827f229b4ed2702c00faf2abb632
Valter Balegas <balegas@live.com>
2024-12-05 15:38:48 +0000

    Fixes for serverless deployment

--- examples/nextjs/app/api/items/route.ts
+++ examples/nextjs/app/api/items/route.ts
@@ -1,17 +1,32 @@
-import { db } from "../../db"
 import { NextResponse } from "next/server"
+import { Client } from "pg"
+
+const client = new Client({
+  connectionString: process.env.DATABASE_POOLED_URL,
+})
 
 export async function POST(request: Request) {
   const body = await request.json()
-  const result = await db.query(
-    `INSERT INTO items (id)
-    VALUES ($1) RETURNING id;`,
-    [body.uuid]
-  )
-  return NextResponse.json({ id: result.rows[0].id })
+
+  try {
+    await client.connect()
+    const result = await client.query(
+      `INSERT INTO items (id)
+      VALUES ($1) RETURNING id;`,
+      [body.uuid]
+    )
+    return NextResponse.json({ id: result.rows[0].id })
+  } finally {
+    await client.end()
+  }
 }
 
 export async function DELETE() {
-  await db.query(`DELETE FROM items;`)
+  try {
+    await client.connect()
+    await client.query(`DELETE FROM items;`)
+  } finally {
+    await client.end()
+  }
   return NextResponse.json(`ok`)
 }
