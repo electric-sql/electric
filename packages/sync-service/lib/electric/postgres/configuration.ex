dc74fe5661b47fffc71be3cac6fa24b6aabf10c9
msfstef <msfstef@gmail.com>
2024-12-05 17:53:12 +0200

    Implement publication manager

--- packages/sync-service/lib/electric/postgres/configuration.ex
+++ packages/sync-service/lib/electric/postgres/configuration.ex
@@ -4,11 +4,10 @@ defmodule Electric.Postgres.Configuration do
   a provided connection.
   """
   require Logger
+  alias Electric.Postgres.PublicationManager.RelationFilter
   alias Electric.Utils
-  alias Electric.Shapes.Shape
 
-  @type filter() :: String.t() | nil
-  @type filters() :: %{Electric.relation() => filter()}
+  @type filters() :: %{Electric.relation() => RelationFilter.t()}
 
   @pg_15 150_000
 
@@ -25,34 +24,53 @@ defmodule Electric.Postgres.Configuration do
   """
   @spec configure_tables_for_replication!(
           Postgrex.conn(),
-          [Shape.table_with_where_clause()],
+          [RelationFilter.t()],
           (-> String.t()),
           float()
         ) ::
           {:ok, [:ok]}
-  def configure_tables_for_replication!(pool, relations, get_pg_version, publication_name) do
+  def configure_tables_for_replication!(pool, relation_filters, get_pg_version, publication_name) do
     configure_tables_for_replication_internal!(
       pool,
-      relations,
+      relation_filters,
       get_pg_version.(),
       publication_name
     )
   end
 
-  defp configure_tables_for_replication_internal!(pool, relations, pg_version, publication_name)
+  defp configure_tables_for_replication_internal!(
+         pool,
+         relation_filters,
+         pg_version,
+         publication_name
+       )
        when pg_version < @pg_15 do
     Postgrex.transaction(pool, fn conn ->
-      for {relation, _} <- relations,
-          table = Utils.relation_to_sql(relation),
-          publication = Utils.quote_name(publication_name) do
+      publication = Utils.quote_name(publication_name)
+
+      prev_published_tables =
+        get_publication_tables(conn, publication_name)
+        |> Enum.map(&Utils.relation_to_sql/1)
+        |> MapSet.new()
+
+      new_published_tables =
+        Enum.map(relation_filters, & &1.relation)
+        |> Enum.map(&Utils.relation_to_sql/1)
+        |> MapSet.new()
+
+      alter_ops =
+        Enum.concat(
+          MapSet.difference(new_published_tables, prev_published_tables)
+          |> Enum.map(&{&1, "ADD"}),
+          MapSet.difference(prev_published_tables, new_published_tables)
+          |> Enum.map(&{&1, "DROP"})
+        )
+
+      for {table, op} <- alter_ops do
         Postgrex.query!(conn, "SAVEPOINT before_publication", [])
 
         # PG 14 and below do not support filters on tables of publications
-        case Postgrex.query(
-               conn,
-               "ALTER PUBLICATION #{publication} ADD TABLE #{table}",
-               []
-             ) do
+        case Postgrex.query(conn, "ALTER PUBLICATION #{publication} #{op} TABLE #{table}", []) do
           {:ok, _} ->
             Postgrex.query!(conn, "RELEASE SAVEPOINT before_publication", [])
             :ok
@@ -68,41 +86,31 @@ defmodule Electric.Postgres.Configuration do
         end
       end
 
-      set_replica_identity!(conn, relations)
+      set_replica_identity!(conn, relation_filters)
     end)
   end
 
-  defp configure_tables_for_replication_internal!(pool, relations, _pg_version, publication_name) do
+  defp configure_tables_for_replication_internal!(
+         pool,
+         relation_filters,
+         _pg_version,
+         publication_name
+       ) do
     Postgrex.transaction(pool, fn conn ->
-      # We're using advisory locks to prevent race conditions when multiple
-      # processes try to read-then-update the publication configuration. We're not using `SELECT FOR UPDATE`
-      # because it doesn't read the value that was updated by other transaction holding the lock. This lock
-      # is thus acquired before reading the existing configuration, so the first read sees the latest value.
-      Postgrex.query!(conn, "SELECT pg_advisory_xact_lock($1)", [:erlang.phash2(publication_name)])
-
-      filters = get_publication_filters(conn, publication_name)
-
-      # Get the existing filter for the table
-      # and extend it with the where clause for the table
-      # and update the table in the map with the new filter
-      filters =
-        Enum.reduce(relations, filters, fn {relation, clause}, acc ->
-          Map.update(acc, relation, clause, &extend_where_clause(&1, clause))
-        end)
-
-      Postgrex.query!(conn, make_alter_publication_query(publication_name, filters), [])
+      # Update the entire publication with the new filters
+      Postgrex.query!(conn, make_alter_publication_query(publication_name, relation_filters), [])
 
       # `ALTER TABLE` should be after the publication altering, because it takes out an exclusive lock over this table,
       # but the publication altering takes out a shared lock on all mentioned tables, so a concurrent transaction will
       # deadlock if the order is reversed.
-      set_replica_identity!(conn, relations)
+      set_replica_identity!(conn, relation_filters)
 
       [:ok]
     end)
   end
 
-  defp set_replica_identity!(conn, relations) do
-    for {relation, _} <- relations,
+  defp set_replica_identity!(conn, relation_filters) do
+    for %RelationFilter{relation: relation} <- relation_filters,
         table = Utils.relation_to_sql(relation) do
       %Postgrex.Result{rows: [[correct_identity?]]} =
         Postgrex.query!(
@@ -118,50 +126,15 @@ defmodule Electric.Postgres.Configuration do
     end
   end
 
-  # Returns the filters grouped by table for the given publication.
-  @spec get_publication_filters(Postgrex.conn(), String.t()) :: filters()
-  defp get_publication_filters(conn, publication) do
+  @spec get_publication_tables(Postgrex.conn(), String.t()) :: list(Electric.relation())
+  defp get_publication_tables(conn, publication) do
     Postgrex.query!(
       conn,
-      "SELECT schemaname, tablename, rowfilter FROM pg_publication_tables WHERE pubname = $1",
+      "SELECT schemaname, tablename FROM pg_publication_tables WHERE pubname = $1",
       [publication]
     )
     |> Map.fetch!(:rows)
-    |> Enum.map(&{Enum.take(&1, 2) |> List.to_tuple(), Enum.at(&1, 2)})
-    |> Map.new()
-  end
-
-  @doc """
-  Drops all tables from the given publication.
-  """
-  @spec drop_all_publication_tables(Postgrex.conn(), String.t()) :: Postgrex.Result.t()
-  def drop_all_publication_tables(conn, publication_name) do
-    Postgrex.query!(
-      conn,
-      "
-      DO $$
-      DECLARE
-        r RECORD;
-      BEGIN
-        FOR r IN (SELECT schemaname, tablename FROM pg_publication_tables WHERE pubname = '#{publication_name}')
-        LOOP
-          EXECUTE 'ALTER PUBLICATION #{Utils.quote_name(publication_name)} DROP TABLE ' || r.schemaname || '.' || r.tablename || ';';
-        END LOOP;
-      END $$;
-      ",
-      []
-    )
-  end
-
-  # Joins the existing filter for the table with the where clause for the table.
-  # If one of them is `nil` (i.e. no filter) then the resulting filter is `nil`.
-  @spec extend_where_clause(filter(), filter()) :: filter()
-  defp extend_where_clause(filter, where_clause) when is_nil(filter) or is_nil(where_clause) do
-    nil
-  end
-
-  defp extend_where_clause(filter, where_clause) do
-    "(#{filter} OR #{where_clause})"
+    |> Enum.map(&(Enum.take(&1, 2) |> List.to_tuple()))
   end
 
   # Makes an SQL query that alters the given publication whith the given tables and filters.
@@ -177,13 +150,18 @@ defmodule Electric.Postgres.Configuration do
     base_sql <> tables
   end
 
-  @spec make_table_clause({Electric.relation(), filter()}) :: String.t()
-  defp make_table_clause({{schema, tbl}, nil}) do
-    Utils.relation_to_sql({schema, tbl})
-  end
+  @spec make_table_clause(RelationFilter.t()) :: String.t()
+  defp make_table_clause(%RelationFilter{
+         relation: relation,
+         where_clauses: where_clauses,
+         selected_columns: cols
+       }) do
+    table = Utils.relation_to_sql(relation)
+    cols = if cols == nil, do: "", else: " (#{Enum.join(cols, ", ")})"
 
-  defp make_table_clause({{schema, tbl}, where}) do
-    table = Utils.relation_to_sql({schema, tbl})
-    table <> " WHERE " <> where
+    where =
+      if where_clauses == nil, do: "", else: " WHERE " <> "(#{Enum.join(where_clauses, " OR ")})"
+
+    table <> cols <> where
   end
 end
--- packages/sync-service/lib/electric/postgres/publication_manager.ex
+++ packages/sync-service/lib/electric/postgres/publication_manager.ex
@@ -1,101 +1,289 @@
 defmodule Electric.Postgres.PublicationManager do
+  require Logger
+
   use GenServer
+
+  alias Electric.Postgres.Configuration
+  alias Electric.Replication.Eval.Expr
   alias Electric.Shapes.Shape
 
-  @type relation_filter :: %{
-          relation: Electric.relation(),
-          where_clauses: MapSet.t(Electric.Replication.Eval.Expr.t()),
-          selected_columns: MapSet.t(String.t())
-        }
-
   defstruct [
-    :publication_filters_table
+    :relation_filter_counters,
+    :prepared_relation_filters,
+    :committed_relation_filters,
+    :update_debounce_timeout,
+    :scheduled_updated_ref,
+    :waiters,
+    :publication_name,
+    :pool,
+    :get_pg_version
   ]
 
-  @relation_filters :relation_filters
+  @type opts() :: %{
+          required(:publication_name) => String.t(),
+          required(:pool) => term(),
+          required(:get_pg_version) => (-> String.t()),
+          optional(:name) => atom(),
+          optional(:update_debounce_timeout) => timeout(),
+          optional(:server) => any()
+        }
+
+  @typep state() :: %__MODULE__{
+           relation_filter_counters: %{Electric.relation() => map()},
+           prepared_relation_filters: %{Electric.relation() => __MODULE__.RelationFilter.t()},
+           committed_relation_filters: %{Electric.relation() => __MODULE__.RelationFilter.t()},
+           update_debounce_timeout: timeout(),
+           scheduled_updated_ref: nil | reference(),
+           waiters: list(GenServer.from()),
+           publication_name: String.t(),
+           pool: term(),
+           get_pg_version: (-> String.t())
+         }
+  @typep filter_operation :: :add | :remove
+
+  defmodule RelationFilter do
+    defstruct [:relation, :where_clauses, :selected_columns]
+
+    @type t :: %__MODULE__{
+            relation: Electric.relation(),
+            where_clauses: [Electric.Replication.Eval.Expr.t()] | nil,
+            selected_columns: [String.t()] | nil
+          }
+  end
+
+  @retry_timeout 1_000
+  @default_debounce_timeout 50
+
+  @relation_counter :relation_counter
   @relation_where :relation_where
   @relation_column :relation_column
 
-  def add_shape(%__MODULE__{} = publication_manager, shape) do
-    update_relation_counters(publication_manager, shape, +1)
+  @spec add_shape(Shape.t(), Keyword.t()) :: :ok
+  def add_shape(shape, opts \\ []) do
+    server = Access.get(opts, :server, __MODULE__)
+    GenServer.call(server, {:add_shape, shape})
   end
 
-  def remove_shape(%__MODULE__{} = publication_manager, shape) do
-    update_relation_counters(publication_manager, shape, -1)
+  @spec remove_shape(Shape.t(), Keyword.t()) :: :ok
+  def remove_shape(shape, opts \\ []) do
+    server = Access.get(opts, :server, __MODULE__)
+    GenServer.call(server, {:remove_shape, shape})
   end
 
+  @spec start_link(opts()) :: GenServer.on_start()
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, opts)
+    GenServer.start_link(Access.get(opts, :name, __MODULE__), opts)
   end
 
-  def init(%{stack_id: stack_id} = opts) do
-    publication_filters_table =
-      :ets.new(:"#{stack_id}:publication_filters", [:named_table, :public, :ordered_set])
+  @impl true
+  def init(opts) do
+    state = %__MODULE__{
+      relation_filter_counters: %{},
+      prepared_relation_filters: %{},
+      committed_relation_filters: %{},
+      scheduled_updated_ref: nil,
+      update_debounce_timeout:
+        Access.get(opts, :update_debounce_timeout, @default_debounce_timeout),
+      publication_name: Access.fetch!(opts, :publication_name),
+      pool: Access.fetch!(opts, :pool),
+      get_pg_version: Access.fetch!(opts, :get_pg_version)
+    }
 
-    {:ok,
-     %{
-       publication_filters_table: publication_filters_table
-     }}
+    {:ok, state}
   end
 
-  defp update_relation_counters(
-         %__MODULE__{publication_filters_table: ets_table} = _publication_manager,
-         %{root_table: table, where: where, selected_columns: columns} = _shape,
-         increment
+  @impl true
+  def handle_call({:add_shape, shape}, from, state) do
+    state = update_relation_filters_for_shape(shape, :add, state)
+    state = add_waiter(from, state)
+    state = schedule_update_publication(state.update_debounce_timeout, state)
+    {:noreply, state}
+  end
+
+  def handle_call({:remove_shape, shape}, from, state) do
+    state = update_relation_filters_for_shape(shape, :remove, state)
+    state = add_waiter(from, state)
+    state = schedule_update_publication(state.update_debounce_timeout, state)
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info(
+        :update_publication,
+        %__MODULE__{prepared_relation_filters: relation_filters} = state
+      ) do
+    case update_publication(state) do
+      :ok ->
+        state = reply_to_waiters(:ok, state)
+        {:noreply, %{state | committed_relation_filters: relation_filters}}
+
+      {:error, err} ->
+        Logger.error("Failed to configure publication for replication: #{inspect(err)}")
+        state = schedule_update_publication(@retry_timeout, state)
+        {:noreply, state}
+    end
+  end
+
+  @spec schedule_update_publication(timeout(), state()) :: state()
+  defp schedule_update_publication(timeout, %__MODULE__{scheduled_updated_ref: nil} = state) do
+    ref = Process.send_after(self(), :update_publication, timeout)
+    %{state | scheduled_updated_ref: ref}
+  end
+
+  @spec update_publication(state()) :: :ok | {:error, term()}
+  defp update_publication(
+         %__MODULE__{
+           committed_relation_filters: committed_filters,
+           prepared_relation_filters: current_filters
+         } = _state
+       )
+       when current_filters == committed_filters,
+       do: :ok
+
+  defp update_publication(
+         %__MODULE__{
+           prepared_relation_filters: relation_filters,
+           publication_name: publication_name,
+           pool: pool,
+           get_pg_version: get_pg_version
+         } = _state
        ) do
-    key = {@relation_filters, table, @relation_where, where}
-    :ets.update_counter(ets_table, key, increment, {key, 0})
-
-    Enum.each(columns, fn col ->
-      key = {@relation_filters, table, @relation_column, col}
-      :ets.update_counter(ets_table, key, increment, {key, 0})
-    end)
+    Configuration.configure_tables_for_replication!(
+      pool,
+      Map.values(relation_filters),
+      get_pg_version,
+      publication_name
+    )
 
     :ok
+  rescue
+    err -> {:error, err}
   end
 
-  @spec get_shape_filters(%__MODULE__{}, Electric.Shapes.Shape.t()) ::
-          nil | %{Electric.relation() => relation_filter()}
-  defp get_shape_filters(%__MODULE__{publication_filters_table: ets_table}, shape) do
-    relations = Shape.affected_tables(shape)
+  @spec update_relation_filters_for_shape(Shape.t(), filter_operation(), state()) :: state()
+  defp update_relation_filters_for_shape(
+         %Shape{root_table: relation} = shape,
+         operation,
+         %__MODULE__{prepared_relation_filters: prepared_relation_filters} = state
+       ) do
+    state = update_relation_filter_counters(shape, operation, state)
+    new_relation_filter = get_relation_filter(relation, state)
 
-    matched_filters =
-      :ets.select(ets_table, [
-        {
-          {{@relation_filters, :"$1", :"$2", :"$3"}, :"$4"},
-          [Enum.map(relations, &{:==, :"$1", &1}) | [{:>, :"$4", 0}]],
-          [{:"$1", :"$2", :"$3"}]
-        }
-      ])
+    new_relation_filters =
+      if new_relation_filter == nil,
+        do: Map.delete(prepared_relation_filters, relation),
+        else: Map.put(prepared_relation_filters, relation, new_relation_filter)
 
-    case matched_filters do
-      [] ->
+    %{state | prepared_relation_filters: new_relation_filters}
+  end
+
+  @spec get_relation_filter(Electric.relation(), state()) :: RelationFilter.t() | nil
+  defp get_relation_filter(
+         relation,
+         %__MODULE__{relation_filter_counters: relation_filter_counters} = _state
+       ) do
+    case Map.get(relation_filter_counters, relation) do
+      nil ->
         nil
 
-      filters ->
-        filters
-        |> Enum.group_by(fn {relation, _, _} -> relation end, fn {_, filter_tag, filter} ->
-          {filter_tag, filter}
-        end)
-        |> Enum.map(fn {relation, filters} ->
-          {relation,
-           Enum.reduce(
-             filters,
-             %{
-               relation: relation,
-               where_clauses: MapSet.new(),
-               selected_columns: MapSet.new()
-             },
-             fn
-               {@relation_where, where}, acc ->
-                 %{acc | where_clauses: MapSet.put(acc.where_clauses, where)}
+      filter_counters ->
+        Enum.reduce(
+          Map.keys(filter_counters),
+          %RelationFilter{relation: relation, where_clauses: [], selected_columns: []},
+          fn
+            {@relation_counter, _}, acc ->
+              acc
 
-               {@relation_column, column}, acc ->
-                 %{acc | selected_columns: MapSet.put(acc.selected_columns, column)}
-             end
-           )}
-        end)
-        |> Map.new()
+            {@relation_column, nil}, acc ->
+              %RelationFilter{acc | selected_columns: nil}
+
+            {@relation_column, _col}, %{selected_columns: nil} = acc ->
+              acc
+
+            {@relation_column, col}, %{selected_columns: cols} = acc ->
+              %RelationFilter{acc | selected_columns: [col | cols]}
+
+            {@relation_where, nil}, acc ->
+              %RelationFilter{acc | where_clauses: nil}
+
+            {@relation_where, _where}, %{where_clauses: nil} = acc ->
+              acc
+
+            {@relation_where, where}, %{where_clauses: wheres} = acc ->
+              %RelationFilter{acc | where_clauses: [where | wheres]}
+          end
+        )
     end
+  end
+
+  @spec update_relation_filter_counters(Shape.t(), filter_operation(), state()) :: state()
+  defp update_relation_filter_counters(
+         %Shape{root_table: table} = shape,
+         operation,
+         %__MODULE__{relation_filter_counters: relation_filter_counters} = state
+       ) do
+    increment = if operation == :add, do: 1, else: -1
+    filter_counters = Map.get(relation_filter_counters, table, %{})
+
+    {relation_ctr, filter_counters} =
+      update_map_counter(filter_counters, @relation_counter, increment)
+
+    if relation_ctr > 0 do
+      filter_counters =
+        Enum.concat(
+          get_selected_columns_for_shape(shape) |> Enum.map(&{@relation_column, &1}),
+          get_where_clauses_for_shape(shape) |> Enum.map(&{@relation_where, &1})
+        )
+        |> Enum.reduce(filter_counters, fn col, filter ->
+          {_, filter} = update_map_counter(filter, col, increment)
+          filter
+        end)
+
+      %{
+        state
+        | prepared_relation_filters: Map.put(relation_filter_counters, table, filter_counters)
+      }
+    else
+      %{state | prepared_relation_filters: Map.delete(relation_filter_counters, table)}
+    end
+  end
+
+  @spec update_map_counter(map(), any(), integer()) :: {any(), map()}
+  defp update_map_counter(map, key, inc) do
+    Map.get_and_update(map, key, fn
+      nil when inc < 0 -> :pop
+      ctr when ctr + inc < 0 -> :pop
+      nil -> {nil, inc}
+      ctr -> {ctr, ctr + inc}
+    end)
+  end
+
+  @spec get_selected_columns_for_shape(Shape.t()) :: MapSet.t(String.t() | nil)
+  defp get_selected_columns_for_shape(%Shape{where: _, selected_columns: nil}),
+    do: MapSet.new(nil)
+
+  defp get_selected_columns_for_shape(%Shape{where: nil, selected_columns: columns}),
+    do: MapSet.new(columns)
+
+  defp get_selected_columns_for_shape(%Shape{where: where, selected_columns: columns}) do
+    # If columns are selected, include columns used in the where clause
+    where_cols = where |> Expr.current_table_refs() |> MapSet.new()
+    MapSet.union(MapSet.new(columns), where_cols)
+  end
+
+  @spec get_where_clauses_for_shape(Shape.t()) ::
+          MapSet.t(Electric.Replication.Eval.Expr.t() | nil)
+  defp get_where_clauses_for_shape(%Shape{where: nil}), do: MapSet.new(nil)
+  # TODO: flatten where clauses by splitting top level ANDs
+  defp get_where_clauses_for_shape(%Shape{where: where}), do: MapSet.new(where)
+
+  @spec add_waiter(GenServer.from(), state()) :: state()
+  defp add_waiter(from, %__MODULE__{waiters: waiters} = state),
+    do: %{state | waiters: [from | waiters]}
+
+  @spec reply_to_waiters(any(), state()) :: state()
+  defp reply_to_waiters(reply, %__MODULE__{waiters: waiters} = state) do
+    for from <- waiters, do: GenServer.reply(from, reply)
+    %{state | waiters: []}
   end
 end
