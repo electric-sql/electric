985ad8431109262192b35874b055cf75c3871d56
Valter Balegas <balegas@live.com>
2024-12-06 13:08:11 +0000

    Hybrid implementation with server and serverless

--- examples/yjs-provider/app/api/operation/route.ts
+++ examples/yjs-provider/app/api/operation/route.ts
@@ -1,55 +1,53 @@
-import { Client } from "pg"
+import { Pool } from "pg"
 import { NextResponse } from "next/server"
+import { neon } from "@neondatabase/serverless"
 
-// TODO: need to use a connection pool for non-serverless deployments
+// hybrid implementation for connection pool and serverless
 
 const connectionString =
   process.env.POOLED_DATABASE_URL ||
   process.env.DATABASE_URL ||
   `postgresql://postgres:password@localhost:54321/electric`
 
+const sql = process.env.POOLED_DATABASE_URL ? neon(connectionString) : undefined
+
+const pool = process.env.DATABASE_URL
+  ? new Pool({ connectionString: process.env.DATABASE_URL })
+  : undefined
+let connected = false
+
 export async function POST(request: Request) {
-  const client = new Client({
-    connectionString,
-  })
+  console.log(`Received request: ${request}`)
   try {
     const { room, op, clientId } = await getRequestParams(request)
-    await client.connect()
-
     if (!clientId) {
-      await saveOperation(room, op, client)
+      await saveOperation(room, op)
     } else {
-      await saveAwarenessOperation(room, op, clientId, client)
+      await saveAwarenessOperation(room, op, clientId)
     }
-
     return NextResponse.json({})
   } catch (e) {
     const resp = e instanceof Error ? e.message : e
     return NextResponse.json(resp, { status: 400 })
-  } finally {
-    client.end()
   }
 }
 
-async function saveOperation(room: string, op: string, connection: Client) {
-  await connection.query(
-    `INSERT INTO ydoc_operations (room, op) VALUES ($1, decode($2, 'base64'))`,
-    [room, op]
-  )
+async function saveOperation(room: string, op: string) {
+  const q = `INSERT INTO ydoc_operations (room, op) VALUES ($1, decode($2, 'base64'))`
+  const params = [room, op]
+  await runQuery(q, params)
 }
 
 async function saveAwarenessOperation(
   room: string,
   op: string,
-  clientId: string,
-  connection: Client
+  clientId: string
 ) {
-  await connection.query(
-    `INSERT INTO ydoc_awareness (room, clientId, op) VALUES ($1, $2, decode($3, 'base64'))
+  const q = `INSERT INTO ydoc_awareness (room, clientId, op) VALUES ($1, $2, decode($3, 'base64'))
        ON CONFLICT (clientId, room)
-       DO UPDATE SET op = decode($3, 'base64')`,
-    [room, clientId, op]
-  )
+       DO UPDATE SET op = decode($3, 'base64')`
+  const params = [room, clientId, op]
+  await runQuery(q, params)
 }
 
 async function getRequestParams(
@@ -64,4 +62,21 @@ async function getRequestParams(
   }
 
   return { room, op, clientId }
+}
+
+async function runQuery(q: string, params: string[]) {
+  console.log(`Running query: ${q} with params: ${params}`, pool, sql)
+  if (pool) {
+    if (pool && !connected) {
+      await pool.connect()
+      connected = true
+    }
+
+    await pool.query(q, params)
+  }
+  if (sql) {
+    await sql(q, params)
+  } else {
+    throw new Error(`No database driver provided`)
+  }
 }
