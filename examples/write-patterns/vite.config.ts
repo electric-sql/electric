db0eb3af5d7f35fa9fda11ad0d282648b1436495
James Arthur <thruflo@gmail.com>
2024-12-10 19:27:47 +0100

    example: reenable vite-pwa, not the culprit!

--- examples/write-patterns/vite.config.ts
+++ examples/write-patterns/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-// import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import react from '@vitejs/plugin-react'
 
@@ -13,33 +13,33 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   workbox: {
-    //     maximumFileSizeToCacheInBytes: 10 * 1024 ** 2,
-    //   },
-    //   devOptions: {
-    //     enabled: false,
-    //   },
-    //   includeAssets: ['shared/app/icons/*'],
-    //   manifest: {
-    //     name: 'Write Patterns Example - ElectricSQL ',
-    //     short_name: 'Writes',
-    //     description: 'Four different write-patterns that work with Electric.',
-    //     theme_color: '#1c1e20',
-    //     icons: [
-    //       {
-    //         src: './shared/app/icons/icon-192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: './shared/app/icons/icon-512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //     ],
-    //   },
-    // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 ** 2,
+      },
+      devOptions: {
+        enabled: false,
+      },
+      includeAssets: ['shared/app/icons/*'],
+      manifest: {
+        name: 'Write Patterns Example - ElectricSQL ',
+        short_name: 'Writes',
+        description: 'Four different write-patterns that work with Electric.',
+        theme_color: '#1c1e20',
+        icons: [
+          {
+            src: './shared/app/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './shared/app/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
 })
