eeba768409970c1bb68921e73d800152b1ae40c8
Kyle Mathews <mathews.kyle@gmail.com>
2024-12-10 13:28:42 -0700

    fix experimental package build (#2144)
    Align it with typescript-client/react-hooks packages.

--- packages/experimental/src/match.ts
+++ packages/experimental/src/match.ts
@@ -6,6 +6,7 @@ import {
   type Operation,
   type Row,
   type Value,
+  type Message,
 } from '@electric-sql/client'
 
 export function matchStream<T extends Row<unknown>>(
@@ -15,27 +16,33 @@ export function matchStream<T extends Row<unknown>>(
   timeout = 60000 // ms
 ): Promise<ChangeMessage<T>> {
   return new Promise<ChangeMessage<T>>((resolve, reject) => {
-    const unsubscribe: () => void = stream.subscribe((messages) => {
-      const message = messages.filter(isChangeMessage).find((message) => {
-        const operation = message.headers.operation
+    const unsubscribe: () => void = stream.subscribe(
+      (messages: Array<unknown>) => {
+        const message = messages
+          .filter((msg): msg is ChangeMessage<T> =>
+            isChangeMessage(msg as Message<Row<never>>)
+          )
+          .find((message) => {
+            const operation: Operation = message.headers.operation
 
-        return operations.includes(operation) && matchFn(message)
-      })
+            return operations.includes(operation) && matchFn(message)
+          })
 
-      if (message) {
-        return finish(message)
+        if (message) {
+          return finish(message)
+        }
       }
-    })
+    )
 
-    const timeoutId = setTimeout(() => {
-      const msg = `matchStream timed out after ${timeout}ms`
+    const timeoutId: NodeJS.Timeout = setTimeout(() => {
+      const msg: string = `matchStream timed out after ${timeout}ms`
 
       console.error(msg)
 
       reject(msg)
     }, timeout)
 
-    function finish(message: ChangeMessage<T>) {
+    function finish(message: ChangeMessage<T>): void {
       clearTimeout(timeoutId)
 
       unsubscribe()
@@ -49,5 +56,5 @@ export function matchBy<T extends Row<unknown>>(
   column: string,
   value: Value<GetExtensions<T>>
 ): (message: ChangeMessage<T>) => boolean {
-  return (message) => message.value[column] === value
+  return (message: ChangeMessage<T>) => message.value[column] === value
 }
