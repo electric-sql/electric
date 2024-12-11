12d849d1a6f0246793208e7043baf0d7140e97b0
Kevin De Porre <kevin@electric-sql.com>
2024-12-11 15:27:10 +0100

    Fixed formatting

--- examples/remix/app/db.ts
+++ examples/remix/app/db.ts
@@ -1,7 +1,9 @@
 import pgPkg from "pg"
 const { Pool } = pgPkg
 
-const connectionString = process.env.DATABASE_URL ?? `postgresql://postgres:password@localhost:54321/electric`
+const connectionString =
+  process.env.DATABASE_URL ??
+  `postgresql://postgres:password@localhost:54321/electric`
 const pool = new Pool({ connectionString })
 
 export { pool }
