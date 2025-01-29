244135127802a4d2dbeca145ff84324a828af246
Valter Balegas <balegas@electric-sql.com>
2025-01-29 13:08:56 +0000

    unstuck

--- examples/write-patterns/sst.config.ts
+++ examples/write-patterns/sst.config.ts
@@ -145,31 +145,28 @@ function applyMigrations(uri: string) {
 }
 
 async function addDatabaseToElectric(
-  database_url: string,
-  region: 'us-east-1' | 'eu-west-1' = 'us-east-1'
+  uri: string
 ): Promise<{ id: string; token: string }> {
   const adminApi = process.env.ELECTRIC_ADMIN_API
   const teamId = process.env.ELECTRIC_TEAM_ID
 
-  const result = await fetch(new URL('v1/sources', adminApi), {
-    method: 'PUT',
+  const result = await fetch(`${adminApi}/v1/sources`, {
+    method: `PUT`,
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': `application/json`,
       'CF-Access-Client-Id': adminApiTokenId ?? '',
       'CF-Access-Client-Secret': adminApiTokenSecret ?? '',
     },
     body: JSON.stringify({
-      database_url,
-      region,
+      database_url: uri,
+      region: `us-east-1`,
       team_id: teamId,
     }),
   })
 
   if (!result.ok) {
     throw new Error(
-      `Could not add database to Electric (${
-        result.status
-      }): ${await result.text()}`
+      `Could not add database to Electric (${result.status}): ${await result.text()}`
     )
   }
 
