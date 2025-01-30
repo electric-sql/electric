0c620c8e0f8d322e34fd8a87e79eb1ba9e758506
msfstef <msfstef@gmail.com>
2025-01-30 15:44:19 +0200

    Move things around

--- examples/yjs/sst.config.ts
+++ examples/yjs/sst.config.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
 
-import { getSharedCluster, isProduction } from "../.shared/infra"
-import { createDatabaseForCloudElectric } from "../.shared/database"
+import { getSharedCluster, isProduction } from "../.shared/lib/infra"
+import { createDatabaseForCloudElectric } from "../.shared/lib/database"
 
 export default $config({
   app(input) {
@@ -22,45 +22,41 @@ export default $config({
     }
   },
   async run() {
-    try {
-      const dbName = isProduction() ? `yjs` : `yjs-db-${$app.stage}`
+    const dbName = isProduction() ? `yjs` : `yjs-db-${$app.stage}`
 
-      const { databaseUri, sourceId, sourceSecret } =
-        createDatabaseForCloudElectric({
-          dbName,
-          migrationsDirectory: `./db/migrations`,
-        })
-
-      const cluster = getSharedCluster(`yjs-service`)
-
-      const service = cluster.addService(`yjs-service`, {
-        loadBalancer: {
-          ports: [{ listen: `443/https`, forward: `3000/http` }],
-          domain: {
-            name: `yjs${isProduction() ? `` : `-${$app.stage}`}.examples.electric-sql.com`,
-            dns: sst.cloudflare.dns(),
-          },
-        },
-        environment: {
-          ELECTRIC_URL: process.env.ELECTRIC_API!,
-          DATABASE_URL: databaseUri,
-          ELECTRIC_SOURCE_ID: sourceId,
-          ELECTRIC_SOURCE_SECRET: sourceSecret,
-        },
-        image: {
-          context: `../..`,
-          dockerfile: `Dockerfile`,
-        },
-        dev: {
-          command: `npm run dev`,
-        },
+    const { databaseUri, sourceId, sourceSecret } =
+      createDatabaseForCloudElectric({
+        dbName,
+        migrationsDirectory: `./db/migrations`,
       })
 
-      return {
-        website: service.url,
-      }
-    } catch (e) {
-      console.error(e)
+    const cluster = getSharedCluster(`yjs-service`)
+
+    const service = cluster.addService(`yjs-service`, {
+      loadBalancer: {
+        ports: [{ listen: `443/https`, forward: `3000/http` }],
+        domain: {
+          name: `yjs${isProduction() ? `` : `-${$app.stage}`}.examples.electric-sql.com`,
+          dns: sst.cloudflare.dns(),
+        },
+      },
+      environment: {
+        ELECTRIC_URL: process.env.ELECTRIC_API!,
+        DATABASE_URL: databaseUri,
+        ELECTRIC_SOURCE_ID: sourceId,
+        ELECTRIC_SOURCE_SECRET: sourceSecret,
+      },
+      image: {
+        context: `../..`,
+        dockerfile: `Dockerfile`,
+      },
+      dev: {
+        command: `npm run dev`,
+      },
+    })
+
+    return {
+      website: service.url,
     }
   },
 })
