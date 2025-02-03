3e3fc8c3e9d7655f76922b978f9ba09420ad372d
Garry Hill <garry@magnetised.net>
2025-02-03 14:16:56 +0000

    ignore 404s from delete shape
    it's probabaly just that the shape wasn't created

--- packages/typescript-client/test/support/test-context.ts
+++ packages/typescript-client/test/support/test-context.ts
@@ -48,20 +48,26 @@ export const testWithDbClient = test.extend<{
       ) => {
         const baseUrl = inject(`baseUrl`)
         const url = new URL(`${baseUrl}/v1/shape`)
-        url.searchParams.set(`table`, table)
 
         if (options.handle) {
           url.searchParams.set(SHAPE_HANDLE_QUERY_PARAM, options.handle)
+        } else {
+          url.searchParams.set(`table`, table)
         }
 
         const resp = await fetch(url.toString(), { method: `DELETE` })
         if (!resp.ok) {
-          console.error(
-            await FetchError.fromResponse(resp, `DELETE ${url.toString()}`)
-          )
-          throw new Error(
-            `Could not delete shape ${table} with ID ${options.handle}`
-          )
+          // if we've been passed a shape handle then we should expect this delete call to succeed.
+          if (!options.handle && resp.status === 404) {
+            // the shape wasn't found, so maybe it wasn't created in the first place
+          } else {
+            console.error(
+              await FetchError.fromResponse(resp, `DELETE ${url.toString()}`)
+            )
+            throw new Error(
+              `Could not delete shape ${table} with ID ${options.handle}`
+            )
+          }
         }
       }
     )
