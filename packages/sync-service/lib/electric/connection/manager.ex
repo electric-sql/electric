electric-sql/electric

commit 5516d70816eda330ab5067e12d1ca45927f9a00f

fix: Fallback to IPv4 from IPv6 (#2283)
When `DATABASE_USE_IPV6` is enabled, but the provided DB URL has no
resolvable IPv6 address (no AAAA record), naively fallback to IPv4.

A more general discussion on this fallback behaviour for Erlang's
networking stack can be found here
https://github.com/erlang/otp/issues/8548

Ideally we would be able to handle this sort of fallback directly with
Postgrex, and it used to have the possibility to specify multiple
endpoints with different options that it would try sequentially, but it
is no longer supported.

I have opened an issue to see if there is a potential solution to this
that can be supported by Postgrex directly
https://github.com/elixir-ecto/postgrex/issues/730

Perhaps once we are happy with both IPv6 and IPv4 being supported out of
the box we can remove the `DATABASE_USE_IPV6` option and always default
to supporting it, but I think it's safer to explore this a bit further.
This PR introduces a naive fix that does not cover all possible failure
and fallback cases, but unblocks the basic case of AAAA records missing
while also supporting IPv6.

packages/sync-service/lib/electric/connection/manager.ex
@@ -62,6 +62,7 @@ defmodule Electric.Connection.Manager do
       # Registry used for stack events
       :stack_events_registry,
       :tweaks,
+      :ipv6_enabled,
       awaiting_active: [],
       drop_slot_requested: false,
       monitoring_started?: false
@@ -187,7 +188,8 @@ defmodule Electric.Connection.Manager do
         backoff: {:backoff.init(1000, 10_000), nil},
         stack_id: Keyword.fetch!(opts, :stack_id),
         stack_events_registry: Keyword.fetch!(opts, :stack_events_registry),
-        tweaks: Keyword.fetch!(opts, :tweaks)
+        tweaks: Keyword.fetch!(opts, :tweaks),
+        ipv6_enabled: connection_opts[:ipv6]
       }
 
     # Try to acquire the connection lock on the replication slot
@@ -525,6 +527,33 @@ defmodule Electric.Connection.Manager do
     handle_connection_error(error, state, mode)
   end
 
+  defp handle_connection_error(
+         %DBConnection.ConnectionError{message: message} = error,
+         %State{connection_opts: connection_opts, ipv6_enabled: true} = state,
+         mode
+       ) do
+    # If domain cannot be resolved, assume there is no AAAA record for it
+    # and fall back to IPv4 and regular A records
+    if String.starts_with?(message, "tcp connect (") and
+         String.ends_with?(message, "): non-existing domain - :nxdomain") do
+      Logger.warning(
+        "Database connection in #{mode} mode failed to find valid IPv6 address for #{connection_opts[:hostname]} - falling back to IPv4"
+      )
+
+      # disable IPv6 and retry immediately
+      state = %{
+        state
+        | ipv6_enabled: false,
+          connection_opts: connection_opts |> Keyword.put(:ipv6, false) |> update_tcp_opts()
+      }
+
+      step = current_connection_step(state)
+      handle_continue(step, state)
+    else
+      handle_connection_error(error, state, mode)
+    end
+  end
+
   defp handle_connection_error(error, state, mode) do
     halt_if_fatal_error!(error)
 
@@ -548,16 +577,19 @@ defmodule Electric.Connection.Manager do
       {:database_connection_failed, message}
     )
 
-    step =
-      cond do
-        is_nil(state.lock_connection_pid) -> :start_lock_connection
-        is_nil(state.replication_client_pid) -> :start_replication_client
-        is_nil(state.pool_pid) -> :start_connection_pool
-      end
-
+    step = current_connection_step(state)
     state = schedule_reconnection(step, state)
     {:noreply, state}
   end
+
+  defp current_connection_step(%State{lock_connection_pid: pid}) when not is_pid(pid),
+    do: :start_lock_connection
+
+  defp current_connection_step(%State{replication_client_pid: pid}) when not is_pid(pid),
+    do: :start_replication_client
+
+  defp current_connection_step(%State{pool_pid: pid}) when not is_pid(pid),
+    do: :start_connection_pool
 
   defp pg_error_extra_info(pg_error) do
     extra_info_items =

commit a13573189af49566e2fac0a9897dcbe5d5b4f567

fix: Passthrough errors to avoid infinite loop when falling back to IPv4 (#2293)
Fixes issue introduced in
https://github.com/electric-sql/electric/pull/2283

The passthrough would not work and if an error came through that wasn't
`:nxdomain` it would enter an infinite loop (whoops).

I've added the case of an error where network is unreachable that
@samwillis run into, assuming it's because IPv6 is not enabled on the
machine, but am reluctant on disabling IPv6 on _all_ `DBConnection`
errors or tcp errors.

We definitely do not want to make IPv6 enabled by default until we sort
this out.

packages/sync-service/lib/electric/connection/manager.ex
@@ -528,14 +528,16 @@ defmodule Electric.Connection.Manager do
   end
 
   defp handle_connection_error(
-         %DBConnection.ConnectionError{message: message} = error,
+         %DBConnection.ConnectionError{message: message, severity: :error},
          %State{connection_opts: connection_opts, ipv6_enabled: true} = state,
          mode
        ) do
+    # If network is unreachable, IPv6 is not enabled on the machine
     # If domain cannot be resolved, assume there is no AAAA record for it
-    # and fall back to IPv4 and regular A records
+    # Fall back to IPv4 for these cases
     if String.starts_with?(message, "tcp connect (") and
-         String.ends_with?(message, "): non-existing domain - :nxdomain") do
+         (String.ends_with?(message, "): non-existing domain - :nxdomain") or
+            String.ends_with?(message, "): network is unreachable - :enetunreach")) do
       Logger.warning(
         "Database connection in #{mode} mode failed to find valid IPv6 address for #{connection_opts[:hostname]} - falling back to IPv4"
       )
@@ -550,7 +552,8 @@ defmodule Electric.Connection.Manager do
       step = current_connection_step(state)
       handle_continue(step, state)
     else
-      handle_connection_error(error, state, mode)
+      # Pass through other errors to avoid infinite loop
+      handle_connection_error({:passthrough, message}, state, mode)
     end
   end
 
@@ -567,6 +570,9 @@ defmodule Electric.Connection.Manager do
 
         %Postgrex.Error{postgres: %{message: message} = pg_error} ->
           message <> pg_error_extra_info(pg_error)
+
+        {:passthrough, message} ->
+          message
       end
 
     Logger.warning("Database connection in #{mode} mode failed: #{message}")