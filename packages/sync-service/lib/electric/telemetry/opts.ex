86c5e9a2815d44f8c5f2b0f3d9bf55e51fb1e314
rob <rob.acourt@gmail.com>
2025-02-17 14:25:00 +0000

    Fix opts defaults

--- packages/sync-service/lib/electric/telemetry/opts.ex
+++ packages/sync-service/lib/electric/telemetry/opts.ex
@@ -2,8 +2,8 @@ defmodule Electric.Telemetry.Opts do
   def schema do
     [
       instance_id: [type: :string],
-      system_metrics_poll_interval: [type: :integer],
-      statsd_host: [type: :string],
+      system_metrics_poll_interval: [type: :integer, default: :timer.seconds(5)],
+      statsd_host: [type: {:or, [:string, nil]}, default: nil],
       prometheus?: [type: :boolean, default: false],
       call_home_telemetry?: [type: :boolean, default: false],
       otel_metrics?: [type: :boolean, default: false]
