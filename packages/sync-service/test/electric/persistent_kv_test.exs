126317f46a5e54aa18c827bec257140e1913eb54
Ilia Borovitinov <ilia@electric-sql.com>
2025-02-26 14:11:36 +0300

    fix: correctly set global LSN as a number in Filesystem KV (#2377)

--- packages/sync-service/test/electric/persistent_kv_test.exs
+++ packages/sync-service/test/electric/persistent_kv_test.exs
@@ -1,9 +1,26 @@
 defmodule Electric.PersistentKVTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case,
+    async: true,
+    parameterize:
+      for(
+        mod <- [Electric.PersistentKV.Filesystem, Electric.PersistentKV.Memory],
+        do: %{mod: mod}
+      )
+
+  @moduletag :tmp_dir
+  setup %{mod: mod} = ctx do
+    case mod do
+      Electric.PersistentKV.Filesystem ->
+        {:ok, %{kv: mod.new!(root: ctx.tmp_dir)}}
+
+      Electric.PersistentKV.Memory ->
+        {:ok, %{kv: mod.new!()}}
+    end
+  end
 
   alias Electric.PersistentKV, as: KV
 
-  defp test_kv(kv) do
+  test "get and set", %{kv: kv} do
     key = "some/thing/here.data"
     value = :crypto.strong_rand_bytes(32)
 
@@ -12,25 +29,12 @@ defmodule Electric.PersistentKVTest do
     assert {:ok, ^value} = KV.get(kv, key)
   end
 
-  describe "PersistentKV.Filesystem" do
-    @moduletag :tmp_dir
+  test "get and set for non-string values", %{kv: kv} do
+    key = "some/thing/here.data"
+    value = 12345
 
-    setup(cxt) do
-      %{kv: Electric.PersistentKV.Filesystem.new!(root: cxt.tmp_dir)}
-    end
-
-    test "get and set", %{kv: kv} do
-      test_kv(kv)
-    end
-  end
-
-  describe "PersistentKV.Memory" do
-    setup(_cxt) do
-      %{kv: Electric.PersistentKV.Memory.new!()}
-    end
-
-    test "get and set", %{kv: kv} do
-      test_kv(kv)
-    end
+    assert {:error, :not_found} = KV.get(kv, key)
+    assert :ok = KV.set(kv, key, value)
+    assert {:ok, ^value} = KV.get(kv, key)
   end
 end
