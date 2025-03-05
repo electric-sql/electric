91d0beb318124544f1c28fdf673d419b03f1a523
msfstef <msfstef@gmail.com>
2025-03-05 18:43:12 +0200

    Revert "Add changeset" Revert "Separate task for listening to new changes"
    This reverts commit
    c65ba0076d81cbe2e5f62555029226468934d9ec
    6221f2aec7f1e79e0f31f91a7267dbb39b011062

--- .changeset/chilled-papayas-draw.md
+++
@@ -1,5 +0,0 @@
----
-"@core/sync-service": patch
----
-
-Listen to new changes through separate, ephemeral task.
--- packages/sync-service/lib/electric/shapes/api.ex
+++ packages/sync-service/lib/electric/shapes/api.ex
@@ -361,17 +361,7 @@ defmodule Electric.Shapes.Api do
     if LogOffset.compare(offset, last_offset) != :lt or
          last_offset == LogOffset.last_before_real_offsets() do
       ref = make_ref()
-      parent = self()
-
-      # Register for notifications using a separate task as the calling process
-      # might be reused across mutiple connections
-      Task.start_link(fn ->
-        Registry.register(registry, handle, ref)
-
-        receive do
-          msg when elem(msg, 0) == ref -> send(parent, msg)
-        end
-      end)
+      Registry.register(registry, handle, ref)
 
       Logger.debug("Client #{inspect(self())} is registered for changes to #{handle}")
 
