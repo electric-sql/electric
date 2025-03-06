electric-sql/electric

commit bbcc71905232da41182cfb1a6c3325cedb5b2380

fix: Drop transactions if already processed (#2382)
Fixes https://github.com/electric-sql/electric/issues/2372

In the above issue, the replication connection might drop after a slow
processing of a transaction, leading to a replication connection restart
and thus a replay of the transaction that has already been processed.

Rather than trying to maintain the connection alive, which is hard to do
without processing async, and if we do then we cannot throttle/apply
backpressure to the replication, we can just make sure we discard
actually processed transactions.

Currently we handle these replayed transactions by idempotently writing
the transactions, although I'm unsure if that holds under compaction as
well. Instead, since we are tracking the last processed LSN within the
`ShapeLogCollector`, I think we should discard transactions that we have
already seen wholesale.

By doing this, I think we can close
https://github.com/electric-sql/electric/issues/2372 because the
replication connection restarting is a low overhead and without
potential negative consequences because of replayed transactions.

Changes:
- Use the `State` struct for all constructions of the state in
`ReplicationClient`
- Update the `applied_wal` to the most recently applied transaction
commit LSN, rather than the replication message `wal_end` which from
what I understand (and see in testing) can be farther ahead.
- Add clause to `update_applied_wal` for cases where we already have
updated it to a higher LSN (see
[error](https://electricsql-04.sentry.io/issues/25080523/?environment=production&project=4508410462404688&query=is%3Aunresolved%20issue.priority%3A%5Bhigh%2C%20medium%5D&referrer=issue-stream&statsPeriod=24h&stream_index=0))
- Only advance the replication slot on heartbeats if we are not actively
collecting a transaction - I don't think we actually get interleaved
heartbeats between transactions being replicated, but felt like this

packages/sync-service/lib/electric/replication/shape_log_collector.ex
@@ -5,8 +5,7 @@ defmodule Electric.Replication.ShapeLogCollector do
   """
   use GenStage
 
-  alias Electric.PersistentKV
-  alias Electric.Postgres
+  alias Electric.Replication.PersistentReplicationState
   alias Electric.Postgres.Inspector
   alias Electric.Replication.Changes
   alias Electric.Replication.Changes.{Relation, Transaction}
@@ -61,17 +60,18 @@ defmodule Electric.Replication.ShapeLogCollector do
     Logger.metadata(stack_id: opts.stack_id)
     Electric.Telemetry.Sentry.set_tags_context(stack_id: opts.stack_id)
 
-    last_seen_lsn =
-      case PersistentKV.get(opts.persistent_kv, "#{opts.stack_id}:last_processed_lsn") do
-        {:ok, last_seen_lsn} -> last_seen_lsn
-        {:error, :not_found} -> 0
-      end
+    persistent_replication_data_opts = [
+      stack_id: opts.stack_id,
+      persistent_kv: opts.persistent_kv
+    ]
 
     state =
       Map.merge(opts, %{
         producer: nil,
         subscriptions: {0, MapSet.new()},
-        last_seen_lsn: last_seen_lsn
+        persistent_replication_data_opts: persistent_replication_data_opts,
+        last_seen_lsn:
+          PersistentReplicationState.get_last_processed_lsn(persistent_replication_data_opts)
       })
 
     # start in demand: :accumulate mode so that the ShapeCache is able to start
@@ -101,7 +101,14 @@ defmodule Electric.Replication.ShapeLogCollector do
   # client.
   def handle_demand(_demand, %{producer: producer} = state) do
     GenServer.reply(producer, :ok)
-    {:noreply, [], update_last_processed_lsn(%{state | producer: nil})}
+
+    :ok =
+      PersistentReplicationState.set_last_processed_lsn(
+        state.last_seen_lsn,
+        state.persistent_replication_data_opts
+      )
+
+    {:noreply, [], %{state | producer: nil}}
   end
 
   def handle_cancel({:cancel, _}, from, state) do
@@ -137,15 +144,17 @@ defmodule Electric.Replication.ShapeLogCollector do
   # will prompt the `GenServer.reply/2` call.
   defp handle_transaction(txn, _from, %{subscriptions: {0, _}} = state) do
     Logger.debug(fn -> "Dropping transaction #{txn.xid}: no active consumers" end)
+    drop_transaction(txn, state)
+  end
 
-    OpenTelemetry.add_span_attributes("txn.is_dropped": true)
+  # If we've already processed a transaction, then drop it without processing
+  defp handle_transaction(txn, _from, %{last_seen_lsn: last_seen_lsn} = state)
+       when txn.lsn <= last_seen_lsn do
+    Logger.debug(fn ->
+      "Dropping transaction #{txn.xid}: transaction LSN #{txn.lsn} smaller than last processed #{last_seen_lsn}"
+    end)
 
-    state =
-      state
-      |> put_last_seen_lsn(txn.lsn)
-      |> update_last_processed_lsn()
-
-    {:reply, :ok, [], state}
+    drop_transaction(txn, state)
   end
 
   defp handle_transaction(txn, from, state) do
@@ -183,6 +192,25 @@ defmodule Electric.Replication.ShapeLogCollector do
     {:noreply, [rel], %{state | producer: from}}
   end
 
+  defp drop_transaction(txn, state) do
+    OpenTelemetry.add_span_attributes("txn.is_dropped": true)
+
+    state =
+      if txn.lsn > state.last_seen_lsn do
+        :ok =
+          PersistentReplicationState.set_last_processed_lsn(
+            txn.lsn,
+            state.persistent_replication_data_opts
+          )
+
+        put_last_seen_lsn(state, txn.lsn)
+      else
+        state
+      end
+
+    {:reply, :ok, [], state}
+  end
+
   defp remove_subscription(from, %{subscriptions: {count, set}} = state) do
     subscriptions =
       if MapSet.member?(set, from) do
@@ -206,21 +234,7 @@ defmodule Electric.Replication.ShapeLogCollector do
     state
   end
 
-  defp put_last_seen_lsn(state, lsn) do
-    %{state | last_seen_lsn: max(state.last_seen_lsn, Postgres.Lsn.to_integer(lsn))}
-  end
-
-  defp update_last_processed_lsn(state) do
-    # state = %{state | last_processed_lsn: state.last_seen_lsn}
-    Logger.debug("Updating last processed lsn to #{state.last_seen_lsn}")
-
-    :ok =
-      PersistentKV.set(
-        state.persistent_kv,
-        "#{state.stack_id}:last_processed_lsn",
-        state.last_seen_lsn
-      )
-
-    state
+  defp put_last_seen_lsn(%{last_seen_lsn: last_seen_lsn} = state, lsn) do
+    %{state | last_seen_lsn: max(last_seen_lsn, lsn)}
   end
 end

packages/sync-service/test/electric/replication/shape_log_collector_test.exs
@@ -106,6 +106,7 @@ defmodule Electric.Replication.ShapeLogCollectorTest do
       xmin = 100
       xid = 150
       lsn = Lsn.from_string("0/10")
+      next_lsn = Lsn.increment(lsn, 1)
       last_log_offset = LogOffset.new(lsn, 0)
 
       Mock.Inspector
@@ -133,7 +134,7 @@ defmodule Electric.Replication.ShapeLogCollectorTest do
       assert xids == [xmin]
 
       txn2 =
-        %Transaction{xid: xid, lsn: lsn, last_log_offset: last_log_offset}
+        %Transaction{xid: xid, lsn: next_lsn, last_log_offset: last_log_offset}
         |> Transaction.prepend_change(%Changes.NewRecord{
           relation: {"public", "test_table"},
           record: %{"id" => "2", "name" => "bar"}
@@ -144,6 +145,51 @@ defmodule Electric.Replication.ShapeLogCollectorTest do
       xids = Support.TransactionConsumer.assert_consume(ctx.consumers, [txn2])
 
       assert xids == [xid]
+    end
+
+    test "drops transactions if already processed", ctx do
+      xid = 150
+      prev_lsn = Lsn.from_string("0/10")
+      lsn = Lsn.increment(prev_lsn, 1)
+
+      Mock.Inspector
+      |> stub(:load_relation, fn
+        {"public", "test_table"}, _ ->
+          {:ok, %{id: 1234, schema: "public", name: "test_table", parent: nil, children: nil}}
+      end)
+      |> stub(:load_column_info, fn {"public", "test_table"}, _ ->
+        {:ok, [%{pk_position: 0, name: "id"}]}
+      end)
+      |> allow(self(), ctx.server)
+
+      txn =
+        %Transaction{xid: xid, lsn: lsn, last_log_offset: LogOffset.new(lsn, 0)}
+        |> Transaction.prepend_change(%Changes.NewRecord{
+          relation: {"public", "test_table"},
+          record: %{"id" => "2", "name" => "foo"}
+        })
+
+      assert :ok = ShapeLogCollector.store_transaction(txn, ctx.server)
+
+      Support.TransactionConsumer.assert_consume(ctx.consumers, [txn])
+
+      txn2 =
+        %Transaction{xid: xid, lsn: lsn, last_log_offset: LogOffset.new(lsn, 0)}
+        |> Transaction.prepend_change(%Changes.NewRecord{
+          relation: {"public", "test_table"},
+          record: %{"id" => "3", "name" => "foo"}
+        })
+
+      txn3 =
+        %Transaction{xid: xid - 1, lsn: prev_lsn, last_log_offset: LogOffset.new(prev_lsn, 0)}
+        |> Transaction.prepend_change(%Changes.NewRecord{
+          relation: {"public", "test_table"},
+          record: %{"id" => "4", "name" => "foo"}
+        })
+
+      assert :ok = ShapeLogCollector.store_transaction(txn2, ctx.server)
+      assert :ok = ShapeLogCollector.store_transaction(txn3, ctx.server)
+      Support.TransactionConsumer.refute_consume(ctx.consumers)
     end
   end
 

packages/sync-service/test/support/transaction_consumer.ex
@@ -31,6 +31,12 @@ defmodule Support.TransactionConsumer do
     |> List.flatten()
   end
 
+  def refute_consume(consumers, timeout \\ 100) do
+    for consumer <- consumers, into: MapSet.new() do
+      refute_receive {Support.TransactionConsumer, ^consumer, _}, timeout
+    end
+  end
+
   def init(opts) do
     {:ok, producer} = Keyword.fetch(opts, :producer)
     {:ok, parent} = Keyword.fetch(opts, :parent)

packages/sync-service/lib/electric/connection/manager.ex
@@ -312,16 +312,24 @@ defmodule Electric.Connection.Manager do
     case start_connection_pool(state.connection_opts, state.pool_opts) do
       {:ok, pool_pid} ->
         state = mark_connection_succeeded(state)
-        # Checking the timeline continuity to see if we need to purge all shapes persisted so far.
-        check_result =
+        # Checking the timeline continuity to see if we need to purge all shapes persisted so far
+        # and reset any replication related persistent state
+        timeline_changed? =
           Electric.Timeline.check(
             {state.pg_system_identifier, state.pg_timeline_id},
             state.timeline_opts
-          )
+          ) == :timeline_changed
 
         shape_cache_opts =
           state.shape_cache_opts
-          |> Keyword.put(:purge_all_shapes?, check_result == :timeline_changed)
+          |> Keyword.put(:purge_all_shapes?, timeline_changed?)
+
+        if timeline_changed? do
+          Electric.Replication.PersistentReplicationState.reset(
+            stack_id: state.stack_id,
+            persistent_kv: state.persistent_kv
+          )
+        end
 
         {:ok, shapes_sup_pid} =
           Electric.Connection.Supervisor.start_shapes_supervisor(

packages/sync-service/lib/electric/postgres/replication_client.ex
@@ -4,6 +4,7 @@ defmodule Electric.Postgres.ReplicationClient do
   """
   use Postgrex.ReplicationConnection
 
+  require Electric.Postgres.ReplicationClient.Collector
   alias Electric.Replication.Changes.Transaction
   alias Electric.Postgres.LogicalReplication.Decoder
   alias Electric.Postgres.Lsn
@@ -65,7 +66,7 @@ defmodule Electric.Postgres.ReplicationClient do
             txn_collector: Collector.t(),
             step: Electric.Postgres.ReplicationClient.step(),
             display_settings: [String.t()],
-            applied_wal: non_neg_integer
+            applied_wal: non_neg_integer()
           }
 
     @opts_schema NimbleOptions.new!(
@@ -166,7 +167,7 @@ defmodule Electric.Postgres.ReplicationClient do
 
   @impl true
   def handle_connect(state) do
-    %{state | step: :connected}
+    %State{state | step: :connected}
     |> ConnectionSetup.start()
   end
 
@@ -215,6 +216,12 @@ defmodule Electric.Postgres.ReplicationClient do
     end)
 
     case reply do
+      1 when Collector.is_collecting(state.txn_collector) ->
+        {:noreply, [encode_standby_status_update(state)], state}
+
+      # if we are not collecting any transactions, advance the replication slot
+      # with keepalives to avoid it getting filled with irrelevant changes, like
+      # heartbeats from the database provider
       1 ->
         state = update_applied_wal(state, wal_end)
         {:noreply, [encode_standby_status_update(state)], state}
@@ -224,7 +231,7 @@ defmodule Electric.Postgres.ReplicationClient do
     end
   end
 
-  defp process_x_log_data(data, wal_end, %State{stack_id: stack_id} = state) do
+  defp process_x_log_data(data, _wal_end, %State{stack_id: stack_id} = state) do
     OpenTelemetry.timed_fun("decode_message_duration", fn -> decode_message(data) end)
     # # Useful for debugging:
     # |> tap(fn %struct{} = msg ->
@@ -239,7 +246,7 @@ defmodule Electric.Postgres.ReplicationClient do
     |> Collector.handle_message(state.txn_collector)
     |> case do
       %Collector{} = txn_collector ->
-        {:noreply, %{state | txn_collector: txn_collector}}
+        {:noreply, %State{state | txn_collector: txn_collector}}
 
       {%Relation{} = rel, %Collector{} = txn_collector} ->
         {m, f, args} = state.relation_received
@@ -251,10 +258,10 @@ defmodule Electric.Postgres.ReplicationClient do
           fn -> apply(m, f, [rel | args]) end
         )
 
-        {:noreply, %{state | txn_collector: txn_collector}}
+        {:noreply, %State{state | txn_collector: txn_collector}}
 
       {%Transaction{} = txn, %Collector{} = txn_collector} ->
-        state = %{state | txn_collector: txn_collector}
+        state = %State{state | txn_collector: txn_collector}
 
         {m, f, args} = state.transaction_received
 
@@ -297,7 +304,7 @@ defmodule Electric.Postgres.ReplicationClient do
             # new transaction into the shape log store. So, when the applied function
             # returns, we can safely advance the replication slot past the transaction's commit
             # LSN.
-            state = update_applied_wal(state, wal_end)
+            state = update_applied_wal(state, Electric.Postgres.Lsn.to_integer(txn.lsn))
             {:noreply, [encode_standby_status_update(state)], state}
 
           other ->
@@ -329,6 +336,8 @@ defmodule Electric.Postgres.ReplicationClient do
   @epoch DateTime.to_unix(~U[2000-01-01 00:00:00Z], :microsecond)
   defp current_time(), do: System.os_time(:microsecond) - @epoch
 
-  defp update_applied_wal(state, wal) when wal >= state.applied_wal,
-    do: %{state | applied_wal: wal}
+  defp update_applied_wal(state, wal) when is_number(wal) and wal >= state.applied_wal,
+    do: %State{state | applied_wal: wal}
+
+  defp update_applied_wal(state, wal) when is_number(wal), do: state
 end

packages/sync-service/lib/electric/postgres/replication_client/collector.ex
@@ -177,4 +177,6 @@ defmodule Electric.Postgres.ReplicationClient.Collector do
     # This gives us headroom for splitting any operation into 2.
     %{state | transaction: Transaction.prepend_change(txn, change), tx_op_index: tx_op_index + 2}
   end
+
+  defguard is_collecting(collector) when not is_nil(collector.transaction)
 end

packages/sync-service/lib/electric/replication/persistent_replication_state.ex
@@ -1,0 +1,50 @@
+defmodule Electric.Replication.PersistentReplicationState do
+  alias Electric.PersistentKV
+  alias Electric.Postgres.Lsn
+  require Logger
+
+  @type opts() :: [
+          stack_id: String.t(),
+          persistent_kv: Electric.PersistentKV.t()
+        ]
+
+  @last_processed_lsn_key "last_processed_lsn"
+
+  @spec set_last_processed_lsn(Lsn.t() | non_neg_integer(), opts()) :: :ok
+  def set_last_processed_lsn(lsn, opts) when is_struct(lsn, Lsn) do
+    lsn |> Lsn.to_integer() |> set_last_processed_lsn(opts)
+  end
+
+  def set_last_processed_lsn(lsn, opts) when is_integer(lsn) do
+    Logger.debug("Updating last processed lsn to #{lsn}")
+    set(@last_processed_lsn_key, lsn, opts)
+  end
+
+  @spec get_last_processed_lsn(opts()) :: Lsn.t()
+  def get_last_processed_lsn(opts) do
+    case get(@last_processed_lsn_key, opts) do
+      {:ok, last_processed_lsn} -> last_processed_lsn
+      {:error, :not_found} -> 0
+    end
+    |> Lsn.from_integer()
+  end
+
+  @spec reset(opts()) :: :ok
+  def reset(opts) do
+    set(@last_processed_lsn_key, 0, opts)
+  end
+
+  @spec set(String.t(), any(), opts()) :: :ok
+  defp set(key, value, opts) do
+    persistent_kv = Access.fetch!(opts, :persistent_kv)
+    stack_id = Access.fetch!(opts, :stack_id)
+    PersistentKV.set(persistent_kv, "#{stack_id}:#{key}", value)
+  end
+
+  @spec get(String.t(), opts()) :: {:ok, any()} | {:error, :not_found}
+  defp get(key, opts) do
+    persistent_kv = Access.fetch!(opts, :persistent_kv)
+    stack_id = Access.fetch!(opts, :stack_id)
+    PersistentKV.get(persistent_kv, "#{stack_id}:#{key}")
+  end
+end

packages/sync-service/lib/electric/shapes/api.ex
@@ -537,16 +537,11 @@ defmodule Electric.Shapes.Api do
   end
 
   defp get_global_last_seen_lsn(%Request{} = request) do
-    case Electric.PersistentKV.get(
-           request.api.persistent_kv,
-           "#{request.api.stack_id}:last_processed_lsn"
-         ) do
-      {:ok, up_to_date_lsn} ->
-        up_to_date_lsn
-
-      {:error, :not_found} ->
-        0
-    end
+    Electric.Replication.PersistentReplicationState.get_last_processed_lsn(
+      persistent_kv: request.api.persistent_kv,
+      stack_id: request.api.stack_id
+    )
+    |> Electric.Postgres.Lsn.to_integer()
   end
 
   defp update_attrs(%Request{} = request, attrs) do

commit b48973b801fbaa4a6f5fca03a48b9d6bc833778d

Fix incorrect LSN comparisons (#2423)
Fixes transactions getting dropped incorrectly because of invalid LSN
comparisons, introduced in
https://github.com/electric-sql/electric/pull/2382

packages/sync-service/lib/electric/postgres/lsn.ex
@@ -136,6 +136,40 @@ defmodule Electric.Postgres.Lsn do
   def compare(%Lsn{offset: o1}, %Lsn{offset: o2}) when o1 > o2, do: :gt
   def compare(%Lsn{offset: o1}, %Lsn{offset: o2}) when o1 == o2, do: :eq
 
+  @doc """
+  Determine if the first Lsn is larger than the second.
+
+  ## Examples
+
+    iex> lsn1 = %Lsn{segment: 2, offset: 10}
+    iex> lsn2 = %Lsn{segment: 1, offset: 50}
+    iex> is_larger(lsn1, lsn2)
+    true
+
+    iex> lsn1 = %Lsn{segment: 3, offset: 5}
+    iex> lsn2 = %Lsn{segment: 3, offset: 4}
+    iex> is_larger(lsn1, lsn2)
+    true
+
+    iex> lsn1 = Lsn.from_string("166A/91FDFDE8")
+    iex> lsn2 = Lsn.from_string("1667/FFFFFCC8")
+    iex> is_larger(lsn1, lsn2)
+    true
+
+    iex> lsn1 = %{segment: 2, offset: 100}
+    iex> lsn2 = %{segment: 2, offset: 200}
+    iex> is_larger(lsn1, lsn2)
+    false
+
+    iex> lsn1 = %{segment: 1, offset: 30}
+    iex> lsn2 = %{segment: 1, offset: 30}
+    iex> is_larger(lsn1, lsn2)
+    false
+  """
+  defguard is_larger(lsn1, lsn2)
+           when lsn1.segment > lsn2.segment or
+                  (lsn1.segment == lsn2.segment and lsn1.offset > lsn2.offset)
+
   @max_offset 0xFFFFFFFF
 
   @doc """

packages/sync-service/lib/electric/replication/shape_log_collector.ex
@@ -5,6 +5,8 @@ defmodule Electric.Replication.ShapeLogCollector do
   """
   use GenStage
 
+  require Electric.Postgres.Lsn
+  alias Electric.Postgres.Lsn
   alias Electric.Replication.PersistentReplicationState
   alias Electric.Postgres.Inspector
   alias Electric.Replication.Changes
@@ -151,7 +153,7 @@ defmodule Electric.Replication.ShapeLogCollector do
 
   # If we've already processed a transaction, then drop it without processing
   defp handle_transaction(txn, _from, %{last_seen_lsn: last_seen_lsn} = state)
-       when txn.lsn <= last_seen_lsn do
+       when not Electric.Postgres.Lsn.is_larger(txn.lsn, last_seen_lsn) do
     Logger.debug(fn ->
       "Dropping transaction #{txn.xid}: transaction LSN #{txn.lsn} smaller than last processed #{last_seen_lsn}"
     end)
@@ -236,7 +238,9 @@ defmodule Electric.Replication.ShapeLogCollector do
     state
   end
 
-  defp put_last_seen_lsn(%{last_seen_lsn: last_seen_lsn} = state, lsn) do
-    %{state | last_seen_lsn: max(last_seen_lsn, lsn)}
-  end
+  defp put_last_seen_lsn(%{last_seen_lsn: last_seen_lsn} = state, lsn)
+       when Lsn.is_larger(lsn, last_seen_lsn),
+       do: %{state | last_seen_lsn: lsn}
+
+  defp put_last_seen_lsn(state, _lsn), do: state
 end

packages/sync-service/test/electric/replication/shape_log_collector_test.exs
@@ -149,8 +149,8 @@ defmodule Electric.Replication.ShapeLogCollectorTest do
 
     test "drops transactions if already processed", ctx do
       xid = 150
-      prev_lsn = Lsn.from_string("0/10")
-      lsn = Lsn.increment(prev_lsn, 1)
+      prev_lsn = Lsn.from_string("1667/FFFFFCC8")
+      lsn = Lsn.from_string("166A/91FDFDE8")
 
       Mock.Inspector
       |> stub(:load_relation, fn
@@ -177,14 +177,14 @@ defmodule Electric.Replication.ShapeLogCollectorTest do
         %Transaction{xid: xid, lsn: lsn, last_log_offset: LogOffset.new(lsn, 0)}
         |> Transaction.prepend_change(%Changes.NewRecord{
           relation: {"public", "test_table"},
-          record: %{"id" => "3", "name" => "foo"}
+          record: %{"id" => "2", "name" => "foo"}
         })
 
       txn3 =
         %Transaction{xid: xid - 1, lsn: prev_lsn, last_log_offset: LogOffset.new(prev_lsn, 0)}
         |> Transaction.prepend_change(%Changes.NewRecord{
           relation: {"public", "test_table"},
-          record: %{"id" => "4", "name" => "foo"}
+          record: %{"id" => "2", "name" => "foo"}
         })
 
       assert :ok = ShapeLogCollector.store_transaction(txn2, ctx.server)

packages/sync-service/test/support/transaction_consumer.ex
@@ -32,7 +32,7 @@ defmodule Support.TransactionConsumer do
   end
 
   def refute_consume(consumers, timeout \\ 100) do
-    for consumer <- consumers, into: MapSet.new() do
+    for consumer <- consumers do
       refute_receive {Support.TransactionConsumer, ^consumer, _}, timeout
     end
   end