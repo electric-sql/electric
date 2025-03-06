fc72bdf8005d4cac3e347b80c1204284df55b91b
Kevin De Porre <kevin@electric-sql.com>
2025-03-06 15:22:21 +0100

    Removing the secret option from the ShapeStream

--- packages/typescript-client/src/constants.ts
+++ packages/typescript-client/src/constants.ts
@@ -10,6 +10,5 @@ export const LIVE_QUERY_PARAM = `live`
 export const OFFSET_QUERY_PARAM = `offset`
 export const TABLE_QUERY_PARAM = `table`
 export const WHERE_QUERY_PARAM = `where`
-export const API_SECRET_QUERY_PARAM = `api_secret`
 export const REPLICA_PARAM = `replica`
 export const FORCE_DISCONNECT_AND_REFRESH = `force-disconnect-and-refresh`
