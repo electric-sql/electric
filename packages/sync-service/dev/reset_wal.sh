3e4b1c12c9c86d2f0be665ba2f2974faced5419a
msfstef <msfstef@gmail.com>
2025-03-12 16:00:32 +0200

    Always reset test PGs to random WAL position

---
+++ packages/sync-service/dev/reset_wal.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Generate a random timeline ID (1, 2, or 3)
+timeline_id=$((RANDOM % 3 + 1))
+
+# Generate two random 32-bit hexadecimal values for the log segment and offset
+log_segment=$(printf "%08X" $(( (RANDOM << 17) | (RANDOM << 2) | (RANDOM % 4) )))
+log_offset=$(printf "%08X" $(( (RANDOM << 17) | (RANDOM << 2) | (RANDOM % 4) )))
+
+# Combine into LSN format
+lsn=$(printf "%08X%s%s" $timeline_id $log_segment $log_offset)
+
+# Stop PostgreSQL to run pg_resetwal
+pg_ctl stop -D /var/lib/postgresql/data
+
+# Run pg_resetwal with the generated LSN
+echo "Resetting WAL to $lsn"
+pg_resetwal -l $lsn /var/lib/postgresql/data
+
+# Restart PostgreSQL
+pg_ctl start -D /var/lib/postgresql/data
