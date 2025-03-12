714c6d6f0b4b99252e151d46e20df29da483498f
msfstef <msfstef@gmail.com>
2025-03-12 17:31:55 +0200

    Check replication acrosss WAL lifetime

--- packages/sync-service/dev/reset_wal.sh
+++ packages/sync-service/dev/reset_wal.sh
@@ -7,15 +7,16 @@ timeline_id=$((RANDOM % 3 + 1))
 log_segment=$(printf "%08X" $(( (RANDOM << 17) | (RANDOM << 2) | (RANDOM % 4) )))
 log_offset=$(printf "%08X" $(( (RANDOM << 17) | (RANDOM << 2) | (RANDOM % 4) )))
 
-# Combine into LSN format
-lsn=$(printf "%08X%s%s" $timeline_id $log_segment $log_offset)
+# Combine into WAL format
+ELECTRIC_PG_START_WAL=00000001FFFFFFFF000000FF
+wal_pos=${ELECTRIC_PG_START_WAL:-$(printf "%08X%s%s" $timeline_id $log_segment $log_offset)}
 
 # Stop PostgreSQL to run pg_resetwal
 pg_ctl stop -D /var/lib/postgresql/data
 
 # Run pg_resetwal with the generated LSN
-echo "Resetting WAL to $lsn"
-pg_resetwal -l $lsn /var/lib/postgresql/data
+echo "Resetting WAL to $wal_pos"
+pg_resetwal -l $wal_pos /var/lib/postgresql/data
 
 # Restart PostgreSQL
 pg_ctl start -D /var/lib/postgresql/data
