a3f9dcaa83a54d086631ca94c8a114973a6ad677
msfstef <msfstef@gmail.com>
2025-03-12 18:25:09 +0200

    Add test across range of values

--- packages/sync-service/test/electric/postgres/lsn_test.exs
+++ packages/sync-service/test/electric/postgres/lsn_test.exs
@@ -15,4 +15,20 @@ defmodule Electric.Postgres.LsnTest do
   test "LSN implement `List.Chars` protocol" do
     assert to_charlist(%Lsn{segment: 5}) == ~c"5/0"
   end
+
+  test "LSN comparisons work across range of values" do
+    for expA <- 1..64 do
+      for expB <- 1..64 do
+        intA = 2 ** expA - 1
+        intB = 2 ** expB - 2
+        is_larger = intA > intB
+        compare_res = if is_larger, do: :gt, else: :lt
+
+        lsnA = Lsn.from_integer(intA)
+        lsnB = Lsn.from_integer(intB)
+        assert Lsn.is_larger(lsnA, lsnB) == is_larger
+        assert Lsn.compare(lsnA, lsnB) == compare_res
+      end
+    end
+  end
 end
