8ebc402e635fb42e59987560ac440ff5ff9999b4
msfstef <msfstef@gmail.com>
2025-03-12 18:31:47 +0200

    Add some randomness to it

--- packages/sync-service/test/electric/postgres/lsn_test.exs
+++ packages/sync-service/test/electric/postgres/lsn_test.exs
@@ -20,9 +20,18 @@ defmodule Electric.Postgres.LsnTest do
     for expA <- 1..64 do
       for expB <- 1..64 do
         intA = 2 ** expA - 1
-        intB = 2 ** expB - 2
-        is_larger = intA > intB
-        compare_res = if is_larger, do: :gt, else: :lt
+        intB = 2 ** expB - 1
+        intA = intA - min(:rand.uniform(ceil(intA * 0.1)), intA - 1)
+        intB = intA - min(:rand.uniform(ceil(intB * 0.1)), intA - 1)
+
+        compare_res =
+          cond do
+            intA < intB -> :lt
+            intA > intB -> :gt
+            true -> :eq
+          end
+
+        is_larger = compare_res == :gt
 
         lsnA = Lsn.from_integer(intA)
         lsnB = Lsn.from_integer(intB)
