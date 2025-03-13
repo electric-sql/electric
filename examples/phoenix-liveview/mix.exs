ff7b49eaa48d6a9aef50c372a92465651dedcb87
Garry Hill <garry@magnetised.net>
2025-03-13 17:04:12 +0000

    update configuration

--- examples/phoenix-liveview/mix.exs
+++ examples/phoenix-liveview/mix.exs
@@ -56,7 +56,19 @@ defmodule Electric.PhoenixExample.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:phoenix_sync, "~> 0.3.1"},
+      {:phoenix_sync, "~> 0.3"}
+    ] ++ deps_for_env(Mix.env())
+  end
+
+  defp deps_for_env(:test) do
+    # use local electric for tests
+    [
+      {:electric, path: "../../packages/sync-service", only: [:test], override: true}
+    ]
+  end
+
+  defp deps_for_env(_) do
+    [
       {:electric, ">= 1.0.0-beta.20"}
     ]
   end
