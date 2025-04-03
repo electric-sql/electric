be18422c3581d6b132dec8ff9023c732106975b3
Garry Hill <garry@magnetised.net>
2025-04-03 10:33:22 +0100

    include custom error codes in retry test

--- packages/elixir-client/test/electric/client_test.exs
+++ packages/elixir-client/test/electric/client_test.exs
@@ -589,7 +589,7 @@ defmodule Electric.ClientTest do
 
       # see https://hexdocs.pm/req/Req.Steps.html#retry/1 for the list of
       # "safe" responses that will be retried
-      retry_statuses = [408, 429, 500, 502, 503, 504]
+      retry_statuses = [408, 429, 500, 502, 503, 504, 530, 599]
 
       {:ok, responses} =
         start_supervised({Agent,
@@ -654,6 +654,22 @@ defmodule Electric.ClientTest do
           fun.(conn)
       end)
 
+      {:ok, client} =
+        Client.new(
+          base_url: "http://localhost:#{ctx.bypass.port}",
+          fetch:
+            {Fetch.HTTP,
+             [
+               is_transient_fun: &Fetch.HTTP.transient_response?(&1, retry_statuses),
+               request: [
+                 connect_options: [timeout: 100, protocols: [:http1]],
+                 retry_delay: fn _n -> 50 end,
+                 retry_log_level: false,
+                 max_retries: 10
+               ]
+             ]}
+        )
+
       assert [
                %ChangeMessage{
                  headers: @insert,
@@ -665,7 +681,7 @@ defmodule Electric.ClientTest do
                  value: %{"id" => "2222"}
                },
                up_to_date(9999)
-             ] = stream(ctx, 4)
+             ] = Client.stream(client, ctx.shape, []) |> Enum.take(4)
     end
 
     test "redirects to another shape id when given a 409", ctx do
