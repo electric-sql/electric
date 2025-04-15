2dd3165b53a5f684d168d0b87c1939056b10bda5
Stefanos Mousafeiris <msfstef@gmail.com>
2025-04-15 18:16:20 +0300

    chore: Clean up old request holding logic (#2598)
    Cleans up old holding of connections until stack not ready that is
    unused after @robacourt 's changes and introduction of the
    `StatusMonitor`

--- packages/sync-service/lib/electric/plug/utils.ex
+++ packages/sync-service/lib/electric/plug/utils.ex
@@ -126,28 +126,6 @@ defmodule Electric.Plug.Utils do
     end
   end
 
-  def hold_conn_until_stack_ready(conn, _opts) do
-    stack_id = conn.assigns.config[:stack_id]
-    stack_ready_timeout = Access.get(conn.assigns.config, :stack_ready_timeout, 5_000)
-    stack_events_registry = conn.assigns.config[:stack_events_registry]
-
-    ref = Electric.StackSupervisor.subscribe_to_stack_events(stack_events_registry, stack_id)
-
-    if Electric.ProcessRegistry.alive?(stack_id, Electric.Replication.Supervisor) do
-      conn
-    else
-      receive do
-        {:stack_status, ^ref, :ready} ->
-          conn
-      after
-        stack_ready_timeout ->
-          conn
-          |> Plug.Conn.send_resp(503, Jason.encode!(%{message: "Stack not ready"}))
-          |> Plug.Conn.halt()
-      end
-    end
-  end
-
   defmodule CORSHeaderPlug do
     @behaviour Plug
     import Plug.Conn
