3434ecf22eb3d127b68c2f7c6e808b23329b9806
Oleksii Sholik <oleksii@sholik.dev>
2025-04-17 14:46:18 +0200

    Make the log level in tests configurable when capturing is off

--- packages/sync-service/config/runtime.exs
+++ packages/sync-service/config/runtime.exs
@@ -7,7 +7,14 @@ if config_env() in [:dev, :test] do
   source!([".env.#{config_env()}", ".env.#{config_env()}.local", System.get_env()])
 end
 
-config :logger, level: env!("ELECTRIC_LOG_LEVEL", &Electric.Config.parse_log_level!/1, :info)
+user_test_log_level =
+  if config_env() == :test,
+    do: env!("ELECTRIC_TEST_LOG_LEVEL", &Electric.Config.parse_log_level!/1, nil)
+
+user_log_level =
+  user_test_log_level || env!("ELECTRIC_LOG_LEVEL", &Electric.Config.parse_log_level!/1, :info)
+
+config :logger, level: user_log_level
 
 config :logger, :default_formatter,
   # Doubled line breaks serve as long message boundaries
@@ -26,8 +33,7 @@ config :logger,
 
 if config_env() == :test do
   config :electric, pg_version_for_tests: env!("POSTGRES_VERSION", :integer, 150_001)
-  config :logger, backends: [:console]
-  config :logger, :default_handler, level: :error
+  config :logger, :default_handler, level: user_test_log_level
 end
 
 config :sentry,
