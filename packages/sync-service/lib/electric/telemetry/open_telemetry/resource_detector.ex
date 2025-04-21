6f34267685e921fde066de556b7878ca8c0d63da
Oleksii Sholik <oleksii@sholik.dev>
2025-04-21 17:21:08 +0200

    Compile Electric's ResourceDetector only when :otel_resource is available
    When Electric is used as a library, OTEL libraries might not be included
    in the parent application. In that case, we will skip compiling the
    resource detector and resolve Elixir compiler warnings.

--- packages/sync-service/lib/electric/telemetry/open_telemetry/resource_detector.ex
+++ packages/sync-service/lib/electric/telemetry/open_telemetry/resource_detector.ex
@@ -1,15 +1,17 @@
-defmodule Electric.Telemetry.OpenTelemetry.ResourceDetector do
-  @behaviour :otel_resource_detector
+if Code.ensure_loaded?(:otel_resource) do
+  defmodule Electric.Telemetry.OpenTelemetry.ResourceDetector do
+    @behaviour :otel_resource_detector
 
-  def get_resource(_config) do
-    {m, f, a} = Electric.Config.get_env(:persistent_kv)
-    kv = apply(m, f, [a])
+    def get_resource(_config) do
+      {m, f, a} = Electric.Config.get_env(:persistent_kv)
+      kv = apply(m, f, [a])
 
-    try do
-      installation_id = Electric.Config.installation_id!(kv)
-      :otel_resource.create("instance.installation_id": installation_id)
-    rescue
-      _ -> :otel_resource.create([])
+      try do
+        installation_id = Electric.Config.installation_id!(kv)
+        :otel_resource.create("instance.installation_id": installation_id)
+      rescue
+        _ -> :otel_resource.create([])
+      end
     end
   end
 end
