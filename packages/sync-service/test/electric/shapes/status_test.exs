19745828760b12da4ce4d00aa935166b7282182a
Garry Hill <garry@magnetised.net>
2025-05-01 10:25:28 +0100

    handle pids re-subscribing to different shape

--- packages/sync-service/test/electric/shapes/status_test.exs
+++ packages/sync-service/test/electric/shapes/status_test.exs
@@ -1,18 +1,20 @@
 defmodule Electric.Shapes.StatusTest do
   use ExUnit.Case, async: true
 
+  alias File.Stat
   alias Electric.Shapes.Status
 
   import Support.ComponentSetup
 
-  setup [:with_stack_id_from_test]
+  setup [:with_stack_id_from_test, :with_in_memory_storage]
 
-  defp start_stack_status(%{stack_id: stack_id}) do
+  defp start_stack_status(%{stack_id: stack_id, storage: storage}) do
     parent = self()
 
     start_link_supervised!(
       {Status,
        stack_id: stack_id,
+       storage: storage,
        on_remove: fn shape_handle, pid -> send(parent, {:remove, shape_handle, pid}) end}
     )
 
@@ -23,8 +25,10 @@ defmodule Electric.Shapes.StatusTest do
     setup [:start_stack_status]
 
     test "allows double registration", %{stack_id: stack_id} = _ctx do
-      assert {:ok, 1} = Status.register_subscriber(stack_id, "handle-1")
-      assert {:ok, 1} = Status.register_subscriber(stack_id, "handle-1")
+      assert :ok = Status.register_subscriber(stack_id, "handle-1")
+      assert :ok = Status.register_subscriber(stack_id, "handle-1")
+
+      assert {:ok, 1} = Status.subscriber_count(stack_id, "handle-1")
     end
 
     test "tracks process termination", %{stack_id: stack_id} = _ctx do
@@ -40,7 +44,7 @@ defmodule Electric.Shapes.StatusTest do
         for handle <- Enum.take(random_handle_stream, n) do
           {:ok, pid} =
             Task.start_link(fn ->
-              {:ok, _c} = Status.register_subscriber(stack_id, handle)
+              :ok = Status.register_subscriber(stack_id, handle)
 
               send(parent, {:ready, self()})
 
@@ -57,6 +61,10 @@ defmodule Electric.Shapes.StatusTest do
       handle_pid = Enum.group_by(pids, &elem(&1, 0), &elem(&1, 1))
 
       counts = Map.new(handle_pid, fn {handle, pids} -> {handle, length(pids)} end)
+
+      for {handle, count} <- counts do
+        assert {:ok, count} == Status.subscriber_count(stack_id, handle)
+      end
 
       pids
       |> Enum.shuffle()
@@ -76,8 +84,9 @@ defmodule Electric.Shapes.StatusTest do
     end
 
     test "allows for manual deregistration", %{stack_id: stack_id} = _ctx do
-      assert {:ok, 1} = Status.register_subscriber(stack_id, "handle-1")
-      assert {:ok, 0} = Status.unregister_subscriber(stack_id, "handle-1")
+      assert :ok = Status.register_subscriber(stack_id, "handle-1")
+      assert :ok = Status.unregister_subscriber(stack_id, "handle-1")
+      assert {:ok, 0} = Status.subscriber_count(stack_id, "handle-1")
     end
   end
 
@@ -87,17 +96,19 @@ defmodule Electric.Shapes.StatusTest do
     test "sends a message immediately if no subscribers active", %{stack_id: stack_id} = _ctx do
       handle = "some-handle"
       Status.wait_subscriber_termination(stack_id, handle)
-      assert_receive {Status, :subscriber_termination}, 100
+      assert_receive {Status, :subscriber_termination, ^handle}, 100
     end
 
     test "sends a message when all subcribers have terminated", %{stack_id: stack_id} = _ctx do
       handle = "some-handle"
+      parent = self()
 
       {:ok, subscriber1} =
         start_supervised(
           {Task,
            fn ->
-             {:ok, _c} = Status.register_subscriber(stack_id, handle)
+             :ok = Status.register_subscriber(stack_id, handle)
+             send(parent, {:ready, 1})
 
              receive do
                _ -> :ok
@@ -110,7 +121,8 @@ defmodule Electric.Shapes.StatusTest do
         start_supervised(
           {Task,
            fn ->
-             {:ok, _c} = Status.register_subscriber(stack_id, handle)
+             :ok = Status.register_subscriber(stack_id, handle)
+             send(parent, {:ready, 2})
 
              receive do
                _ -> :ok
@@ -119,14 +131,83 @@ defmodule Electric.Shapes.StatusTest do
           id: {:subscriber, 2}
         )
 
+      assert_receive {:ready, 1}
+      assert_receive {:ready, 2}
+
       Status.wait_subscriber_termination(stack_id, handle)
 
       send(subscriber1, :done)
-      refute_receive {Status, :subscriber_termination}, 100
+      refute_receive {Status, :subscriber_termination, _handle}, 100
       send(subscriber2, :done)
-      assert_receive {Status, :subscriber_termination}, 100
+      assert_receive {Status, :subscriber_termination, ^handle}, 100
     end
 
     test "cleans up if consumer exits"
+
+    test "is triggered if same reader pid changes shape handle", %{stack_id: stack_id} = _ctx do
+      handle1 = "some-handle-1"
+      handle2 = "some-handle-2"
+      parent = self()
+
+      {:ok, subscriber1} =
+        start_supervised(
+          {Task,
+           fn ->
+             :ok = Status.register_subscriber(stack_id, handle1)
+
+             send(parent, :ready)
+
+             receive do
+               {:subscribe, handle} ->
+                 :ok = Status.register_subscriber(stack_id, handle)
+
+                 receive do
+                   _ ->
+                     dbg(:done)
+                     :ok
+                 end
+             end
+           end},
+          id: {:subscriber, 1}
+        )
+
+      assert_receive :ready, 100
+      Status.wait_subscriber_termination(stack_id, handle1)
+      send(subscriber1, {:subscribe, handle2})
+      assert_receive {Status, :subscriber_termination, ^handle1}, 100
+    end
+
+    test "is not triggered if same reader pid re-registers under same handle",
+         %{stack_id: stack_id} = _ctx do
+      handle1 = "some-handle-1"
+      parent = self()
+
+      {:ok, subscriber1} =
+        start_supervised(
+          {Task,
+           fn ->
+             :ok = Status.register_subscriber(stack_id, handle1)
+
+             send(parent, :ready)
+
+             receive do
+               {:subscribe, handle} ->
+                 :ok = Status.register_subscriber(stack_id, handle)
+
+                 receive do
+                   _ ->
+                     dbg(:done)
+                     :ok
+                 end
+             end
+           end},
+          id: {:subscriber, 1}
+        )
+
+      assert_receive :ready, 100
+      Status.wait_subscriber_termination(stack_id, handle1)
+      send(subscriber1, {:subscribe, handle1})
+      refute_receive {Status, :subscriber_termination, _handle1}, 100
+    end
   end
 end
