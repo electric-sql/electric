033301b15fe8ea751698d0c7c97666d3557b2c07
Kevin De Porre <kevin@electric-sql.com>
2025-05-07 15:20:04 +0200

    Update AWS provider

--- examples/remix/sst.config.ts
+++ examples/remix/sst.config.ts
@@ -13,7 +13,10 @@ export default $config({
       home: `aws`,
       providers: {
         cloudflare: `5.42.0`,
-        aws: { version: `6.57.0`, region: `eu-west-1` },
+        aws: {
+          version: `6.66.2`,
+          profile: process.env.CI ? undefined : `marketing`,
+        },
         neon: `0.6.3`,
         command: `1.0.1`,
       },
