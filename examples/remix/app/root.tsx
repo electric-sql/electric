670ad693573218f3b61383011d4f313d97b2e5b1
Kevin De Porre <kevin@electric-sql.com>
2025-05-08 11:03:09 +0200

    Pass the backend URL to the remix app

--- examples/remix/app/root.tsx
+++ examples/remix/app/root.tsx
@@ -6,9 +6,20 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react"
+import { json } from "@remix-run/node"
+import type { LoaderFunctionArgs } from "@remix-run/node"
+
+export async function loader(_: LoaderFunctionArgs) {
+  return json({
+    PUBLIC_SERVER_URL: process.env.PUBLIC_SERVER_URL,
+  })
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { PUBLIC_SERVER_URL } = useLoaderData<typeof loader>()
+  
   return (
     <html lang="en">
         <head>
@@ -16,6 +27,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <Meta />
           <Links />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.ENV = ${JSON.stringify({ PUBLIC_SERVER_URL })}`,
+            }}
+          />
         </head>
         <body style={{ margin: 0, padding: 0 }}>
           {children}
