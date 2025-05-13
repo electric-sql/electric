42254d67afd486631c63ccd78a190f5a63340a63
Kevin De Porre <kevin@electric-sql.com>
2025-05-13 14:16:21 +0200

    Add ssl: true

--- examples/phoenix-liveview/config/runtime.exs
+++ examples/phoenix-liveview/config/runtime.exs
@@ -31,6 +31,7 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :electric_phoenix_example, Electric.PhoenixExample.Repo,
+    ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6,
