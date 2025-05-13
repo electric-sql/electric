b2b3ae83593e68efa3932e68fb5c22501db2662e
Kevin De Porre <kevin@electric-sql.com>
2025-05-13 16:21:02 +0200

    Turn map keys into atoms

--- examples/phoenix-liveview/config/runtime.exs
+++ examples/phoenix-liveview/config/runtime.exs
@@ -103,5 +103,5 @@ if config_env() == :prod do
   config :electric_phoenix, Electric.Client,
     base_url:
       System.get_env("ELECTRIC_URL") || raise("ELECTRIC_URL environment variable not set"),
-    params: System.get_env("ELECTRIC_CLIENT_PARAMS", "{}") |> :json.decode()
+    params: System.get_env("ELECTRIC_CLIENT_PARAMS", "{}") |> :json.decode() |> Map.new(fn {k, v} -> {String.to_existing_atom(k), v} end)
 end
