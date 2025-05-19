49765cf5f81840e46e444dd4db9b7d26cb287fb9
msfstef <msfstef@gmail.com>
2025-05-19 16:05:36 +0300

    Remove bundle plugin as we don't use Vite for building

--- packages/react-hooks/vitest.config.ts
+++ packages/react-hooks/vitest.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vitest/config'
-import { codecovVitePlugin } from '@codecov/vite-plugin'
 
 export default defineConfig({
-  plugins: [
-    codecovVitePlugin({
-      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-      bundleName: '@electric-sql/react',
-      uploadToken: process.env.CODECOV_TOKEN,
-    }),
-  ],
   test: {
     globalSetup: `test/support/global-setup.ts`,
     setupFiles: [`test/support/react-setup.ts`],
