ea7a58ec7a738204ace5cfe0fa444bc324cf6e21
Garry Hill <garry@magnetised.net>
2025-05-19 11:34:46 +0100

    refactor cleanup supervisor

--- packages/sync-service/lib/electric/shapes/monitor/cleanup_task_supervisor.ex
+++ packages/sync-service/lib/electric/shapes/monitor/cleanup_task_supervisor.ex
@@ -3,6 +3,11 @@ defmodule Electric.Shapes.Monitor.CleanupTaskSupervisor do
 
   alias Electric.ShapeCache.Storage
 
+  # set a high timeout for the shape cleanup to terminate
+  # we don't want to see errors due to e.g. a slow filesystem.
+  # any actual errors in the processes will be caught and reported
+  @cleanup_timeout 60_000
+
   def child_spec(opts) do
     {:ok, stack_id} = Keyword.fetch(opts, :stack_id)
 
@@ -26,92 +31,98 @@ defmodule Electric.Shapes.Monitor.CleanupTaskSupervisor do
         shape,
         on_cleanup \\ fn _ -> :ok end
       ) do
-    with {:ok, _pid} <-
-           Task.Supervisor.start_child(name(stack_id), fn ->
-             Logger.metadata(shape_handle: shape_handle, stack_id: stack_id)
+    if consumer_alive?(stack_id, shape_handle) do
+      {:error, "Expected shape #{shape_handle} consumer to not be alive before cleaning shape"}
+    else
+      {:ok, _pid} =
+        Task.Supervisor.start_child(name(stack_id), fn ->
+          Logger.debug("Cleaning shape data for shape #{inspect(shape_handle)}")
 
-             try do
-               cleanup(
-                 stack_id,
-                 storage_impl,
-                 publication_manager_impl,
-                 shape_status_impl,
-                 shape_handle,
-                 shape
-               )
-               |> case do
-                 :ok ->
-                   :ok
+          task1 =
+            Task.Supervisor.async(name(stack_id), fn ->
+              Logger.metadata(stack_id: stack_id, shape_handle: shape_handle)
+              cleanup_shape_status(shape_status_impl, shape_handle)
+            end)
 
-                 {:error, reason} ->
-                   Logger.error(["Failed to clean shape #{shape_handle}: ", reason])
-               end
-             catch
-               exception -> Logger.error(exception)
-             after
-               on_cleanup.(shape_handle)
-             end
-           end) do
+          task2 =
+            Task.Supervisor.async(name(stack_id), fn ->
+              Logger.metadata(stack_id: stack_id, shape_handle: shape_handle)
+              cleanup_storage(storage_impl, shape_handle)
+            end)
+
+          task3 =
+            Task.Supervisor.async(name(stack_id), fn ->
+              Logger.metadata(stack_id: stack_id, shape_handle: shape_handle)
+              cleanup_publication_manager(publication_manager_impl, shape_handle, shape)
+            end)
+
+          try do
+            [task1, task2, task3]
+            |> Task.await_many(@cleanup_timeout)
+          after
+            on_cleanup.(shape_handle)
+          end
+        end)
+
       :ok
     end
   end
 
-  defp cleanup(
-         stack_id,
+  defp cleanup_shape_status(shape_status_impl, shape_handle) do
+    {shape_status, shape_status_state} = shape_status_impl
+
+    case shape_status.remove_shape(shape_status_state, shape_handle) do
+      {:ok, _shape} ->
+        Logger.debug("Deregistered shape #{shape_handle}")
+
+      {:error, _reason} ->
+        # this is actually quite likely as during normal shutdown the shape is removed asap
+        # this path is just to make sure we do that in case of a crash
+        Logger.debug(["Shape already de-registered #{shape_handle}"])
+    end
+  end
+
+  defp cleanup_storage(
          storage_impl,
+         shape_handle
+       ) do
+    perform_reporting_errors(
+      fn ->
+        shape_handle
+        |> Storage.for_shape(storage_impl)
+        |> Storage.unsafe_cleanup!()
+      end,
+      "Failed to delete data for shape #{shape_handle}"
+    )
+  end
+
+  defp cleanup_publication_manager(
          publication_manager_impl,
-         shape_status_impl,
          shape_handle,
          shape
        ) do
-    if consumer_alive?(stack_id, shape_handle) do
-      {:error, "Expected shape #{shape_handle} consumer to not be alive before cleaning shape"}
-    else
-      {publication_manager, publication_manager_opts} = publication_manager_impl
-      {shape_status, shape_status_state} = shape_status_impl
+    {publication_manager, publication_manager_opts} = publication_manager_impl
 
-      Logger.debug("cleaning shape data #{inspect(shape_handle)}")
+    perform_reporting_errors(
+      fn ->
+        publication_manager.remove_shape(shape_handle, shape, publication_manager_opts)
+      end,
+      "Failed to remove shape #{shape_handle} from publication"
+    )
+  end
 
-      shape_status.remove_shape(shape_status_state, shape_handle)
+  defp perform_reporting_errors(fun, message) do
+    try do
+      fun.()
+    catch
+      kind, reason when kind in [:exit, :error] ->
+        Logger.error([message, ": ", Exception.format(kind, reason, __STACKTRACE__)])
 
-      data_cleanup =
-        perform_async_catching_errors(
-          fn ->
-            shape_handle
-            |> Storage.for_shape(storage_impl)
-            |> Storage.unsafe_cleanup!()
-          end,
-          "Failed to delete data for shape #{shape_handle}"
-        )
-
-      remove_shape =
-        perform_async_catching_errors(
-          fn ->
-            publication_manager.remove_shape(shape_handle, shape, publication_manager_opts)
-          end,
-          "Failed to remove shape #{shape_handle} from publication"
-        )
-
-      [data_cleanup, remove_shape]
-      |> Task.await_many(60000)
-      |> Enum.find(:ok, &match?({:error, _}, &1))
+        {:error, reason}
     end
   end
 
   defp consumer_alive?(stack_id, shape_handle) do
     !is_nil(Electric.Shapes.Consumer.whereis(stack_id, shape_handle))
-  end
-
-  defp perform_async_catching_errors(fun, message) do
-    Task.async(fn ->
-      try do
-        fun.()
-      catch
-        kind, reason when kind in [:exit, :error] ->
-          Logger.error([message, ": ", Exception.format(kind, reason, __STACKTRACE__)])
-
-          {:error, reason}
-      end
-    end)
   end
 end
