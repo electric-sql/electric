d8ef53819eb17e7045e6d914863af5e23c79053b
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2025-05-21 12:53:02 +0200

    fix(elixir-client): support fragment

--- packages/elixir-client/lib/electric/client/ecto_adapter/postgres.ex
+++ packages/elixir-client/lib/electric/client/ecto_adapter/postgres.ex
@@ -42,20 +42,21 @@ if Code.ensure_loaded?(Ecto) do
     defp handle_call(fun, _arity), do: {:fun, Atom.to_string(fun)}
 
     def where(%{wheres: wheres} = query, sources, bindings) do
+      processed_bindings = process_bindings(query, bindings)
+      boolean("", wheres, sources, query, processed_bindings)
+    end
+
+    defp process_bindings(%{wheres: wheres} = query, bindings) do
       type_map =
         wheres
-        |> Enum.flat_map(fn %Ecto.Query.BooleanExpr{expr: expr} ->
-            collect_param_types(expr, query)
-          end)
+        |> Enum.flat_map(&collect_param_types(&1.expr, query))
         |> Map.new()
 
-      bindings =
-        Enum.with_index(bindings, 1)
-        |> Enum.map(fn
-            {val, idx} -> dump_binding(Map.get(type_map, idx), val)
-          end)
-
-      boolean("", wheres, sources, query, bindings)
+      bindings
+      |> Enum.with_index(1)
+      |> Enum.map(fn {val, idx} ->
+        dump_binding(Map.get(type_map, idx), val)
+      end)
     end
 
     defp boolean(_name, [], _sources, _query, _bindings), do: []
@@ -467,13 +468,16 @@ if Code.ensure_loaded?(Ecto) do
     end
 
     defp collect_param_types(
-         {op, _, [{{:., _, [{:&, _, [_]}, field]}, _, []}, {:^, _, [ix]}]},
-         %Ecto.Query{from: %Ecto.Query.FromExpr{source: {_, schema_mod}}} = _query
-       )
-       when op in @binary_ops and is_atom(field) do
-
+           {op, _, [{{:., _, [{:&, _, [_]}, field]}, _, []}, {:^, _, [ix]}]},
+           %{from: %Ecto.Query.FromExpr{source: {_, schema_mod}}}
+         )
+         when op in @binary_ops and is_atom(field) do
       ecto_type = schema_mod.__schema__(:type, field)
       [{ix, ecto_type}]
+    end
+
+    defp collect_param_types({:fragment, _, parts}, query) do
+      Enum.flat_map(parts, &collect_fragment_part_types(&1, parts, query))
     end
 
     defp collect_param_types({_, _, args}, query) when is_list(args) do
@@ -482,13 +486,61 @@ if Code.ensure_loaded?(Ecto) do
 
     defp collect_param_types(_, _), do: []
 
+    defp collect_fragment_part_types(
+           {:expr, {:splice, _, [{:^, _, [idx, length]}]}},
+           parts,
+           query
+         ) do
+      case find_field_type_in_fragment(parts, query) do
+        nil -> []
+        field_type -> Enum.map(idx..(idx + length - 1), &{&1, field_type})
+      end
+    end
+
+    defp collect_fragment_part_types(
+           {:expr, {:^, _, [idx]}},
+           parts,
+           query
+         ) do
+      case find_field_type_in_fragment(parts, query) do
+        nil -> []
+        field_type -> [{idx, field_type}]
+      end
+    end
+
+    defp collect_fragment_part_types({:raw, _}, _, _), do: []
+
+    defp collect_fragment_part_types({:expr, expr}, _, query),
+      do: collect_param_types(expr, query)
+
+    defp find_field_type_in_fragment(fragment_parts, query) do
+      Enum.find_value(fragment_parts, fn
+        {:expr, {{:., _, [{:&, _, [0]}, field]}, _, []}} when is_atom(field) ->
+          case query do
+            %{from: %Ecto.Query.FromExpr{source: {_, schema_mod}}} ->
+              schema_mod.__schema__(:type, field)
+
+            _ ->
+              nil
+          end
+
+        _ ->
+          nil
+      end)
+    end
+
     defp dump_binding(type, {value, _}) when is_list(value) do
       Enum.map(value, &dump_binding(type, &1))
     end
 
     defp dump_binding({:parameterized, _} = type, {value, _}) do
       case Ecto.Type.dump(type, value) do
-        {:ok, dumped} -> {literal(dumped), dumped}
+        {:ok, dumped} when is_binary(dumped) and byte_size(dumped) == 16 ->
+          {Ecto.UUID.load!(dumped), dumped}
+
+        {:ok, dumped} ->
+          {dumped, dumped}
+
         :error ->
           raise ArgumentError,
                 "cannot dump value #{inspect(value)} for type #{inspect(type)}"
@@ -496,14 +548,5 @@ if Code.ensure_loaded?(Ecto) do
     end
 
     defp dump_binding(_, value), do: value
-
-    defp literal(db_val) when is_binary(db_val) and byte_size(db_val) == 16 do
-      case Ecto.UUID.load(db_val) do
-        {:ok, uuid} -> uuid
-        :error -> literal(db_val)
-      end
-    end
-
-    defp literal(other), do: inspect(other)
   end
 end
