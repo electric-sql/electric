97bb4259a3a1e4880510411841f71928846965c9
rob <rob.acourt@gmail.com>
2025-07-14 16:34:41 +0100

    Turn spans off

--- packages/sync-service/lib/electric/telemetry/optional_spans.ex
+++ packages/sync-service/lib/electric/telemetry/optional_spans.ex
@@ -1,4 +1,6 @@
 defmodule Electric.Telemetry.OptionalSpans do
   def include?("filter." <> _), do: Application.get_env(:electric, :profile_where_clauses?)
+  def include?("pg_txn.replication_client.relation_received"), do: false
+  def include?("pg_txn.replication_client.transaction_received"), do: false
   def include?(_), do: true
 end
