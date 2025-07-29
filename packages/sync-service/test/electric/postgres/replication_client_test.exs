7f126c71376b2d16c7ce18b0a417bf21897658e5
msfstef <msfstef@gmail.com>
2025-07-29 11:55:04 +0300

    Add test for replica identity issues

--- packages/sync-service/test/electric/postgres/replication_client_test.exs
+++ packages/sync-service/test/electric/postgres/replication_client_test.exs
@@ -296,6 +296,34 @@ defmodule Electric.Postgres.ReplicationClientTest do
                      },
                      @assert_receive_db_timeout
     end
+
+    test "exits with irrecoverable slot error for invalid replica identity",
+         %{db_conn: conn} = ctx do
+      pid = start_client(ctx)
+
+      {_id, bin_uuid} = gen_uuid()
+
+      Postgrex.query!(conn, "INSERT INTO items (id, value) VALUES ($1, $2)", [bin_uuid, "test"])
+      assert %NewRecord{record: %{"value" => "test"}} = receive_tx_change()
+      Postgrex.query!(conn, "UPDATE items SET value = $2 WHERE id = $1", [bin_uuid, "new"])
+
+      monitor = Process.monitor(pid)
+      Process.unlink(pid)
+      on_exit(fn -> Process.alive?(pid) && Process.exit(pid, :kill) end)
+
+      # Verify that receiving updates without old values causes an exit
+      assert_receive {
+                       :DOWN,
+                       ^monitor,
+                       :process,
+                       ^pid,
+                       {:irrecoverable_slot, {:replica_not_full, msg}}
+                     },
+                     @assert_receive_db_timeout
+
+      assert msg =~
+               "Received an update from PG for public.items that did not have old data included in the message."
+    end
   end
 
   describe "ReplicationClient against real db (toast)" do
