eb2e2859c37d739d31acb5123a34664802674dd8
Valter Balegas <balegas@electric-sql.com>
2025-08-13 16:57:50 +0100

    sst

--- examples/burn/sst.config.ts
+++ examples/burn/sst.config.ts
@@ -1,7 +1,7 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
-import { createDatabaseForCloudElectric } from "../.shared/lib/database"
 import { getSharedCluster, isProduction } from "../.shared/lib/infra"
+import { createNeonDb, getNeonConnectionString } from "../.shared/lib/neon"
 
 export default $config({
   app(input) {
@@ -30,44 +30,74 @@ export default $config({
       throw new Error(`Env variable SECRET_KEY_BASE must be set`)
     }
 
-    const dbName = isProduction()
-      ? `burn-app`
-      : `burn-app-${$app.stage}`
+    const dbName = isProduction() ? `burn-app` : `burn-app-${$app.stage}`
 
-    const { pooledDatabaseUri, sourceId, sourceSecret } =
-      createDatabaseForCloudElectric({ dbName })
+    const { pooledDatabaseUri } = createNeonDatabase({ dbName })
 
     const domainName = `burn${isProduction() ? `` : `-stage-${$app.stage}`}.examples.electric-sql.com`
 
     const cluster = getSharedCluster(`burn-app-${$app.stage}`)
-    const service = cluster.addService(
-      `burn-app-${$app.stage}-service`,
-      {
-        loadBalancer: {
-          ports: [
-            { listen: `443/https`, forward: `4000/http` },
-            { listen: `80/http`, forward: `4000/http` },
-          ],
-          domain: {
-            name: domainName,
-            dns: sst.cloudflare.dns(),
-          },
+    const service = cluster.addService(`burn-app-${$app.stage}-service`, {
+      loadBalancer: {
+        ports: [
+          { listen: `443/https`, forward: `4000/http` },
+          { listen: `80/http`, forward: `4000/http` },
+        ],
+        domain: {
+          name: domainName,
+          dns: sst.cloudflare.dns(),
         },
-        environment: {
-          ANTHROPIC_KEY: process.env.ANTHROPIC_KEY,
-          DATABASE_URL: pooledDatabaseUri,
-          PHX_HOST: domainName,
-          SECRET_KEY_BASE: process.env.SECRET_KEY_BASE,
-        },
-        image: {
-          context: `.`,
-          dockerfile: `Dockerfile`,
-        },
-      }
-    )
+      },
+      environment: {
+        ANTHROPIC_KEY: process.env.ANTHROPIC_KEY,
+        DATABASE_URL: pooledDatabaseUri,
+        PHX_HOST: domainName,
+        SECRET_KEY_BASE: process.env.SECRET_KEY_BASE,
+      },
+      image: {
+        context: `.`,
+        dockerfile: `Dockerfile`,
+      },
+    })
 
     return {
       website: service.url,
     }
   },
 })
+
+export function createNeonDatabase({ dbName }: { dbName: string }) {
+  const neonProjectId = process.env.NEON_PROJECT_ID
+  if (!neonProjectId) {
+    throw new Error(`NEON_PROJECT_ID is not set`)
+  }
+
+  const project = neon.getProjectOutput({
+    id: neonProjectId,
+  })
+  const { ownerName, dbName: resultingDbName } = createNeonDb({
+    projectId: project.id,
+    branchId: project.defaultBranchId,
+    dbName,
+  })
+
+  const databaseUri = getNeonConnectionString({
+    project,
+    roleName: ownerName,
+    databaseName: resultingDbName,
+    pooled: false,
+  })
+  const pooledDatabaseUri = getNeonConnectionString({
+    project,
+    roleName: ownerName,
+    databaseName: resultingDbName,
+    pooled: true,
+  })
+
+  const res = {
+    databaseUri,
+    pooledDatabaseUri,
+  }
+
+  return res
+}
