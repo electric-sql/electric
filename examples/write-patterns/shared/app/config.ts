8046b26fe3a3ee9fecf2180171f6adeb7e1be16c
Kevin De Porre <kevin@electric-sql.com>
2025-08-18 11:51:24 +0200

    Fix ENV var usage in write patterns app

--- examples/write-patterns/shared/app/config.ts
+++ examples/write-patterns/shared/app/config.ts
@@ -1,3 +1,3 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001'
+export const API_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3001'
 
 export const TODOS_URL = `${API_URL}/todos`
