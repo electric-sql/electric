663fca16ad5fd007eed5df4427bb54a166ce47a0
Oleksii Sholik <oleksii@sholik.dev>
2025-09-26 15:34:44 +0200

    Explain the reasoning behind implementation choices

--- packages/sync-service/lib/electric/shapes/dynamic_consumer_supervisor.ex
+++ packages/sync-service/lib/electric/shapes/dynamic_consumer_supervisor.ex
@@ -8,6 +8,12 @@ defmodule Electric.Shapes.DynamicConsumerSupervisor do
 
   require Logger
 
+  @doc """
+  Returns a child spec for the PartitionSupervisor that starts a pool of
+  DynamicConsumerSupervisor procecesses to shard child processes across.
+
+  The number of dynamic supervisors is equal to the number of CPU cores.
+  """
   def partition_supervisor_spec(stack_id) do
     {PartitionSupervisor, child_spec: {__MODULE__, [stack_id: stack_id]}, name: name(stack_id)}
   end
@@ -16,6 +22,8 @@ defmodule Electric.Shapes.DynamicConsumerSupervisor do
     Electric.ProcessRegistry.name(stack_id, __MODULE__)
   end
 
+  # This function will be invoked for each dynamic supervisor process in PartitionSupervisor's
+  # pool, so we keep these processes unnamed.
   def start_link(opts) do
     stack_id = Keyword.fetch!(opts, :stack_id)
     DynamicSupervisor.start_link(__MODULE__, stack_id: stack_id)
@@ -24,6 +32,10 @@ defmodule Electric.Shapes.DynamicConsumerSupervisor do
   def start_shape_consumer(name, config) do
     Logger.debug(fn -> "Starting consumer for #{Keyword.fetch!(config, :shape_handle)}" end)
 
+    # Use a random integer as the routing key to achieve balanced sharding of child processes
+    # across all dynamic supervisors. The top limit for the key is picked to future-proof it
+    # for cases where Electric runs on a CPU with many cores. 256 should be sufficient for the
+    # foreseeable future.
     key = :rand.uniform(256)
 
     DynamicSupervisor.start_child(
