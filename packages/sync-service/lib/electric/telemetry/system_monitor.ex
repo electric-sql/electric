e0aef042c05a38e1cd9881cbc2747821973e1ae5
Oleksii Sholik <oleksii@sholik.dev>
2025-10-21 16:20:52 +0200

    Add details to Electric.Telemetry.SystemMonitor's metrics
    When a process' message queue length exceed the threshold, we report its
    actual length via :telemetry.execute() and do it 5 times per second.

--- packages/sync-service/lib/electric/telemetry/system_monitor.ex
+++ packages/sync-service/lib/electric/telemetry/system_monitor.ex
@@ -1,9 +1,24 @@
 defmodule Electric.Telemetry.SystemMonitor do
+  @moduledoc """
+  Application-wide process that initializes Erlang's system monitor and consumes monitoring events.
+
+  Currently the follow events are tracked:
+
+    - long_gc
+    - long_schedule
+    - long_message_queue
+
+  """
+
   use GenServer
 
   import Electric.Telemetry.Processes, only: [proc_type: 1]
 
   require Logger
+
+  @vm_monitor_long_gc [:vm, :monitor, :long_gc]
+  @vm_monitor_long_schedule [:vm, :monitor, :long_schedule]
+  @vm_monitor_long_message_queue [:vm, :monitor, :long_message_queue]
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
@@ -17,9 +32,7 @@ defmodule Electric.Telemetry.SystemMonitor do
         {opts.long_message_queue_disable_threshold, opts.long_message_queue_enable_threshold}
     )
 
-    :timer.send_interval(2000, :report_long_queues)
-
-    {:ok, %{long_message_queue_pids: %{}}}
+    {:ok, %{long_message_queue_pids: %{}, long_message_queue_timer: nil}}
   end
 
   def handle_info({:monitor, gc_pid, :long_gc, info}, state) do
@@ -29,7 +42,8 @@ defmodule Electric.Telemetry.SystemMonitor do
       "Long GC detected for pid #{inspect(gc_pid)} (#{inspect(type)}): took #{Keyword.fetch!(info, :timeout)}ms. #{inspect(info, limit: :infinity)}"
     )
 
-    :telemetry.execute([:vm, :monitor, :long_gc], Map.new(info), %{process_type: type})
+    :telemetry.execute(@vm_monitor_long_gc, Map.new(info), %{process_type: type})
+
     {:noreply, state}
   end
 
@@ -38,11 +52,7 @@ defmodule Electric.Telemetry.SystemMonitor do
       "Long schedule detected for port #{inspect(port)}, took #{Keyword.fetch!(info, :timeout)}ms"
     )
 
-    :telemetry.execute(
-      [:vm, :monitor, :long_schedule],
-      %{timeout: Keyword.fetch!(info, :timeout)},
-      %{process_type: :port}
-    )
+    :telemetry.execute(@vm_monitor_long_schedule, Map.new(info), %{process_type: :port})
 
     {:noreply, state}
   end
@@ -50,15 +60,29 @@ defmodule Electric.Telemetry.SystemMonitor do
   def handle_info({:monitor, pid, :long_schedule, info}, state) when is_pid(pid) do
     type = proc_type(pid)
 
-    Logger.debug(
-      "Long schedule detected for pid #{inspect(pid)} (#{inspect(type)}), took #{Keyword.fetch!(info, :timeout)}ms"
-    )
+    Logger.debug(fn ->
+      locations =
+        info
+        |> Keyword.delete(:timeout)
+        |> Map.new(fn {loc, {m, f, a}} when loc in [:in, :out] ->
+          {loc, Exception.format_mfa(m, f, a)}
+        end)
 
-    :telemetry.execute(
-      [:vm, :monitor, :long_schedule],
-      %{timeout: Keyword.fetch!(info, :timeout)},
-      %{process_type: type}
-    )
+      locs_str =
+        if map_size(locations) > 0 do
+          "; " <>
+            (locations |> Enum.map(fn {key, val} -> "#{key}: #{val}" end) |> Enum.join(", "))
+        else
+          ""
+        end
+
+      "Long schedule detected for pid #{inspect(pid)} (#{inspect(type)}), took #{Keyword.fetch!(info, :timeout)}ms" <>
+        locs_str
+    end)
+
+    :telemetry.execute(@vm_monitor_long_schedule, %{timeout: Keyword.fetch!(info, :timeout)}, %{
+      process_type: type
+    })
 
     {:noreply, state}
   end
@@ -68,10 +92,15 @@ defmodule Electric.Telemetry.SystemMonitor do
 
     Logger.debug("Long message queue detected for pid #{inspect(pid)} (#{inspect(type)})")
 
-    :telemetry.execute([:vm, :monitor, :long_message_queue], %{present: 1}, %{process_type: type})
+    {:message_queue_len, queue_len} = Process.info(pid, :message_queue_len)
 
-    {:noreply,
-     %{state | long_message_queue_pids: Map.put(state.long_message_queue_pids, pid, type)}}
+    :telemetry.execute(@vm_monitor_long_message_queue, %{length: queue_len}, %{process_type: type})
+
+    state =
+      %{state | long_message_queue_pids: Map.put(state.long_message_queue_pids, pid, type)}
+      |> maybe_start_long_message_queue_timer()
+
+    {:noreply, state}
   end
 
   def handle_info({:monitor, pid, :long_message_queue, false}, state) do
@@ -79,17 +108,34 @@ defmodule Electric.Telemetry.SystemMonitor do
     {:noreply, %{state | long_message_queue_pids: Map.delete(state.long_message_queue_pids, pid)}}
   end
 
-  def handle_info(:report_long_queues, state) when state.long_message_queue_pids != %{} do
-    for {_, type} <- state.long_message_queue_pids do
-      :telemetry.execute([:vm, :monitor, :long_message_queue], %{present: 1}, %{
+  def handle_info(:recheck_message_queues, state)
+      when map_size(state.long_message_queue_pids) == 0 do
+    :timer.cancel(state.long_message_queue_timer)
+    {:noreply, %{state | long_message_queue_timer: nil}}
+  end
+
+  def handle_info(:recheck_message_queues, state) do
+    Enum.each(state.long_message_queue_pids, fn {pid, type} ->
+      {:message_queue_len, queue_len} = Process.info(pid, :message_queue_len)
+
+      :telemetry.execute(@vm_monitor_long_message_queue, %{length: queue_len}, %{
         process_type: type
       })
-    end
+    end)
 
     {:noreply, state}
   end
 
-  def handle_info(:report_long_queues, state) do
-    {:noreply, state}
+  defp maybe_start_long_message_queue_timer(%{long_message_queue_timer: nil} = state) do
+    # A process whose message queue length exceeds the threshold is likely to be spiraling out of
+    # control. Therefore we need recheck it quite often to capture the dynamics.
+    #
+    # Though there's still no guarantee that the VM will not run out of memory before it
+    # reaches the next metric export tick.
+    {:ok, timer} = :timer.send_interval(200, :recheck_message_queues)
+
+    %{state | long_message_queue_timer: timer}
   end
+
+  defp maybe_start_long_message_queue_timer(state), do: state
 end
