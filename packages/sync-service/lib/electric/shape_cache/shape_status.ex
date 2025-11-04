55c7ca1582e38ef7d2c3ae2e9bb84765de541ecc
Rob A'Court <robacourt@users.noreply.github.com>
2025-11-04 17:40:29 +0000

    feat(sync-service): Speed up shape meta data removal (#3385)
    For 50K shapes and concurrent read and writes of the ShapeStatus ETS
    tables, `ShapeStatus.remove_shape` takes 50ms on Ampere, 23ms locally.
    This only gets slower as the number of shapes increases. This PR speeds
    up that process by 450% to 0.05ms.
    
    It does mean that the metadata delete is not atomic, but as far as I can
    tell there are no race conditions that this introduces.

--- packages/sync-service/lib/electric/shape_cache/shape_status.ex
+++ packages/sync-service/lib/electric/shape_cache/shape_status.ex
@@ -202,18 +202,13 @@ defmodule Electric.ShapeCache.ShapeStatus do
       # is ever matched to a handle with incomplete information, since deleting with
       # select_delete can lead to inconsistent state
       :ets.delete(meta_table, {@shape_hash_lookup, Shape.comparable(shape)})
+      :ets.delete(meta_table, {@shape_meta_data, shape_handle})
+      :ets.delete(meta_table, {@shape_storage_state_backup, shape_handle})
+      :ets.delete(meta_table, {@snapshot_started, shape_handle})
 
-      :ets.select_delete(
-        meta_table,
-        [
-          {{{@shape_meta_data, shape_handle}, :_, :_, :_}, [], [true]},
-          {{{@shape_storage_state_backup, shape_handle}, :_}, [], [true]},
-          {{{@snapshot_started, shape_handle}, :_}, [], [true]}
-          | Enum.map(Shape.list_relations(shape), fn {oid, _} ->
-              {{{@shape_relation_lookup, oid, shape_handle}, :_}, [], [true]}
-            end)
-        ]
-      )
+      Enum.each(Shape.list_relations(shape), fn {oid, _} ->
+        :ets.delete(meta_table, {@shape_relation_lookup, oid, shape_handle})
+      end)
 
       :ets.delete(shape_last_used_table(stack_ref), shape_handle)
 
