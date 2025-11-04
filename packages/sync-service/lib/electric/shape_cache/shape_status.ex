56183c886cdf4b2a36b24ca6c8fa54c819bf53cb
rob <rob.acourt@gmail.com>
2025-11-04 17:20:23 +0000

    Use delete rather than select_delete

--- packages/sync-service/lib/electric/shape_cache/shape_status.ex
+++ packages/sync-service/lib/electric/shape_cache/shape_status.ex
@@ -203,22 +203,15 @@ defmodule Electric.ShapeCache.ShapeStatus do
       # select_delete can lead to inconsistent state
       :ets.delete(meta_table, {@shape_hash_lookup, Shape.comparable(shape)})
 
-      :ets.select_delete(meta_table, [
-        {{{@shape_meta_data, shape_handle}, :_, :_, :_}, [], [true]}
-      ])
+      :ets.delete(meta_table, {@shape_meta_data, shape_handle})
 
-      :ets.select_delete(meta_table, [
-        {{{@shape_storage_state_backup, shape_handle}, :_}, [], [true]}
-      ])
+      :ets.delete(meta_table, {@shape_storage_state_backup, shape_handle})
 
-      :ets.select_delete(meta_table, [{{{@snapshot_started, shape_handle}, :_}, [], [true]}])
+      :ets.delete(meta_table, {@snapshot_started, shape_handle})
 
-      :ets.select_delete(
-        meta_table,
-        Enum.map(Shape.list_relations(shape), fn {oid, _} ->
-          {{{@shape_relation_lookup, oid, shape_handle}, :_}, [], [true]}
-        end)
-      )
+      Enum.each(Shape.list_relations(shape), fn {oid, _} ->
+        :ets.delete(meta_table, {@shape_relation_lookup, oid, shape_handle})
+      end)
 
       :ets.delete(shape_last_used_table(stack_ref), shape_handle)
 
