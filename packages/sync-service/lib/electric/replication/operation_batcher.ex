33fbc857f19edf8c2a8b5aed4f6d91dfd10fe76d
Rob A'Court <robacourt@users.noreply.github.com>
2025-11-24 10:50:13 +0000

    (chore) Refactor operation batching (#3483)
    Addressing @msfstef 's feedback on #3465 this PR:
    - Separates concerns of metrics and message converting
    - Makes txn handling in Replication client more explicit
    - Removes the case where `handle_operations` is called with an empty
    list of operations

--- packages/sync-service/lib/electric/replication/operation_batcher.ex
+++ packages/sync-service/lib/electric/replication/operation_batcher.ex
@@ -2,16 +2,17 @@ defmodule Electric.Replication.OperationBatcher do
   alias Electric.Replication.Changes
   alias Electric.Replication.Changes.{Commit, Relation}
 
-  defstruct [:buffer, :flush?]
+  defstruct [:buffer, :commits, :flush?]
 
   @type t() :: %__MODULE__{
           buffer: [Changes.operation()],
+          commits: [%Commit{}],
           flush?: boolean()
         }
 
   @spec new() :: t()
   def new do
-    %__MODULE__{buffer: [], flush?: false}
+    %__MODULE__{buffer: [], commits: [], flush?: false}
   end
 
   @doc """
@@ -19,11 +20,16 @@ defmodule Electric.Replication.OperationBatcher do
   - A batch is flushed when a Commit operation is seen
   - A batch is flushed when a Relation operation is seen
   - A batch is flushed when the max batch size is reached
-  Returns a tuple of {batched_operations, updated_batcher}
+  Returns a tuple of {:ok, batched_operations, commits, updated_batcher}
+  or {:buffering, updated_batcher} if no flush occurred.
   """
-  @spec batch([Changes.operation()], non_neg_integer(), t()) :: {[Changes.operation()], t()}
-  def batch([operation | operations], max_batch_size, %{buffer: buffer} = batcher) do
-    batcher = %{batcher | buffer: [operation | buffer]}
+  @spec batch([Changes.operation()], non_neg_integer(), t()) ::
+          {:ok, [Changes.operation()], [%Commit{}], t()} | {:buffering, t()}
+  def batch([operation | operations], max_batch_size, batcher) do
+    batcher =
+      batcher
+      |> add_to_buffer(operation)
+      |> maybe_add_commit(operation)
 
     batch(operations, max_batch_size, %{
       batcher
@@ -32,10 +38,11 @@ defmodule Electric.Replication.OperationBatcher do
   end
 
   def batch([], _, %{flush?: true} = batcher) do
-    {Enum.reverse(batcher.buffer), %{batcher | buffer: [], flush?: false}}
+    {:ok, Enum.reverse(batcher.buffer), Enum.reverse(batcher.commits),
+     %{batcher | buffer: [], commits: [], flush?: false}}
   end
 
-  def batch([], _, batcher), do: {[], batcher}
+  def batch([], _, batcher), do: {:buffering, batcher}
 
   defp flush?(_, _, %{flush?: true}), do: true
   defp flush?(%Commit{}, _, _), do: true
@@ -45,4 +52,14 @@ defmodule Electric.Replication.OperationBatcher do
        when length(buffer) >= max_batch_size, do: true
 
   defp flush?(_, _, _), do: false
+
+  defp add_to_buffer(%{buffer: buffer} = batcher, operation) do
+    %{batcher | buffer: [operation | buffer]}
+  end
+
+  defp maybe_add_commit(%{commits: commits} = batcher, %Commit{} = commit) do
+    %{batcher | commits: [commit | commits]}
+  end
+
+  defp maybe_add_commit(batcher, _), do: batcher
 end
