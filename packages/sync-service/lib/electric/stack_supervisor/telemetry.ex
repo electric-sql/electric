96cacdcd50a389b70c840913bf1a49aee9504a4c
Oleksii Sholik <oleksii@sholik.dev>
2025-12-04 12:31:45 +0100

    Report the correct reatined WAL size confirmed_flush_lsn_lag metrics (#3543)
    Fix [#3544](https://github.com/electric-sql/electric/issues/3544).
    
    The metric that is currently reported as
    `electric.postgres.replication.wal_size` is incorrectly named. Its name
    was changed from `electric.postgres.replication.lag` in
    https://github.com/electric-sql/electric/pull/2056.
    
    Comparing `pg_current_wal_lsn()` to the replication slot's
    `confirmed_flush_lsn` shows how caught up the replcation slot consumer
    is to the latest database state. So it does measure the lag in terms of
    the total byte size of changes that had been sent to Electric over the
    replication connection and how much of those bytes Electric confirmed to
    have successfully flushed to disk. This values tends to 0 (or -1 but we
    can it at 0) as time progresses and no new changes appear in PG.
    
    However, the other metric that is super critical to Electric's uptime is
    the difference between `pg_current_wal_lsn()` and the replication slot's
    `restart_lsn`. This shows how much WAL Postgres is forced to keep for
    the slot and this is the value that, when it exceeds the
    `max_slot_wal_keep_size` config, causes the replication slot to get
    invalidated. We have neglected collecting this metric until now.

--- packages/sync-service/lib/electric/stack_supervisor/telemetry.ex
+++ packages/sync-service/lib/electric/stack_supervisor/telemetry.ex
@@ -18,8 +18,8 @@ defmodule Electric.StackSupervisor.Telemetry do
 
   def child_spec(%{stack_telemetry: stack_telemetry}), do: stack_telemetry
 
-  def child_spec(config) do
-    if Code.ensure_loaded?(ElectricTelemetry.StackTelemetry) do
+  if Code.ensure_loaded?(ElectricTelemetry.StackTelemetry) do
+    def child_spec(config) when is_map(config) do
       telemetry_opts =
         config.telemetry_opts
         |> Keyword.put(:stack_id, config.stack_id)
@@ -29,85 +29,112 @@ defmodule Electric.StackSupervisor.Telemetry do
           default_periodic_measurements(config),
           & &1
         )
+        # Add metrics for the default periodic measurements regardless of whether the
+        # measurements themselves are occuring.
+        |> Keyword.update(
+          :additional_metrics,
+          default_metrics_from_periodic_measurements(),
+          &(default_metrics_from_periodic_measurements() ++ &1)
+        )
 
       {ElectricTelemetry.StackTelemetry, telemetry_opts}
     end
-  end
 
-  defp default_periodic_measurements(%{stack_id: stack_id} = config) do
-    [
-      {__MODULE__, :count_shapes, [stack_id]},
-      {__MODULE__, :count_active_shapes, [stack_id]},
-      {__MODULE__, :report_retained_wal_size, [stack_id, config.replication_opts[:slot_name]]}
-    ]
-  end
+    defp default_metrics_from_periodic_measurements do
+      [
+        Telemetry.Metrics.last_value("electric.shapes.total_shapes.count"),
+        Telemetry.Metrics.last_value("electric.shapes.active_shapes.count"),
+        Telemetry.Metrics.last_value("electric.postgres.replication.pg_current_wal_lsn",
+          unit: :byte
+        ),
+        Telemetry.Metrics.last_value("electric.postgres.replication.slot_retained_wal_size",
+          unit: :byte
+        ),
+        Telemetry.Metrics.last_value("electric.postgres.replication.slot_confirmed_flush_lsn_lag",
+          unit: :byte
+        )
+      ]
+    end
 
-  def count_shapes(stack_id, _telemetry_opts) do
-    # Telemetry is started before everything else in the stack, so we need to handle
-    # the case where the shape cache is not started yet.
-    case Electric.ShapeCache.count_shapes(stack_id) do
-      :error ->
-        :ok
+    defp default_periodic_measurements(%{stack_id: stack_id} = config) do
+      [
+        {__MODULE__, :count_shapes, [stack_id]},
+        {__MODULE__, :report_retained_wal_size, [stack_id, config.replication_opts[:slot_name]]}
+      ]
+    end
 
-      num_shapes ->
+    def count_shapes(stack_id, _telemetry_opts) do
+      # Telemetry is started before everything else in the stack, so we need to handle
+      # the case where the shape cache is not started yet.
+      with num_shapes when is_integer(num_shapes) <- Electric.ShapeCache.count_shapes(stack_id) do
         Electric.Telemetry.OpenTelemetry.execute(
           [:electric, :shapes, :total_shapes],
           %{count: num_shapes},
           %{stack_id: stack_id}
         )
-    end
-  end
-
-  def count_active_shapes(stack_id, _telemetry_opts) do
-    Electric.Telemetry.OpenTelemetry.execute(
-      [:electric, :shapes, :active_shapes],
-      %{count: Electric.Shapes.ConsumerRegistry.active_consumer_count(stack_id)},
-      %{stack_id: stack_id}
-    )
-  end
-
-  @retained_wal_size_query """
-  SELECT
-    pg_wal_lsn_diff(pg_current_wal_lsn(), confirmed_flush_lsn)::int8
-  FROM
-    pg_replication_slots
-  WHERE
-    slot_name = $1
-  """
-
-  @doc false
-  @spec report_retained_wal_size(Electric.stack_id(), binary(), map()) :: :ok
-  def report_retained_wal_size(stack_id, slot_name, _telemetry_opts) do
-    try do
-      %Postgrex.Result{rows: [[wal_size]]} =
-        Postgrex.query!(
-          Electric.Connection.Manager.admin_pool(stack_id),
-          @retained_wal_size_query,
-          [slot_name],
-          timeout: 3_000,
-          deadline: 3_000
-        )
-
-      # The query above can return `-1` which I'm assuming means "up-to-date".
-      # This is a confusing stat if we're measuring in bytes, so normalise to
-      # [0, :infinity)
+      end
 
       Electric.Telemetry.OpenTelemetry.execute(
-        [:electric, :postgres, :replication],
-        %{wal_size: max(0, wal_size)},
+        [:electric, :shapes, :active_shapes],
+        %{count: Electric.Shapes.ConsumerRegistry.active_consumer_count(stack_id)},
         %{stack_id: stack_id}
       )
-    catch
-      :exit, {:noproc, _} ->
-        :ok
-
-      # catch all errors to not log them as errors, those are reporing issues at best
-      type, reason ->
-        Logger.warning(
-          "Failed to query retained WAL size\nError: #{Exception.format(type, reason)}",
-          stack_id: stack_id,
-          slot_name: slot_name
-        )
     end
+
+    @retained_wal_size_query """
+    SELECT
+      pg_wal_lsn_diff(pg_current_wal_lsn(), '0/0')::int8 AS pg_current_wal_lsn,
+      pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn)::int8 AS retained_wal_size,
+      pg_wal_lsn_diff(pg_current_wal_lsn(), confirmed_flush_lsn)::int8 AS confirmed_flush_lsn_lag
+    FROM
+      pg_replication_slots
+    WHERE
+      slot_name = $1
+    """
+
+    @doc false
+    @spec report_retained_wal_size(Electric.stack_id(), binary(), map()) :: :ok
+    def report_retained_wal_size(stack_id, slot_name, _telemetry_opts) do
+      try do
+        %Postgrex.Result{rows: [[pg_current_wal_lsn, retained_wal_size, confirmed_flush_lsn_lag]]} =
+          Postgrex.query!(
+            Electric.Connection.Manager.admin_pool(stack_id),
+            @retained_wal_size_query,
+            [slot_name],
+            timeout: 3_000,
+            deadline: 3_000
+          )
+
+        # The query above can return `-1` for `confirmed_flush_lsn_lag` which means that Electric
+        # is caught up with Postgres' replication stream.
+        # This is a confusing stat if we're measuring in bytes, so use 0 as the bottom limit.
+
+        Electric.Telemetry.OpenTelemetry.execute(
+          [:electric, :postgres, :replication],
+          %{
+            # The absolute value of pg_current_wal_lsn doesn't convey any useful info but by
+            # plotting its rate of change we can see how fast Postgres is writing new WAL
+            # entries.
+            pg_current_wal_lsn: pg_current_wal_lsn,
+            slot_retained_wal_size: retained_wal_size,
+            slot_confirmed_flush_lsn_lag: max(0, confirmed_flush_lsn_lag)
+          },
+          %{stack_id: stack_id}
+        )
+      catch
+        :exit, {:noproc, _} ->
+          :ok
+
+        # catch all errors to not log them as errors, those are reporing issues at best
+        type, reason ->
+          Logger.warning(
+            "Failed to query retained WAL size\nError: #{Exception.format(type, reason)}",
+            stack_id: stack_id,
+            slot_name: slot_name
+          )
+      end
+    end
+  else
+    def child_spec(_), do: nil
   end
 end
