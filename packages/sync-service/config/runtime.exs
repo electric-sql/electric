electric-sql/electric

commit 03c10a298ca694558fc6e1d76b71582512906ff8

Integration test for Otel metric export (#3553)
Follow-up to https://github.com/electric-sql/electric/pull/3543.

It was a good idea to add a test harness for metric export because in
the process I discovered an issue with the WAL size query:

12:18:52.404 pid=<0.464.0> [warning] Failed to query retained WAL size
Error: ** (Postgrex.Error) ERROR 22003 (numeric_value_out_of_range)
bigint out of range

I have updated the query and the metric name for `pg_wal_offset` since
it no longer matches the value returned by `pg_current_wal_lsn()`. See
explanation in code.

Right now it doesn't matter that the offset stay within the signed
64-bit integer range because our otel_metric_exporter converts values
for the LastValue metric to double. But once
https://github.com/electric-sql/elixir-otel-metric-exporter/pull/28 is
released and Electric upgrades its dep to that version, it will be able
to export integer values as integers.

packages/sync-service/config/runtime.exs
@@ -274,6 +274,15 @@ config :electric,
       nil
     )
 
+# Disable opentelemetry_exporter by default.
+#
+# Without any explicit config, opentelemetry starts some resource detectors and initializes
+# otel_batch_processor which then tries to communicate with a remote OTLP server
+# (localhost:4318 by default) periodically.
+#
+# We don't want any of that unless OpenTelemetry export is explicitly enabled further down.
+config :opentelemetry, processors: []
+
 if Electric.telemetry_enabled?() do
   # Disable the default telemetry_poller process since we start our own in
   # `ElectricTelemetry.{ApplicationTelemetry, StackTelemetry}`.

.github/workflows/elixir_tests.yml
@@ -76,10 +76,10 @@ jobs:
         uses: actions/cache@v4
         with:
           path: packages/sync-service/deps
-          key: "${{ runner.os }}-sync-service-deps-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}"
+          key: "${{ runner.os }}-sync-service-deps-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}"
           restore-keys: |
-            ${{ runner.os }}-sync-service-deps-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}
-            ${{ runner.os }}-sync-service-deps-${{ env.MIX_ENV }}-
+            ${{ runner.os }}-sync-service-deps-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}
+            ${{ runner.os }}-sync-service-deps-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-
             ${{ runner.os }}-sync-service-deps-
 
       - name: Cache compiled code
@@ -88,10 +88,10 @@ jobs:
           path: |
             packages/sync-service/_build/*/lib
             !packages/sync-service/_build/*/lib/electric
-          key: "${{ runner.os }}-sync-service-build-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}"
+          key: "${{ runner.os }}-sync-service-build-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}"
           restore-keys: |
-            ${{ runner.os }}-sync-service-build-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}
-            ${{ runner.os }}-sync-service-build-${{ env.MIX_ENV }}-
+            ${{ runner.os }}-sync-service-build-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}
+            ${{ runner.os }}-sync-service-build-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-
             ${{ runner.os }}-sync-service-build-
 
       - name: Install dependencies

.github/workflows/integration_tests.yml
@@ -23,6 +23,7 @@ jobs:
         working-directory: integration-tests
     env:
       MIX_ENV: dev
+      MIX_TARGET: application
       MIX_OS_DEPS_COMPILE_PARTITION_COUNT: 4
     steps:
       - uses: actions/checkout@v4
@@ -36,10 +37,10 @@ jobs:
         uses: actions/cache@v4
         with:
           path: packages/sync-service/deps
-          key: "${{ runner.os }}-sync-service-deps-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}"
+          key: "${{ runner.os }}-sync-service-deps-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}"
           restore-keys: |
-            ${{ runner.os }}-sync-service-deps-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}
-            ${{ runner.os }}-sync-service-deps-${{ env.MIX_ENV }}-
+            ${{ runner.os }}-sync-service-deps-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}
+            ${{ runner.os }}-sync-service-deps-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-
             ${{ runner.os }}-sync-service-deps-
 
       - name: Cache compiled code
@@ -48,10 +49,10 @@ jobs:
           path: |
             packages/sync-service/_build/*/lib
             !packages/sync-service/_build/*/lib/electric
-          key: "${{ runner.os }}-sync-service-build-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}"
+          key: "${{ runner.os }}-sync-service-build-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}"
           restore-keys: |
-            ${{ runner.os }}-sync-service-build-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}
-            ${{ runner.os }}-sync-service-build-${{ env.MIX_ENV }}-
+            ${{ runner.os }}-sync-service-build-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}
+            ${{ runner.os }}-sync-service-build-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-
             ${{ runner.os }}-sync-service-build-
 
       - name: Install dependencies
@@ -70,7 +71,7 @@ jobs:
         run: ./run.sh
       - name: Upload lux logs
         uses: actions/upload-artifact@v4
-        if: ${{ failure() && steps.tests.outcome == 'failure'  }}
+        if: ${{ failure() && steps.tests.outcome == 'failure' }}
         with:
           name: lux_logs
           path: integration-tests/**/lux_logs/run_*

.github/workflows/ts_test.yml
@@ -88,10 +88,10 @@ jobs:
         uses: actions/cache@v4
         with:
           path: packages/sync-service/deps
-          key: "${{ runner.os }}-sync-service-deps-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}"
+          key: "${{ runner.os }}-sync-service-deps-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}"
           restore-keys: |
-            ${{ runner.os }}-sync-service-deps-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}
-            ${{ runner.os }}-sync-service-deps-${{ env.MIX_ENV }}-
+            ${{ runner.os }}-sync-service-deps-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}
+            ${{ runner.os }}-sync-service-deps-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-
             ${{ runner.os }}-sync-service-deps-
 
       - name: Cache compiled code
@@ -100,10 +100,10 @@ jobs:
           path: |
             packages/sync-service/_build/*/lib
             !packages/sync-service/_build/*/lib/electric
-          key: "${{ runner.os }}-sync-service-build-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}"
+          key: "${{ runner.os }}-sync-service-build-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}"
           restore-keys: |
-            ${{ runner.os }}-sync-service-build-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}
-            ${{ runner.os }}-sync-service-build-${{ env.MIX_ENV }}-
+            ${{ runner.os }}-sync-service-build-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-${{ hashFiles('packages/sync-service/mix.lock') }}
+            ${{ runner.os }}-sync-service-build-${{ env.MIX_TARGET }}-${{ env.MIX_ENV }}-
             ${{ runner.os }}-sync-service-build-
 
       - name: Install dependencies

integration-tests/scripts/electric_dev.sh
@@ -6,4 +6,7 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 cd "$SCRIPT_DIR"/../../packages/sync-service
 
-ELECTRIC_STORAGE_DIR="$SCRIPT_DIR/../_storage" ELECTRIC_REPLICATION_STREAM_ID=integration iex -r "$SCRIPT_DIR/../test_utils/*.exs" "$@" -S mix
+MIX_TARGET=application \
+ELECTRIC_STORAGE_DIR="$SCRIPT_DIR/../_storage" \
+ELECTRIC_REPLICATION_STREAM_ID=integration \
+iex -r "$SCRIPT_DIR/../test_utils/*.exs" "$@" -S mix

integration-tests/support_files/otel-collector-config.yaml
@@ -1,0 +1,25 @@
+exporters:
+  debug:
+    verbosity: detailed
+receivers:
+  otlp:
+    protocols:
+      http:
+        endpoint: '0.0.0.0:4318'
+service:
+  pipelines:
+    metrics:
+      receivers:
+        - otlp
+      exporters:
+        - debug
+    logs:
+      receivers:
+        - otlp
+      exporters:
+        - debug
+    traces:
+      receivers:
+        - otlp
+      exporters:
+        - debug

integration-tests/tests/_macros.luxinc
@@ -7,6 +7,8 @@
 [global database_url=postgresql://postgres:password@localhost:$pg_host_port/electric?sslmode=disable]
 [global pooled_database_url=postgresql://postgres:password@localhost:$pg_pooler_host_port/electric?sslmode=disable]
 
+[global otel_collector_container_name=electric_integration_otel_collector]
+
 [macro setup_pg_with_shell_name shell_name container_env_vars config_opts extra_volumes]
   [shell $shell_name]
     -$fail_pattern
@@ -216,6 +218,18 @@
     [invoke wait-for "curl --silent -X GET http://localhost:3000/v1/health" "\{\"status\":\"$expected_status\"\}" 10 $PS1]
 [endmacro]
 
+[macro setup_otel_collector]
+  [shell otel_collector]
+    !docker run \
+      --name $otel_collector_container_name \
+      -p 4318:4318 \
+      -v $(realpath ../support_files/otel-collector-config.yaml):/conf/otel-collector-config.yaml \
+      otel/opentelemetry-collector-contrib --config=/conf/otel-collector-config.yaml
+    ??Starting HTTP server
+    ?"endpoint": "(0\.0\.0\.0|\[::\]):4318"
+    ??Everything is ready. Begin running and processing data.
+[endmacro]
+
 [macro teardown_container container_name]
   -$fail_pattern
   !docker rm -f -v $container_name 2>/dev/null || true
@@ -225,6 +239,7 @@
 [macro teardown]
   [invoke teardown_container "${pg_container_name}-pooler"]
   [invoke teardown_container $pg_container_name]
+  [invoke teardown_container $otel_collector_container_name]
   !../scripts/clean_up.sh
   ?$PS1
 [endmacro]

integration-tests/tests/stack-telemetry.lux
@@ -1,0 +1,59 @@
+[doc Verify that all of the expected stack metrics are exported via Otel]
+
+[include _macros.luxinc]
+
+[global pg_container_name=stack-telemetry__pg]
+
+###
+
+[invoke setup_otel_collector]
+
+[invoke setup_pg]
+
+[invoke setup_electric_with_env "ELECTRIC_OTLP_ENDPOINT=http://localhost:4318 ELECTRIC_SYSTEM_METRICS_POLL_INTERVAL=1s ELECTRIC_OTEL_EXPORT_PERIOD=2s DO_NOT_START_CONN_MAN_PING=1 ELECTRIC_LOG_LEVEL=info OTEL_RESOURCE_ATTRIBUTES=custom.attr=electric.val"]
+
+[shell otel_collector]
+  # Verify that the Collector receives metrics from Electric with expected resource attributes
+  ??info	ResourceMetrics
+  ??Resource attributes:
+  ??     -> custom.attr: Str(electric.val)
+  ??     -> instance.id: Str(
+  ??     -> name: Str(metrics)
+  ??     -> service.name: Str(electric)
+  ??     -> service.version: Str(
+
+  # Verify that LSN metrics are exported
+  ??Metric #
+  ??Descriptor:
+  ??     -> Name: electric.postgres.replication.pg_current_wal_lsn
+  ??     -> Unit: By
+  ??     -> DataType: Gauge
+  ??NumberDataPoints #
+  ??StartTimestamp:
+  ??Timestamp:
+  ??Value:
+
+  ??Metric #
+  ??Descriptor:
+  ??     -> Name: electric.postgres.replication.slot_confirmed_flush_lsn_lag
+  ??     -> Unit: By
+  ??     -> DataType: Gauge
+  ??NumberDataPoints #
+  ??StartTimestamp:
+  ??Timestamp:
+  ??Value:
+
+  ??Metric #
+  ??Descriptor:
+  ??     -> Name: electric.postgres.replication.slot_retained_wal_size
+  ??     -> Unit: By
+  ??     -> DataType: Gauge
+  ??NumberDataPoints #
+  ??StartTimestamp:
+  ??Timestamp:
+  ??Value:
+
+###
+
+[cleanup]
+  [invoke teardown]

commit 585736cd62ab54fcd2ad27d77a134f827467c967

Fix Mix warning about missing :opentelemetry app when MIX_TARGET != application (#3557)
The problem got introduced in
https://github.com/electric-sql/electric/commit/03c10a298ca694558fc6e1d76b71582512906ff8#diff-e39d5a2dccf3960074b1107d725583e91ddc8599a7643f63144df19981bc3c6e.

packages/sync-service/config/runtime.exs
@@ -274,15 +274,6 @@ config :electric,
       nil
     )
 
-# Disable opentelemetry_exporter by default.
-#
-# Without any explicit config, opentelemetry starts some resource detectors and initializes
-# otel_batch_processor which then tries to communicate with a remote OTLP server
-# (localhost:4318 by default) periodically.
-#
-# We don't want any of that unless OpenTelemetry export is explicitly enabled further down.
-config :opentelemetry, processors: []
-
 if Electric.telemetry_enabled?() do
   # Disable the default telemetry_poller process since we start our own in
   # `ElectricTelemetry.{ApplicationTelemetry, StackTelemetry}`.
@@ -357,5 +348,14 @@ if Electric.telemetry_enabled?() do
          }
        }}
     ]
+  else
+    # Disable opentelemetry_exporter.
+    #
+    # Without any explicit config, opentelemetry starts some resource detectors and initializes
+    # otel_batch_processor which then tries to communicate with a remote OTLP server
+    # (localhost:4318 by default) periodically.
+    #
+    # We don't want any of that unless OpenTelemetry export is explicitly enabled.
+    config :opentelemetry, processors: []
   end
 end