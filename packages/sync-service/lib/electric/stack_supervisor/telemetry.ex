ceef72cd68f51689109d960f439b6565863b264c
Oleksii Sholik <oleksii@sholik.dev>
2025-12-10 15:12:38 +0100

    Fix incorrect metric name (#3577)
    I forgot to update the name of the metric in the export definition after
    having changed it in the measurement in
    https://github.com/electric-sql/electric/commit/03c10a298ca694558fc6e1d76b71582512906ff8#diff-162dde42d82189b4d1cf8fe6355cde2c5c2fe5795ff92ddb00fa06562f37815cL115-R122.
    
    Here I've also bumped the `otel_metric_exporter` dependency version to
    0.4.3 to include [the
    fix](https://github.com/electric-sql/elixir-otel-metric-exporter/commit/5c20a32e1f656c0e0b987520de6b226185d62df1)
    for integer gauge values that exceed the 2^53 limit of precise integer
    representation in float numbers.

--- packages/sync-service/lib/electric/stack_supervisor/telemetry.ex
+++ packages/sync-service/lib/electric/stack_supervisor/telemetry.ex
@@ -44,9 +44,7 @@ defmodule Electric.StackSupervisor.Telemetry do
       [
         Telemetry.Metrics.last_value("electric.shapes.total_shapes.count"),
         Telemetry.Metrics.last_value("electric.shapes.active_shapes.count"),
-        Telemetry.Metrics.last_value("electric.postgres.replication.pg_current_wal_lsn",
-          unit: :byte
-        ),
+        Telemetry.Metrics.last_value("electric.postgres.replication.pg_wal_offset"),
         Telemetry.Metrics.last_value("electric.postgres.replication.slot_retained_wal_size",
           unit: :byte
         ),
