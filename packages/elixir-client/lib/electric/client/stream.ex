9dfed62ab80962fd038615ad10f0c566036432cf
rob <rob.acourt@gmail.com>
2026-01-27 14:20:32 +0000

    stream to use poll

--- packages/elixir-client/lib/electric/client/stream.ex
+++ packages/elixir-client/lib/electric/client/stream.ex
@@ -1,31 +1,20 @@
 defmodule Electric.Client.Stream do
   @moduledoc false
 
-  alias Electric.Client.Fetch
   alias Electric.Client.Message
-  alias Electric.Client.TagTracker
+  alias Electric.Client.Poll
+  alias Electric.Client.ShapeState
   alias Electric.Client
 
   defstruct [
     :id,
     :client,
-    :shape,
-    :schema,
-    :value_mapper_fun,
+    :poll_state,
     parser: {Electric.Client.ValueMapper, []},
     buffer: :queue.new(),
-    up_to_date?: false,
     replica: :default,
-    offset: Client.Offset.before_all(),
-    shape_handle: nil,
-    next_cursor: nil,
     state: :init,
-    opts: %{},
-    # Move-out support: tracks tags per key and keys per tag
-    # tag_to_keys: %{tag_value => MapSet<key>} - which keys have this tag
-    # key_data: %{key => %{tags: MapSet<tag>, msg: msg}} - each key's tags and latest message
-    tag_to_keys: %{},
-    key_data: %{}
+    opts: %{}
   ]
 
   @external_options [
@@ -97,20 +86,12 @@ defmodule Electric.Client.Stream do
   @type t :: %__MODULE__{
           id: integer(),
           client: Client.t(),
-          shape: Client.shape() | nil,
-          schema: Client.schema(),
-          value_mapper_fun: Client.ValueMapper.mapper_fun(),
+          poll_state: ShapeState.t(),
           parser: nil | {module(), term()},
           buffer: :queue.queue(),
-          up_to_date?: boolean(),
-          offset: Client.offset(),
           replica: Client.replica(),
-          shape_handle: nil | Client.shape_handle(),
-          next_cursor: binary() | nil,
           state: :init | :stream | :done,
-          opts: opts(),
-          tag_to_keys: %{optional(term()) => MapSet.t()},
-          key_data: %{optional(term()) => %{tags: MapSet.t(), msg: Message.ChangeMessage.t()}}
+          opts: opts()
         }
 
   alias __MODULE__, as: S
@@ -134,8 +115,9 @@ defmodule Electric.Client.Stream do
     opts = NimbleOptions.validate!(Map.new(opts), @opts_schema)
 
     id = generate_id()
+    poll_state = ShapeState.new()
 
-    struct(__MODULE__, Keyword.put(core, :opts, opts) |> Keyword.put(:id, id))
+    struct(__MODULE__, Keyword.merge(core, id: id, opts: opts, poll_state: poll_state))
   end
 
   defp generate_id do
@@ -160,113 +142,58 @@ defmodule Electric.Client.Stream do
 
   defp fetch(%S{state: :init} = stream) do
     stream
-    |> resume()
-    |> stream_state()
+    |> maybe_resume()
+    |> Map.put(:state, :stream)
     |> fetch()
   end
 
   defp fetch(%S{} = stream) do
-    stream
-    |> make_request()
-    |> handle_response(stream)
-    |> after_fetch()
-  end
+    # Use the parser from stream config or fall back to client's parser
+    parser = stream.parser || stream.client.parser
+    client_with_parser = %{stream.client | parser: parser}
 
-  defp ensure_enum(body) do
-    case Enumerable.impl_for(body) do
-      nil -> List.wrap(body)
-      Enumerable.Map -> List.wrap(body)
-      _impl -> body
+    case Poll.request(client_with_parser, stream.poll_state, replica: stream.replica) do
+      {:ok, messages, new_poll_state} ->
+        stream
+        |> Map.put(:poll_state, new_poll_state)
+        |> handle_messages(messages)
+        |> dispatch()
+
+      {:must_refetch, messages, new_poll_state} ->
+        stream
+        |> Map.put(:poll_state, new_poll_state)
+        |> Map.put(:buffer, :queue.new())
+        |> handle_messages(messages)
+        |> dispatch()
+
+      {:error, error} ->
+        handle_error(error, stream)
     end
   end
 
-  defp handle_response(%Fetch.Response{status: status} = resp, stream)
-       when status in 200..299 do
-    shape_handle = shape_handle!(resp)
-    final_offset = last_offset(resp, stream.offset)
-    next_cursor = resp.next_cursor
-
-    %{value_mapper_fun: value_mapper_fun} =
-      stream =
-      handle_schema(resp, %{stream | shape_handle: shape_handle, next_cursor: next_cursor})
-      |> Map.put(:offset, final_offset)
-
-    resp.body
-    |> ensure_enum()
-    |> Enum.flat_map(&Message.parse(&1, shape_handle, value_mapper_fun, resp.request_timestamp))
-    |> Enum.reduce_while(stream, &handle_msg/2)
-    |> dispatch()
-  end
-
-  # 409: Upon receiving a 409, we should start from scratch with the newly
-  #      provided shape handle or with a fallback pseudo-handle to ensure
-  #      a consistent cache buster is used
-  defp handle_response({:error, %Fetch.Response{status: status} = resp}, stream)
-       when status in [409] do
-    %{value_mapper_fun: value_mapper_fun} = stream
-    handle = shape_handle(resp) || "#{stream.shape_handle}-next"
-
-    stream
-    |> reset(handle)
-    |> buffer(
-      Enum.flat_map(
-        resp.body,
-        &Message.parse(&1, handle, value_mapper_fun, resp.request_timestamp)
-      )
-    )
-    |> dispatch()
-  end
-
-  defp handle_response({:error, %Fetch.Response{} = resp}, stream) do
-    %Fetch.Response{body: body} = resp
-
-    handle_error(%Client.Error{message: unwrap_error(body), resp: resp}, stream)
-  end
-
-  defp handle_response({:error, error}, stream) do
-    handle_error(%Client.Error{message: "Unable to retrieve data stream", resp: error}, stream)
+  defp handle_messages(stream, messages) do
+    Enum.reduce_while(messages, stream, &handle_msg/2)
   end
 
   defp handle_msg(%Message.ControlMessage{control: :up_to_date} = msg, stream) do
-    handle_up_to_date(%{stream | buffer: :queue.in(msg, stream.buffer), up_to_date?: true})
+    handle_up_to_date(%{stream | buffer: :queue.in(msg, stream.buffer)})
   end
 
-  defp handle_msg(%Message.ControlMessage{control: :snapshot_end} = _msg, stream) do
+  defp handle_msg(%Message.ControlMessage{control: :snapshot_end}, stream) do
     {:cont, stream}
   end
 
   defp handle_msg(%Message.ChangeMessage{} = msg, stream) do
-    {tag_to_keys, key_data} =
-      TagTracker.update_tag_index(stream.tag_to_keys, stream.key_data, msg)
-
-    {:cont,
-     %{stream | buffer: :queue.in(msg, stream.buffer), tag_to_keys: tag_to_keys, key_data: key_data}}
+    {:cont, %{stream | buffer: :queue.in(msg, stream.buffer)}}
   end
 
-  defp handle_msg(
-         %Message.MoveOutMessage{patterns: patterns, request_timestamp: request_timestamp} = _msg,
-         stream
-       ) do
-    # Assumption: move-out events are only emitted after the initial snapshot is complete.
-    # We therefore apply them immediately and do not buffer for later inserts.
+  defp handle_msg(%Client.Error{} = error, stream) do
+    # Errors from Poll are passed through as messages
+    {:cont, %{stream | buffer: :queue.in(error, stream.buffer), state: :done}}
+  end
 
-    # Generate synthetic deletes for rows matching the move-out patterns
-    {synthetic_deletes, updated_tag_to_keys, updated_key_data} =
-      TagTracker.generate_synthetic_deletes(
-        stream.tag_to_keys,
-        stream.key_data,
-        patterns,
-        request_timestamp
-      )
-
-    # Add synthetic deletes to the buffer
-    buffer =
-      Enum.reduce(synthetic_deletes, stream.buffer, fn delete_msg, buf ->
-        :queue.in(delete_msg, buf)
-      end)
-
-    {:cont,
-     %{stream | buffer: buffer, tag_to_keys: updated_tag_to_keys, key_data: updated_key_data}}
+  defp handle_msg(_other, stream) do
+    {:cont, stream}
   end
 
   defp handle_up_to_date(%{opts: %{live: true}} = stream) do
@@ -274,21 +201,9 @@ defmodule Electric.Client.Stream do
   end
 
   defp handle_up_to_date(%{opts: %{live: false}} = stream) do
-    resume_message = %Message.ResumeMessage{
-      schema: stream.schema,
-      offset: stream.offset,
-      shape_handle: stream.shape_handle,
-      tag_to_keys: stream.tag_to_keys,
-      key_data: stream.key_data
-    }
-
+    resume_message = ShapeState.to_resume(stream.poll_state)
     {:halt, %{stream | buffer: :queue.in(resume_message, stream.buffer), state: :done}}
   end
-
-  defp unwrap_error([]), do: "Unknown error"
-  defp unwrap_error([msg]), do: msg
-  defp unwrap_error([_ | _] = msgs), do: msgs
-  defp unwrap_error(msg), do: msg
 
   defp handle_error(error, %{opts: %{errors: :stream}} = stream) do
     %{stream | buffer: :queue.in(error, stream.buffer), state: :done}
@@ -299,10 +214,6 @@ defmodule Electric.Client.Stream do
     raise error
   end
 
-  defp after_fetch({msgs, stream}) do
-    {msgs, stream}
-  end
-
   defp dispatch(%{buffer: buffer} = stream) do
     case :queue.out(buffer) do
       {{:value, elem}, buffer} -> {[elem], %{stream | buffer: buffer}}
@@ -310,126 +221,12 @@ defmodule Electric.Client.Stream do
     end
   end
 
-  defp build_request(stream) do
-    %{
-      id: id,
-      client: client,
-      up_to_date?: up_to_date?,
-      replica: replica,
-      shape_handle: shape_handle,
-      offset: offset,
-      next_cursor: cursor
-    } = stream
-
-    Client.request(client,
-      stream_id: id,
-      offset: offset,
-      shape_handle: shape_handle,
-      replica: replica,
-      live: up_to_date?,
-      next_cursor: cursor
-    )
+  defp maybe_resume(%{opts: %{resume: %Message.ResumeMessage{} = resume}} = stream) do
+    %{stream | poll_state: ShapeState.from_resume(resume)}
   end
 
-  defp make_request(stream) do
+  defp maybe_resume(stream) do
     stream
-    |> build_request()
-    |> make_request(stream)
-  end
-
-  defp make_request(request, stream) do
-    Fetch.request(stream.client, request)
-  end
-
-  defp reset(stream, shape_handle) do
-    %{
-      stream
-      | offset: Client.Offset.before_all(),
-        shape_handle: shape_handle,
-        up_to_date?: false,
-        buffer: :queue.new(),
-        schema: nil,
-        value_mapper_fun: nil,
-        tag_to_keys: %{},
-        key_data: %{}
-    }
-  end
-
-  defp buffer(stream, msgs) when is_list(msgs) do
-    %{stream | buffer: Enum.reduce(msgs, stream.buffer, &:queue.in/2)}
-  end
-
-  defp shape_handle!(resp) do
-    shape_handle(resp) ||
-      raise Client.Error, message: "Missing electric-handle header", resp: resp
-  end
-
-  defp shape_handle(%Fetch.Response{shape_handle: shape_handle}) do
-    shape_handle
-  end
-
-  defp last_offset(%Fetch.Response{last_offset: nil}, offset) do
-    offset
-  end
-
-  defp last_offset(%Fetch.Response{last_offset: offset}, _offset) do
-    offset
-  end
-
-  defp last_offset(_resp, offset) do
-    offset
-  end
-
-  defp handle_schema(%Fetch.Response{schema: schema}, %{value_mapper_fun: nil} = stream)
-       when is_map(schema) do
-    generate_value_mapper(schema, stream)
-  end
-
-  defp handle_schema(%Fetch.Response{}, %{value_mapper_fun: nil} = stream) do
-    stream
-  end
-
-  defp handle_schema(_resp, %{value_mapper_fun: fun} = stream) when is_function(fun, 1) do
-    stream
-  end
-
-  defp generate_value_mapper(schema, stream) do
-    # by default the parser is defined in the shape definition, but we can
-    # override that in the stream config
-    {parser_module, parser_opts} = stream.parser || stream.client.parser
-
-    value_mapper_fun = parser_module.for_schema(schema, parser_opts)
-
-    %{stream | schema: schema, value_mapper_fun: value_mapper_fun}
-  end
-
-  defp resume(%{opts: %{resume: %Message.ResumeMessage{} = resume}} = stream) do
-    %{shape_handle: shape_handle, offset: offset, schema: schema} = resume
-    tag_to_keys = Map.get(resume, :tag_to_keys, %{})
-    key_data = Map.get(resume, :key_data, %{})
-
-    stream = %{
-      stream
-      | shape_handle: shape_handle,
-        offset: offset,
-        tag_to_keys: tag_to_keys,
-        key_data: key_data,
-        up_to_date?: true
-    }
-
-    if schema do
-      generate_value_mapper(schema, stream)
-    else
-      stream
-    end
-  end
-
-  defp resume(stream) do
-    stream
-  end
-
-  defp stream_state(%{state: :init} = stream) do
-    %{stream | state: :stream}
   end
 
   defimpl Enumerable do
