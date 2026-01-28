39f76f6da84dc18f2e51e2efaedf6b91591ed8fa
Oleksii Sholik <oleksii@sholik.dev>
2026-01-28 12:14:30 +0100

    Update consumer tests to cover fragment-aware storage function calls

--- packages/sync-service/test/electric/shapes/consumer_test.exs
+++ packages/sync-service/test/electric/shapes/consumer_test.exs
@@ -209,7 +209,7 @@ defmodule Electric.Shapes.ConsumerTest do
       assert :ok = ShapeLogCollector.handle_event(txn, ctx.stack_id)
       assert_receive {^ref, :new_changes, ^last_log_offset}, @receive_timeout
       assert_receive {Support.TestStorage, :append_to_log!, @shape_handle1, _}
-      refute_receive {Support.TestStorage, :append_to_log!, @shape_handle2, _}
+      refute_storage_calls_for_txn_fragment(@shape_handle2)
 
       txn2 =
         transaction(xid, next_lsn, [
@@ -223,7 +223,7 @@ defmodule Electric.Shapes.ConsumerTest do
       assert :ok = ShapeLogCollector.handle_event(txn2, ctx.stack_id)
       assert_receive {^ref, :new_changes, ^next_log_offset}, @receive_timeout
       assert_receive {Support.TestStorage, :append_to_log!, @shape_handle1, _}
-      refute_receive {Support.TestStorage, :append_to_log!, @shape_handle2, _}
+      refute_storage_calls_for_txn_fragment(@shape_handle2)
     end
 
     test "correctly writes only relevant changes to multiple shape logs", ctx do
@@ -303,7 +303,7 @@ defmodule Electric.Shapes.ConsumerTest do
       assert :ok = ShapeLogCollector.handle_event(txn, ctx.stack_id)
 
       assert_receive {Support.TestStorage, :append_to_log!, @shape_handle2, _}
-      refute_receive {Support.TestStorage, :append_to_log!, @shape_handle1, _}
+      refute_storage_calls_for_txn_fragment(@shape_handle1)
 
       refute_receive {^ref1, :new_changes, _}
       assert_receive {^ref2, :new_changes, _}
@@ -384,7 +384,7 @@ defmodule Electric.Shapes.ConsumerTest do
         assert :ok = ShapeLogCollector.handle_event(txn, ctx.stack_id)
       end)
 
-      refute_receive {Support.TestStorage, :append_to_log!, @shape_handle1, _}
+      refute_storage_calls_for_txn_fragment(@shape_handle1)
 
       assert_shape_cleanup(@shape_handle1)
 
@@ -1119,5 +1119,11 @@ defmodule Electric.Shapes.ConsumerTest do
       changes: changes,
       affected_relations: MapSet.new(changes, & &1.relation)
     }
+  end
+
+  defp refute_storage_calls_for_txn_fragment(shape_handle) do
+    refute_receive {Support.TestStorage, :append_to_log!, ^shape_handle, _}
+    refute_receive {Support.TestStorage, :append_fragment_to_log!, ^shape_handle, _}
+    refute_receive {Support.TestStorage, :signal_txn_commit!, ^shape_handle, _}
   end
 end
