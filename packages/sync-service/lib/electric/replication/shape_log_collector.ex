455d12a6f1256017645dd1aca6f14837f18ff129
Oleksii Sholik <oleksii@sholik.dev>
2026-02-10 12:19:38 +0100

    In ShapeLogCollector make sure the FlushTracker is only invoked when processing txn fragments with commit

--- packages/sync-service/lib/electric/replication/shape_log_collector.ex
+++ packages/sync-service/lib/electric/replication/shape_log_collector.ex
@@ -564,10 +564,12 @@ defmodule Electric.Replication.ShapeLogCollector do
     LsnTracker.set_last_processed_lsn(state.lsn_tracker_ref, lsn)
 
     flush_tracker =
-      if is_struct(event, TransactionFragment) do
-        FlushTracker.handle_txn_fragment(state.flush_tracker, event, affected_shapes)
-      else
-        state.flush_tracker
+      case event do
+        %TransactionFragment{commit: commit} when not is_nil(commit) ->
+          FlushTracker.handle_txn_fragment(state.flush_tracker, event, affected_shapes)
+
+        _ ->
+          state.flush_tracker
       end
 
     %{state | flush_tracker: flush_tracker}
