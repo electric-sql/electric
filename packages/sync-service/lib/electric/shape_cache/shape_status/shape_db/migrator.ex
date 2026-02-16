d6d10e279f1e99a9c37c2cf4bf8e868156f68cf0
Garry Hill <garry@magnetised.net>
2026-02-16 15:21:53 +0000

    Add exclusive mode to support AWS EFS
    this is done by registering the write pool twice when in exclusive_mode,
    so the runtime cost is equivalent to the version without the mode tweak.
    
    while we're there allow for configuring other bits of the sqlite
    behaviour via config and env-vars

--- packages/sync-service/lib/electric/shape_cache/shape_status/shape_db/migrator.ex
+++ packages/sync-service/lib/electric/shape_cache/shape_status/shape_db/migrator.ex
@@ -14,28 +14,50 @@ defmodule Electric.ShapeCache.ShapeStatus.ShapeDb.Migrator do
   @impl GenServer
   def init(args) do
     {:ok, stack_id} = Keyword.fetch(args, :stack_id)
+    exclusive_mode = Keyword.get(args, :exclusive_mode, false)
 
     Process.set_label({:shape_db_migrator, stack_id})
     Logger.metadata(stack_id: stack_id)
     Electric.Telemetry.Sentry.set_tags_context(stack_id: stack_id)
 
-    with {:ok, conn} <- ShapeDb.Connection.open(args),
-         {:ok, _version} <- ShapeDb.Connection.migrate(conn),
+    with {:ok, conn} <- apply_migration(stack_id, args, exclusive_mode) do
+      {:ok, schedule_optimize(stack_id, conn), :hibernate}
+    end
+  end
+
+  defp apply_migration(_stack_id, _opts, true = _exclusive?) do
+    # In exclusive  mode we *must* apply the migrations within the pool
+    # connection initialization because we might be using a memory db.
+    # We return nil to trigger checkout-mode.
+    {:ok, nil}
+  end
+
+  defp apply_migration(_stack_id, opts, false = _exclusive?) do
+    with {:ok, conn} <- ShapeDb.Connection.open(opts),
+         {:ok, _version} <- ShapeDb.Connection.migrate(conn, opts),
          :ok = ShapeDb.Connection.optimize(conn) do
-      {:ok, schedule_optimize(conn), :hibernate}
+      {:ok, conn}
     end
   end
 
   @impl GenServer
-  def handle_info(:optimize, conn) do
+  def handle_info(:optimize, {stack_id, nil}) do
+    ShapeDb.Connection.checkout_write!(stack_id, :optimize, fn %{conn: conn} ->
+      :ok = ShapeDb.Connection.optimize(conn)
+    end)
+
+    {:noreply, schedule_optimize(stack_id, nil), :hibernate}
+  end
+
+  def handle_info(:optimize, {stack_id, conn}) do
     Logger.info("Optimizing shape db tables")
     :ok = ShapeDb.Connection.optimize(conn)
 
-    {:noreply, schedule_optimize(conn), :hibernate}
+    {:noreply, schedule_optimize(stack_id, conn), :hibernate}
   end
 
-  defp schedule_optimize(conn) do
+  defp schedule_optimize(stack_id, conn) do
     Process.send_after(self(), :optimize, @optimization_period)
-    conn
+    {stack_id, conn}
   end
 end
