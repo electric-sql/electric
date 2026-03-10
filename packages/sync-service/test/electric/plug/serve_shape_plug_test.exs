734537838c516cb1aed62202be4ac8208ab46451
Oleksii Sholik <oleksii@sholik.dev>
2026-03-10 23:19:31 +0100

    Remove invalid assertion
    The conn is artificially executed in a task and after Task.await()
    returns we can be sure the process has exited.

--- packages/sync-service/test/electric/plug/serve_shape_plug_test.exs
+++ packages/sync-service/test/electric/plug/serve_shape_plug_test.exs
@@ -630,9 +630,6 @@ defmodule Electric.Plug.ServeShapePlugTest do
 
       conn = Task.await(task)
 
-      # The conn process should exit after sending the response
-      refute Process.alive?(task.pid)
-
       assert conn.status == 409
       assert [%{"headers" => %{"control" => "must-refetch"}}] = Jason.decode!(conn.resp_body)
     end
